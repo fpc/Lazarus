33882c5fb33f14271ad8531f049626ea1c3e3f00
vincents <vincents@lazarus-ide.org>
2008-10-06 13:20:16 +0000

    Merged revisions 16865-16866 #57e0691fe8-#57e0691fe8,16869 #fce2a78da2,16875 #4b73fa2e5f via svnmerge from svn+ssh://svn.freepascal.org/FPC/svn/lazarus/trunk
    ........
      r16865 #57e0691fe8 | mattias | 2008-10-04 10:56:22 +0200 (Sat, 04 Oct 2008) | 1 line
      
      rpm: fpc-src: fixed arch
    ........
      r16866 #b57df0675a | mattias | 2008-10-04 11:46:26 +0200 (Sat, 04 Oct 2008) | 1 line
      
      rpm: fixed links for x86_64
    ........
      r16869 #fce2a78da2 | mattias | 2008-10-04 14:45:26 +0200 (Sat, 04 Oct 2008) | 1 line
      
      rpm: fpc_crosswin32: updated for 2.2.2
    ........
      r16875 #4b73fa2e5f | mattias | 2008-10-04 22:01:43 +0200 (Sat, 04 Oct 2008) | 1 line
      
      synedit: fixed crash on ecLineBreak when Lines empty
    ........
    
    git-svn-id: branches/fixes_0_9_26@16894 -

--- tools/install/create_fpc-src_rpm.sh
+++ tools/install/create_fpc-src_rpm.sh
@@ -23,9 +23,7 @@ else
   shift
 fi
 
-#------------------------------------------------------------------------------
-# patching
-#------------------------------------------------------------------------------
+Arch=$(rpm --eval "%{_arch}")
 
 # retrieve the version information
 echo -n "getting FPC version from local svn ..."
@@ -47,24 +45,24 @@ sh create_fpc_tgz_from_local_dir.sh $FPCSourceDir $FPCTGZ
 
 echo "building fpc-src rpm ..."
 
-# create spec file
-SpecFile=rpm/fpc-src-$LazVersion-$LazRelease.spec
-cat rpm/fpc-src.spec | \
-  sed -e "s/LAZVERSION/$LazVersion/g" -e "s/LAZRELEASE/$LazRelease/" \
-  > $SpecFile
-  
 # copy custom rpm scripts
-TmpDir=~/tmp
+TmpDir=$HOME/tmp
 mkdir -p $TmpDir
 cp smart_strip.sh $TmpDir/smart_strip.sh
 chmod a+x $TmpDir/smart_strip.sh
 cp do_nothing.sh $TmpDir/do_nothing.sh
 chmod a+x $TmpDir/do_nothing.sh
 
+# create spec file
+SpecFile=rpm/fpc-src-$LazVersion-$LazRelease.spec
+cat rpm/fpc-src.spec | \
+  sed -e "s/LAZVERSION/$LazVersion/g" -e "s/LAZRELEASE/$LazRelease/g" -e "s#LAZSCRIPTDIR#$TmpDir#g" \
+  > $SpecFile
+  
 # build rpm
 rpmbuild -ba $SpecFile || rpm -ba $SpecFile
 
-echo "The new rpm can be found in $(./rpm/get_rpm_source_dir.sh)/RPMS/i386/fpc-src-$LazVersion-$LazRelease.i386.rpm"
+echo "The new rpm can be found in $(./rpm/get_rpm_source_dir.sh)/RPMS/$Arch/fpc-src-$LazVersion-$LazRelease.$Arch.rpm"
 
 # end.
 
