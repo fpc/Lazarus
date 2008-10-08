081d34eb907c210ec2741317493bf57bb1fcb149
vincents <vincents@lazarus-ide.org>
2008-10-08 08:46:25 +0000

    Merged revisions 16830 #c8d14a2b7e,16833-16837 #e5735f96fc-#e5735f96fc,16842-16844 #e0a5b9322b-#e0a5b9322b,16859 #992d5f74ab,16862 #0e0ed50080,16864 #a5bb936929,16868 #8c3ef8f770,16876 #0294f3618f,16880 #e417cd9cef,16890 #e81a5aa817,16892 #6361b90a57,16920 #abd306e88c via svnmerge from svn+ssh://svn.freepascal.org/FPC/svn/lazarus/trunk
    ........
      r16830 #c8d14a2b7e | mattias | 2008-10-01 15:37:14 +0200 (Wed, 01 Oct 2008) | 1 line
      
      IDE: added verbosity flag VerboseSrcEditBufClean
    ........
      r16833 #e5735f96fc | paul | 2008-10-02 03:03:02 +0200 (Thu, 02 Oct 2008) | 2 lines
      
      images: add keyboard and character image
      ide: replace Key treeview images by common images
    ........
      r16834 #994b5be33a | paul | 2008-10-02 03:05:56 +0200 (Thu, 02 Oct 2008) | 1 line
      
      gtk2: implement GetItemRect for ListBox (and descendants)
    ........
      r16835 #c9899ebca4 | paul | 2008-10-02 03:47:29 +0200 (Thu, 02 Oct 2008) | 1 line
      
      lcl, widgetsets: hope fix misuse of TScrollBar.PageSize (#0011788 based on ideas of Sergey Bulba)
    ........
      r16836 #3807a31112 | paul | 2008-10-02 04:05:51 +0200 (Thu, 02 Oct 2008) | 1 line
      
      lcl: correct BottomLeft coordinate in basic RoundRect implementation (#0012274)
    ........
      r16837 #2080c834cf | paul | 2008-10-02 04:19:00 +0200 (Thu, 02 Oct 2008) | 3 lines
      
      ide:
        - remove dummy warning when you cancel export in todolist dialog
        - add at least some related icon to "export" button in todolist dialog
    ........
      r16842 #e0a5b9322b | mattias | 2008-10-02 11:17:38 +0200 (Thu, 02 Oct 2008) | 1 line
      
      codetools: scanning for comments in type nodes too
    ........
      r16843 #626815db77 | mattias | 2008-10-02 11:19:47 +0200 (Thu, 02 Oct 2008) | 1 line
      
      codetools: scanning for comments in type nodes end
    ........
      r16844 #36f21372de | mattias | 2008-10-02 11:29:21 +0200 (Thu, 02 Oct 2008) | 1 line
      
      IDE: codehelp: removing pasdoc
    ........
      r16859 #992d5f74ab | mattias | 2008-10-03 11:34:52 +0200 (Fri, 03 Oct 2008) | 1 line
      
      codetools: fixed TCodeTreeNode.ConsistencyCheck
    ........
      r16862 #0e0ed50080 | mattias | 2008-10-03 12:12:38 +0200 (Fri, 03 Oct 2008) | 1 line
      
      lazbuild: fixed converting line endings of message dialogs
    ........
      r16864 #a5bb936929 | zeljko | 2008-10-04 10:45:18 +0200 (Sat, 04 Oct 2008) | 2 lines
      
      Qt: Enable titlebar buttons (close,min,max) on modal windows under MacOsX
    ........
      r16868 #8c3ef8f770 | tombo | 2008-10-04 14:40:46 +0200 (Sat, 04 Oct 2008) | 1 line
      
      LCL UTF-16 conversion bug: do not ignore code point values > $DFFF and <= $FFFF, unrolled by Brad Campbell
    ........
      r16876 #0294f3618f | zeljko | 2008-10-05 09:31:50 +0200 (Sun, 05 Oct 2008) | 2 lines
      
      Qt: enable toolbar buttons for TQtFileDialog
    ........
      r16880 #e417cd9cef | mattias | 2008-10-05 11:33:46 +0200 (Sun, 05 Oct 2008) | 1 line
      
      codetools: static keywords for class variables, bug #12293
    ........
      r16890 #e81a5aa817 | vincents | 2008-10-06 10:10:05 +0200 (Mon, 06 Oct 2008) | 1 line
      
      updated Hebrew translations from Ido Kanner (issue #12287)
    ........
      r16892 #6361b90a57 | vincents | 2008-10-06 12:44:30 +0200 (Mon, 06 Oct 2008) | 1 line
      
      win32 interface: don't change spinedit, if readonly=true (issue #12306)
    ........
      r16920 #abd306e88c | mattias | 2008-10-07 17:24:47 +0200 (Tue, 07 Oct 2008) | 1 line
      
      deb: fixed environment options to setup lazarus path
    ........
    
    git-svn-id: branches/fixes_0_9_26@16925 -

--- components/codetools/codecache.pas
+++ components/codetools/codecache.pas
@@ -923,7 +923,7 @@ end;
 procedure TCodeBuffer.SetIsDeleted(const NewValue: boolean);
 begin
   if FIsDeleted=NewValue then exit;
-  debugln(['TCodeBuffer.SetIsDeleted ',Filename,' ',NewValue]);
+  //debugln(['TCodeBuffer.SetIsDeleted ',Filename,' ',NewValue]);
   FIsDeleted:=NewValue;
   if FIsDeleted then begin
     Clear;
