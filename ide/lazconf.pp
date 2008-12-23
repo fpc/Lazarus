06d4e74ff66d722add6a2f57ab1374cb6aa81842
vincents <vincents@lazarus-ide.org>
2008-12-23 22:11:13 +0000

    Merged revisions 17709 #ebe1b9d29a,17813 #ffdbc27e0e,17815 #ff17c1142f via svnmerge from svn+ssh://svn.freepascal.org/FPC/svn/lazarus/trunk
    ........
      r17709 #ebe1b9d29a | vincents | 2008-12-07 20:18:34 +0100 (Sun, 07 Dec 2008) | 2 lines
      
      LCL: published TImage MouseWheel events, tested by Maxim Ganetsky
    ........
      r17813 #ffdbc27e0e | vincents | 2008-12-12 15:53:33 +0100 (Fri, 12 Dec 2008) | 1 line
      
      IDE: unified Primary and Secondary config path functions for unix and windows, they were the same after fixing issue #12518
    ........
      r17815 #ff17c1142f | vincents | 2008-12-12 20:57:15 +0100 (Fri, 12 Dec 2008) | 1 line
      
      IDE: fixed compilation on unix
    ........
    
    git-svn-id: branches/fixes_0_9_26@17908 -

--- ide/lazconf.pp
+++ ide/lazconf.pp
@@ -234,6 +234,48 @@ begin
 end;
 
 {---------------------------------------------------------------------------
+  getPrimaryConfigPath function
+ ---------------------------------------------------------------------------}
+function GetPrimaryConfigPath: String;
+begin
+  Result := PrimaryConfigPath;
+end;
+
+{---------------------------------------------------------------------------
+  getSecondaryConfigPath function
+ ---------------------------------------------------------------------------}
+function GetSecondaryConfigPath: String;
+begin
+  Result := SecondaryConfigPath;
+end;
+
+{---------------------------------------------------------------------------
+  createPrimaryConfigPath procedure
+ ---------------------------------------------------------------------------}
+procedure CreatePrimaryConfigPath;
+begin
+  CreateDirUTF8(GetPrimaryConfigPath);
+end;
+
+{---------------------------------------------------------------------------
+  SetPrimaryConfigPath procedure
+ ---------------------------------------------------------------------------}
+procedure SetPrimaryConfigPath(const NewValue: String);
+begin
+  debugln('SetPrimaryConfigPath NewValue="',NewValue,'" -> "',ExpandFileNameUTF8(NewValue),'"');
+  PrimaryConfigPath := ExpandFileNameUTF8(NewValue);
+end;
+
+{---------------------------------------------------------------------------
+  SetSecondaryConfigPath procedure
+ ---------------------------------------------------------------------------}
+procedure SetSecondaryConfigPath(const NewValue: String);
+begin
+  debugln('SetSecondaryConfigPath NewValue="',NewValue,'" -> "',ExpandFileNameUTF8(NewValue),'"');
+  SecondaryConfigPath := ExpandFileNameUTF8(NewValue);
+end;
+
+{---------------------------------------------------------------------------
   CopySecondaryConfigFile procedure
  ---------------------------------------------------------------------------}
 procedure CopySecondaryConfigFile(const AFilename: String);
