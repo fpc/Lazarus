afd3fd01104053fdf7607c2639d9a2f568e2bf6a
paul <paul@lazarus-ide.org>
2009-04-11 16:05:26 +0000

    qt: move RegisterWS calls to qtwsfactory
    git-svn-id: branches/lcl-smartlink@19352 -

--- interfaces/qt/qtint.pp
+++ interfaces/qt/qtint.pp
@@ -175,32 +175,9 @@ uses
 // To get as little as possible circles,
 // uncomment only those units with implementation
 ////////////////////////////////////////////////////
-// QtWSActnList,
- QtWSArrow,
- QtWSButtons,
- QtWSCalendar,
- QtWSCheckLst,
- QtWSComCtrls,
- QtWSControls,
-// QtWSDbCtrls,
-// QtWSDBGrids,
- QtWSDialogs,
-// QtWSEditBtn,
- QtWSExtCtrls,
-// QtWSExtDlgs,
-// QtWSFileCtrl,
- QtWSForms,
-// QtWSGrids,
-// QtWSImgList,
-// QtWSMaskEdit,
- QtWSMenus,
-// QtWSPairSplitter,
- QtWSSpin,
- QtWSStdCtrls,
-// QtWSToolwin,
+ QtWSFactory,
  QtCaret,
  QtThemes,
- QtWsDesigner,
 ////////////////////////////////////////////////////
   Graphics, buttons, Menus,
   // Bindings
@@ -277,10 +254,5 @@ end;
 {$I qtobject.inc}
 {$I qtwinapi.inc}
 {$I qtlclintf.inc}
-
-
-initialization
-
-finalization
 
 end.
