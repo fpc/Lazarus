6c834a59af5716d95e9d568abb4977e09253507b
vincents <vincents@lazarus-ide.org>
2009-11-15 20:22:02 +0000

    Merged revisions 21942-21943 #36a2b1ea07-#36a2b1ea07,21948-21949 #22e77f546c-#22e77f546c,21952-21954 #fdf7bc3def-#fdf7bc3def,21962 #c96b081480,21964-21966 #d99d72dc20-#d99d72dc20,21992-21995 #456235d0ee-#456235d0ee,22029 #e9b34e0a5f,22104 #0f7808d593,22116-22120 #78a617c4e2-#78a617c4e2,22122 #251bb2f596 via svnmerge from svn+ssh://svn.freepascal.org/FPC/svn/lazarus/trunk
    ........
      r21942 #36a2b1ea07 | mattias | 2009-10-01 14:24:32 +0200 (Thu, 01 Oct 2009) | 1 line
      
      added aggpas
    ........
      r21943 #880b4f97b6 | mattias | 2009-10-01 14:26:35 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas: added lcl canvas
    ........
      r21948 #22e77f546c | mattias | 2009-10-01 16:27:37 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas: added aggpaslcl.lpk
    ........
      r21949 #86798d3d1d | mattias | 2009-10-01 16:46:58 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas: example
    ........
      r21952 #fdf7bc3def | mattias | 2009-10-01 19:01:32 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas: removed agg_platform_support.pas for lcl
    ........
      r21953 #c5887d7016 | mattias | 2009-10-01 19:14:57 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas removed file_util_.pas for lcl
    ........
      r21954 #bd667b6959 | mattias | 2009-10-01 19:16:57 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas: removed platform directory
    ........
      r21962 #c96b081480 | mattias | 2009-10-01 22:25:42 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas: define xml_large_size for 64bit
    ........
      r21964 #d99d72dc20 | mattias | 2009-10-01 23:09:31 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas debugging
    ........
      r21965 #221a749240 | mattias | 2009-10-01 23:33:10 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas: 64bit fixes
    ........
      r21966 #ee7e45f2a2 | mattias | 2009-10-01 23:56:26 +0200 (Thu, 01 Oct 2009) | 1 line
      
      aggpas: 64bit fixes
    ........
      r21992 #456235d0ee | mattias | 2009-10-03 17:33:15 +0200 (Sat, 03 Oct 2009) | 1 line
      
      aggpas: reduce debugging
    ........
      r21993 #87711ee8b3 | mattias | 2009-10-03 17:33:56 +0200 (Sat, 03 Oct 2009) | 1 line
      
      aggpas: undo 64bit changes
    ........
      r21994 #1fe0e7fad4 | mattias | 2009-10-03 17:34:29 +0200 (Sat, 03 Oct 2009) | 1 line
      
      aggpas: reduce debugging
    ........
      r21995 #50874df45f | mattias | 2009-10-03 17:42:27 +0200 (Sat, 03 Oct 2009) | 1 line
      
      aggpas: fixed win32 compilation
    ........
      r22029 #e9b34e0a5f | mattias | 2009-10-05 01:18:24 +0200 (Mon, 05 Oct 2009) | 1 line
      
      aggpas: removed writeln
    ........
      r22104 #0f7808d593 | mattias | 2009-10-12 13:30:24 +0200 (Mon, 12 Oct 2009) | 1 line
      
      aggpas: added defines AGG2D_USE_WINFONTS and AGG2D_NO_FONT
    ........
      r22116 #78a617c4e2 | mattias | 2009-10-12 17:11:12 +0200 (Mon, 12 Oct 2009) | 1 line
      
      aggpas: fixed Line positions
    ........
      r22117 #157277fda7 | mattias | 2009-10-12 17:12:28 +0200 (Mon, 12 Oct 2009) | 1 line
      
      aggpas: added example for comparing LCL/aggpas comparison
    ........
      r22118 #f29dd380ea | mattias | 2009-10-12 17:17:16 +0200 (Mon, 12 Oct 2009) | 1 line
      
      aggpas: example diag line
    ........
      r22119 #a4e6d005ef | mattias | 2009-10-12 17:26:40 +0200 (Mon, 12 Oct 2009) | 1 line
      
      aggpas: fixed FillRect border and position
    ........
      r22120 #8d86a00845 | mattias | 2009-10-12 18:42:38 +0200 (Mon, 12 Oct 2009) | 1 line
      
      aggpas: started GradientFill
    ........
      r22122 #251bb2f596 | mattias | 2009-10-12 18:52:22 +0200 (Mon, 12 Oct 2009) | 1 line
      
      aggpas: Frame
    ........
    
    git-svn-id: branches/fixes_0_9_28@22605 -

---
+++ components/aggpas/lazarus/example/unit2.pas
@@ -0,0 +1,96 @@
+unit Unit2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LCLProc, FileUtil, LResources, Forms, Controls, Graphics,
+  Dialogs, FPimage, agg_fpimage, Agg_LCL;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormPaint(Sender: TObject);
+  private
+  public
+    AggLCLCanvas: TAggLCLCanvas;
+    Bitmap1: TBitmap;
+  end;
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Bitmap1:=TBitmap.Create;
+  AggLCLCanvas:=TAggLCLCanvas.Create;
+  with AggLCLCanvas do begin
+    Image.PixelFormat:=afpimRGBA32;
+    Image.SetSize(250,250);
+  end;
+
+  // paint to agg canvas
+  with AggLCLCanvas do begin
+    // solid white background
+    Brush.Color:=clWhite;
+    FillRect(0,0,Width,Height);
+
+    Brush.Color:=clRed;
+    Pen.Color:=clBlue;
+    Pen.Width:=1;
+
+    Line(12,10,22,10);
+    Line(10,12,10,22);
+    Line(12,12,22,22);
+
+    FillRect(40,10,50,20);
+    Ellipse(55,10,65,20);
+    //GradientFill(Rect(70,10,80,20),clRed,clBlue,gdVertical);
+    Frame(85,10,95,20);
+  end;
+
+  // convert to LCL native pixel format
+  Bitmap1.LoadFromIntfImage(AggLCLCanvas.Image.IntfImg);
+
+  // paint with widgetset to bitmap
+  with Bitmap1.Canvas do begin
+    Brush.Color:=clBlue;
+    Pen.Color:=clRed;
+    Pen.Width:=1;
+
+    Line(24,10,34,10);
+    Line(10,24,10,34);
+    Line(24,24,34,34);
+
+    FillRect(40,22,50,32);
+    Ellipse(55,22,65,32);
+    GradientFill(Rect(70,22,80,32),clRed,clBlue,gdVertical);
+    Frame(85,22,95,32);
+  end;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  AggLCLCanvas.Free;
+  Bitmap1.Free;
+end;
+
+procedure TForm1.FormPaint(Sender: TObject);
+begin
+  Canvas.Draw(0,0,Bitmap1);
+end;
+
+initialization
+  {$I unit2.lrs}
+
+end.
+
---
+++ components/aggpas/src/agg_lcl.pas
@@ -0,0 +1,373 @@
+{ agg_lcl.pas }
+unit
+ Agg_LCL;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+ sysutils,
+ {$IFDEF AGG_WINDOWS}
+ Windows ,
+ {$ENDIF}
+ Classes ,Graphics, LCLProc, IntfGraphics, GraphType, FPimage, FPCanvas,
+ agg_fpimage;
+
+type
+
+  { TAggLCLImage }
+
+  TAggLCLImage = class(TAggFPImage)
+  private
+    FIntfImg: TLazIntfImage;
+    procedure ReallocData; override;
+  public
+    constructor Create(AWidth, AHeight: integer); override;
+    destructor Destroy; override;
+    property IntfImg: TLazIntfImage read FIntfImg;
+  end;
+
+  { TAggLCLBrush }
+
+  TAggLCLBrush = class(TAggFPBrush)
+  private
+    FColor: TColor;
+  protected
+    procedure SetColor(const AValue: TColor); virtual;
+    procedure SetFPColor(const AValue: TFPColor); override;
+  public
+    property Color: TColor read FColor write SetColor;
+  end;
+
+  { TAggLCLPen }
+
+  TAggLCLPen = class(TAggFPPen)
+  private
+    FColor: TColor;
+  protected
+    procedure SetColor(const AValue: TColor); virtual;
+    procedure SetFPColor(const AValue: TFPColor); override;
+  public
+    property Color: TColor read FColor write SetColor;
+  end;
+
+  { TAggLCLFont }
+
+  TAggLCLFont = class(TAggFPFont)
+  private
+    FColor: TColor;
+  protected
+    procedure SetColor(const AValue: TColor); virtual;
+    procedure SetFPColor(const AValue: TFPColor); override;
+  public
+    property Color: TColor read FColor write SetColor;
+  end;
+
+  { TAggLCLCanvas }
+
+  TAggLCLCanvas = class(TAggFPCanvas)
+  private
+    FALBrush: TAggLCLBrush;
+    FALFont: TAggLCLFont;
+    FALImage: TAggLCLImage;
+    FALPen: TAggLCLPen;
+    FLockInitialzed: boolean;
+    FLock: TRTLCriticalSection;// FLock is initialized on demand
+    FOnChange: TNotifyEvent;
+    FOnChanging: TNotifyEvent;
+  protected
+    function DoCreateDefaultBrush: TFPCustomBrush; override;
+    function DoCreateDefaultFont: TFPCustomFont; override;
+    function DoCreateDefaultPen: TFPCustomPen; override;
+    function DoCreateDefaultImage: TAggFPImage; override;
+    procedure DoLockCanvas; override;
+    procedure DoUnlockCanvas; override;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure ClearSettings; override;
+
+    property Pen: TAggLCLPen read FALPen;
+    property Brush: TAggLCLBrush read FALBrush;
+    property Font: TAggLCLFont read FALFont;
+    property Image: TAggLCLImage read FALImage;
+    property OnChange: TNotifyEvent read FOnChange write FOnChange;
+    property OnChanging: TNotifyEvent read FOnChanging write FOnChanging;
+
+    procedure Lock; virtual;
+    function TryLock: Boolean;
+    procedure Unlock; virtual;
+    procedure Changing; virtual;
+    procedure Changed; virtual;
+
+    // extra drawing methods (there are more in the ancestor TFPCustomCanvas)
+    //procedure Arc(ALeft, ATop, ARight, ABottom, angle1, angle2: Integer); virtual;
+    //procedure Arc(ALeft, ATop, ARight, ABottom, SX, SY, EX, EY: Integer); virtual;
+
+    procedure FillRect(const ARect: TRect); virtual; // no border
+    procedure FillRect(X1,Y1,X2,Y2: Integer);        // no border
+
+    procedure Frame(const ARect: TRect); virtual; // border using pen
+    procedure Frame(X1,Y1,X2,Y2: Integer);        // border using pen
+
+    procedure GradientFill(ARect: TRect; AStart, AStop: TColor; ADirection: TGradientDirection);
+  end;
+
+procedure InitAggPasRawImageDescriptor(APixelFormat: TAggFPImgPixelFormat;
+                       AWidth, AHeight: cardinal; out Desc: TRawImageDescription);
+function AggToLCLColor(const c: TAggColor): TColor;
+function LCLToAggColor(const c: TColor): TAggColor;
+
+implementation
+
+procedure InitAggPasRawImageDescriptor(APixelFormat: TAggFPImgPixelFormat;
+  AWidth, AHeight: cardinal; out Desc: TRawImageDescription);
+begin
+  FillByte(Desc, SizeOf(Desc), 0);
+  with Desc do begin
+    Format := ricfRGBA;
+    if APixelFormat=afpimRGB24 then
+      Depth := 24 // used bits per pixel
+    else
+      Depth := 32; // used bits per pixel
+    Width := AWidth;
+    Height := AHeight;
+    BitOrder := riboBitsInOrder;
+    ByteOrder := riboLSBFirst;
+    LineOrder := riloTopToBottom;
+    BitsPerPixel := Depth; // bits per pixel. can be greater than Depth.
+    LineEnd := rileByteBoundary;
+    RedPrec := 8; // red precision. bits for red
+    RedShift := 0;
+    GreenPrec := 8;
+    GreenShift := 8; // bitshift. Direction: from least to most significant
+    BluePrec := 8;
+    BlueShift := 16;
+    if APixelFormat=afpimRGBA32 then begin
+      AlphaPrec := 8;
+      AlphaShift := 24;
+    end;
+  end;
+end;
+
+function AggToLCLColor(const c: TAggColor): TColor;
+begin
+  Result:=RGBToColor(c.r,c.g,c.b);
+end;
+
+function LCLToAggColor(const c: TColor): TAggColor;
+begin
+  Result.a:=0;
+  RedGreenBlue(ColorToRGB(c),Result.r,Result.g,Result.b);
+end;
+
+{ TAggLCLCanvas }
+
+function TAggLCLCanvas.DoCreateDefaultBrush: TFPCustomBrush;
+begin
+  Result:=TAggLCLBrush.Create;
+end;
+
+function TAggLCLCanvas.DoCreateDefaultFont: TFPCustomFont;
+begin
+  Result:=TAggLCLFont.Create;
+end;
+
+function TAggLCLCanvas.DoCreateDefaultPen: TFPCustomPen;
+begin
+  Result:=TAggLCLPen.Create;
+end;
+
+function TAggLCLCanvas.DoCreateDefaultImage: TAggFPImage;
+begin
+  Result:=TAggLCLImage.Create(0,0);
+end;
+
+procedure TAggLCLCanvas.DoLockCanvas;
+begin
+  if not FLockInitialzed then begin
+    InitCriticalSection(FLock);
+    FLockInitialzed:=true;
+  end;
+  EnterCriticalsection(FLock);
+  inherited DoLockCanvas;
+end;
+
+procedure TAggLCLCanvas.DoUnlockCanvas;
+begin
+  LeaveCriticalsection(FLock);
+  inherited DoUnlockCanvas;
+end;
+
+constructor TAggLCLCanvas.Create;
+begin
+  inherited Create;
+  FALFont := TAggLCLFont(inherited Font);
+  FALPen := TAggLCLPen(inherited Pen);
+  FALBrush := TAggLCLBrush(inherited Brush);
+  FALImage := TAggLCLImage(inherited Image);
+end;
+
+destructor TAggLCLCanvas.Destroy;
+begin
+  if FLockInitialzed then begin
+    DoneCriticalsection(FLock);
+    FLockInitialzed:=false;
+  end;
+  inherited Destroy;
+  // set resources to nil, so that dangling pointers are spotted early
+  FALBrush:=nil;
+  FALPen:=nil;
+  FALFont:=nil;
+  FALImage:=nil;
+end;
+
+procedure TAggLCLCanvas.ClearSettings;
+begin
+  inherited ClearSettings;
+end;
+
+procedure TAggLCLCanvas.Lock;
+begin
+  LockCanvas;
+end;
+
+function TAggLCLCanvas.TryLock: Boolean;
+begin
+  Result := not Locked;
+  if Result then
+    Lock;
+end;
+
+procedure TAggLCLCanvas.Unlock;
+begin
+  UnlockCanvas;
+end;
+
+procedure TAggLCLCanvas.Changing;
+begin
+  if Assigned(FOnChanging) then FOnChanging(Self);
+end;
+
+procedure TAggLCLCanvas.Changed;
+begin
+  if Assigned(FOnChange) then FOnChange(Self);
+end;
+
+procedure TAggLCLCanvas.FillRect(const ARect: TRect);
+begin
+  Fillrect(ARect.Left,ARect.Top,ARect.Right,ARect.Bottom);
+end;
+
+procedure TAggLCLCanvas.FillRect(X1, Y1, X2, Y2: Integer);
+begin
+  Path.m_path.remove_all;
+  Path.m_path.move_to(X1,Y1);
+  Path.m_path.line_to(X2,Y1);
+  Path.m_path.line_to(X2,Y2);
+  Path.m_path.line_to(X1,Y2);
+  AggClosePolygon;
+  AggDrawPath(AGG_FillOnly);
+end;
+
+procedure TAggLCLCanvas.Frame(const ARect: TRect);
+begin
+  Frame(ARect.Left,ARect.Top,ARect.Right,ARect.Bottom);
+end;
+
+procedure TAggLCLCanvas.Frame(X1, Y1, X2, Y2: Integer);
+begin
+  Path.m_path.remove_all;
+  Path.m_path.move_to(X1+0.5,Y1+0.5);
+  Path.m_path.line_to(X2+0.5,Y1+0.5);
+  Path.m_path.line_to(X2+0.5,Y2+0.5);
+  Path.m_path.line_to(X1+0.5,Y2+0.5);
+  AggClosePolygon;
+  AggDrawPath(AGG_StrokeOnly);
+end;
+
+procedure TAggLCLCanvas.GradientFill(ARect: TRect; AStart, AStop: TColor;
+  ADirection: TGradientDirection);
+begin
+  raise Exception.Create('TAggLCLCanvas.GradientFill ToDo');
+  AggFillLinearGradient(ARect.Left,ARect.Top,ARect.Right,ARect.Bottom,
+    LCLToAggColor(AStart),LCLToAggColor(AStop));
+  FillRect(ARect);
+end;
+
+{ TAggLCLBrush }
+
+procedure TAggLCLBrush.SetColor(const AValue: TColor);
+begin
+  FPColor:=TColorToFPColor(AValue);
+end;
+
+procedure TAggLCLBrush.SetFPColor(const AValue: TFPColor);
+begin
+  if FPColor=AValue then exit;
+  inherited SetFPColor(AValue);
+  FColor:=FPColorToTColor(FPColor);
+end;
+
+{ TAggLCLPen }
+
+procedure TAggLCLPen.SetColor(const AValue: TColor);
+begin
+  FPColor:=TColorToFPColor(AValue);
+end;
+
+procedure TAggLCLPen.SetFPColor(const AValue: TFPColor);
+begin
+  if FPColor=AValue then exit;
+  inherited SetFPColor(AValue);
+  FColor:=FPColorToTColor(FPColor);
+end;
+
+{ TAggLCLFont }
+
+procedure TAggLCLFont.SetColor(const AValue: TColor);
+begin
+  FPColor:=TColorToFPColor(AValue);
+end;
+
+procedure TAggLCLFont.SetFPColor(const AValue: TFPColor);
+begin
+  if FPColor=AValue then exit;
+  inherited SetFPColor(AValue);
+  FColor:=FPColorToTColor(FPColor);
+end;
+
+{ TAggLCLImage }
+
+procedure TAggLCLImage.ReallocData;
+var
+  ARawImage: TRawImage;
+begin
+  inherited ReallocData;
+
+  // create FIntfImg
+  if FIntfImg=nil then
+    FIntfImg:=TLazIntfImage.Create(0,0);
+
+  FillByte(ARawImage, SizeOf(ARawImage), 0);
+  InitAggPasRawImageDescriptor(PixelFormat,Width,Height,ARawImage.Description);
+  ARawImage.Data:=Data;
+  ARawImage.DataSize:=DataSize;
+
+  FIntfImg.SetRawImage(ARawImage,false);
+end;
+
+constructor TAggLCLImage.Create(AWidth, AHeight: integer);
+begin
+  inherited Create(AWidth, AHeight);
+end;
+
+destructor TAggLCLImage.Destroy;
+begin
+  FreeAndNil(FIntfImg);
+  inherited Destroy;
+end;
+
+end.
+
