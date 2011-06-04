b5a31bd944e16142c49f095596d2e19c4a6270bf
juha <juha@lazarus-ide.org>
2011-06-04 15:29:54 +0000

    Merged revisions 30472 #a5e1656dfb via svnmerge from http://svn.freepascal.org/svn/lazarus/trunk
    ........
      r30472 #a5e1656dfb | juha | 2011-04-25 23:39:44 +0300 (ma, 25 huhti  2011) | 1 line
      
      LCL: Change button color when enabling/disabling TUpDown. Patch from Anton, issue #18913
    ........
    
    git-svn-id: branches/fixes_0_9_30@31075 -

--- lcl/comctrls.pp
+++ lcl/comctrls.pp
@@ -1424,6 +1424,7 @@ type
     procedure AssociateKeyDown(Sender: TObject; var Key: Word; ShiftState : TShiftState);
     procedure OnAssociateChangeBounds(Sender: TObject);
     procedure DoOnResize; override;
+    procedure SetEnabled(Value: Boolean); override;
     class function GetControlClassDefaultSize: TSize; override;
     function CanChange: Boolean; virtual;
     procedure Notification(AComponent: TComponent; Operation: TOperation); override;
