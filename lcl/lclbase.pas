9c48d5aa7f6ca218e097e1686a0d8669c303a057
mattias <nc-gaertnma@netcologne.de>
2011-07-07 06:42:59 +0000

    lclbase: added dummy package to give downgraders a hint what went wrong and how to fix
    git-svn-id: branches/fixes_0_9_30@31582 -

---
+++ lcl/lclbase.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit lclbase; 
+
+interface
+
+uses
+  lclbasedowngrade, LazarusPackageIntf;
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('lclbase', @Register); 
+end.
