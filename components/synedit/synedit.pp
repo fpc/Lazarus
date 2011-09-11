3697ac64f0cc5e294d10904a4e1f4e9273163b8f
juha <juha@lazarus-ide.org>
2011-09-11 07:50:30 +0000

    Merged revisions 32049 #df270d4561 via svnmerge from http://svn.freepascal.org/svn/lazarus/trunk
    ........
      r32049 #df270d4561 | martin | 2011-08-25 14:08:14 +0300 (to, 25 elo    2011) | 1 line
      
      SynEdit: (Un)Indent current line, if no selection. Issue #0019724
    ........
    
    git-svn-id: branches/fixes_0_9_30@32276 -

--- components/synedit/synedit.pp
+++ components/synedit/synedit.pp
@@ -7828,13 +7828,17 @@ var
   end;
 
 begin
-  if not SelAvail then exit;
   IncPaintLock;
   FBlockSelection.IncPersistentLock;
   try
     // build text to insert
-    BB := BlockBegin;
-    BE := BlockEnd;
+    if not SelAvail then begin
+      BB := CaretXY;
+      BE := CaretXY;
+    end else begin
+      BB := BlockBegin;
+      BE := BlockEnd;
+    end;
     if (BE.X = 1) then
       e := BE.y - 1
     else
@@ -7889,10 +7893,13 @@ var
   end;
 
 begin
-  if not SelAvail then exit;
-
-  BB := BlockBegin;
-  BE := BlockEnd;
+  if not SelAvail then begin
+    BB := CaretXY;
+    BE := CaretXY;
+  end else begin
+    BB := BlockBegin;
+    BE := BlockEnd;
+  end;
   // convert selection to complete lines
   if BE.X = 1 then
     e := BE.y - 1
