de8047e1baa642de68a6bc26bb7c5402ccbcad87
juha <juha@lazarus-ide.org>
2011-09-11 12:15:08 +0000

    Merged revisions 32058 #ab90477674 via svnmerge from http://svn.freepascal.org/svn/lazarus/trunk
    ........
      r32058 #ab90477674 | martin | 2011-08-26 02:46:14 +0300 (pe, 26 elo    2011) | 1 line
      
      SynEdit: Check if sufficent space for caret exists at line-end. fixes issue #0020053
    ........
    
    git-svn-id: branches/fixes_0_9_30@32283 -

--- components/synedit/synedit.pp
+++ components/synedit/synedit.pp
@@ -7522,10 +7522,10 @@ end;
 procedure TCustomSynEdit.RecalcCharsAndLinesInWin(CheckCaret: Boolean);
 var
   NewLinesInWindow: Integer;
+  w: Integer;
 begin
-  FCharsInWindow := Max(1, (ClientWidth
-                            - TextLeftPixelOffset - TextRightPixelOffset
-                            - ScrollBarWidth) div fCharWidth);
+  w := ClientWidth - TextLeftPixelOffset - TextRightPixelOffset - ScrollBarWidth;
+  FCharsInWindow := Max(1, w div fCharWidth);
 
   NewLinesInWindow := Max(0,ClientHeight - ScrollBarWidth) div Max(1,fTextHeight);
   if NewLinesInWindow <> FLinesInWindow then begin
@@ -7535,9 +7535,12 @@ begin
     StatusChanged([scLinesInWindow]);
   end;
 
+  FScreenCaret.Lock;
   FScreenCaret.ClipRect := Rect(TextLeftPixelOffset(False), 0,
                                 ClientWidth - TextRightPixelOffset - ScrollBarWidth + 1,
                                 ClientHeight - ScrollBarWidth);
+  FScreenCaret.ClipExtraPixel := w - fCharsInWindow * fCharWidth;
+  FScreenCaret.UnLock;
 
   if CheckCaret then begin
     if not (eoScrollPastEol in Options) then
