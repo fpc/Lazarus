de433357cd6c1ea8bbedbf251cbf9bfba99897e3
vincents <vincents@lazarus-ide.org>
2011-11-04 18:05:09 +0000

    set version to 0.9.30.3
    git-svn-id: branches/fixes_0_9_30@33308 -

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -30,8 +30,8 @@ const
   lcl_major = 0;
   lcl_minor = 9;
   lcl_release = 30;
-  lcl_patch = 2;
-  lcl_version = '0.9.30.2';
+  lcl_patch = 3;
+  lcl_version = '0.9.30.3';
   lcl_fullversion = ((lcl_major *  100 + lcl_minor) * 100 + lcl_release) * 100 + lcl_patch;
 
 implementation
