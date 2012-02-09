fpc/Lazarus

commit 29b6dfde94611c0e1132e15aa215b6b9f0fa6235

Gtk2: fixed showing and handling of popup forms over modal forms.issues #20074 and #20369
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@34676 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/gtk2/gtk2wsforms.pp
@@ -617,9 +617,18 @@ begin
     gtk_window_set_default_size(GtkWindow, Max(1,AForm.Width), Max(1,AForm.Height));
     gtk_widget_set_uposition(PGtkWidget(GtkWindow), AForm.Left, AForm.Top);
     GtkWindowShowModal(AForm, GtkWindow);
-  end
-  else
+  end else
+  begin
+    if (AForm.FormStyle <> fsMDIChild) and AForm.HandleObjectShouldBeVisible and
+      (ModalWindows <> nil) and (ModalWindows.Count > 0) and
+      (AForm.PopupParent = nil) and (AForm.BorderStyle = bsNone) then
+    begin
+      GtkWindow := PGtkWindow(AForm.Handle);
+      gtk_window_set_transient_for(GtkWindow, nil);
+      gtk_window_set_modal(GtkWindow, True);
+    end;
     Gtk2WidgetSet.SetVisible(AWinControl, AForm.HandleObjectShouldBeVisible);
+  end;
   InvalidateLastWFPResult(AWinControl, AWinControl.BoundsRect);
 end;
 

commit 948afeefd58affa73ffddd69e42fa6cd5999f180

Gtk2: fixed regression with THintWindow introduced in r34676 #93ebe05b41. issue #21194
git-svn-id: trunk@35277 -

lcl/interfaces/gtk2/gtk2wsforms.pp
@@ -112,6 +112,7 @@ type
     class procedure SetCallbacks(const AWidget: PGtkWidget; const AWidgetInfo: PWidgetInfo); virtual;
   published
     class function  CreateHandle(const AWinControl: TWinControl; const AParams: TCreateParams): TLCLIntfHandle; override;
+    class procedure ShowHide(const AWinControl: TWinControl); override;
   end;
 
   { TGtk2WSScreen }
@@ -897,4 +898,13 @@ begin
   Set_RC_Name(AWinControl, P);
   SetCallbacks(P, WidgetInfo);
 end;
+
+class procedure TGtk2WSHintWindow.ShowHide(const AWinControl: TWinControl);
+begin
+  if not WSCheckHandleAllocated(AWinControl, 'SetColor') then
+    exit;
+  Gtk2WidgetSet.SetVisible(AWinControl, AWinControl.HandleObjectShouldBeVisible);
+  InvalidateLastWFPResult(AWinControl, AWinControl.BoundsRect);
+end;
+
 end.