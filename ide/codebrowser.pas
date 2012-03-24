fpc/Lazarus

commit 44ec0c447143d3a81b260515d4e7450389936311

SrcEdit: Tab-Menu: names
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36225 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/lazarusidestrconsts.pas
@@ -467,7 +467,8 @@ resourcestring
   lisEditorWindowManager = 'Editor Window Manager';
 
   lisSearchProjectsFrom = 'Search projects from';
-  lisMEOther = 'Other';
+  lisMEOther = 'Other tabs';
+  lisTabsFor = 'Tabs for %s';
   lisIncludeExamples = 'Include Examples';
   lisIncludeTestcases = 'Include Testcases';
   lisMEProjects = 'Projects';
@@ -1108,7 +1109,7 @@ resourcestring
   dlgAutoSave = 'Auto Save';
   lisAskBeforeSavingProjectSSession = 'Ask before saving project''s session';
   dlgEdFiles = 'Editor Files';
-  dlgEnvProject = 'Project';
+  dlgEnvProject = 'Tabs for project';
   lisNumberOfFilesToConvert = 'Number of files to convert: %s';
   lisConvertEncoding = 'Convert Encoding';
   lisConvertProjectOrPackage = 'Convert project or package';

ide/sourceeditor.pp
@@ -5441,8 +5441,10 @@ begin
     SrcEditMenuMoveEditorFirst.MenuItem.Enabled:= (PageCount>1) and (PageIndex>0);
     SrcEditMenuMoveEditorLast.MenuItem.Enabled:= (PageCount>1) and (PageIndex<(PageCount-1));
 
-    if SourceEditorManager<>nil then begin
-      SrcEditMenuSectionEditors.Clear;
+    SrcEditMenuSectionEditors.Clear;
+    if Manager <> nil then begin
+      RegisterIDESubMenu(SrcEditMenuSectionEditors, dlgEnvProject, dlgEnvProject).Visible := False;
+      RegisterIDESubMenu(SrcEditMenuSectionEditors, lisMEOther, lisMEOther).Visible := False;
 
       //first add all pages in the correct order since the editor order can be different from the tab order
       for i := 0 to EditorCount - 1 do
@@ -5454,13 +5456,14 @@ begin
         else begin
           Manager.OnPackageForSourceEditor(P, EditorCur);
           if P <> nil then
-            s := p.Name;
+            s := Format(lisTabsFor, [p.Name]);
         end;
 
         if SrcEditMenuSectionEditors.FindByName(S) is TIDEMenuSection then
           M := TIDEMenuSection(SrcEditMenuSectionEditors.FindByName(S))
         else
           M := RegisterIDESubMenu(SrcEditMenuSectionEditors, S, S);
+        M.Visible := True;
 
         S := ExtractFileName(EditorCur.FileName);
         // check for modification

commit efff4531e30c0ef54f1441dd747cf1318059f854

IDE: fixed translation related regressions after r36225 #575e24d917
git-svn-id: trunk@36287 -

ide/codebrowser.pas
@@ -442,7 +442,7 @@ begin
   FOptions:=TCodeBrowserViewOptions.Create;
   
   FIDEDescription:=lisLazarusIDE;
-  FProjectDescription:=dlgEnvProject;
+  FProjectDescription:=dlgProject;
 
   Name:=NonModalIDEWindowNames[nmiwCodeBrowser];
   Caption := lisCodeBrowser;

ide/frames/desktop_options.pas
@@ -129,7 +129,7 @@ begin
   lblAutoSave.Caption := dlgAutoSave;
   AskSavingOnlySessionCheckBox.Caption:=lisAskBeforeSavingProjectSSession;
   AutoSaveEditorFilesCheckBox.Caption := dlgEdFiles;
-  AutoSaveProjectCheckBox.Caption := dlgEnvProject;
+  AutoSaveProjectCheckBox.Caption := dlgProject;
   AutoSaveIntervalInSecsLabel.Caption := dlgIntvInSec;
 
   // desktop files

ide/lazarusidestrconsts.pas
@@ -2246,6 +2246,7 @@ resourcestring
   dlgFromCursor = '&From cursor';
   dlgFromBeginning = 'From b&eginning';
   dlgScope = 'Scope';
+  dlgProject = 'Project';
   lisWithRequiredPackages = 'With required packages';
   lisLevels = 'Levels';
   lisShowPackages = 'Show packages';

ide/newdialog.pas
@@ -576,7 +576,7 @@ end;
 
 function TNewLazIDEItemCategoryProject.LocalizedName: string;
 begin
-  Result := dlgEnvProject;
+  Result := dlgProject;
 end;
 
 function TNewLazIDEItemCategoryProject.Description: string;

ide/unitdependencies.pas
@@ -424,7 +424,7 @@ begin
 
   SelectUnitButton.Caption:=lisOpen;
   RefreshButton.Caption:=dlgUnitDepRefresh;
-  ShowProjectButton.Caption:=dlgEnvProject;
+  ShowProjectButton.Caption:=dlgProject;
   CloseButton.Caption:=lisBtnClose;
   UnitTreeView.Images := IDEImages.Images_16;
   ToolBar.Images := IDEImages.Images_16;

packager/frames/package_usage_options.pas
@@ -243,7 +243,7 @@ begin
   end;
   LibraryPathEdit.AnchorToNeighbour(akRight,0,LibraryPathButton);
 
-  ProjectGroupBox.Caption := dlgEnvProject;
+  ProjectGroupBox.Caption := dlgProject;
   AddPackageUnitToProjectCheckBox.Caption := podAddPackageUnitToUsesSection;
 end;
 