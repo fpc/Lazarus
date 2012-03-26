fpc/Lazarus

commit 617b7517f7e423e32aad55bdea0415b31aae01ce

IDE: unify some single word resource strings.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36332 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/buildfiledlg.pas
@@ -551,13 +551,13 @@ begin
   OverrideRunProjectCheckbox.Caption:=
     lisBFOnRunProjectExecuteTheRunFileCommandInstead;
 
-  BuildPage.Caption:=lisBFBuild;
+  BuildPage.Caption:=lisBuild;
   BuildWorkingDirGroupbox.Caption:=lisBFWorkingDirectoryLeaveEmptyForFilePath;
   BuildCommandGroupbox.Caption:=lisBFBuildCommand;
   BuildScanForFPCMsgCheckbox.Caption:=lisCOScanForFPCMessages;
   BuildScanForMakeMsgCheckbox.Caption:=lisCOScanForMakeMessages;
 
-  RunPage.Caption:=lisBFRun;
+  RunPage.Caption:=lisRun;
   AlwaysCompileFirstCheckbox.Caption:=lisBFAlwaysBuildBeforeRun;
   RunWorkDirGroupbox.Caption:=lisBFWorkingDirectoryLeaveEmptyForFilePath;
   RunCommandGroupbox.Caption:=lisBFRunCommand;

ide/frames/compiler_compilation_options.pas
@@ -70,9 +70,9 @@ begin
   chkCreateMakefile.Caption := dlgCOCreateMakefile;
 
   ExecuteBeforeGroupBox.Caption := lisCOExecuteBefore;
-  chkExecBeforeBuild.Caption := lisCOCallOnBuild;
-  chkExecBeforeCompile.Caption := lisCOCallOnCompile;
-  chkExecBeforeRun.Caption := lisCOCallOnRun;
+  chkExecBeforeBuild.Caption := lisBuild;
+  chkExecBeforeCompile.Caption := lisCompile;
+  chkExecBeforeRun.Caption := lisRun;
   ExecuteBeforeCommandEdit.Text := '';
   ExecuteBeforeCommandLabel.Caption := lisCOCommand;
   ExecuteBeforeScanLabel.Caption := lisCOScanForMessages;
@@ -82,20 +82,20 @@ begin
   lblRunIfExecBefore.Caption := lisCOCallOn;
 
   grpCompiler.Caption := lisCompiler;
-  chkCompilerBuild.Caption := lisCOCallOnBuild;
+  chkCompilerBuild.Caption := lisBuild;
   chkCompilerBuild.Checked := True;
-  chkCompilerCompile.Caption := lisCOCallOnCompile;
+  chkCompilerCompile.Caption := lisCompile;
   chkCompilerCompile.Checked := True;
-  chkCompilerRun.Caption := lisCOCallOnRun;
+  chkCompilerRun.Caption := lisRun;
   chkCompilerRun.Checked := True;
   edtCompiler.Text := '';
   lblCompiler.Caption := lisCOCommand;
   lblRunIfCompiler.Caption := lisCOCallOn;
 
   ExecuteAfterGroupBox.Caption := lisCOExecuteAfter;
-  chkExecAfterBuild.Caption := lisCOCallOnBuild;
-  chkExecAfterCompile.Caption := lisCOCallOnCompile;
-  chkExecAfterRun.Caption := lisCOCallOnRun;
+  chkExecAfterBuild.Caption := lisBuild;
+  chkExecAfterCompile.Caption := lisCompile;
+  chkExecAfterRun.Caption := lisRun;
   ExecuteAfterCommandEdit.Text := '';
   ExecuteAfterCommandLabel.Caption := lisCOCommand;
   ExecuteAfterScanLabel.Caption := lisCOScanForMessages;
@@ -145,7 +145,7 @@ begin
       chkCompilerCompile.Checked := crCompile in CompileReasons;
       chkCompilerBuild.Checked := crBuild in CompileReasons;
       chkCompilerRun.Checked := crRun in CompileReasons;
-      chkCompilerCompile.Caption := lisCOCallOnCompile;
+      chkCompilerCompile.Caption := lisCompile;
       chkCompilerCompile.Visible := True;
       chkCompilerBuild.Visible := True;
       chkCompilerRun.Visible := True;

ide/lazarusidestrconsts.pas
@@ -97,6 +97,13 @@ resourcestring
   lisVariable = 'Variable';
   lisPath = 'Path';
   lisId = 'ID';
+  lisCompile = 'Compile';
+  lisBuild = 'Build';
+  lisRun = 'Run';
+  lisPause = 'Pause';
+  lisStop = 'Stop';
+  lisLess = 'Less';
+  lisMore = 'More';
 
   // errors
   lisErrInvalidOption = 'Invalid option at position %d: "%s"';
@@ -375,12 +382,8 @@ resourcestring
   lisMenuViewProjectSource = '&View Project Source';
   lisMenuProjectOptions = 'Project Options ...';
 
-  lisMenuCompile = 'Compile';
-  lisBFBuild = 'Build';
-  lisBFRun = 'Run';
   lisBFWorkingDirectoryLeaveEmptyForFilePath = 'Working directory (leave empty for file path)';
   lisBFBuildCommand = 'Build Command';
-  lisMenuBuild = 'Build';
   lisMenuQuickCompile = 'Quick Compile';
   lisMenuCleanUpCompiled = 'Clean up Build Files ...';
   lisMenuAbortBuild = 'Abort Build';
@@ -392,7 +395,6 @@ resourcestring
   lisDisAssGotoAddressHint = 'Goto Address';
 
   lisBFRunCommand = 'Run Command';
-  lisMenuPause = 'Pause';
   lisMenuShowExecutionPoint = 'S&how Execution Point';
   lisMenuStepInto = 'Step In&to';
   lisMenuStepOver = '&Step Over';
@@ -405,7 +407,6 @@ resourcestring
   lisMenuStepOut = 'Step O&ut';
   lisMenuRunToCursor = 'Run to &Cursor';
   lisKMStopProgram = 'Stop Program';
-  lisMenuStop = 'Stop';
   lisContinueAndDoNotAskAgain = 'Continue and do not ask again';
   lisSuspiciousUnitPath = 'Suspicious unit path';
   lisThePackageAddsThePathToTheUnitPathOfTheIDEThisIsPr = 'The package %s '
@@ -950,9 +951,6 @@ resourcestring
   lisHintToggleFormUnit = 'Toggle Form/Unit';
   lisHintViewUnits = 'View Units';
   lisHintViewForms = 'View Forms';
-  lisHintRun = 'Run';
-  lisHintPause = 'Pause';
-  lisHintStop = 'Stop';
   lisHintStepInto = 'Step Into';
   lisHintStepOver = 'Step Over';
   lisHintStepOut  = 'Run until function returns';
@@ -1976,9 +1974,6 @@ resourcestring
     +'standard config.';
   lisCOClickOKIfAreSureToDoThat = '%s%sClick OK if you are sure to do that.';
   lisCOCallOn = 'Call on:';
-  lisCOCallOnCompile = 'Compile';
-  lisCOCallOnBuild = 'Build';
-  lisCOCallOnRun = 'Run';
   dlgCOCreateMakefile = 'Create Makefile';
   lisCOExecuteAfter = 'Execute after';
   lisCOExecuteBefore = 'Execute before';
@@ -2949,7 +2944,6 @@ resourcestring
   lisLazBuildRenameProfInfo = 'New name for profile:';
   lisCTDTemplates = 'Templates';
   lisSaveSettings = 'Save Settings';
-  lisBuild = 'Build';
   lisCleanUp = 'Clean up';
   lisLazBuildBuildMany = 'Build Many';
   lisAutomatically = 'Automatically';
@@ -4052,7 +4046,6 @@ resourcestring
     +'preferred filename of dependency';
   lisRemoveNonExistingFiles = 'Remove non existing files';
   lisPckEditReAddDependency = 'Re-Add dependency';
-  lisPckEditCompile = 'Compile';
   lisPckEditRecompileClean = 'Recompile Clean';
   lisPckEditRecompileAllRequired = 'Recompile All Required';
   lisPckEditCreateMakefile = 'Create Makefile';
@@ -4666,7 +4659,6 @@ resourcestring
   lisViewSource = 'View Source';
   lisViewSourceDisass = 'View Assembler';
   lisMaxS = 'Max %d';
-  lisMore = 'More';
   lisGotoSelectedSourceLine = 'Goto selected source line';
   lisCopyAll = 'Copy All';
   lisIndex = 'Index';
@@ -5403,9 +5395,6 @@ resourcestring
   lisDebuggerFeedbackInformation = 'Debugger Information';
   lisDebuggerFeedbackWarning = 'Debugger Warning';
   lisDebuggerFeedbackError = 'Debugger Error';
-  lisDebuggerFeedbackStop = 'Stop';
-  lisDebuggerFeedbackLess = 'Less';
-  lisDebuggerFeedbackMore = 'More';
 
   // breakpointgroups
   dbgBreakGroupDlgCaptionEnable = 'Select Groups';

debugger/feedbackdlg.pp
@@ -55,9 +55,9 @@ begin
   AutoSize := False;
   Memo1.Visible := not Memo1.Visible;
   if Memo1.Visible then
-    ButtonPanel1.HelpButton.Caption := lisDebuggerFeedbackLess
+    ButtonPanel1.HelpButton.Caption := lisLess
   else
-    ButtonPanel1.HelpButton.Caption := lisDebuggerFeedbackMore;
+    ButtonPanel1.HelpButton.Caption := lisMore;
   AutoSize := True;
 end;
 
@@ -79,7 +79,7 @@ begin
   memo1.Text := AInfo;
   memo1.Visible := False;
   if AInfo <> '' then begin
-    ButtonPanel1.HelpButton.Caption := lisDebuggerFeedbackMore;
+    ButtonPanel1.HelpButton.Caption := lisMore;
     ButtonPanel1.HelpButton.Visible := True;
   end
   else begin
@@ -90,7 +90,7 @@ begin
   ButtonPanel1.CancelButton.Visible := frStop in AButtons;
 
   ButtonPanel1.OKButton.Caption := lisMenuOk;
-  ButtonPanel1.CancelButton.Caption := lisDebuggerFeedbackStop;
+  ButtonPanel1.CancelButton.Caption := lisStop;
 
   case ShowModal of
     mrOk: Result := frOk;

ide/main.pp
@@ -1955,9 +1955,9 @@ begin
   MainIDEBar.ViewFormsSpeedBtn   := CreateButton(MainIDEBar.tbViewDebug, 'ViewFormsSpeedBtn'  , 'menu_view_forms'           , @mnuViewFormsClicked, lisHintViewForms);
   MainIDEBar.tbDivider3          := CreateDivider(MainIDEBar.tbViewDebug);
   MainIDEBar.BuildModeSpeedButton:= CreateButton(MainIDEBar.tbViewDebug, 'BuildModeSpeedButton', 'menu_compiler_options'    , @mnuChgBuildModeClicked, lisChangeBuildMode);
-  MainIDEBar.RunSpeedButton      := CreateButton(MainIDEBar.tbViewDebug, 'RunSpeedButton'     , 'menu_run'                  , @mnuRunProjectClicked, lisHintRun);
-  MainIDEBar.PauseSpeedButton    := CreateButton(MainIDEBar.tbViewDebug, 'PauseSpeedButton'   , 'menu_pause'                , @mnuPauseProjectClicked, lisHintPause);
-  MainIDEBar.StopSpeedButton     := CreateButton(MainIDEBar.tbViewDebug, 'StopSpeedButton'    , 'menu_stop'                 , @mnuStopProjectClicked, lisHintStop);
+  MainIDEBar.RunSpeedButton      := CreateButton(MainIDEBar.tbViewDebug, 'RunSpeedButton'     , 'menu_run'                  , @mnuRunProjectClicked, lisRun);
+  MainIDEBar.PauseSpeedButton    := CreateButton(MainIDEBar.tbViewDebug, 'PauseSpeedButton'   , 'menu_pause'                , @mnuPauseProjectClicked, lisPause);
+  MainIDEBar.StopSpeedButton     := CreateButton(MainIDEBar.tbViewDebug, 'StopSpeedButton'    , 'menu_stop'                 , @mnuStopProjectClicked, lisStop);
   MainIDEBar.StepIntoSpeedButton := CreateButton(MainIDEBar.tbViewDebug, 'StepIntoSpeedButton', 'menu_stepinto'             , @mnuStepIntoProjectClicked, lisHintStepInto);
   MainIDEBar.StepOverSpeedButton := CreateButton(MainIDEBar.tbViewDebug, 'StepOverpeedButton' , 'menu_stepover'             , @mnuStepOverProjectClicked, lisHintStepOver);
   MainIDEBar.StepOutSpeedButton  := CreateButton(MainIDEBar.tbViewDebug, 'StepOutSpeedButton' , 'menu_stepout'              , @mnuStepOutProjectClicked, lisHintStepOut);
@@ -3838,7 +3838,7 @@ begin
         if (rfInteractive in AFlags)
         and (IDEQuestionDialog(lisStopDebugging,
             lisStopTheDebugging, mtConfirmation,
-            [mrYes, lisMenuStop, mrCancel, lisContinue]) <> mrYes)
+            [mrYes, lisStop, mrCancel, lisContinue]) <> mrYes)
         then exit;
         if (DebugBoss.DoStopProject = mrOK) and (ToolStatus = itDebugger) and (rfCloseOnDone in AFlags) then
           FWaitForClose := True;

ide/mainbase.pas
@@ -666,8 +666,8 @@ begin
   with MainIDEBar do begin
     CreateMenuSeparatorSection(mnuRun,itmRunBuilding,'itmRunBuilding');
     ParentMI:=itmRunBuilding;
-    CreateMenuItem(ParentMI,itmRunMenuCompile,'itmRunMenuCompile',lisMenuCompile,'menu_build');
-    CreateMenuItem(ParentMI,itmRunMenuBuild,'itmRunMenuBuild',lisMenuBuild,'menu_build_all');
+    CreateMenuItem(ParentMI,itmRunMenuCompile,'itmRunMenuCompile',lisCompile,'menu_build');
+    CreateMenuItem(ParentMI,itmRunMenuBuild,'itmRunMenuBuild',lisBuild,'menu_build_all');
     CreateMenuItem(ParentMI,itmRunMenuQuickCompile,'itmRunMenuQuickCompile',lisMenuQuickCompile,'menu_quick_compile');
     CreateMenuItem(ParentMI,itmRunMenuCleanUpCompiled,'itmRunMenuCleanUpCompiled',lisMenuCleanUpCompiled,'menu_build');
     CreateMenuItem(ParentMI,itmRunMenuAbortBuild,'itmRunMenuAbortBuild',lisMenuAbortBuild,'menu_abort_build');
@@ -675,14 +675,14 @@ begin
     CreateMenuSeparatorSection(mnuRun,itmRunnning,'itmRunnning');
     ParentMI:=itmRunnning;
     CreateMenuItem(ParentMI,itmRunMenuRun,'itmRunMenuRun',lisMenuProjectRun,'menu_run');
-    CreateMenuItem(ParentMI,itmRunMenuPause,'itmRunMenuPause',lisMenuPause,'menu_pause', False);
+    CreateMenuItem(ParentMI,itmRunMenuPause,'itmRunMenuPause',lisPause,'menu_pause', False);
     CreateMenuItem(ParentMI,itmRunMenuShowExecutionPoint,'itmRunMenuShowExecutionPoint',
                    lisMenuShowExecutionPoint,'debugger_show_execution_point', False);
     CreateMenuItem(ParentMI,itmRunMenuStepInto,'itmRunMenuStepInto',lisMenuStepInto,'menu_stepinto');
     CreateMenuItem(ParentMI,itmRunMenuStepOver,'itmRunMenuStepOver',lisMenuStepOver,'menu_stepover');
     CreateMenuItem(ParentMI,itmRunMenuStepOut,'itmRunMenuStepOut',lisMenuStepOut,'menu_stepout');
     CreateMenuItem(ParentMI,itmRunMenuRunToCursor,'itmRunMenuRunToCursor',lisMenuRunToCursor,'menu_run_cursor');
-    CreateMenuItem(ParentMI,itmRunMenuStop,'itmRunMenuStop',lisMenuStop,'menu_stop', False);
+    CreateMenuItem(ParentMI,itmRunMenuStop,'itmRunMenuStop',lisStop,'menu_stop', False);
     CreateMenuItem(ParentMI,itmRunMenuRunParameters,'itmRunMenuRunParameters',lisMenuRunParameters, 'menu_run_parameters');
     CreateMenuItem(ParentMI,itmRunMenuResetDebugger,'itmRunMenuResetDebugger',lisMenuResetDebugger, 'menu_reset_debugger');
 

packager/packageeditor.pas
@@ -461,7 +461,7 @@ begin
   // register the section for compiling the package
   PkgEditMenuSectionCompile:=RegisterIDEMenuSection(PackageEditorMenuRoot,'Compile');
   AParent:=PkgEditMenuSectionCompile;
-  PkgEditMenuCompile:=RegisterIDEMenuCommand(AParent,'Compile',lisPckEditCompile);
+  PkgEditMenuCompile:=RegisterIDEMenuCommand(AParent,'Compile',lisCompile);
   PkgEditMenuRecompileClean:=RegisterIDEMenuCommand(AParent,'Recompile Clean',lisPckEditRecompileClean);
   PkgEditMenuRecompileAllRequired:=RegisterIDEMenuCommand(AParent,'Recompile All Required',lisPckEditRecompileAllRequired);
   PkgEditMenuCreateMakefile:=RegisterIDEMenuCommand(AParent,'Create Makefile',lisPckEditCreateMakefile);
@@ -1282,7 +1282,7 @@ begin
   FilterEdit.OnGetImageIndex:=@ChooseImageIndex;
 
   SaveBitBtn    := CreateToolButton('SaveBitBtn', lisMenuSave, lisPckEditSavePackage, 'laz_save', @SaveBitBtnClick);
-  CompileBitBtn := CreateToolButton('CompileBitBtn', lisPckEditCompile, lisPckEditCompilePackage, 'pkg_compile', @CompileBitBtnClick);
+  CompileBitBtn := CreateToolButton('CompileBitBtn', lisCompile, lisPckEditCompilePackage, 'pkg_compile', @CompileBitBtnClick);
   UseBitBtn     := CreateToolButton('UseBitBtn', lisPckEditInstall, lisPckEditInstallPackageInTheIDE, 'pkg_install', nil);
   CreateDivider;
   AddBitBtn     := CreateToolButton('AddBitBtn', lisAdd, lisPckEditAddAnItem, 'laz_add', @AddBitBtnClick);

commit e162d9ebdc85b030688ba348de9031d188f04f6c

IDE: fixed 'Build' constant translation regressions after r36332 #22c9cf054e
git-svn-id: trunk@36361 -

ide/buildfiledlg.pas
@@ -551,7 +551,7 @@ begin
   OverrideRunProjectCheckbox.Caption:=
     lisBFOnRunProjectExecuteTheRunFileCommandInstead;
 
-  BuildPage.Caption:=lisBuild;
+  BuildPage.Caption:=lisBuildCaption;
   BuildWorkingDirGroupbox.Caption:=lisBFWorkingDirectoryLeaveEmptyForFilePath;
   BuildCommandGroupbox.Caption:=lisBFBuildCommand;
   BuildScanForFPCMsgCheckbox.Caption:=lisCOScanForFPCMessages;

ide/frames/compiler_compilation_options.pas
@@ -70,7 +70,7 @@ begin
   chkCreateMakefile.Caption := dlgCOCreateMakefile;
 
   ExecuteBeforeGroupBox.Caption := lisCOExecuteBefore;
-  chkExecBeforeBuild.Caption := lisBuild;
+  chkExecBeforeBuild.Caption := lisBuildStage;
   chkExecBeforeCompile.Caption := lisCompile;
   chkExecBeforeRun.Caption := lisRun;
   ExecuteBeforeCommandEdit.Text := '';
@@ -82,7 +82,7 @@ begin
   lblRunIfExecBefore.Caption := lisCOCallOn;
 
   grpCompiler.Caption := lisCompiler;
-  chkCompilerBuild.Caption := lisBuild;
+  chkCompilerBuild.Caption := lisBuildStage;
   chkCompilerBuild.Checked := True;
   chkCompilerCompile.Caption := lisCompile;
   chkCompilerCompile.Checked := True;
@@ -93,7 +93,7 @@ begin
   lblRunIfCompiler.Caption := lisCOCallOn;
 
   ExecuteAfterGroupBox.Caption := lisCOExecuteAfter;
-  chkExecAfterBuild.Caption := lisBuild;
+  chkExecAfterBuild.Caption := lisBuildStage;
   chkExecAfterCompile.Caption := lisCompile;
   chkExecAfterRun.Caption := lisRun;
   ExecuteAfterCommandEdit.Text := '';

ide/lazarusidestrconsts.pas
@@ -99,6 +99,8 @@ resourcestring
   lisId = 'ID';
   lisCompile = 'Compile';
   lisBuild = 'Build';
+  lisBuildCaption = 'Build';
+  lisBuildStage = 'Build';
   lisRun = 'Run';
   lisPause = 'Pause';
   lisStop = 'Stop';