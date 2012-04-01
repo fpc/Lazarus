fpc/Lazarus

commit 70badc5a188222fb4b23b6af4d76bd8ccda29da7

lazreport: fixed TfrBandView.GetClipRgn
git-svn-id: trunk@36424 -

components/lazreport/source/lr_class.pas
@@ -3800,7 +3800,7 @@ begin
   R2:=CreateRectRgn(0,0,0,0);
 
   RR:=CombineRgn(R2, R, R1, RGN_OR);
-  Result:=R2;
+  Result:=RR;
 
   
   DeleteObject(R);

commit a0bccb6c1db4b66971513126d5432204cdd0527c

LazReport, undo r36424 #70badc5a18 which broke bands clipregion
git-svn-id: trunk@36495 -

components/lazreport/source/lr_class.pas
@@ -3807,7 +3807,7 @@ begin
   R2:=CreateRectRgn(0,0,0,0);
 
   RR:=CombineRgn(R2, R, R1, RGN_OR);
-  Result:=RR;
+  Result:=R2;
 
   
   DeleteObject(R);