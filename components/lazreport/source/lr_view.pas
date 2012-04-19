fpc/Lazarus

commit e19218be2240a2a7c45ce529006ebacb792bd518

LazReport, fix preview scrolling broken in r36420, issue 21633
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36887 4005530d-fff6-0310-9dd1-cebe43e6787f

components/lazreport/source/lr_view.pas
@@ -888,18 +888,19 @@ end;
 procedure TfrPreviewForm.VScrollBarChange(Sender: TObject);
 var
   {$IFDEF WIN32}
-  p, pp: Integer;
   r: TRect;
+  pp: Integer;
   {$ENDIF}
+  p: Integer;
   i: integer;
   Pages: TfrEMFPages;
 begin
   if EMFPages = nil then Exit;
-  {$IFDEF WIN32}
   p := VScrollBar.Position;
-  pp := OldV - p;
-  OldV := p;
   ofy := -p;
+  {$IFDEF WIN32}
+  OldV := p;
+  pp := OldV - p;
   r := Rect(0, 0, PBox.Width, PBox.Height);
   ScrollWindowEx(PBox.Handle, 0, pp, @r, @r, 0, nil, SW_INVALIDATE);
   UpdateWindow(Pbox.Handle);
@@ -918,18 +919,19 @@ begin
 end;
 
 procedure TfrPreviewForm.HScrollBarChange(Sender: TObject);
-{$IFDEF WIN32}
 var
-  p, pp: Integer;
+  p: Integer;
+{$IFDEF WIN32}
+  pp: Integer;
   r: TRect;
 {$ENDIF}
 begin
   if EMFPages = nil then Exit;
-  {$IFDEF WIN32}
   p := HScrollBar.Position;
+  ofx := -p;
+  {$IFDEF WIN32}
   pp := OldH - p;
   OldH := p;
-  ofx := -p;
   r := Rect(0, 0, PBox.Width, PBox.Height);
   ScrollWindowEx(PBox.Handle, pp, 0, @r, @r, 0, nil, SW_INVALIDATE);
   UpdateWindow(Pbox.Handle);

commit 09183483690c9d05e4d20a85786258cdb318f63e

LazReport, now fix preview scrolling in windows, broken in r36887 #ef8cf9df31
git-svn-id: trunk@36909 -

components/lazreport/source/lr_view.pas
@@ -899,8 +899,8 @@ begin
   p := VScrollBar.Position;
   ofy := -p;
   {$IFDEF WIN32}
-  OldV := p;
   pp := OldV - p;
+  OldV := p;
   r := Rect(0, 0, PBox.Width, PBox.Height);
   ScrollWindowEx(PBox.Handle, 0, pp, @r, @r, 0, nil, SW_INVALIDATE);
   UpdateWindow(Pbox.Handle);