2e60caba668340ab577d43d9a670553c8905b45f
zeljko <zeljko@lazarus-ide.org>
2012-06-07 09:27:36 +0000

    Merged revisions 37444 #c319ad6bd8 from /trunk ------------------------------------------------------------------------ r37444 | martin | 2012-05-28 17:20:17 +0200 (Mon, 28 May 2012) | 1 line
    DBG: Watches, save "Use instance class" to config
    ------------------------------------------------------------------------
    
    git-svn-id: branches/fixes_1_0@37553 -

--- debugger/debugger.pp
+++ debugger/debugger.pp
@@ -4775,6 +4775,9 @@ begin
   FThreadId   := AConfig.GetValue(APath + 'ThreadId', -1);
   FStackFrame := AConfig.GetValue(APath + 'StackFrame', -1);
   FValue      := AConfig.GetValue(APath + 'Value', '');
+  if AConfig.GetValue(APath + 'ClassAutoCast', False)
+  then Include(FEvaluateFlags, defClassAutoCast)
+  else Exclude(FEvaluateFlags, defClassAutoCast);
   try    ReadStr(AConfig.GetValue(APath + 'DisplayFormat', 'wdfDefault'), FDisplayFormat);
   except FDisplayFormat := wdfDefault; end;
   try    ReadStr(AConfig.GetValue(APath + 'Validity', 'ddsValid'), FValidity);
@@ -4792,6 +4795,7 @@ begin
   AConfig.SetDeleteValue(APath + 'DisplayFormat', s, 'wdfDefault');
   WriteStr(s, FValidity);
   AConfig.SetDeleteValue(APath + 'Validity', s, 'ddsValid');
+  AConfig.SetDeleteValue(APath + 'ClassAutoCast', defClassAutoCast in FEvaluateFlags, False);
 end;
 
 constructor TWatchValue.Create;
@@ -8235,6 +8239,9 @@ procedure TWatch.LoadDataFromXMLConfig(const AConfig: TXMLConfig; const APath: s
 begin
   FEnabled    := AConfig.GetValue(APath + 'Enabled', True);
   FExpression := AConfig.GetValue(APath + 'Expression', '');
+  if AConfig.GetValue(APath + 'ClassAutoCast', False)
+  then Include(FEvaluateFlags, defClassAutoCast)
+  else Exclude(FEvaluateFlags, defClassAutoCast);
   try    ReadStr(AConfig.GetValue(APath + 'DisplayFormat', 'wdfDefault'), FDisplayFormat);
   except FDisplayFormat := wdfDefault; end;
 
@@ -8249,6 +8256,7 @@ begin
   AConfig.SetDeleteValue(APath + 'Expression', FExpression, '');
   WriteStr(s{%H-}, FDisplayFormat);
   AConfig.SetDeleteValue(APath + 'DisplayFormat', s, 'wdfDefault');
+  AConfig.SetDeleteValue(APath + 'ClassAutoCast', defClassAutoCast in FEvaluateFlags, False);
 
   FValueList.SaveDataToXMLConfig(AConfig, APath + 'ValueList/');
 end;
@@ -8341,6 +8349,9 @@ var
 begin
   Expression := AConfig.GetValue(APath + 'Expression/Value', '');
   Enabled := AConfig.GetValue(APath + 'Enabled/Value', true);
+  if AConfig.GetValue(APath + 'ClassAutoCast', False)
+  then Include(FEvaluateFlags, defClassAutoCast)
+  else Exclude(FEvaluateFlags, defClassAutoCast);
   i := StringCase
     (AConfig.GetValue(APath + 'DisplayStyle/Value', TWatchDisplayFormatNames[wdfDefault]),
     TWatchDisplayFormatNames);
@@ -8355,6 +8366,7 @@ begin
   AConfig.SetDeleteValue(APath + 'Enabled/Value', Enabled, true);
   AConfig.SetDeleteValue(APath + 'DisplayStyle/Value',
     TWatchDisplayFormatNames[DisplayFormat], TWatchDisplayFormatNames[wdfDefault]);
+  AConfig.SetDeleteValue(APath + 'ClassAutoCast', defClassAutoCast in FEvaluateFlags, False);
 end;
 
 { =========================================================================== }
