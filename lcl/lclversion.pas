690370c970c22a41117132b43808074afadf64aa
mattias <nc-gaertnma@netcologne.de>
2012-08-28 17:38:35 +0000

    fixes: set version to 1.0.1.4
    git-svn-id: branches/fixes_1_0@38420 -

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -40,9 +40,9 @@ const
   lcl_major = 1;
   lcl_minor = 0;
   lcl_release = 1;
-  lcl_patch = 3;
+  lcl_patch = 4;
   lcl_fullversion = ((lcl_major *  100 + lcl_minor) * 100 + lcl_release) * 100 + lcl_patch;
-  lcl_version = '1.0.1.3';
+  lcl_version = '1.0.1.4';
 
 implementation
 
