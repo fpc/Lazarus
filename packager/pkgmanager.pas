9491b22d465f1300fb9d2cef7fa477e52542351b
maxim <maxim@lazarus-ide.org>
2012-10-09 19:57:38 +0000

    Merged revision(s) 38989 #07e313123f from trunk: Packager: Use a new component's class name as icon resource name. Issue #23075 ........
    git-svn-id: branches/fixes_1_0@39028 -

--- packager/pkgmanager.pas
+++ packager/pkgmanager.pas
@@ -630,8 +630,8 @@ var
   BinFileStream: TFileStreamUTF8;
   BinMemStream: TMemoryStream;
   BinExt: String;
-  ResourceType: String;
-  ResourceName: String;
+  ResType: String;
+  ResName: String;
   ResMemStream: TMemoryStream;
   CodeBuf: TCodeBuffer;
 begin
@@ -655,10 +655,9 @@ begin
           BinMemStream.CopyFrom(BinFileStream,BinFileStream.Size);
           BinMemStream.Position:=0;
           BinExt:=uppercase(ExtractFileExt(Params.IconFile));
-          ResourceType:=copy(BinExt,2,length(BinExt)-1);
-          ResourceName:=ExtractFileNameOnly(Params.IconFile);
-          BinaryToLazarusResourceCode(BinMemStream,ResMemStream
-             ,ResourceName,ResourceType);
+          ResType:=copy(BinExt,2,length(BinExt)-1);
+          ResName:=ExtractFileNameOnly(Params.NewClassName);
+          BinaryToLazarusResourceCode(BinMemStream,ResMemStream,ResName,ResType);
           ResMemStream.Position:=0;
           CodeBuf.LoadFromStream(ResMemStream);
           Result:=SaveCodeBuffer(CodeBuf);
