fpc/Lazarus

commit 29c2bc32d03354e4b2ddf5c2bf663ea64352176a

Gtk2: do not render listbox item by default if we are customdrawn. issue #23093
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@39032 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/gtk2/gtk2cellrenderer.pas
@@ -214,8 +214,24 @@ begin
 
   // draw default
   CellClass := PLCLIntfCellRendererClass(gtk_object_get_class(cell));
-  CellClass^.DefaultGtkRender(cell, Window, Widget, background_area, cell_area,
-                              expose_area, flags);
+
+  // do not call DefaultGtkRender when we are custom drawn listbox.issue #23093
+  AWinControl := nil;
+  if ColumnIndex < 0 then
+  begin
+    AWinControl := GetControl(cell, widget);
+    if [csDestroying,csLoading]*AWinControl.ComponentState<>[] then
+      AWinControl := nil;
+    if AWinControl is TCustomListbox then
+      if TCustomListbox(AWinControl).Style = lbStandard then
+        AWinControl := nil;
+    if AWinControl is TCustomCombobox then
+      AWinControl := nil;
+  end;
+  // do default draw only if we are customdrawn.
+  if (AWinControl = nil) then
+    CellClass^.DefaultGtkRender(cell, Window, Widget, background_area, cell_area,
+      expose_area, flags);
   
   if ColumnIndex < 0 then  // is a listbox or combobox
   begin

commit 99d869e0a14e9b361e6714c8760ec8c6604b23c5

Gtk2: fixed crash with TListView introduced with r39032 #be121459c7.
git-svn-id: trunk@39033 -

lcl/interfaces/gtk2/gtk2cellrenderer.pas
@@ -216,11 +216,10 @@ begin
   CellClass := PLCLIntfCellRendererClass(gtk_object_get_class(cell));
 
   // do not call DefaultGtkRender when we are custom drawn listbox.issue #23093
-  AWinControl := nil;
   if ColumnIndex < 0 then
   begin
     AWinControl := GetControl(cell, widget);
-    if [csDestroying,csLoading]*AWinControl.ComponentState<>[] then
+    if [csDestroying,csLoading,csDesigning]*AWinControl.ComponentState<>[] then
       AWinControl := nil;
     if AWinControl is TCustomListbox then
       if TCustomListbox(AWinControl).Style = lbStandard then
@@ -229,7 +228,7 @@ begin
       AWinControl := nil;
   end;
   // do default draw only if we are customdrawn.
-  if (AWinControl = nil) then
+  if (ColumnIndex > -1) or ((ColumnIndex < 0) and (AWinControl = nil)) then
     CellClass^.DefaultGtkRender(cell, Window, Widget, background_area, cell_area,
       expose_area, flags);
   