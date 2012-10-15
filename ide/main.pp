fpc/Lazarus

commit 1414fef9021cb71ee61a62c86c7213f973622814

IDE: Save changes in Object Inspector before compiling the project. Issues #22601 and #17795
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@39082 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/main.pp
@@ -3067,16 +3067,19 @@ begin
     ShowContextHelpEditor(Sender);
 
   ecSave:
-    if (Sender is TDesigner) then begin
-      GetDesignerUnit(TDesigner(Sender),ASrcEdit,AnUnitInfo);
-      if (AnUnitInfo<>nil) and (AnUnitInfo.OpenEditorInfoCount > 0) then
-        DoSaveEditorFile(ASrcEdit, [sfCheckAmbiguousFiles]);
-    end else if (Sender is TObjectInspectorDlg) then begin
-      GetObjectInspectorUnit(ASrcEdit,AnUnitInfo);
-      if (AnUnitInfo<>nil) and (AnUnitInfo.OpenEditorInfoCount > 0) then
-        DoSaveEditorFile(ASrcEdit, [sfCheckAmbiguousFiles]);
-    end else if Sender is TSourceNotebook then
-      mnuSaveClicked(Self);
+    begin
+      ObjectInspector1.GetActivePropertyGrid.SaveChanges;
+      if (Sender is TDesigner) or (Sender is TObjectInspectorDlg) then begin
+        if (Sender is TDesigner) then
+          GetDesignerUnit(TDesigner(Sender),ASrcEdit,AnUnitInfo)
+        else
+          GetObjectInspectorUnit(ASrcEdit,AnUnitInfo);
+        if (AnUnitInfo<>nil) and (AnUnitInfo.OpenEditorInfoCount > 0) then
+          DoSaveEditorFile(ASrcEdit, [sfCheckAmbiguousFiles]);
+      end
+      else if Sender is TSourceNotebook then
+        mnuSaveClicked(Self);
+    end;
 
   ecOpen:
     mnuOpenClicked(Self);
@@ -8180,14 +8183,16 @@ function TMainIDE.PrepareForCompile: TModalResult;
 begin
   Result:=mrOk;
   if ToolStatus=itDebugger then begin
-    Result:=IDEQuestionDialog(lisStopDebugging2,
-      lisStopCurrentDebuggingAndRebuildProject,
-      mtConfirmation,[mrYes, mrCancel, lisNo],'');
+    Result:=IDEQuestionDialog(lisStopDebugging2, lisStopCurrentDebuggingAndRebuildProject,
+                              mtConfirmation,[mrYes, mrCancel, lisNo],'');
     if Result<>mrYes then exit;
 
     Result:=DebugBoss.DoStopProject;
     if Result<>mrOk then exit;
   end;
+
+  // Save the property editor value in Object Inspector
+  ObjectInspector1.GetActivePropertyGrid.SaveChanges;
 
   if MainBuildBoss.CompilerOnDiskChanged then
     MainBuildBoss.RescanCompilerDefines(false,false,false,false);

commit ec95f00f98a74bdf0d22606b629014fc200bbe42

IDE: prevent crash when Object Inspector is not assigned and project is compiled. Caused by r39082 #3b90468c10
git-svn-id: trunk@39091 -

ide/main.pp
@@ -3068,7 +3068,8 @@ begin
 
   ecSave:
     begin
-      ObjectInspector1.GetActivePropertyGrid.SaveChanges;
+      if Assigned(ObjectInspector1) then
+        ObjectInspector1.GetActivePropertyGrid.SaveChanges;
       if (Sender is TDesigner) or (Sender is TObjectInspectorDlg) then begin
         if (Sender is TDesigner) then
           GetDesignerUnit(TDesigner(Sender),ASrcEdit,AnUnitInfo)
@@ -8192,7 +8193,8 @@ begin
   end;
 
   // Save the property editor value in Object Inspector
-  ObjectInspector1.GetActivePropertyGrid.SaveChanges;
+  if Assigned(ObjectInspector1) then
+    ObjectInspector1.GetActivePropertyGrid.SaveChanges;
 
   if MainBuildBoss.CompilerOnDiskChanged then
     MainBuildBoss.RescanCompilerDefines(false,false,false,false);