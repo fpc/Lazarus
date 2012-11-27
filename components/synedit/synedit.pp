63454a2110348c634614b2b939c813609341ca66
martin <laz.git@mfriebe.de>
2012-11-27 16:42:03 +0000

    Merged revision(s) 39390 #4aa05cbcf0 from trunk: SynEdit: Fixed do not copy/paste with middle mouse, if SynEdit is readonly. Issue #0023392 ........
    git-svn-id: branches/fixes_1_0@39393 -

--- components/synedit/synedit.pp
+++ components/synedit/synedit.pp
@@ -2998,19 +2998,21 @@ begin
         end;
       emcPasteSelection:
         begin
-          ClipHelper := TSynClipboardStream.Create;
-          try
-            ClipHelper.ReadFromClipboard(PrimarySelection);
-            if ClipHelper.TextP <> nil then begin
-              MoveCaret;
-              if (not FBlockSelection.Persistent) then
-                FBlockSelection.Clear;
-              Result := PasteFromClipboardEx(ClipHelper);
-            end
-            else
-              Result := False;
-          finally
-            ClipHelper.Free;
+          if not ReadOnly then begin
+            ClipHelper := TSynClipboardStream.Create;
+            try
+              ClipHelper.ReadFromClipboard(PrimarySelection);
+              if ClipHelper.TextP <> nil then begin
+                MoveCaret;
+                if (not FBlockSelection.Persistent) then
+                  FBlockSelection.Clear;
+                Result := PasteFromClipboardEx(ClipHelper);
+              end
+              else
+                Result := False;
+            finally
+              ClipHelper.Free;
+            end;
           end;
         end;
       emcMouseLink:
