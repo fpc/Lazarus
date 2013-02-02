97c8b5f5e8299ff685a38e6abcc54ba2819f246d
mattias <nc-gaertnma@netcologne.de>
2013-02-02 08:43:01 +0000

    Version 1.0.7
    git-svn-id: branches/fixes_1_0@40118 -

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -39,10 +39,10 @@ interface
 const
   lcl_major = 1;
   lcl_minor = 0;
-  lcl_release = 6;
+  lcl_release = 7;
   lcl_patch = 0;
   lcl_fullversion = ((lcl_major *  100 + lcl_minor) * 100 + lcl_release) * 100 + lcl_patch;
-  lcl_version = '1.0.6.0';
+  lcl_version = '1.0.7.0';
 
 implementation
 
