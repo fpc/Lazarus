8dff08ef045fdec45eff9a039e626ce3dbfd58b3
mattias <nc-gaertnma@netcologne.de>
2013-06-09 09:25:14 +0000

    set version 1.0.10
    git-svn-id: branches/fixes_1_0@41612 -

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -39,10 +39,10 @@ interface
 const
   lcl_major = 1;
   lcl_minor = 0;
-  lcl_release = 9;
+  lcl_release = 10;
   lcl_patch = 0;
   lcl_fullversion = ((lcl_major *  100 + lcl_minor) * 100 + lcl_release) * 100 + lcl_patch;
-  lcl_version = '1.0.9.0';
+  lcl_version = '1.0.10.0';
 
 implementation
 
