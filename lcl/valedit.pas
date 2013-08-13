d65452bdf6af17fa086d2313ddda642f7fbccaa9
martin <laz.git@mfriebe.de>
2013-08-13 09:40:13 +0000

    Merged revision(s) 42205 #24dd7801d7, 42319 #f40d0f314f from trunk: ValEdit: fix crash in ValueListEditor.AdjustColumnWidths. Patch by Cyrax. Issue #0024779.
    ........
    LCL: Prevent crash in TValueListEditor when resizing a parent form. Issue #24827, patch from Cyrax
    ........
    
    git-svn-id: branches/fixes_1_0@42395 -

--- lcl/valedit.pas
+++ lcl/valedit.pas
@@ -885,13 +885,20 @@ procedure TValueListEditor.AdjustColumnWidths;
 // If key column is fixed in width then adjust only the second column,
 //  otherwise adjust both columns propertionally.
 var
-  CW: Integer;
+  CW, AWidth, BWidth: Integer;
 begin
   CW := ClientWidth;
   if  (doKeyColFixed in DisplayOptions) then
   begin
     //AutoSizeColumn(0);
-    ColWidths[1] := CW - ColWidths[0];
+    If ColWidths[0] > CW Then Begin
+      BWidth := CW - 1;
+      If BWidth > 0 Then
+        ColWidths[0] := BWidth;
+    end;
+    AWidth := CW - ColWidths[0];
+    If AWidth > 0 Then
+      ColWidths[1] := AWidth;
   end
   else
   begin
