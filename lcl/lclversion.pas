cf5dc2847385981e440ce132aab66d1bff6cf75d
mattias <nc-gaertnma@netcologne.de>
2013-11-03 09:33:08 +0000

    set version to 1.2RC2
    git-svn-id: branches/fixes_1_2@43360 -

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -35,9 +35,9 @@ const
   lcl_major = 1;
   lcl_minor = 2;
   lcl_release = 0;
-  lcl_patch = 1;
+  lcl_patch = 2;
   lcl_fullversion = ((lcl_major *  100 + lcl_minor) * 100 + lcl_release) * 100 + lcl_patch;
-  lcl_version = '1.2.0.1';
+  lcl_version = '1.2.0.2';
 
 implementation
 
