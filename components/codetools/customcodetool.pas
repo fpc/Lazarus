fpc/Lazarus

commit 4345ad48817b48c94df6d913fcb098c0cd5c9d1b

Qt: fixed changing font color of themed text draw. issue #25253
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@43385 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/qt/qtthemes.pas
@@ -418,7 +418,15 @@ end;
 procedure TQtThemeServices.DrawText(ACanvas: TPersistent;
   Details: TThemedElementDetails; const S: String; R: TRect; Flags,
   Flags2: Cardinal);
+var
+  AQColor: TQColor;
 begin
+  if (TCanvas(ACanvas).Font.Color <> clDefault) then
+  begin
+    // issue #25253
+    ColorRefToTQColor(ColorToRGB(TCanvas(ACanvas).Font.Color), AQColor);
+    TQtDeviceContext(TCanvas(ACanvas).Handle).pen.setColor(AQColor);
+  end;
   DrawText(TCanvas(ACanvas).Handle, Details, S, R, Flags, Flags2);
 end;
 
@@ -432,6 +440,7 @@ var
   TextRect: TRect;
   AOldMode: Integer;
   ATextPalette: Cardinal;
+  AQColor: TQColor;
 begin
   // DebugLn('TQtThemeServices.DrawText ');
   Context := TQtDeviceContext(DC);
@@ -541,6 +550,7 @@ begin
         QApplication_palette(Palette);
       end;
       try
+        AQColor := TQtDeviceContext(DC).pen.getColor; // issue #25253
         if Details.Element in [teEdit, teListView, teTreeView, teWindow] then
           ATextPalette := QPaletteWindowText
         else
@@ -548,6 +558,8 @@ begin
           ATextPalette := QPaletteButtonText
         else
           ATextPalette := QPaletteText;
+
+        QPalette_setColor(Palette, ATextPalette, @AQColor); // issue #25253
 
         QStyle_drawItemText(Style, Context.Widget, @R,
           DTFlagsToQtFlags(Flags), Palette,

commit ac9e11c26e444c0218c85a6ed794e978d6ea3903

Merged revision(s) 43378 #6123660445, 43384-43387 #cd29849872-#cd29849872, 43395-43398 #4928979536-#4928979536, 43424 #fc0030c327 from trunk: codetools: fixed TCustomCodeTool.ReadBackTilBracketOpen when there is no bracket ........ carbon: fix selecting an item in a TListView on Carbon when OwnerDraw is true.patch by Joost van der Sluis. issue #25274 ........ Qt: fixed changing font color of themed text draw. issue #25253 ........ Qt: restore pen color if we change it during draw themed text. regression of r43385 #957507fca6. issue #25253 ........ Qt: fixed another regression introduced with r43385 and r43386 #3af257647d ........ LazUtils: allow ReleaseRef on nil ........ Win, installer: delete correct conf folder, if updating 2ndary install and choose to clear conf ........ Win, installer: convert Spanish to ansi ........ EditorMAcroScript: Add some more try except dealing with self-test. Save reason of failure ........ SynEdit: Fixed painting highlight frame-borders. (do not paint extra left/right borders)
git-svn-id: branches/fixes_1_2@43436 -

components/codetools/customcodetool.pas
@@ -1954,25 +1954,25 @@ begin
     exit;
   end;
   Start:=CurPos;
-  repeat
+  while CurPos.StartPos>1 do begin
     ReadPriorAtom;
-    if (CurPos.Flag=OpenBracket) then break;
+    if (CurPos.Flag=OpenBracket) then exit(true);
     if (CurPos.StartPos<1)
     or (CurPos.Flag in [AntiOpenBracket,cafEND])
     or ((CurPos.Flag=cafWord)
         and UnexpectedKeyWordInBrackets.DoItCaseInsensitive(Src,
              CurPos.StartPos,CurPos.EndPos-CurPos.StartPos))
     then begin
-      CurPos:=Start;
-      if ExceptionOnNotFound then
-        RaiseBracketNotFound;
-      exit;
+      break;
     end;
     if CurPos.Flag in [cafRoundBracketClose,cafEdgedBracketClose] then begin
       if not ReadBackTilBracketOpen(ExceptionOnNotFound) then exit;
     end;
-  until false;
-  Result:=true;
+  end;
+  CurPos:=Start;
+  if ExceptionOnNotFound then
+    RaiseBracketNotFound;
+  Result:=false;
 end;
 
 procedure TCustomCodeTool.ReadTillCommentEnd;

components/lazutils/lazclasses.pas
@@ -99,6 +99,7 @@ end;
 
 procedure TRefCountedObject.ReleaseReference;
 begin
+  if Self = nil then exit;
   Assert(FRefCount > 0, 'TRefCountedObject.ReleaseReference  RefCount > 0');
   Dec(FRefCount);
   if FRefCount = 0 then DoFree;

components/macroscript/emscriptmacro.pas
@@ -78,6 +78,7 @@ type
   TEMSConfig = class(TAbstractIDEEnvironmentOptions)
   private
     FSelfTestActive: Boolean;
+    FSelfTestError: String;
     FSelfTestFailed: Integer; // stores EMSVersion that failed
   protected
     function GetXmlConf: TRttiXMLConfig;
@@ -92,6 +93,7 @@ type
   published
     property SelfTestActive: Boolean read FSelfTestActive write FSelfTestActive;
     property SelfTestFailed: Integer read FSelfTestFailed write FSelfTestFailed;
+    property SelfTestError: String read FSelfTestError write FSelfTestError;
   end;
 
 function GetEMSConf: TEMSConfig;

components/macroscript/registerems.pas
@@ -25,10 +25,26 @@ begin
   if not EMSSupported then exit;
 
   conf := GetEMSConf;
-  conf.Load;
+  try
+    conf.Load;
+  except
+    try
+      conf.SelfTestFailed := EMSVersion;
+      conf.SelfTestActive := False;
+      conf.SelfTestError := 'load error';
+      conf.Save;
+    except
+    end;
+    MessageDlg(EmsSelfTestErrCaption,
+               format(EmsSelfTestFailedLastTime, [LineEnding]),
+               mtError, [mbOK], 0);
+    exit;
+  end;
+
   if conf.SelfTestActive then begin
     conf.SelfTestFailed := EMSVersion;
     conf.SelfTestActive := False;
+    conf.SelfTestError := 'failed last time';
     conf.Save;
     MessageDlg(EmsSelfTestErrCaption,
                format(EmsSelfTestFailedLastTime, [LineEnding]),
@@ -50,6 +66,7 @@ begin
   if not ok then begin
     conf.SelfTestFailed := EMSVersion;
     conf.SelfTestActive := False;
+    conf.SelfTestError := SelfTestErrorMsg;
     conf.Save;
     MessageDlg(EmsSelfTestErrCaption,
                format(EmsSelfTestFailed, [LineEnding, SelfTestErrorMsg]),
@@ -58,6 +75,7 @@ begin
   end;
 
   conf.SelfTestActive := False;
+  conf.SelfTestError := '';
   conf.Save;
 
   EditorMacroPlayerClass := TEMSEditorMacro;

components/synedit/syneditmiscclasses.pp
@@ -272,6 +272,8 @@ type
     function GetFrameSideOrigin(Side: TLazSynBorderSide): TSynFrameEdges;
     function GetFrameSidePriority(Side: TLazSynBorderSide): integer;
     function GetFrameSideStyles(Side: TLazSynBorderSide): TSynLineStyle;
+    procedure SetCurrentEndX(AValue: TLazSynDisplayTokenBound);
+    procedure SetCurrentStartX(AValue: TLazSynDisplayTokenBound);
   protected
     procedure AssignFrom(Src: TLazSynCustomTextAttributes); override;
     procedure DoClear; override;
@@ -290,8 +292,8 @@ type
     property FrameSideColors[Side: TLazSynBorderSide]: TColor read GetFrameSideColors;
     property FrameSideStyles[Side: TLazSynBorderSide]: TSynLineStyle read GetFrameSideStyles;
     // boundaries for current paint
-    property CurrentStartX: TLazSynDisplayTokenBound read FCurrentStartX write FCurrentStartX;
-    property CurrentEndX: TLazSynDisplayTokenBound read FCurrentEndX write FCurrentEndX;
+    property CurrentStartX: TLazSynDisplayTokenBound read FCurrentStartX write SetCurrentStartX;
+    property CurrentEndX: TLazSynDisplayTokenBound read FCurrentEndX write SetCurrentEndX;
   public
     procedure InitMergeInfo;    // (called automatically) Set all MergeInfo to the start values. After this was called, ay Changes to the color properties are ignored
     procedure ProcessMergeInfo; // copy the merge result, to the actual color properties
@@ -664,10 +666,11 @@ begin
     exit
   end;
 
-  case Side of
-    bsLeft:  if not IsMatching(FCurrentStartX, FStartX) then exit(clNone);
-    bsRight: if not IsMatching(FCurrentEndX,   FEndX)   then exit(clNone);
-  end;
+  if (FCurrentStartX.Logical >= 0) or (FCurrentStartX.Physical >= 0) then
+    case Side of
+      bsLeft:  if not IsMatching(FCurrentStartX, FStartX) then exit(clNone);
+      bsRight: if not IsMatching(FCurrentEndX,   FEndX)   then exit(clNone);
+    end;
 
   if (Side in SynFrameEdgeToSides[FrameEdges])
   then Result := FrameColor
@@ -690,10 +693,11 @@ begin
     exit
   end;
 
-  case Side of
-    bsLeft:  if not IsMatching(FCurrentStartX, FStartX) then exit(0);
-    bsRight: if not IsMatching(FCurrentEndX,   FEndX)   then exit(0);
-  end;
+  if (FCurrentStartX.Logical >= 0) or (FCurrentStartX.Physical >= 0) then
+    case Side of
+      bsLeft:  if not IsMatching(FCurrentStartX, FStartX) then exit(0);
+      bsRight: if not IsMatching(FCurrentEndX,   FEndX)   then exit(0);
+    end;
 
   if (Side in SynFrameEdgeToSides[FrameEdges])
   then Result := FramePriority
@@ -708,6 +712,28 @@ begin
   if Side in SynFrameEdgeToSides[FrameEdges]
   then Result := FrameStyle
   else Result := slsSolid;
+end;
+
+procedure TSynSelectedColorMergeResult.SetCurrentEndX(AValue: TLazSynDisplayTokenBound);
+begin
+  //if FCurrentEndX = AValue then Exit;
+  FCurrentEndX := AValue;
+  if not IsMatching(FCurrentEndX, FEndX) then begin
+    FFrameSideColors[bsRight] := clNone;
+    FMergeInfos[sscFrameRight].BaseColor := clNone;
+    FMergeInfos[sscFrameRight].AlphaCount := 0;
+  end;
+end;
+
+procedure TSynSelectedColorMergeResult.SetCurrentStartX(AValue: TLazSynDisplayTokenBound);
+begin
+  //if FCurrentStartX = AValue then Exit;
+  FCurrentStartX := AValue;
+  if not IsMatching(FCurrentStartX, FStartX) then begin
+    FFrameSideColors[bsLeft] := clNone;
+    FMergeInfos[sscFrameLeft].BaseColor := clNone;
+    FMergeInfos[sscFrameLeft].AlphaCount := 0;
+  end;
 end;
 
 procedure TSynSelectedColorMergeResult.AssignFrom(Src: TLazSynCustomTextAttributes);

lcl/interfaces/carbon/carbonlistviews.pp
@@ -134,6 +134,7 @@ type
       Data: DataBrowserItemDataRef; ASetValue: Boolean): OSStatus; virtual;
     procedure NotificationCallBack(ID: DataBrowserItemId; 
       AMessage: DataBrowserItemNotification); virtual;
+    function IsIndexOutOfRange(ID: DataBrowserItemId): boolean; virtual;
 
   public
     procedure BoundsChanged; override;
@@ -235,6 +236,7 @@ type
     function GetReadOnly: Boolean; override;
     function MultiSelect: Boolean; override;
     function IsOwnerDrawn: Boolean; override;
+    function IsIndexOutOfRange(ID: DataBrowserItemId): boolean; override;
   public
     constructor Create(const AObject: TWinControl; const AParams: TCreateParams);
     destructor Destroy; override;
@@ -819,7 +821,8 @@ begin
   FItemIndex := -1;
   
   FItemsCheck := TList.Create;
-  
+  NotifySelectionChange := True;
+
   inherited;
 end;
 
@@ -1165,7 +1168,7 @@ end;
 procedure TCarbonDataBrowser.NotificationCallBack(ID: DataBrowserItemId; 
   AMessage: DataBrowserItemNotification); 
 begin
-  if (ID < 1) or (ID > DataBrowserItemId(GetItemsCount)) then Exit;
+  if IsIndexOutOfRange(ID) then  Exit;
 
   if NotifySelectionChange then
     case AMessage of
@@ -1178,6 +1181,11 @@ begin
       // kDataBrowserItemDoubleClicked:;
     end;
 
+end;
+
+function TCarbonDataBrowser.IsIndexOutOfRange(ID: DataBrowserItemId): boolean;
+begin
+  result := (ID < 1) or (ID > DataBrowserItemId(GetItemsCount));
 end;
 
 function TCarbonDataBrowser.GetTopItem: Integer;
@@ -1273,7 +1281,7 @@ begin
 end;
 
 procedure TCarbonDataBrowser.SetItemState(AIndex: Integer;
-  AState: TListItemState; AIsSet: BooleaN);
+  AState: TListItemState; AIsSet: Boolean);
 begin
   case AState of
     lisFocused:
@@ -1618,6 +1626,14 @@ end;
 function TCarbonListView.IsOwnerDrawn: Boolean;
 begin
   Result := False; // TODO
+end;
+
+function TCarbonListView.IsIndexOutOfRange(ID: DataBrowserItemId): boolean;
+begin
+  if not FOwnerData then
+    Result:=inherited IsIndexOutOfRange(ID)
+  else
+    result := (ID < 1 ) or (ID > TListView(LCLObject).Items.Count);
 end;
 
 constructor TCarbonListView.Create(const AObject: TWinControl;

lcl/interfaces/qt/qtthemes.pas
@@ -418,8 +418,32 @@ end;
 procedure TQtThemeServices.DrawText(ACanvas: TPersistent;
   Details: TThemedElementDetails; const S: String; R: TRect; Flags,
   Flags2: Cardinal);
+var
+  AQColor, AOldColor: TQColor;
+  B: Boolean;
+  OldCanvasFontColor: TColor;
 begin
+  B := False;
+
+  // issue #25253
+  if (Details.Element in [teButton, teComboBox]) then
+  begin
+    B := True;
+    AOldColor := TQtDeviceContext(TCanvas(ACanvas).Handle).pen.getColor;
+    OldCanvasFontColor := TCanvas(ACanvas).Font.Color;
+    if TCanvas(ACanvas).Font.Color = clDefault then
+      TCanvas(ACanvas).Font.Color := clBtnText;
+    ColorRefToTQColor(ColorToRGB(TCanvas(ACanvas).Font.Color), AQColor);
+    TQtDeviceContext(TCanvas(ACanvas).Handle).pen.setColor(AQColor);
+  end;
+
   DrawText(TCanvas(ACanvas).Handle, Details, S, R, Flags, Flags2);
+
+  if B then
+  begin
+    TQtDeviceContext(TCanvas(ACanvas).Handle).pen.setColor(AOldColor);
+    TCanvas(ACanvas).Font.Color := OldCanvasFontColor;
+  end;
 end;
 
 procedure TQtThemeServices.DrawText(DC: HDC; Details: TThemedElementDetails;
@@ -432,6 +456,7 @@ var
   TextRect: TRect;
   AOldMode: Integer;
   ATextPalette: Cardinal;
+  AQColor: TQColor;
 begin
   // DebugLn('TQtThemeServices.DrawText ');
   Context := TQtDeviceContext(DC);
@@ -541,6 +566,9 @@ begin
         QApplication_palette(Palette);
       end;
       try
+        if Details.Element in [teButton, teComboBox] then
+          AQColor := TQtDeviceContext(DC).pen.getColor; // issue #25253
+
         if Details.Element in [teEdit, teListView, teTreeView, teWindow] then
           ATextPalette := QPaletteWindowText
         else
@@ -548,6 +576,9 @@ begin
           ATextPalette := QPaletteButtonText
         else
           ATextPalette := QPaletteText;
+
+        if Details.Element in [teButton, teComboBox] then
+          QPalette_setColor(Palette, ATextPalette, @AQColor); // issue #25253
 
         QStyle_drawItemText(Style, Context.Widget, @R,
           DTFlagsToQtFlags(Flags), Palette,

tools/install/win/innoscript/common.pas
@@ -23,7 +23,7 @@ function GetPCPForDelete(param:string): String;
 // Name: {code:GetPCPForDelete}*.xml; Type: files; Tasks: delusersettings
 // ... delete primary conf
 begin
-  if (IsSecondaryCheckBoxChecked) then // or IsSecondaryUpdate
+  if (IsSecondaryCheckBoxChecked) or IsSecondaryUpdate then 
   begin
     if SecondPCP = '' then
       Result := AddBackslash(WizardDirValue) // some fallback

tools/install/win/lazarus.es.isl
@@ -3,31 +3,31 @@
 [CustomMessages]
 
 
-DelUserConf=Eliminar todos los archivos de configuraciÃ³n de usuario de instalaciones anteriores
+DelUserConf=Eliminar todos los archivos de configuración de usuario de instalaciones anteriores
 CleanUp=Limpiar
 
 InstallQt=Instalar QT interfaz dll
 InstallChm=Instalar archivos de ayuda chm
 AssociateGroup=Asociar extensiones de archivo
 
-FolderHasSpaces=Carpeta seleccionada contiene espacios, por favor, seleccione una carpeta sin espacios en Ã©l.
-FolderNotEmpty=La carpeta de destino no estÃ¡ vacÃ­a. Â¿Continuar con la instalaciÃ³n?
+FolderHasSpaces=Carpeta seleccionada contiene espacios, por favor, seleccione una carpeta sin espacios en él.
+FolderNotEmpty=La carpeta de destino no está vacía. ¿Continuar con la instalación?
 
-FolderNotEmpty2=La carpeta de destino no estÃ¡ vacÃ­a.
+FolderNotEmpty2=La carpeta de destino no está vacía.
 
-AskUninstallTitle1=InstalaciÃ³n anterior
-AskUninstallTitle2=Â¿Desea ejecutar el programa de desinstalaciÃ³n?
+AskUninstallTitle1=Instalación anterior
+AskUninstallTitle2=¿Desea ejecutar el programa de desinstalación?
 BtnUninstall=Desinstalar
 ChkContinue=Continuar sin desinstalar
 
-OldInDestFolder1=Otra instalaciÃ³n de %1:s existe en la carpeta de destino. Si desea desinstalar primero, utilice el botÃ³n de abajo.
+OldInDestFolder1=Otra instalación de %1:s existe en la carpeta de destino. Si desea desinstalar primero, utilice el botón de abajo.
 OldInDestFolder2=
 OldInDestFolder3=
 OldInDestFolder4=
 
-OldInOtherFolder1=Se encontrÃ³ otra instalaciÃ³n de %1:s en %2:s. Por favor utilice el botÃ³n de abajo para desinstalarlo ahora. Si usted desea mantenerlo, marque la casilla de verificaciÃ³n para continuar.
-OldInOtherFolder2=Nota: Usar mÃºltiples copias de Lazarus no es compatible con este instalador.
-OldInOtherFolder3=Usando varias instalaciones de LÃ¡zaro puede conducir a conflictos de archivos compartidos por todas las instalaciones, tales como la configuraciÃ³n de IDE.
-OldInOtherFolder4=Si desea utilizar mÃ¡s de una instalaciÃ³n, debe hacer alguna configuraciÃ³n adicional despuÃ©s de esta instalaciÃ³n. Por favor vea la pÃ¡gina de web de Lazarus para esto y cÃ³mo utilizar --primaria-config-path
+OldInOtherFolder1=Se encontró otra instalación de %1:s en %2:s. Por favor utilice el botón de abajo para desinstalarlo ahora. Si usted desea mantenerlo, marque la casilla de verificación para continuar.
+OldInOtherFolder2=Nota: Usar múltiples copias de Lazarus no es compatible con este instalador.
+OldInOtherFolder3=Usando varias instalaciones de Lázaro puede conducir a conflictos de archivos compartidos por todas las instalaciones, tales como la configuración de IDE.
+OldInOtherFolder4=Si desea utilizar más de una instalación, debe hacer alguna configuración adicional después de esta instalación. Por favor vea la página de web de Lazarus para esto y cómo utilizar --primaria-config-path
 
-DuringInstall=InformaciÃ³n de nuestras preguntas frecuentes: http://wiki.lazarus.freepascal.org/Lazarus_Faq%0:s%0:s    Â¿QuÃ© es Lazarus?%0:sLazarus es un IDE multi-plataforma IDE para Pascal. Su objetivo es escribir una vez, compilar en cualquier lugar.%0:s%0:s    CÃ³mo reducir el tamaÃ±o del archivo exe?%0:sLos binarios por defecto son muy grandes, ya que incluyen informaciÃ³n de depuraciÃ³n. Para compilaciones de producciÃ³n puede desactivar esto en la configuraciÃ³n del proyecto.%0:s%0:s    ConcesiÃ³n de licencias:%0:s- El LCL es con licencia LGPL con excepciÃ³n del enlaze. Esto permite crear aplicaciones con cualquier licencia que desee, incluyendo comercial.%0:s- El IDE es licencia GPL. Si distribuye un IDE modificado debe seguir la GPL.%0:s- Otros paquetes y componentes tienen varias licencias. Consulte el archivo LÃ©ame de cada paquete.
+DuringInstall=Información de nuestras preguntas frecuentes: http://wiki.lazarus.freepascal.org/Lazarus_Faq%0:s%0:s    ¿Qué es Lazarus?%0:sLazarus es un IDE multi-plataforma IDE para Pascal. Su objetivo es escribir una vez, compilar en cualquier lugar.%0:s%0:s    Cómo reducir el tamaño del archivo exe?%0:sLos binarios por defecto son muy grandes, ya que incluyen información de depuración. Para compilaciones de producción puede desactivar esto en la configuración del proyecto.%0:s%0:s    Concesión de licencias:%0:s- El LCL es con licencia LGPL con excepción del enlaze. Esto permite crear aplicaciones con cualquier licencia que desee, incluyendo comercial.%0:s- El IDE es licencia GPL. Si distribuye un IDE modificado debe seguir la GPL.%0:s- Otros paquetes y componentes tienen varias licencias. Consulte el archivo Léame de cada paquete.