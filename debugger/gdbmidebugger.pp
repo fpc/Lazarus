eb1409358b32acacf6e32925e13308e06f6ab7b3
martin <laz.git@mfriebe.de>
2013-11-27 15:25:50 +0000

    Merged revision(s) 43464 #b5772a5157, 43467 #5bcb702049, 43469 #2ff9c62e4a, 43472 #cc36b92b7b, 43475 #4cb8725f1f from trunk: *r43467 Cocoa: fix winapi Rectangle() implementation *r43475 Cocoa: some corrections to window creation *r43464 Debugger: Update registers, if stackframe changes *r43469 carbon: fix for freez if wakemainthread is called from the main thread prior to event loop initialization. #23393 *r43472 examples: TSQLScript database sample program. #25350
    git-svn-id: branches/fixes_1_2@43488 -

--- debugger/gdbmidebugger.pp
+++ debugger/gdbmidebugger.pp
@@ -6386,7 +6386,7 @@ var
   n, idx: Integer;
 begin
   Result := True;
-  FContext.StackContext := ccNotRequired;
+  //FContext.StackContext := ccNotRequired;
 
   if length(FRegistersToUpdate) = 0
   then exit;
@@ -10116,6 +10116,7 @@ begin
   if TGDBMIDebugger(Debugger).FCurrentStackFrame = idx then Exit;
 
   TGDBMIDebugger(Debugger).FCurrentStackFrame := idx;
+  TGDBMIRegisters(Debugger.Registers).Changed;
   if cs <> nil then
     cs.CurrentIndex := idx;
 end;
