ce75eb7d15146627552a9487cbd8aa65a164807b
maxim <maxim@lazarus-ide.org>
2014-01-08 16:39:01 +0000

    Merged revision(s) 43671 #79d881edce, 43675-43676 #14406dfe3d-#14406dfe3d from trunk: SqlDb: Improve SQL editor. Patch by Reinier Olislagers. ........ Components, SQLdb: updated Russian translation ........ Components, SQLdb: improved localization, updated Russian translation ........
    git-svn-id: branches/fixes_1_2@43677 -

--- components/sqldb/sqlstringspropertyeditordlg.pas
+++ components/sqldb/sqlstringspropertyeditordlg.pas
@@ -94,10 +94,18 @@ resourcestring
   SMetaPleaseSpecifyATableInTheObjectField = 'Please specify a table in the '
     +'object field.';
   SMetaSysTables    = 'SysTables';
-  SSQLOK            = 'SQL OK';
-  SQLSyntaxOK       = 'No syntax errors in SQL statement.';
-  SSQLError         = 'SQL Error';
-  SSQLSyntaxError   = 'Syntax error in SQL statement:'+slineBreak+'%s';
+
+  SLoadSQLCodeHint = 'Load SQL code ...';
+  SSaveSQLCodeHint = 'Save SQL code ...';
+  SRunSQLCodeHint = 'Run SQL code';
+  SQuickCheckOfSQLSyntaxHint = 'Quick check of SQL syntax';
+
+  // SQL Parser results:
+  // Note: sql parser is not quite exact, so indicate it's not completely sure
+  SSQLOK            = 'Quick SQL check OK';
+  SQLSyntaxOK       = 'No syntax errors in SQL statement found.';
+  SSQLError         = 'Probable SQL error';
+  SSQLSyntaxError   = 'Probable syntax error in SQL statement:'+slineBreak+'%s';
 
 { TSQLStringsPropertyEditorDlg }
 
@@ -110,6 +118,10 @@ begin
   EditorTabSheet.Caption := SSQLTabCaption;
   ResultTabSheet.Caption := SResultTabCaption;
   MetaTabSheet.Caption := SMetaTabCaption;
+  OpenToolButton.Hint := SLoadSQLCodeHint;
+  SaveToolButton.Hint := SSaveSQLCodeHint;
+  ExecuteToolButton.Hint := SRunSQLCodeHint;
+  TBCheck.Hint := SQuickCheckOfSQLSyntaxHint;
   CbxMetaData.Items.Add(SMetaTables);
   CbxMetaData.Items.Add(SMetaSysTables);
   CbxMetaData.Items.Add(SMetaColumns);
