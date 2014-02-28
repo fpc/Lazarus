f6b65edd2546302559ea397b5dea3fd029d3d050
martin <laz.git@mfriebe.de>
2014-02-28 17:43:54 +0000

    Merged revision(s) 44296 #4ea228a9e3 from trunk: LazLogger: fixed logfile with relative path
    git-svn-id: branches/fixes_1_2@44297 -

--- components/lazutils/lazlogger.pas
+++ components/lazutils/lazlogger.pas
@@ -244,7 +244,7 @@ begin
   if FLogName = AValue then Exit;
   DoCloseFile;
 
-  FLogName := CreateAbsolutePath(AValue, GetCurrentDirUTF8);
+  FLogName := CleanAndExpandFilename(AValue);
 
   FLogTextFailed := False;
 end;
