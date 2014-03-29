6f2e70e5c0869025d8d818cbb0b85e0bff9071a6
martin <laz.git@mfriebe.de>
2014-03-29 00:21:33 +0000

    Merged revision(s) 44543 #825ebd7c8b from trunk: Revision: 44543,44544 Debugger: GDBMI, fix range check
    git-svn-id: branches/fixes_1_2@44545 -

--- debugger/gdbmidebugger.pp
+++ debugger/gdbmidebugger.pp
@@ -11765,12 +11765,12 @@ var
            end;
         end;
         '}': begin
-           if (i > 0) and (AInput[i-1] <> '{') and
+           if (i > 1) and (AInput[i-1] <> '{') and
               ((not IsArray) or (InRounds = 0))
            then Delete(Indent, 1, Length(INDENTSTRING));
         end;
         ' ': begin
-           if ((i > 0) and (AInput[i-1] = ',')) and
+           if ((i > 1) and (AInput[i-1] = ',')) and
               ( (not IsArray) or
                 ((Indent = '') and (InRounds <= 1)) or
                 ((Indent = INDENTSTRING) and (InRounds = 0))
