fpc/Lazarus

commit 244193d7a98c2ec6d888972e46b1206c92e9b5dd

LazDebuggerFp (pure): Fixed AV on Windows
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@44690 4005530d-fff6-0310-9dd1-cebe43e6787f

components/fpdebug/fpdbgcontroller.pas
@@ -167,10 +167,11 @@ begin
       FCurrentProcess := FMainProcess;
 
     if not FCurrentProcess.GetThread(AThreadIdentifier, FCurrentThread)
-    then Log('LOOP: Unable to retrieve current thread');
+    then Log('LOOP: Unable to retrieve current thread')
+    else
+      FCurrentThread.SingleStepping:=false;
 
     FPDEvent:=FCurrentProcess.ResolveDebugEvent(FCurrentThread);
-    FCurrentThread.SingleStepping:=false;
     case FPDEvent of
       deCreateProcess :
         begin

commit 0e62389d0851ac69d94f1bacda372f107849dc20

LazDebuggerFp (pure): Fixed single-stepping which was broken since r44690 #14698396ff
git-svn-id: trunk@44703 -

components/fpdebug/fpdbgcontroller.pas
@@ -167,11 +167,11 @@ begin
       FCurrentProcess := FMainProcess;
 
     if not FCurrentProcess.GetThread(AThreadIdentifier, FCurrentThread)
-    then Log('LOOP: Unable to retrieve current thread')
-    else
-      FCurrentThread.SingleStepping:=false;
+    then Log('LOOP: Unable to retrieve current thread');
 
     FPDEvent:=FCurrentProcess.ResolveDebugEvent(FCurrentThread);
+    if assigned(FCurrentThread) then
+      FCurrentThread.SingleStepping:=false;
     case FPDEvent of
       deCreateProcess :
         begin