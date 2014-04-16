fpc/Lazarus

commit 1a3d4613a79c505b6d20b5d318fe8a7426d23ff1

Gtk2: fixed showing of popup window over fsStayOnTop forms.issue #26018
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@44723 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/gtk2/gtk2int.pas
@@ -298,7 +298,7 @@ type
     function GetWindowManager: String;
     function IsCurrentDesktop(AWindow: PGdkWindow): Boolean;
     function X11GetActiveWindow: HWND;
-
+    function GetAlwaysOnTopX11(AWindow: PGdkWindow): boolean;
     procedure HideAllHints;
     procedure RestoreAllHints;
     {$ENDIF}

lcl/interfaces/gtk2/gtk2widgetset.inc
@@ -4459,6 +4459,55 @@ begin
   if Valid then Result := {%H-}HWND(gdk_window_foreign_new(ResultWindow));
 end;
 
+function TGtk2WidgetSet.GetAlwaysOnTopX11(AWindow: PGdkWindow): boolean;
+var
+  Display: PDisplay;
+  X11Window: TWindow;
+  WMAtom: TAtom;
+  typeReturned: TAtom;
+  formatReturned: Integer;
+  nitemsReturned: PtrInt;
+  unused: PtrInt;
+  data: Pointer;
+begin
+  Result := False;
+  Display := gdk_x11_get_default_xdisplay;
+  if Display = nil then
+    exit;
+  X11Window := gdk_x11_drawable_get_xid(PGdkDrawable(AWindow));
+  if X11Window = 0 then
+    exit;
+  WMAtom := XInternAtom(Display,'_NET_WM_STATE', False);
+  if WMAtom > 0 then
+  begin
+    data := nil;
+    if XGetWindowProperty(Display, X11Window, WMAtom, 0, 1024, False, XA_ATOM,
+      @typeReturned, @formatReturned, @nitemsReturned,
+      @unused, @data) = Success then
+    begin
+      if (typeReturned = XA_ATOM) and (formatReturned = 32) and
+        (Data <> nil) then
+      begin
+        while nitemsReturned > 0 do
+        begin
+          // make happy ancient x11 or old kde ?
+          if XInternAtom(Display,'_NET_WM_STATE_STAYS_ON_TOP', False) = TAtom(Data^) then
+            Result := True
+          else
+          if XInternAtom(Display,'_NET_WM_STATE_ABOVE', False) = TAtom(Data^) then
+            Result := True;
+          dec(nItemsReturned);
+          if Result or (nItemsReturned = 0) then
+            break;
+          inc(Data);
+        end;
+        XFree(Data);
+        Data := nil;
+      end;
+    end;
+  end;
+end;
+
 procedure TGtk2WidgetSet.HideAllHints;
 var
   TopList, List: PGList;

lcl/interfaces/gtk2/gtk2wsforms.pp
@@ -675,6 +675,9 @@ end;
 
 class procedure TGtk2WSCustomForm.ShowHide(const AWinControl: TWinControl);
 var
+  {$IFDEF HASX}
+  TempGdkWindow: PGdkWindow;
+  {$ENDIF}
   AForm: TCustomForm;
   GtkWindow: PGtkWindow;
   Geometry: TGdkGeometry;
@@ -741,6 +744,27 @@ begin
       gtk_window_set_transient_for(GtkWindow, nil); //untransient
       gtk_window_set_modal(GtkWindow, False);
     end;
+
+    {$IFDEF HASX}
+    // issue #26018
+    if AWinControl.HandleObjectShouldBeVisible and
+      not (csDesigning in AForm.ComponentState) and
+      not (TCustomForm(AWinControl).FormStyle in fsAllStayOnTop) and
+      not (fsModal in TCustomForm(AWinControl).FormState) and
+      (TCustomForm(AWinControl).PopupMode = pmAuto) and
+      (TCustomForm(AWinControl).BorderStyle = bsNone) and
+      (TCustomForm(AWinControl).PopupParent = nil) then
+    begin
+      TempGdkWindow := PGdkWindow(Gtk2WidgetSet.GetForegroundWindow);
+      if (TempGdkWindow <> nil) then
+      begin
+        if ((gdk_window_get_state(TempGdkWindow) and GDK_WINDOW_STATE_ABOVE) = GDK_WINDOW_STATE_ABOVE) or
+          GTK2WidgetSet.GetAlwaysOnTopX11(TempGdkWindow) then
+            gtk_window_set_keep_above(GtkWindow, True);
+      end;
+    end;
+    {$ENDIF}
+
     Gtk2WidgetSet.SetVisible(AWinControl, AForm.HandleObjectShouldBeVisible);
   end;
 

commit 7b531539bcd7688b749d35b98cf954bd8293f5ee

Merged revision(s) 44723-44724 #e92a302d5b-#e92a302d5b, 44731-44732 #1890ef17b7-#1890ef17b7 from trunk: Gtk2: fixed showing of popup window over fsStayOnTop forms.issue #26018 ........ Qt: fixed showing of popup window over fsStayOnTop form.issue #26018 ........ Qt,Gtk2: fixed crash introduced with r44723 #e92a302d5b,44724 #4987038ee9 when popup window is shown above modal form. issue #26018 ........ Qt: use cheaper method to check modality. ........
git-svn-id: branches/fixes_1_2@44746 -

lcl/interfaces/gtk2/gtk2int.pas
@@ -298,7 +298,7 @@ type
     function GetWindowManager: String;
     function IsCurrentDesktop(AWindow: PGdkWindow): Boolean;
     function X11GetActiveWindow: HWND;
-
+    function GetAlwaysOnTopX11(AWindow: PGdkWindow): boolean;
     procedure HideAllHints;
     procedure RestoreAllHints;
     {$ENDIF}

lcl/interfaces/gtk2/gtk2widgetset.inc
@@ -4459,6 +4459,55 @@ begin
   if Valid then Result := {%H-}HWND(gdk_window_foreign_new(ResultWindow));
 end;
 
+function TGtk2WidgetSet.GetAlwaysOnTopX11(AWindow: PGdkWindow): boolean;
+var
+  Display: PDisplay;
+  X11Window: TWindow;
+  WMAtom: TAtom;
+  typeReturned: TAtom;
+  formatReturned: Integer;
+  nitemsReturned: PtrInt;
+  unused: PtrInt;
+  data: Pointer;
+begin
+  Result := False;
+  Display := gdk_x11_get_default_xdisplay;
+  if Display = nil then
+    exit;
+  X11Window := gdk_x11_drawable_get_xid(PGdkDrawable(AWindow));
+  if X11Window = 0 then
+    exit;
+  WMAtom := XInternAtom(Display,'_NET_WM_STATE', False);
+  if WMAtom > 0 then
+  begin
+    data := nil;
+    if XGetWindowProperty(Display, X11Window, WMAtom, 0, 1024, False, XA_ATOM,
+      @typeReturned, @formatReturned, @nitemsReturned,
+      @unused, @data) = Success then
+    begin
+      if (typeReturned = XA_ATOM) and (formatReturned = 32) and
+        (Data <> nil) then
+      begin
+        while nitemsReturned > 0 do
+        begin
+          // make happy ancient x11 or old kde ?
+          if XInternAtom(Display,'_NET_WM_STATE_STAYS_ON_TOP', False) = TAtom(Data^) then
+            Result := True
+          else
+          if XInternAtom(Display,'_NET_WM_STATE_ABOVE', False) = TAtom(Data^) then
+            Result := True;
+          dec(nItemsReturned);
+          if Result or (nItemsReturned = 0) then
+            break;
+          inc(Data);
+        end;
+        XFree(Data);
+        Data := nil;
+      end;
+    end;
+  end;
+end;
+
 procedure TGtk2WidgetSet.HideAllHints;
 var
   TopList, List: PGList;

lcl/interfaces/gtk2/gtk2wsforms.pp
@@ -675,6 +675,9 @@ end;
 
 class procedure TGtk2WSCustomForm.ShowHide(const AWinControl: TWinControl);
 var
+  {$IFDEF HASX}
+  TempGdkWindow: PGdkWindow;
+  {$ENDIF}
   AForm: TCustomForm;
   GtkWindow: PGtkWindow;
   Geometry: TGdkGeometry;
@@ -741,6 +744,27 @@ begin
       gtk_window_set_transient_for(GtkWindow, nil); //untransient
       gtk_window_set_modal(GtkWindow, False);
     end;
+
+    {$IFDEF HASX}
+    // issue #26018
+    if AWinControl.HandleObjectShouldBeVisible and
+      not (csDesigning in AForm.ComponentState) and
+      not (TCustomForm(AWinControl).FormStyle in fsAllStayOnTop) and
+      not (fsModal in TCustomForm(AWinControl).FormState) and
+      (TCustomForm(AWinControl).PopupMode = pmAuto) and
+      (TCustomForm(AWinControl).BorderStyle = bsNone) and
+      (TCustomForm(AWinControl).PopupParent = nil) then
+    begin
+      TempGdkWindow := PGdkWindow(Gtk2WidgetSet.GetForegroundWindow);
+      if (TempGdkWindow <> nil) and (GdkWindowObject_modal_hint(GDK_WINDOW_OBJECT(TempGdkWindow)^) = 0) then
+      begin
+        if ((gdk_window_get_state(TempGdkWindow) and GDK_WINDOW_STATE_ABOVE) = GDK_WINDOW_STATE_ABOVE) or
+          GTK2WidgetSet.GetAlwaysOnTopX11(TempGdkWindow) then
+            gtk_window_set_keep_above(GtkWindow, True);
+      end;
+    end;
+    {$ENDIF}
+
     Gtk2WidgetSet.SetVisible(AWinControl, AForm.HandleObjectShouldBeVisible);
   end;
 

lcl/interfaces/qt/qtint.pp
@@ -241,6 +241,8 @@ type
   function X11GetActiveWindow: QWidgetH;
   function GetWindowManager: String;
   procedure SetSkipX11Taskbar(Widget: QWidgetH; const ASkipTaskBar: Boolean);
+  {check if XWindow have _NET_WM_STATE_ABOVE and our form doesn''t know anything about it}
+  function GetAlwaysOnTopX11(Widget: QWidgetH): boolean;
   {check if we are running under kde3 installation}
   function IsOldKDEInstallation: Boolean;
   {$ENDIF}

lcl/interfaces/qt/qtwsforms.pp
@@ -524,7 +524,6 @@ begin
   Widget.BeginUpdate;
   if not (csDesigning in AWinControl.ComponentState) then
   begin
-
     if ShowNonModalOverModal then
     // issue #12459
     else
@@ -545,13 +544,37 @@ begin
             QApplication_activeModalWidget());
     end else
     begin
-      if (TCustomForm(AWinControl).FormStyle in fsAllStayOnTop)
-      and not (csDestroying in AWinControl.ComponentState) then
+      if (TCustomForm(AWinControl).FormStyle in fsAllStayOnTop) then
       begin
-        Flags := Widget.windowFlags;
-        Flags := Flags and not QtWindowStaysOnTopHint;
-        Widget.setWindowFlags(Flags);
+        if not (csDestroying in AWinControl.ComponentState) then
+        begin
+          Flags := Widget.windowFlags;
+          Flags := Flags and not QtWindowStaysOnTopHint;
+          Widget.setWindowFlags(Flags);
+        end;
       end;
+      {$IFDEF HASX11}
+      // issue #26018
+      if AWinControl.HandleObjectShouldBeVisible and
+        not (TCustomForm(AWinControl).FormStyle in fsAllStayOnTop) and
+        not (fsModal in TCustomForm(AWinControl).FormState) and
+        (TCustomForm(AWinControl).PopupMode = pmAuto) and
+        (TCustomForm(AWinControl).BorderStyle = bsNone) and
+        (TCustomForm(AWinControl).PopupParent = nil) then
+      begin
+        W := QApplication_activeWindow;
+        if (W <> nil) and not QWidget_isModal(W) then
+        begin
+          Flags := QWidget_windowFlags(W);
+          if (Flags and QtWindowStaysOnTopHint <> QtWindowStaysOnTopHint) and
+            GetAlwaysOnTopX11(W) then
+          begin
+            Flags := Widget.windowFlags;
+            Widget.setWindowFlags(Flags or QtWindowStaysOnTopHint);
+          end;
+        end;
+      end;
+      {$ENDIF}
     end;
   end;
 

lcl/interfaces/qt/qtx11.inc
@@ -256,6 +256,58 @@ begin
 	      @Xclient);
 end;
 
+function GetAlwaysOnTopX11(Widget: QWidgetH): boolean;
+var
+  Display: PDisplay;
+  X11Window: TWindow;
+  WMAtom: TAtom;
+  typeReturned: TAtom;
+  formatReturned: Integer;
+  nitemsReturned: PtrInt;
+  unused: PtrInt;
+  data: Pointer;
+begin
+  Result := False;
+  Display := QX11Info_display();
+
+  if Display = nil then
+    exit;
+
+  X11Window := TWindow(QWidget_winId(Widget));
+  if X11Window = 0 then
+    exit;
+
+  WMAtom := XInternAtom(Display,'_NET_WM_STATE', False);
+  if WMAtom > 0 then
+  begin
+    data := nil;
+    if XGetWindowProperty(Display, X11Window, WMAtom, 0, 1024, False, XA_ATOM,
+      @typeReturned, @formatReturned, @nitemsReturned,
+      @unused, @data) = Success then
+    begin
+      if (typeReturned = XA_ATOM) and (formatReturned = 32) and
+        (Data <> nil) then
+      begin
+        while nitemsReturned > 0 do
+        begin
+          // make happy ancient x11 or old kde ?
+          if XInternAtom(Display,'_NET_WM_STATE_STAYS_ON_TOP', False) = TAtom(Data^) then
+            Result := True
+          else
+          if XInternAtom(Display,'_NET_WM_STATE_ABOVE', False) = TAtom(Data^) then
+            Result := True;
+          dec(nItemsReturned);
+          if Result or (nItemsReturned = 0) then
+            break;
+          inc(Data);
+        end;
+        XFree(Data);
+        Data := nil;
+      end;
+    end;
+  end;
+end;
+
 function GetKeyLockState(const AKey: Byte): Boolean;
 var
   Display: PDisplay;