ba80455dbfc8340f57390f0c5954c4c48aa6d7aa
maxim <maxim@lazarus-ide.org>
2014-04-27 17:47:40 +0000

    Merged revision(s) 44470 #d7a192f709, 44559 #1e97dab2e1, 44583 #e21111e2bf, 44588 #de8f43032c, 44609-44610 #5783936b29-#5783936b29, 44706 #6ed245f1f2 from trunk: TAChart: add sources of bar series tutorial ........ TAChart: fix wmf drawer crashing due to missing canvas in TADrawerWMF ........ TAChart: Add event OnDrawLegend to TChart ........ TAChart: add code to legenddemo to demonstrate the ownerdrawn legend, fix some layout issues. ........ TAChart: Fix TFitSeries not working correctly in demo/distance/distancedemo.lpi ........ TAChart: fix divide-by-zero error in demos/distance/distancedemo project, minor layout changes. ........ TAChart: initial implementation of Calculate(x) for TBSplineSeries ........
    git-svn-id: branches/fixes_1_2@44819 -

--- components/tachart/tagraph.pas
+++ components/tachart/tagraph.pas
@@ -35,6 +35,11 @@ type
     ASender: TChart; ASeriesIndex, AIndex: Integer;
     const AData: TDoublePoint) of object;
 
+  TChartDrawLegendEvent = procedure(
+    ASender: TChart; ADrawer: IChartDrawer; ALegendItems: TChartLegendItems;
+    ALegendItemSize: TPoint; const ALegendRect: TRect;
+    AColCount, ARowCount: Integer) of object;
+
   { TBasicChartSeries }
 
   TBasicChartSeries = class(TIndexedComponent)
@@ -189,6 +194,7 @@ type
     FOnBeforeDrawBackWall: TChartBeforeDrawEvent;
     FOnChartPaint: TChartPaintEvent;
     FOnDrawReticule: TDrawReticuleEvent;
+    FOnDrawLegend: TChartDrawLegendEvent;
     FProportional: Boolean;
     FSeries: TChartSeriesList;
     FTitle: TChartTitle;
@@ -248,6 +254,7 @@ type
     procedure SetOnBeforeDrawBackground(AValue: TChartBeforeDrawEvent);
     procedure SetOnBeforeDrawBackWall(AValue: TChartBeforeDrawEvent);
     procedure SetOnChartPaint(AValue: TChartPaintEvent);
+    procedure SetOnDrawLegend(AValue: TChartDrawLegendEvent);
     procedure SetOnDrawReticule(AValue: TDrawReticuleEvent);
     procedure SetProportional(AValue: Boolean);
     procedure SetRenderingParams(AValue: TChartRenderingParams);
@@ -393,6 +400,8 @@ type
       read FOnBeforeDrawBackground write SetOnBeforeDrawBackground;
     property OnBeforeDrawBackWall: TChartBeforeDrawEvent
       read FOnBeforeDrawBackWall write SetOnBeforeDrawBackWall;
+    property OnDrawLegend: TChartDrawLegendEvent
+      read FOnDrawLegend write SetOnDrawLegend;
     property OnDrawReticule: TDrawReticuleEvent
       read FOnDrawReticule write SetOnDrawReticule;
     property OnExtentChanged: TChartEvent
@@ -807,6 +816,7 @@ begin
   ldd.FItems := nil;
   if Legend.Visible then
     ldd := PrepareLegend(ADrawer, FClipRect);
+
   try
     PrepareAxis(ADrawer);
     if (FPrevLogicalExtent <> FLogicalExtent) and Assigned(OnExtentChanging) then
@@ -819,8 +829,13 @@ begin
     FFoot.Draw(ADrawer);
     DrawBackWall(ADrawer);
     DisplaySeries(ADrawer);
-    if Legend.Visible then
-      Legend.Draw(ldd);
+    if Legend.Visible then begin
+      if Assigned(FOnDrawLegend) then
+        FOnDrawlegend(Self, ldd.FDrawer, ldd.FItems, ldd.FItemSize, ldd.FBounds,
+          ldd.FColCount, ldd.FRowCount)
+      else
+        Legend.Draw(ldd);
+    end;
   finally
     ldd.FItems.Free;
   end;
@@ -1519,6 +1534,13 @@ begin
   if TMethod(FOnChartPaint) = TMethod(AValue) then exit;
   FOnChartPaint := AValue;
   StyleChanged(Self);
+end;
+
+procedure TChart.SetOnDrawLegend(AValue: TChartDrawLegendEvent);
+begin
+  if TMethod(FOnDrawLegend) = TMethod(AValue) then exit;
+  FOnDrawLegend := AValue;
+  StyleChanged(self);
 end;
 
 procedure TChart.SetOnDrawReticule(AValue: TDrawReticuleEvent);
