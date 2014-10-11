ebfcee3ec2c708a62b240856a0451f7871769c88
mattias <nc-gaertnma@netcologne.de>
2014-10-11 08:28:06 +0000

    version 1.2.6
    git-svn-id: branches/fixes_1_2@46521 -

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -34,10 +34,10 @@ interface
 const
   lcl_major = 1;
   lcl_minor = 2;
-  lcl_release = 5;
+  lcl_release = 6;
   lcl_patch = 0;
   lcl_fullversion = ((lcl_major *  100 + lcl_minor) * 100 + lcl_release) * 100 + lcl_patch;
-  lcl_version = '1.2.5.0';
+  lcl_version = '1.2.6.0';
 
 implementation
 
