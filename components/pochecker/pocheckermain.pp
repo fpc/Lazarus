fpc/Lazarus

commit 3483d3f88254dd696a18fe954041a223f3e5609c

PoChecker: - use natural names in LangFilter, sort them alphabetically. - adapt save and restore mechanisme for LangFilter. - regenerate language files.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@46530 4005530d-fff6-0310-9dd1-cebe43e6787f

components/pochecker/pocheckermain.pp
@@ -81,6 +81,9 @@ type
     procedure SetSelectedMasterFiles(S: TStrings);
     procedure LoadConfig;
     procedure SaveConfig;
+    function LangFilterIndexToLangID(Index: Integer): TLangID;
+    function LangIdToLangFilterIndex(LangID: TLangID): Integer;
+    procedure PopulateLangFilter;
   published
     IgnoreFuzzyCheckBox: TCheckBox;
     UnselectAllBtn: TButton;
@@ -169,6 +172,7 @@ begin
   SelectAllBtn.Caption := sSelectAllTests;
   SelectBasicBtn.Caption := sSelectBasicTests;
   UnselectAllBtn.Caption := sUnselectAllTests;
+  PopulateLangFilter;
   LoadConfig;
   LangFilter.Invalidate; //Items[0] may have been changed
 end;
@@ -705,7 +709,8 @@ end;
 procedure TPoCheckerForm.LoadConfig;
 var
   ARect: TRect;
-  Idx: Integer;
+  Abbr: String;
+  ID: TLangID;
 begin
   FPoCheckerSettings := TPoCheckerSettings.Create;
   FPoCheckerSettings.LoadConfig;
@@ -719,33 +724,23 @@ begin
   SetTestOptionCheckBoxes(FPoCheckerSettings.TestOptions);
   SelectDirectoryDialog.Filename := FPoCheckerSettings.SelectDirectoryFilename;
   OpenDialog.FileName := FPoCheckerSettings.OpenDialogFilename;
-  Idx := FPoCheckerSettings.LangFilterIndex;
-  if (Idx > -1) and (Idx < LangFilter.Items.Count -1) then
-    LangFilter.ItemIndex := Idx;
+  Abbr := FPoCheckerSettings.LangFilterLanguageAbbr;
+  ID := LangAbbrToLangId(Abbr);
+  LangFilter.ItemIndex := LangIdToLangFilterIndex(ID);
   AddToMasterPoList(FPoCheckerSettings.MasterPoList);
   SetSelectedMasterFiles(FPoCheckerSettings.MasterPoSelList);
-  //AddToChildPoList(FPoCheckerSettings.ChildPoList);
-  {
-  if (CompareText(ExtractFileExt(FPoCheckerSettings.LastSelectedFile), '.po') = 0) then
-  begin
-    if IsMasterPoName(FPoCheckerSettings.LastSelectedFile) then
-      AddToMasterPoList(FPoCheckerSettings.LastSelectedFile)
-    else
-      AddToChildPoList(FPoCheckerSettings.LastSelectedFile);
-    UpdateGUI(True)
-  end
-  else
-    UpdateGUI(False);
-  }
 end;
 
 procedure TPoCheckerForm.SaveConfig;
 var
   SL: TStringList;
+  ID: TLangID;
 begin
   FPoCheckerSettings.SelectDirectoryFilename := SelectDirectoryDialog.Filename;
   FPoCheckerSettings.OpenDialogFilename := OpenDialog.FileName;
-  FPoCheckerSettings.LangFilterIndex := LangFilter.ItemIndex;
+  //FPoCheckerSettings.LangFilterIndex := LangFilter.ItemIndex;
+  ID := LangFilterIndexToLangID(LangFilter.ItemIndex);
+  FPoCheckerSettings.LangFilterLanguageAbbr := LanguageAbbr[ID];
   FPoCheckerSettings.TestTypes := GetTestTypesFromListBox;
   FPoCheckerSettings.TestOptions := GetTestOptions;
   FPoCheckerSettings.MainFormWindowState := WindowState;
@@ -761,6 +756,79 @@ begin
     SL.Free;
   end;
   FPoCheckerSettings.SaveConfig;
+end;
+function ListSortFunc(List: TStringList; Index1, Index2: Integer): Integer;
+begin
+  Result := Utf8CompareText(List.Strings[Index1], List.Strings[Index2]);
+end;
+
+function TPoCheckerForm.LangFilterIndexToLangID(Index: Integer): TLangID;
+//Requires that items for a language end in [%lang_abbr%]
+var
+  S, Abbr: String;
+  p: Integer;
+begin
+  Result := lang_all;
+  S := LangFilter.Items[Index];
+  p := Length(S); //no need to use Utf8 functions, we look for lower ASCII
+  if (p = 0) or (not (S[p] = ']')) then Exit;
+  repeat
+    Dec(p);
+  until (p = 0) or (S[p] = '[');
+  if (p = 0) then Exit;
+  Abbr := Copy(S, p+1, Length(S)-p-1);
+  //DbgOut('Abbr = ',Abbr);
+  Result := LangAbbrToLangID(Abbr);
+  //debugln(' ID = ',Result);
+end;
+
+function TPoCheckerForm.LangIdToLangFilterIndex(LangID: TLangID): Integer;
+//Requires that items for a language end in [%lang_abbr%]
+var
+  Abbr, S: String;
+  p: SizeInt;
+  i: Integer;
+begin
+  Result := 0; // All Languages
+  if (LangID = lang_all) then Exit;
+  Abbr := LanguageAbbr[LangID];
+  for i := 1 to LangFilter.Items.Count - 1 do
+  begin
+    S := LangFilter.Items[i];
+    //no need to use Utf8 functions, we look for lower ASCII
+    p := Pos('['+Abbr+']',S);
+    if (p > 0) then
+      Exit(i);
+  end;
+end;
+
+procedure TPoCheckerForm.PopulateLangFilter;
+var
+  ID: TLangID;
+  Abbr, LangName, S: String;
+  SL: TStringList;
+begin
+  LangFilter.Items.BeginUpdate;
+  SL := TStringList.Create;
+  try
+    LangFilter.Items.Clear;
+    for ID := Succ(Low(TLangID)) to High(TLangID) do
+    begin
+      Abbr := LanguageAbbr[ID];
+      LangName := LanguageNames[ID];
+      S := Format('%s  [%s]',[LangName, Abbr]);
+      SL.Add(S);
+      SL.CustomSort(@ListSortFunc);
+    end;
+    SL.Sorted := False;
+    SL.Insert(0, LanguageNames[lang_all]);
+    LangFilter.Items.Assign(SL);
+    LangFilter.Items.EndUpdate;
+    LangFilter.ItemIndex := 0;
+  finally
+    SL.Free;
+    LangFilter.Items.EndUpdate;
+  end;
 end;
 
 

components/pochecker/Proj/pochecker.lpi
@@ -129,7 +129,7 @@
         <ComponentName Value="MemoForm"/>
         <HasResources Value="True"/>
         <ResourceBaseClass Value="Form"/>
-        <UnitName Value="pocheckermemodlg"/>
+        <UnitName Value="PoCheckerMemoDlg"/>
       </Unit9>
     </Units>
   </ProjectOptions>

components/pochecker/languages/pocheckerconsts.cs.po
@@ -17,6 +17,102 @@ msgstr ""
 msgid "PO File Checker"
 msgstr "PO File Checker"
 
+#: pocheckerconsts.rs_lang_af_za
+msgid "Afrikaans"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_all
+msgid "All languages"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ar
+msgid "Arabic"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ca
+msgid "Catalan"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_cs
+msgid "Czech"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_de
+msgid "German"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_en
+msgid "English"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_es
+msgid "Spanish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fi
+msgid "Finnish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fr
+msgid "French"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_he
+msgid "Hebrew"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_hu
+msgid "Hungarian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_id
+msgid "Indonesian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_it
+msgid "Italian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ja
+msgid "Japanese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_lt
+msgid "Lithuanian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_nl
+msgid "Dutch"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pl
+msgid "Polish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pt_br
+msgid "Brazilian Portuguese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ru
+msgid "Russian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_sk
+msgid "Slovak"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_tr
+msgid "Turkish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_uk
+msgid "Ukrainian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_zh_cn
+msgid "Chinese, simplified"
+msgstr ""
+
 #: pocheckerconsts.salllanguages
 msgid "All Languages"
 msgstr ""

components/pochecker/languages/pocheckerconsts.de.po
@@ -14,6 +14,102 @@ msgstr ""
 msgid "PO File Checker"
 msgstr "po-Datei-Prüfer"
 
+#: pocheckerconsts.rs_lang_af_za
+msgid "Afrikaans"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_all
+msgid "All languages"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ar
+msgid "Arabic"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ca
+msgid "Catalan"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_cs
+msgid "Czech"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_de
+msgid "German"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_en
+msgid "English"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_es
+msgid "Spanish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fi
+msgid "Finnish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fr
+msgid "French"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_he
+msgid "Hebrew"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_hu
+msgid "Hungarian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_id
+msgid "Indonesian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_it
+msgid "Italian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ja
+msgid "Japanese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_lt
+msgid "Lithuanian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_nl
+msgid "Dutch"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pl
+msgid "Polish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pt_br
+msgid "Brazilian Portuguese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ru
+msgid "Russian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_sk
+msgid "Slovak"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_tr
+msgid "Turkish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_uk
+msgid "Ukrainian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_zh_cn
+msgid "Chinese, simplified"
+msgstr ""
+
 #: pocheckerconsts.salllanguages
 msgid "All Languages"
 msgstr "Alle Sprachen"

components/pochecker/languages/pocheckerconsts.es.po
@@ -13,6 +13,102 @@ msgstr ""
 msgid "PO File Checker"
 msgstr "Comprobador de archivos de PO"
 
+#: pocheckerconsts.rs_lang_af_za
+msgid "Afrikaans"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_all
+msgid "All languages"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ar
+msgid "Arabic"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ca
+msgid "Catalan"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_cs
+msgid "Czech"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_de
+msgid "German"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_en
+msgid "English"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_es
+msgid "Spanish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fi
+msgid "Finnish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fr
+msgid "French"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_he
+msgid "Hebrew"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_hu
+msgid "Hungarian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_id
+msgid "Indonesian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_it
+msgid "Italian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ja
+msgid "Japanese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_lt
+msgid "Lithuanian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_nl
+msgid "Dutch"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pl
+msgid "Polish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pt_br
+msgid "Brazilian Portuguese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ru
+msgid "Russian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_sk
+msgid "Slovak"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_tr
+msgid "Turkish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_uk
+msgid "Ukrainian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_zh_cn
+msgid "Chinese, simplified"
+msgstr ""
+
 #: pocheckerconsts.salllanguages
 msgid "All Languages"
 msgstr ""

components/pochecker/languages/pocheckerconsts.hu.po
@@ -15,6 +15,102 @@ msgstr ""
 msgid "PO File Checker"
 msgstr "PO Fájl Ellenőrző"
 
+#: pocheckerconsts.rs_lang_af_za
+msgid "Afrikaans"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_all
+msgid "All languages"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ar
+msgid "Arabic"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ca
+msgid "Catalan"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_cs
+msgid "Czech"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_de
+msgid "German"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_en
+msgid "English"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_es
+msgid "Spanish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fi
+msgid "Finnish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fr
+msgid "French"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_he
+msgid "Hebrew"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_hu
+msgid "Hungarian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_id
+msgid "Indonesian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_it
+msgid "Italian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ja
+msgid "Japanese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_lt
+msgid "Lithuanian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_nl
+msgid "Dutch"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pl
+msgid "Polish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pt_br
+msgid "Brazilian Portuguese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ru
+msgid "Russian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_sk
+msgid "Slovak"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_tr
+msgid "Turkish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_uk
+msgid "Ukrainian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_zh_cn
+msgid "Chinese, simplified"
+msgstr ""
+
 #: pocheckerconsts.salllanguages
 msgid "All Languages"
 msgstr ""

components/pochecker/languages/pocheckerconsts.it.po
@@ -13,6 +13,102 @@ msgstr ""
 msgid "PO File Checker"
 msgstr "Verificatore di files PO"
 
+#: pocheckerconsts.rs_lang_af_za
+msgid "Afrikaans"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_all
+msgid "All languages"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ar
+msgid "Arabic"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ca
+msgid "Catalan"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_cs
+msgid "Czech"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_de
+msgid "German"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_en
+msgid "English"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_es
+msgid "Spanish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fi
+msgid "Finnish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fr
+msgid "French"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_he
+msgid "Hebrew"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_hu
+msgid "Hungarian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_id
+msgid "Indonesian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_it
+msgid "Italian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ja
+msgid "Japanese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_lt
+msgid "Lithuanian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_nl
+msgid "Dutch"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pl
+msgid "Polish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pt_br
+msgid "Brazilian Portuguese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ru
+msgid "Russian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_sk
+msgid "Slovak"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_tr
+msgid "Turkish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_uk
+msgid "Ukrainian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_zh_cn
+msgid "Chinese, simplified"
+msgstr ""
+
 #: pocheckerconsts.salllanguages
 msgid "All Languages"
 msgstr ""

components/pochecker/languages/pocheckerconsts.ja.po
@@ -15,6 +15,102 @@ msgstr ""
 msgid "PO File Checker"
 msgstr "PO ファイルチェッカー"
 
+#: pocheckerconsts.rs_lang_af_za
+msgid "Afrikaans"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_all
+msgid "All languages"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ar
+msgid "Arabic"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ca
+msgid "Catalan"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_cs
+msgid "Czech"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_de
+msgid "German"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_en
+msgid "English"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_es
+msgid "Spanish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fi
+msgid "Finnish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_fr
+msgid "French"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_he
+msgid "Hebrew"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_hu
+msgid "Hungarian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_id
+msgid "Indonesian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_it
+msgid "Italian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ja
+msgid "Japanese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_lt
+msgid "Lithuanian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_nl
+msgid "Dutch"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pl
+msgid "Polish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_pt_br
+msgid "Brazilian Portuguese"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_ru
+msgid "Russian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_sk
+msgid "Slovak"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_tr
+msgid "Turkish"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_uk
+msgid "Ukrainian"
+msgstr ""
+
+#: pocheckerconsts.rs_lang_zh_cn
+msgid "Chinese, simplified"
+msgstr ""
+
 #: pocheckerconsts.salllanguages
 msgid "All Languages"
 msgstr ""

commit cb35b5185bd0095bb68f25585743e80cfe35d340

PoChecker: fix broken construction of language filename, introduced by me in r46530 #d1a01f5804.
git-svn-id: trunk@46531 -

components/pochecker/pocheckermain.pp
@@ -71,7 +71,7 @@ type
     procedure ShowError(const Msg: string);
     function TrySelectFile(out Filename: String): Boolean;
     procedure ScanDirectory(ADir: String);
-    function TryCreatepoFamilyList(MasterList: TStrings; const Lang: String): Boolean;
+    function TryCreatepoFamilyList(MasterList: TStrings; const LangID: TLangID): Boolean;
     procedure RunSelectedTests;
     procedure ClearStatusBar;
     procedure UpdateGUI(HasSelection: Boolean);
@@ -212,16 +212,13 @@ procedure TPoCheckerForm.RunBtnClick(Sender: TObject);
 var
   AMasterList: TStringList;
   LangIdx: Integer;
-  ALang: String;
+  ALangID: TLangID;
 begin
   LangIdx := LangFilter.ItemIndex;
-  if (LangIdx > 0) then
-    ALang := LangFilter.Items[LangIdx]
-  else
-    ALang := langAll;
+  ALangID := LangFilterIndexToLangID(LangIdx);
   AMasterList := GetSelectedMasterFiles;
   try
-    if TryCreatePoFamilyList(AMasterList, ALang) then
+    if TryCreatePoFamilyList(AMasterList, ALangID) then
       RunSelectedTests
     else
     begin
@@ -466,7 +463,7 @@ begin
 end;
 
 
-function TPoCheckerForm.TryCreatepoFamilyList(MasterList: TStrings; const Lang: String): Boolean;
+function TPoCheckerForm.TryCreatepoFamilyList(MasterList: TStrings; const LangID: TLangID): Boolean;
 var
   Fn, Msg: String;
   i, Cnt: Integer;
@@ -498,7 +495,7 @@ begin
   end;
   try
     if Assigned(PoFamilyList) then PoFamilyList.Free;
-    PoFamilyList := TPoFamilyList.Create(MasterList, Lang, Msg);
+    PoFamilyList := TPoFamilyList.Create(MasterList, LangID, Msg);
     if (Msg <> '') then
     begin
       //MessageDlg('PoChecker',Format(sFilesNotFoundAndRemoved,[Msg]), mtInformation, [mbOk], 0);

components/pochecker/pofamilylists.pp
@@ -7,7 +7,7 @@ interface
 Uses
   Classes, SysUtils, LCLProc, FileUtil, StringHashList, ContNrs,
   //{$IFDEF UNIX}{$IFNDEF DisableCWString}, cwstring{$ENDIF}{$ENDIF},
-  PoFamilies;
+  PoFamilies, PoCheckerConsts;
 
 const
   langAll = '*';
@@ -19,7 +19,7 @@ type
 
   TPoFamilyList = class
   private
-    FLang: String;
+    FLangID: TLangID;
     FList: TFPObjectList;
     FOnTestEnd: TTestEndEvent;
     FOnTestStart: TTestStartEvent;
@@ -30,7 +30,7 @@ type
     procedure DoTestStart(const ATestName, APoFileName: String);
     procedure DoTestEnd(const ATestName: String; const ErrorCount: Integer);
   public
-    constructor Create(AMasterList: TStrings; ALang: String; out Msg: String);
+    constructor Create(AMasterList: TStrings; ALangID: TLangID; out Msg: String);
     destructor Destroy; override;
     procedure Add(PoFamily: TPofamily);
     function Count: Integer;
@@ -62,7 +62,7 @@ begin
   if Assigned(FOnTestEnd) then FOnTestEnd(ATestName, ErrorCount);
 end;
 
-constructor TPoFamilyList.Create(AMasterList: TStrings; ALang: String; out Msg: String);
+constructor TPoFamilyList.Create(AMasterList: TStrings; ALangID: TLangID; out Msg: String);
 var
   i: Integer;
   MasterName, ChildName: String;
@@ -71,18 +71,18 @@ begin
   FList := TFPObjectList.Create(True);
   Msg := '';
   FPoFamilyStats := TPoFamilyStats.Create;
-  FLang := ALang;
+  FLangID := ALangID;
   for i :=  0 to AMasterList.Count - 1 do
   begin
     MasterName := AMasterList[i];
     ChildName := '';
     if FileExistsUtf8(MasterName) then
     begin
-      if (ALang <> langAll) then
-        ChildName := ChangeFileExt(MasterName, '.' + ALang + '.po');
+      if (ALangID <> lang_all) then
+        ChildName := ChangeFileExt(MasterName, '.' + LanguageAbbr[ALangID] + '.po');
       //debugln('TPoFamilyList.Create: i = ',DbgS(i),' Adding TPoFamily.Create(''',ExtractFileName(MasterName),
       //        ''',',ExtractFileName(ChildName),''')');
-      if (ALang = langAll) or FileExistsUtf8(ChildName) then
+      if (ALangID = lang_all) or FileExistsUtf8(ChildName) then
       begin
         APoFamily := TPoFamily.Create(MasterName, ChildName);
         Add(APoFamily);
@@ -121,7 +121,7 @@ var
   PoFamily: TPoFamily;
   //ThisLog: TStringList;
 begin
-  if (FLang = langAll) then
+  if (FLangID = lang_all) then
     Include(TestOptions,ptoFindAllChildren)
   else
     Exclude(TestOptions,ptoFindAllChildren);