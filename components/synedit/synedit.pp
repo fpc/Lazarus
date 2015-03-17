6f7c9734e66991e700320cc19effb28b544824d8
martin <laz.git@mfriebe.de>
2015-03-17 02:44:32 +0000

    Merged revision(s) 48402 #f95d44c481 from trunk: SynEdit: fix crash with negative extra line/char spacing ........
    git-svn-id: branches/fixes_1_4@48403 -

--- components/synedit/synedit.pp
+++ components/synedit/synedit.pp
@@ -7803,7 +7803,7 @@ begin
     FScreenCaret.Lock;
     try
       FScreenCaret.CharWidth := CharWidth;
-      FScreenCaret.CharHeight := LineHeight - Max(0, ExtraLineSpacing);
+      FScreenCaret.CharHeight := LineHeight - Max(0, FPaintArea.TextArea.ExtraLineSpacing);
       SizeOrFontChanged(TRUE);
     finally
       FScreenCaret.UnLock;
