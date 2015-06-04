fpc/Lazarus

commit 39eb029e62fddcd954e06b1cc70778815f34a7b1

IDE: Clean resourcestrings.
git-svn-id: trunk@49248 -

ide/frames/codetools_identifiercompletion_options.pas
@@ -69,12 +69,12 @@ end;
 procedure TCodetoolsIndentifierCompletionOptionsFrame.Setup(
   ADialog: TAbstractOptionsEditorDialog);
 begin
-  ICOpenDividerBevel.Caption:=lisIdCOpen;
+  ICOpenDividerBevel.Caption:=lisOpen;
   ICAutoStartAfterPointCheckBox.Caption:=lisAutomaticallyInvokeAfterPoint;
   ICShowHelpCheckBox.Caption:=lisShowHelp;
   ICShowHelpCheckBox.Hint:=lisBestViewedByInstallingAHTMLControlLikeTurbopowerip;
 
-  ICAddDividerBevel.Caption:=lisIdCAdd;
+  ICAddDividerBevel.Caption:=lisAdd;
   ICAddSemicolonCheckBox.Caption:=dlgAddSemicolon;
   ICAddAssignOperatorCheckBox.Caption:=dlgAddAssignmentOperator;
   ICAddDoCheckBox.Caption:=lisAddKeywordDo;

ide/lazarusidestrconsts.pas
@@ -2060,11 +2060,6 @@ resourcestring
   dlgCDTStoredPostfix = 'Stored postfix';
   dlgCDTVariablePrefix = 'Variable prefix';
   dlgSetPropertyVariable = 'Set property Variable';
-  dlgSetPropertyVariableHint = 'The parameter name for the default setter procedure.';
-  dlgSetPropertyVariableIsPrefix = 'is prefix';
-  dlgSetPropertyVariableIsPrefixHint = 'If checked, the "Set property Variable" is a prefix. Otherwise it is a fixed name.';
-  dlgSetPropertyVariableUseConst = 'use const';
-  dlgSetPropertyVariableUseConstHint = 'If checked, the setter parameter is marked with "const".';
   dlgMaxLineLength = 'Max line length:';
   dlgNotSplitLineFront = 'Do not split line in front of';
   dlgNotSplitLineAfter = 'Do not split line after';
@@ -2072,11 +2067,9 @@ resourcestring
   dlgInsSpaceFront = 'Insert space in front of';
   dlgInsSpaceAfter = 'Insert space after';
   dlgWRDPreview = 'Preview';
-  lisIdCAdd = 'Add';
   dlgAddSemicolon = 'Add semicolon';
   dlgAddAssignmentOperator = 'Add assignment operator :=';
   lisAddKeywordDo = 'Add keyword "do"';
-
   dlgUserSchemeError = 'Failed to load user-scheme file %s';
 
   // source editor
@@ -5413,7 +5406,6 @@ resourcestring
   lisClassesAndPropertiesExistValuesWereNotChecked = 'Classes and properties '
     +'exist. Values were not checked.';
   lisInsertPrintShortTag = 'Insert PrintShort tag';
-  lisIdCOpen = 'Open';
   lisAutomaticallyInvokeAfterPoint = 'Automatically invoke after point';
   lisAddParameterBrackets = 'Add parameter brackets';
   lisReplaceWholeIdentifier = 'Replace whole identifier';

commit 327fafb24fc67f7f19dd42ace6044bf5d05190c8

IDE: reverted parts of r49248 #39eb029e62, which caused regressions e.g. in Russian translation
git-svn-id: trunk@49258 -

ide/frames/codetools_identifiercompletion_options.pas
@@ -69,12 +69,12 @@ end;
 procedure TCodetoolsIndentifierCompletionOptionsFrame.Setup(
   ADialog: TAbstractOptionsEditorDialog);
 begin
-  ICOpenDividerBevel.Caption:=lisOpen;
+  ICOpenDividerBevel.Caption:=lisIdCOpen;
   ICAutoStartAfterPointCheckBox.Caption:=lisAutomaticallyInvokeAfterPoint;
   ICShowHelpCheckBox.Caption:=lisShowHelp;
   ICShowHelpCheckBox.Hint:=lisBestViewedByInstallingAHTMLControlLikeTurbopowerip;
 
-  ICAddDividerBevel.Caption:=lisAdd;
+  ICAddDividerBevel.Caption:=lisIdCAdd;
   ICAddSemicolonCheckBox.Caption:=dlgAddSemicolon;
   ICAddAssignOperatorCheckBox.Caption:=dlgAddAssignmentOperator;
   ICAddDoCheckBox.Caption:=lisAddKeywordDo;

ide/lazarusidestrconsts.pas
@@ -2067,6 +2067,7 @@ resourcestring
   dlgInsSpaceFront = 'Insert space in front of';
   dlgInsSpaceAfter = 'Insert space after';
   dlgWRDPreview = 'Preview';
+  lisIdCAdd = 'Add';
   dlgAddSemicolon = 'Add semicolon';
   dlgAddAssignmentOperator = 'Add assignment operator :=';
   lisAddKeywordDo = 'Add keyword "do"';
@@ -5406,6 +5407,7 @@ resourcestring
   lisClassesAndPropertiesExistValuesWereNotChecked = 'Classes and properties '
     +'exist. Values were not checked.';
   lisInsertPrintShortTag = 'Insert PrintShort tag';
+  lisIdCOpen = 'Open';
   lisAutomaticallyInvokeAfterPoint = 'Automatically invoke after point';
   lisAddParameterBrackets = 'Add parameter brackets';
   lisReplaceWholeIdentifier = 'Replace whole identifier';