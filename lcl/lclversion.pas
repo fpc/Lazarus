635c85dc450fd6441cab18030674c2be73ed77ac
mattias <nc-gaertnma@netcologne.de>
2015-10-03 16:42:36 +0000

    set Lazarus version 1.4.4
    git-svn-id: branches/fixes_1_4@49929 -

--- lcl/lclversion.pas
+++ lcl/lclversion.pas
@@ -33,10 +33,10 @@ interface
 const
   lcl_major = 1;
   lcl_minor = 4;
-  lcl_release = 3;
+  lcl_release = 4;
   lcl_patch = 0;
   lcl_fullversion = ((lcl_major *  100 + lcl_minor) * 100 + lcl_release) * 100 + lcl_patch;
-  lcl_version = '1.4.3.0';
+  lcl_version = '1.4.4.0';
 
 implementation
 
