fpc/Lazarus

commit 27167dd17db9e30eec684742954ed811f2c53357

IDE: Add a sanity check in DoSetMainIDEHeight for height returned from CalcNonClientHeight. Don't update height while IDE starts.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@50072 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/main.pp
@@ -6799,7 +6799,6 @@ begin
   MainIDEBar.RefreshCoolbar;
   MainIDEBar.DoSetViewComponentPalette(EnvironmentOptions.Desktop.ComponentPaletteOptions.Visible);
   MainIDEBar.DoSetMainIDEHeight(MainIDEBar.WindowState = wsMaximized, 55);
-  MainIDEBar.SetMainIDEHeight;
 end;
 
 procedure TMainIDE.DoExecuteRemoteControl;

ide/componentpalette.pas
@@ -507,17 +507,16 @@ end;
 
 procedure TComponentPalette.ActivePageChanged(Sender: TObject);
 begin
-  if FPageControl=nil then exit;
+  if (FPageControl=nil) or fUpdatingPageControl then exit;
   if (Selected<>nil)
   and ((Selected.RealPage as TComponentPage).PageComponent=FPageControl.ActivePage) then exit;
-  if fUpdatingPageControl then exit;
   {$IFDEF VerboseComponentPalette}
   DebugLn('TComponentPalette.ActivePageChanged: Calling ReAlignButtons, setting Selected:=nil.');
   {$ENDIF}
   ReAlignButtons(FPageControl.ActivePage);
   Selected:=nil;
 
-  if Assigned(FOnChangeActivePage) then
+  if MainIDE.IDEStarted and Assigned(FOnChangeActivePage) then
     FOnChangeActivePage(Sender);
 end;
 

ide/mainbar.pas
@@ -411,8 +411,9 @@ begin
     FOnActive(Self);
 end;
 
-procedure TMainIDEBar.DoSetMainIDEHeight(const AIDEIsMaximized: Boolean;
-  ANewHeight: Integer);
+procedure TMainIDEBar.DoSetMainIDEHeight(const AIDEIsMaximized: Boolean; ANewHeight: Integer);
+var
+  NonClientH: Integer;
 begin
   if not Showing then
     Exit;
@@ -430,7 +431,11 @@ begin
   begin
     if (AIDEIsMaximized or EnvironmentOptions.Desktop.AutoAdjustIDEHeight) then
     begin
-      ANewHeight := ANewHeight + CalcNonClientHeight;
+      NonClientH := CalcNonClientHeight;
+      // Sanity check. CalcNonClientHeight can return negative values when
+      //  both Coolbar and Palette are hidden.
+      if NonClientH < 0 then Exit;
+      Inc(ANewHeight, NonClientH);
       if ANewHeight <> Constraints.MaxHeight then
       begin
         Constraints.MaxHeight := ANewHeight;

commit 5564280ff891b2336a04e899c2e5747f5d541af2

IDE: Fix regression in main window height update caused by r50072 #d6f0f771b5. Issue #28859.
git-svn-id: trunk@50089 -

ide/main.pp
@@ -6799,6 +6799,7 @@ begin
   MainIDEBar.RefreshCoolbar;
   MainIDEBar.DoSetViewComponentPalette(EnvironmentOptions.Desktop.ComponentPaletteOptions.Visible);
   MainIDEBar.DoSetMainIDEHeight(MainIDEBar.WindowState = wsMaximized, 55);
+  MainIDEBar.SetMainIDEHeight;
 end;
 
 procedure TMainIDE.DoExecuteRemoteControl;