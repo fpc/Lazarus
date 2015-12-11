fpc/Lazarus

commit 045ca90d79536aea3ccc8f81bc72dc0864df5bcd

Qt: pass mousemove event to the viewport of TQtWindowArea, stop mouse propagation to parent. issue #29159
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@50742 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/qt/qtwidgets.pas
@@ -606,6 +606,7 @@ type
     {abstractscrollarea events}
     function EventFilter(Sender: QObjectH; Event: QEventH): Boolean; cdecl; override;
     {viewport events}
+    procedure SetNoMousePropagation(Sender: QWidgetH; const ANoMousePropagation: Boolean); override;
     function ScrollViewEventFilter(Sender: QObjectH; Event: QEventH): Boolean; cdecl;
     function getWindowState: QtWindowStates; override;
   end;
@@ -6420,6 +6421,13 @@ begin
   Result := inherited EventFilter(Sender, Event);
 end;
 
+procedure TQtWindowArea.SetNoMousePropagation(Sender: QWidgetH;
+  const ANoMousePropagation: Boolean);
+begin
+  {must be overrided, see issue #29159}
+  QWidget_setAttribute(Sender, QtWA_NoMousePropagation, ANoMousePropagation);
+end;
+
 function TQtWindowArea.ScrollViewEventFilter(Sender: QObjectH; Event: QEventH
   ): Boolean; cdecl;
 var
@@ -6433,7 +6441,9 @@ begin
   BeginEventProcessing;
   try
     if (QEvent_Type(Event) in [QEventContextMenu, QEventHoverEnter, QEventPaint,
-                               QEventHoverMove, QEventHoverLeave, QEventHide]) then
+                               QEventHoverMove, QEventHoverLeave, QEventHide,
+                               {must be added, see issue #29159}
+                               QEventMouseMove]) then
     begin
       Result := inherited EventFilter(Sender, Event);
     end else

commit 6675012061f6e302720d2a4d9434787f8cd42f05

Qt: fixed regression from r50742 #26b75dd4d0. issue #29159
git-svn-id: trunk@50743 -

lcl/interfaces/qt/qtwidgets.pas
@@ -606,7 +606,6 @@ type
     {abstractscrollarea events}
     function EventFilter(Sender: QObjectH; Event: QEventH): Boolean; cdecl; override;
     {viewport events}
-    procedure SetNoMousePropagation(Sender: QWidgetH; const ANoMousePropagation: Boolean); override;
     function ScrollViewEventFilter(Sender: QObjectH; Event: QEventH): Boolean; cdecl;
     function getWindowState: QtWindowStates; override;
   end;
@@ -6421,13 +6420,6 @@ begin
   Result := inherited EventFilter(Sender, Event);
 end;
 
-procedure TQtWindowArea.SetNoMousePropagation(Sender: QWidgetH;
-  const ANoMousePropagation: Boolean);
-begin
-  {must be overrided, see issue #29159}
-  QWidget_setAttribute(Sender, QtWA_NoMousePropagation, ANoMousePropagation);
-end;
-
 function TQtWindowArea.ScrollViewEventFilter(Sender: QObjectH; Event: QEventH
   ): Boolean; cdecl;
 var
@@ -6993,6 +6985,9 @@ begin
           end;
         end;
       end;
+      {$IFDEF QTSCROLLABLEFORMS}
+      QEventMouseMove: ; // issue #29159
+      {$ENDIF}
       QEventMouseButtonPress,
       QEventMouseButtonRelease,
       QEventMouseButtonDblClick: