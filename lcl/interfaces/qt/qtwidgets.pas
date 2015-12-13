fpc/Lazarus

commit 045ca90d79536aea3ccc8f81bc72dc0864df5bcd

Qt: pass mousemove event to the viewport of TQtWindowArea, stop mouse propagation to parent. issue #29159
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@50742 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/qt/qtwidgets.pas
@@ -606,6 +606,7 @@ type
     {abstractscrollarea events}
     function EventFilter(Sender: QObjectH; Event: QEventH): Boolean; cdecl; override;
     {viewport events}
+    procedure SetNoMousePropagation(Sender: QWidgetH; const ANoMousePropagation: Boolean); override;
     function ScrollViewEventFilter(Sender: QObjectH; Event: QEventH): Boolean; cdecl;
     function getWindowState: QtWindowStates; override;
   end;
@@ -6420,6 +6421,13 @@ begin
   Result := inherited EventFilter(Sender, Event);
 end;
 
+procedure TQtWindowArea.SetNoMousePropagation(Sender: QWidgetH;
+  const ANoMousePropagation: Boolean);
+begin
+  {must be overrided, see issue #29159}
+  QWidget_setAttribute(Sender, QtWA_NoMousePropagation, ANoMousePropagation);
+end;
+
 function TQtWindowArea.ScrollViewEventFilter(Sender: QObjectH; Event: QEventH
   ): Boolean; cdecl;
 var
@@ -6433,7 +6441,9 @@ begin
   BeginEventProcessing;
   try
     if (QEvent_Type(Event) in [QEventContextMenu, QEventHoverEnter, QEventPaint,
-                               QEventHoverMove, QEventHoverLeave, QEventHide]) then
+                               QEventHoverMove, QEventHoverLeave, QEventHide,
+                               {must be added, see issue #29159}
+                               QEventMouseMove]) then
     begin
       Result := inherited EventFilter(Sender, Event);
     end else

commit a73d9cc40d6607c92eb2bebd0b23f47c1d5fd99c

Merged revision(s) 50742-50743 #26b75dd4d0-#26b75dd4d0, 50750 #7453cd0ef6, 50752 #d8f4fd2b8e, 50759 #27a603e4fe, 50761 #a8b48c4b1a from trunk: Qt: pass mousemove event to the viewport of TQtWindowArea, stop mouse propagation to parent. issue #29159 ........ Qt: fixed regression from r50742 #26b75dd4d0. issue #29159 ........ Qt: fixed mousemove with mdi child forms. issue #29159 ........ Qt: fixed design form cursor change. related to issue #29159 ........ Qt: eat less cpu in ownerdata mode when imagelist is assigned without imageindexes per item. issue #29179 ........ Qt: optimized TQtTreeWidget when OwnerDraw=true and imagelist is assigned, but without imageindexes per item.issue #29179 ........
git-svn-id: branches/fixes_1_6@50766 -

lcl/interfaces/qt/qtwidgets.pas
@@ -6433,7 +6433,9 @@ begin
   BeginEventProcessing;
   try
     if (QEvent_Type(Event) in [QEventContextMenu, QEventHoverEnter, QEventPaint,
-                               QEventHoverMove, QEventHoverLeave, QEventHide]) then
+                               QEventHoverMove, QEventHoverLeave, QEventHide,
+                               {must be added, see issue #29159}
+                               QEventMouseMove]) then
     begin
       Result := inherited EventFilter(Sender, Event);
     end else
@@ -6983,6 +6985,16 @@ begin
           end;
         end;
       end;
+      {$IFDEF QTSCROLLABLEFORMS}
+      QEventMouseMove: // issue #29159
+      begin
+        if IsMdiChild or
+          (Assigned(LCLObject) and (csDesigning in LCLObject.ComponentState)) then
+          Result := inherited EventFilter(Sender, Event)
+        else
+          Result := False;
+      end;
+      {$ENDIF}
       QEventMouseButtonPress,
       QEventMouseButtonRelease,
       QEventMouseButtonDblClick:
@@ -11617,13 +11629,13 @@ var
   VHeight: Integer; // viewport height
   RowHeight: Integer;
   item: QListWidgetItemH;
-  v: QVariantH;
+  v, v2, v3: QVariantH;
   WStr: WideString;
   DataStr: WideString;
   ImgList: TCustomImageList;
   AImageIndex: TImageIndex;
   Bmp: TBitmap;
-  v2: QVariantH;
+  AIcon: QIconH;
   AOk: Boolean;
 begin
 
@@ -11697,9 +11709,25 @@ begin
           if (AImageIndex < 0) then
           begin
             v2 := QVariant_create;
-            QListWidgetItem_setData(item, QtListViewOwnerDataRole, v2);
-            QVariant_destroy(v2);
-            QListWidgetItem_setIcon(item, nil);
+            AIcon := QIcon_create;
+            try
+              QListWidgetItem_data(item, v2, QtListViewOwnerDataRole);
+              if not QVariant_isNull(v2) then
+              begin
+                v3 := QVariant_create;
+                try
+                  QListWidgetItem_setData(item, QtListViewOwnerDataRole, v3);
+                finally
+                  QVariant_destroy(v3);
+                end;
+              end;
+              QListWidgetItem_icon(item, AIcon);
+              if not QIcon_isNull(AIcon) then
+                QListWidgetItem_setIcon(item, nil);
+            finally
+              QVariant_destroy(v2);
+              QIcon_destroy(AIcon);
+            end;
           end;
         end;
 
@@ -13706,13 +13734,14 @@ var
   RowHeight: Integer;
   item: QTreeWidgetItemH;
   itemChild: QTreeWidgetItemH;
-  v,v2: QVariantH;
+  v,v2,v3: QVariantH;
   WStr, TempStr: WideString;
   ASelected: Boolean;
   ImgList: TCustomImageList;
   AImageIndex: TImageIndex;
   Bmp: TBitmap;
   AOk: Boolean;
+  AIcon: QIconH;
 begin
   {do not set items during design time}
   if csDesigning in LCLObject.ComponentState then
@@ -13803,9 +13832,25 @@ begin
             if (AImageIndex < 0) then
             begin
               v2 := QVariant_create;
-              QTreeWidgetItem_setData(item, 0, QtListViewOwnerDataRole, v2);
-              QVariant_destroy(v2);
-              QTreeWidgetItem_setIcon(item, 0, nil);
+              AIcon := QIcon_create;
+              try
+                QTreeWidgetItem_data(item, v2, 0, QtListViewOwnerDataRole);
+                if not QVariant_isNull(v2) then
+                begin
+                  v3 := QVariant_create;
+                  try
+                    QTreeWidgetItem_setData(item, 0, QtListViewOwnerDataRole, v3);
+                  finally
+                    QVariant_destroy(v3);
+                  end;
+                end;
+                QTreeWidgetItem_icon(item, AIcon, 0);
+                if not QIcon_isNull(AIcon) then
+                  QTreeWidgetItem_setIcon(item, 0, nil);
+              finally
+                QVariant_destroy(v2);
+                QIcon_destroy(AIcon);
+              end;
             end;
           end;
 