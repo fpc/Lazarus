fpc/Lazarus

commit 48d5297d8ec08368c70e7f1802f37d80c73f3682

IDE, IDEIntf: add icons to project files in recent drop-down menu
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51233 4005530d-fff6-0310-9dd1-cebe43e6787f

components/ideintf/projectintf.pas
@@ -18,7 +18,7 @@ interface
 uses
   Classes, SysUtils, FileUtil, LazFileUtils, Controls, Forms, AvgLvlTree,
   NewItemIntf, ProjPackIntf, CompOptsIntf, ObjInspStrConsts, LazFileCache,
-  LazMethodList;
+  LazMethodList, ImgList, Graphics;
 
 const
   FileDescGroupName = 'File';
@@ -585,6 +585,9 @@ const
     );
 
 
+function LoadProjectIconIntoImages(const ProjFile: string;
+  const Images: TCustomImageList; const Index: TStrings): Integer;
+
 function ProjectFlagsToStr(Flags: TProjectFlags): string;
 function StrToProjectSessionStorage(const s: string): TProjectSessionStorage;
 function CompilationExecutableTypeNameToType(const s: string
@@ -692,6 +695,44 @@ end;
 function ProjectDescriptorEmptyProject: TProjectDescriptor;
 begin
   Result:=ProjectDescriptors.FindByName(ProjDescNameEmpty);
+end;
+
+function LoadProjectIconIntoImages(const ProjFile: string;
+  const Images: TCustomImageList; const Index: TStrings): Integer;
+var
+  xIconFile: RawByteString;
+  xIcon: TIcon;
+  I: Integer;
+begin
+  //ToDo: better index
+
+  I := Index.IndexOf(ProjFile);
+  if I >= 0 then
+    Exit(NativeInt(Index.Objects[I]));
+
+  Result := -1;
+  xIconFile := ChangeFileExt(ProjFile, '.ico');
+  if FileExists(xIconFile) then
+  begin
+    xIcon := TIcon.Create;
+    try
+      xIcon.LoadFromFile(xIconFile);
+      for I := 0 to xIcon.Count-1 do
+      begin
+        xIcon.Current := I;
+        if (xIcon.Width = Images.Width)
+        and(xIcon.Height = Images.Height) then
+        begin
+          Result := Images.AddIcon(xIcon);
+          Break;
+        end;
+      end;
+    finally
+      xIcon.Free;
+    end;
+  end;
+
+  Index.AddObject(ProjFile, TObject(Result));
 end;
 
 function ProjectFlagsToStr(Flags: TProjectFlags): string;

ide/mainbase.pas
@@ -68,7 +68,7 @@ uses
   Debugger, IDEOptionDefs, Splash, Designer,
   SourceEditor, FindInFilesDlg,
   MainBar, MainIntf, SourceSynEditor, PseudoTerminalDlg,
-  DesktopManager;
+  DesktopManager, ImgList;
 
 type
   TResetToolFlag = (
@@ -218,10 +218,15 @@ type
 
   TOpenFileToolButton = class(TIDEToolButton)
   private
+    FIndex: TStringList;
+
     procedure RefreshMenu(Sender: TObject);
     procedure mnuOpenFile(Sender: TObject);
     procedure mnuProjectFile(Sender: TObject);
   public
+    constructor Create(aOwner: TComponent); override;
+    destructor Destroy; override;
+
     procedure DoOnAdded; override;
   end;
 
@@ -411,12 +416,32 @@ end;
 
 { TOpenFileToolButton }
 
+constructor TOpenFileToolButton.Create(aOwner: TComponent);
+begin
+  inherited Create(aOwner);
+
+  FIndex := TStringList.Create;
+  FIndex.Sorted := True;
+  FIndex.Duplicates := dupIgnore;
+  FIndex.CaseSensitive := False;
+end;
+
+destructor TOpenFileToolButton.Destroy;
+begin
+  FIndex.Free;
+
+  inherited Destroy;
+end;
+
 procedure TOpenFileToolButton.DoOnAdded;
 begin
   inherited DoOnAdded;
 
   DropdownMenu := TPopupMenu.Create(Self);
   DropdownMenu.OnPopup := @RefreshMenu;
+  DropdownMenu.Images := TCustomImageList.Create(Self);
+  DropdownMenu.Images.Width := 16;
+  DropdownMenu.Images.Height := 16;
   Style := tbsDropDown;
 end;
 
@@ -439,12 +464,16 @@ procedure TOpenFileToolButton.RefreshMenu(Sender: TObject);
   procedure AddFile(const AFileName: string; const AOnClick: TNotifyEvent);
   var
     AMenuItem: TOpenFileMenuItem;
+    xExt: string;
   begin
     AMenuItem := TOpenFileMenuItem.Create(DropdownMenu);
+    DropdownMenu.Items.Add(AMenuItem);
     AMenuItem.OnClick := AOnClick;
     AMenuItem.FileName := AFileName;
-    DropdownMenu.Items.Add(AMenuItem);
     AMenuItem.Caption := AFilename;
+    xExt := ExtractFileExt(AFileName);
+    if SameFileName(xExt, '.lpi') or SameFileName(xExt, '.lpr') then
+      AMenuItem.ImageIndex := LoadProjectIconIntoImages(AFileName, DropdownMenu.Images, FIndex);
   end;
 
   procedure AddFiles(List: TStringList; MaxCount: integer; const AOnClick: TNotifyEvent);

commit 798ce08c4274b9b3364bad20420587b015baf25d

IDEIntf: fixed compilation error introduced in r51233 #3a1c8fac99
git-svn-id: trunk@51245 -

components/favorites/favorites_impl.pas
@@ -95,9 +95,6 @@ begin
   inherited Create(aOwner);
 
   FIndex := TStringList.Create;
-  FIndex.Sorted := True;
-  FIndex.Duplicates := dupIgnore;
-  FIndex.CaseSensitive := False;
 
   if FavHandler.FOldToolButtonClass<>nil then
     FOrigButton := FavHandler.FOldToolButtonClass.Create(Self)

components/ideintf/projectintf.pas
@@ -586,7 +586,7 @@ const
 
 
 function LoadProjectIconIntoImages(const ProjFile: string;
-  const Images: TCustomImageList; const Index: TStrings): Integer;
+  const Images: TCustomImageList; const Index: TStringList): Integer;
 
 function ProjectFlagsToStr(Flags: TProjectFlags): string;
 function StrToProjectSessionStorage(const s: string): TProjectSessionStorage;
@@ -697,18 +697,32 @@ begin
   Result:=ProjectDescriptors.FindByName(ProjDescNameEmpty);
 end;
 
+type
+  TLoadProjectIconIntoImagesObject = class
+    ImageIndex: Integer;
+  end;
+
 function LoadProjectIconIntoImages(const ProjFile: string;
-  const Images: TCustomImageList; const Index: TStrings): Integer;
+  const Images: TCustomImageList; const Index: TStringList): Integer;
 var
   xIconFile: RawByteString;
   xIcon: TIcon;
   I: Integer;
+  xObj: TLoadProjectIconIntoImagesObject;
 begin
   //ToDo: better index
 
   I := Index.IndexOf(ProjFile);
   if I >= 0 then
-    Exit(NativeInt(Index.Objects[I]));
+    Exit(TLoadProjectIconIntoImagesObject(Index.Objects[I]).ImageIndex);
+
+  if not Index.Sorted or (Index.Count = 0) then
+  begin // initialize index
+    Index.Sorted := True;
+    Index.Duplicates := dupIgnore;
+    Index.CaseSensitive := False;
+    Index.OwnsObjects := True;
+  end;
 
   Result := -1;
   xIconFile := ChangeFileExt(ProjFile, '.ico');
@@ -732,7 +746,9 @@ begin
     end;
   end;
 
-  Index.AddObject(ProjFile, TObject(Result));
+  xObj := TLoadProjectIconIntoImagesObject.Create;
+  xObj.ImageIndex := Result;
+  Index.AddObject(ProjFile, xObj);
 end;
 
 function ProjectFlagsToStr(Flags: TProjectFlags): string;

ide/mainbase.pas
@@ -421,9 +421,6 @@ begin
   inherited Create(aOwner);
 
   FIndex := TStringList.Create;
-  FIndex.Sorted := True;
-  FIndex.Duplicates := dupIgnore;
-  FIndex.CaseSensitive := False;
 end;
 
 destructor TOpenFileToolButton.Destroy;