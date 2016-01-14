fpc/Lazarus

commit 940c882f8870f2602b3b0ae9deb5e85742690eb5

ide: sourcenotebook better PopupMenu handling. Fixes also issue 29295
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51169 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/sourceeditor.pp
@@ -727,6 +727,8 @@ type
 
     procedure NotebookMouseDown(Sender: TObject; Button: TMouseButton;
           {%H-}Shift: TShiftState; X,Y: Integer);
+    procedure NotebookMouseUp(Sender: TObject; Button: TMouseButton;
+          {%H-}Shift: TShiftState; X,Y: Integer);
     procedure NotebookDragDropEx(Sender, Source: TObject;
                                   OldIndex, NewIndex: Integer; CopyDrag: Boolean;
                                   var Done: Boolean);
@@ -6300,7 +6302,6 @@ Begin
     APage.Caption:='unit1';
     APage.Parent:=FNotebook;
     PageIndex := 0;   // Set it to the first page
-    PopupMenu := TabPopupMenu;
     if EditorOpts.ShowTabCloseButtons then
       Options:=Options+[nboShowCloseButtons]
     else
@@ -6315,6 +6316,7 @@ Begin
     OnChange := @NotebookPageChanged;
     OnCloseTabClicked  := @CloseTabClicked;
     OnMouseDown:=@NotebookMouseDown;
+    OnMouseUp:=@NotebookMouseUp;
     TabDragMode := dmAutomatic;
     OnTabDragOverEx  := @NotebookDragOverEx;
     OnTabDragDropEx  := @NotebookDragDropEx;
@@ -8366,6 +8368,26 @@ begin
     if TabIndex>=0 then
       CloseClicked(NoteBookPage[TabIndex],
                    (GetKeyState(VK_CONTROL) < 0) and EditorOpts.CtrlMiddleTabClickClosesOthers);
+  end else
+  if (Button = mbRight) then
+  begin
+    //select on right click
+    TabIndex:=FNotebook.TabIndexAtClientPos(Point(X,Y));
+    if TabIndex>=0 then
+      FNotebook.ActivePageIndex := TabIndex;
+  end;
+end;
+
+procedure TSourceNotebook.NotebookMouseUp(Sender: TObject;
+  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+var
+  TabIndex: Integer;
+begin
+  if (Button = mbRight) then
+  begin
+    TabIndex:=FNotebook.TabIndexAtClientPos(Point(X,Y));
+    if TabIndex>=0 then
+      TabPopUpMenu.PopUp;
   end;
 end;
 

commit ff8222112a5cba1a791464f89c83251167f5e9f7

IDE: disable default popup menu for IDE source notebook, regression from r51169 #416b79849e. Issue #29295
git-svn-id: trunk@51278 -

ide/sourceeditor.pp
@@ -47,7 +47,7 @@ uses
   ExtendedNotebook, LCLProc, LCLType, LResources, LCLIntf, FileUtil, LazFileUtils,
   Translations, ClipBrd, HelpIntfs,
   LConvEncoding, Messages, LazLoggerBase, lazutf8classes, LazLogger, AvgLvlTree,
-  LazFileCache, LazUTF8,
+  LazFileCache, LazUTF8, LMessages,
   // codetools
   BasicCodeTools, CodeBeautifier, CodeToolManager, CodeCache, SourceLog,
   LinkScanner, CodeTree, SourceChanger,
@@ -583,6 +583,13 @@ type
   TSourceNotebookUpdateFlags = set of TSourceNotebookUpdateFlag;
 
   TBrowseEditorTabHistoryDialog = class;
+
+  { TIDEExtendedNotebook }
+
+  TIDEExtendedNotebook = class(TExtendedNotebook)
+  protected
+    procedure WMContextMenu(var Message: TLMContextMenu); message LM_CONTEXTMENU;
+  end;
 
   { TSourceNotebook }
 
@@ -1730,6 +1737,27 @@ var
   SE1: TSourceEditorInterface absolute SrcEdit;
 begin
   Result:=CompareFilenames(AnsiString(FileNameStr),SE1.FileName);
+end;
+
+{ TIDEExtendedNotebook }
+
+procedure TIDEExtendedNotebook.WMContextMenu(var Message: TLMContextMenu);
+var
+  Child: TControl;
+begin
+  // Gtk2 workaround to disable default popup menu
+  // -> Gtk2 shows default popup menu for a page control, disable it
+  if Message.XPos <> -1 then
+  begin
+    Child := ControlAtPos(ScreenToClient(SmallPointToPoint(Message.Pos)), [capfHasScrollOffset]);
+    if (Child = nil) or (Child = Self) then
+    begin
+      Message.Result := 1;
+      Exit;
+    end;
+  end;
+
+  inherited WMContextMenu(Message);
 end;
 
 { TBrowseEditorTabHistoryDialog }
@@ -6283,7 +6311,7 @@ Begin
   {$IFDEF IDE_MEM_CHECK}
   CheckHeapWrtMemCnt('[TSourceNotebook.CreateNotebook] A ');
   {$ENDIF}
-  FNotebook := TExtendedNotebook.Create(self);
+  FNotebook := TIDEExtendedNotebook.Create(self);
   {$IFDEF IDE_DEBUG}
   debugln('[TSourceNotebook.CreateNotebook] B');
   {$ENDIF}