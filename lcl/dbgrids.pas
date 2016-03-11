fpc/Lazarus

commit a56a531d610a917cde065edd57d5b3f75df67df8

LCL, grids: check that clicked cell is within valid grid zone. Do not trigger cellClick on dbgrid indicator column. Fixes issue 29802
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51914 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/dbgrids.pas
@@ -2816,9 +2816,9 @@ begin
       if (Editor=nil) or not EditorMode then
         SwapCheckBox
     end;
+    if Assigned(OnCellClick) then
+      OnCellClick(TColumn(ColumnFromGridColumn(aCol)));
   end;
-  if Assigned(OnCellClick) then
-    OnCellClick(TColumn(ColumnFromGridColumn(aCol)));
 end;
 
 procedure TCustomDBGrid.EndLayout;
@@ -3483,6 +3483,7 @@ begin
 
   // What a dilema!, we need ssAutoHorizontal and ssVertical!!!
   ScrollBars:=ssBoth;
+  AllowOutboundEvents := false;
 end;
 
 procedure TCustomDBGrid.AutoSizeColumns;

lcl/grids.pas
@@ -5814,7 +5814,7 @@ begin
     if Y<FGcache.FixedHeight+aBorderWidth then
       Result:= gzFixedCells
     else
-    if RowCount>FixedRows then
+    if (RowCount>FixedRows) and (Y<=FGCache.GridHeight) then
       Result:= gzFixedRows
     else
       Result:= gzInvalid
@@ -5824,7 +5824,7 @@ begin
     if FlipX(X)<FGCache.FixedWidth+aBorderWidth then
       Result:=gzFixedCells
     else
-    if ColCount>FixedCols then
+    if (ColCount>FixedCols) and (FlipX(X)<=FGCache.GridWidth) then
       Result:=gzFixedCols
     else
       Result:=gzInvalid
@@ -5843,6 +5843,9 @@ end;
 
 function TCustomGrid.CellToGridZone(aCol, aRow: Integer): TGridZone;
 begin
+  if (aCol<0) or (aRow<0) then
+    Result := gzInvalid
+  else
   if (aCol<FFixedCols) then
     if aRow<FFixedRows then
       Result:= gzFixedCells
@@ -6331,16 +6334,24 @@ procedure TCustomGrid.MouseUp(Button: TMouseButton; Shift: TShiftState; X,
   Y: Integer);
 var
    Cur: TPoint;
+   Gz: TGridZone;
+
+   function IsValidCellClick: boolean;
+   begin
+     result := (Cur.X=FGCache.ClickCell.X) and (Cur.Y=FGCache.ClickCell.Y) and (gz<>gzInvalid);
+   end;
+
 begin
   inherited MouseUp(Button, Shift, X, Y);
   {$IfDef dbgGrid}DebugLn('MouseUP INIT');{$Endif}
 
   Cur:=MouseToCell(Point(x,y));
+  Gz :=CellToGridZone(cur.x, cur.y);
 
   case fGridState of
 
     gsHeaderClicking, gsButtonColumnClicking:
-      if (Cur.X=FGCache.ClickCell.X) and (Cur.Y=FGCache.ClickCell.Y) then begin
+      if IsValidCellClick then begin
         if fGridState=gsHeaderClicking then
           HeaderClick(True, FGCache.ClickCell.X)
         else
@@ -6349,7 +6360,7 @@ begin
       end;
 
     gsNormal:
-      if not FixedGrid and (Cur.X=FGCache.ClickCell.X) and (Cur.Y=FGCache.ClickCell.Y) then
+      if not FixedGrid and IsValidCellClick then
         CellClick(cur.x, cur.y, Button);
 
     gsSelecting:

commit 712eae76cda51b48aaa99d98de90e8cf266bc261

LCL, dbgrid: fix exception on adding second record (regression caused by r51914 #5224c3c339), fixes issues #29802, #29813
git-svn-id: trunk@51918 -

lcl/dbgrids.pas
@@ -3381,6 +3381,7 @@ end;
 procedure TCustomDBGrid.UpdateActive;
 var
   PrevRow: Integer;
+  NewRow: Integer;
 begin
   if (csDestroying in ComponentState) or
     (FDatalink=nil) or (not FDatalink.Active) or
@@ -3391,7 +3392,10 @@ begin
   		[ClassName, Name, FDataLink.ActiveRecord, FixedRows, Row]);
   {$endif}
   PrevRow := Row;
-  Row:= FixedRows + FDataLink.ActiveRecord;
+  NewRow:= FixedRows + FDataLink.ActiveRecord;
+  if NewRow>RowCount-1 then
+    NewRow := RowCount-1;
+  Row := NewRow;
   if PrevRow<>Row then
     InvalidateCell(0, PrevRow);//(InvalidateRow(PrevRow);
   InvalidateRow(Row);