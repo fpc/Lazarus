fpc/Lazarus

commit 6621eee3735b4bc7d122784bd2644bb20eb959ce

qt: add support for WM_NCHITTEST=HTTRANSPARENT windows.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@52359 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/qt/qtwidgets.pas
@@ -3521,18 +3521,32 @@ end;
 function TQtWidget.SlotMouse(Sender: QObjectH; Event: QEventH): Boolean; cdecl;
 var
   Msg: TLMMouse;
-  MousePos: TQtPoint;
   MButton: QTMouseButton;
   Modifiers: QtKeyboardModifiers;
   SaveWidget: QWidgetH;
   LazButton: Byte;
   LazPos: TPoint;
+  TrgHandle: TQtWidget;
+  TrgControl: TWinControl;
 begin
   {$ifdef VerboseQt}
     WriteLn('TQtWidget.SlotMouse');
   {$endif}
 
   Result := False; // allow qt to handle message
+
+  TrgHandle := Self;
+  TrgControl := LCLObject;
+  CheckTransparentWindow(TLCLIntfHandle(TrgHandle), TrgControl);
+  if (TrgHandle=nil) or (TrgControl=nil) then
+  begin
+    Exit;
+  end else
+  if (TrgHandle<>Self) then
+  begin
+    Result := TQtWidget(TrgHandle).SlotMouse(Sender, Event);
+    Exit;
+  end;
 
   if not CanSendLCLMessage then
     exit(True);
@@ -3545,17 +3559,15 @@ begin
 
   FillChar(Msg{%H-}, SizeOf(Msg), #0);
   
-  MousePos := QMouseEvent_pos(QMouseEventH(Event))^;
-  OffsetMousePos(@MousePos);
+  LazPos := LCLObject.ScreenToClient(Mouse.CursorPos);
 
   Modifiers := QInputEvent_modifiers(QInputEventH(Event));
   Msg.Keys := QtKeyModifiersToKeyState(Modifiers, False, nil);
 
-  Msg.XPos := SmallInt(MousePos.X);
-  Msg.YPos := SmallInt(MousePos.Y);
+  Msg.XPos := SmallInt(LazPos.X);
+  Msg.YPos := SmallInt(LazPos.Y);
   
   MButton := QMouseEvent_Button(QMouseEventH(Event));
-  LazPos := Point(MousePos.X, MousePos.Y);
   Msg.Keys := Msg.Keys or QtButtonsToLCLButtons(MButton);
   case MButton of
     QtLeftButton: LazButton := 1;

commit 3cbdcfa38bcfcfb51eef38b817ffcc0d68e25f31

Qt: fixed regression from r52359 #db2d5e907e
git-svn-id: trunk@52418 -

lcl/interfaces/qt/qtwidgets.pas
@@ -3526,6 +3526,7 @@ var
   SaveWidget: QWidgetH;
   LazButton: Byte;
   LazPos: TPoint;
+  MousePos: TQtPoint;
   TrgHandle: TQtWidget;
   TrgControl: TWinControl;
 begin
@@ -3558,8 +3559,11 @@ begin
   // idea of multi click implementation is taken from gtk
 
   FillChar(Msg{%H-}, SizeOf(Msg), #0);
-  
-  LazPos := LCLObject.ScreenToClient(Mouse.CursorPos);
+
+  MousePos := QMouseEvent_pos(QMouseEventH(Event))^;
+  OffsetMousePos(@MousePos);
+
+  LazPos := Point(MousePos.X, MousePos.Y);
 
   Modifiers := QInputEvent_modifiers(QInputEventH(Event));
   Msg.Keys := QtKeyModifiersToKeyState(Modifiers, False, nil);