fpc/Lazarus

commit 7a40f2b2fad0f75a0b0ec9d86fcd37d7b035c74c

LCL: Make TTreeView more compatible with Delphi, use Color instead of BackgroundColor, respect Brush.Color in CustonDrawItem. Issue #30288, patch from Anton Kavalenka.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@52515 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/include/treeview.inc
@@ -3159,7 +3159,7 @@ begin
   FSBHorzShowing:=-1;
   TabStop := True;
   ParentColor := False;
-  FBackgroundColor := clWindow;
+  //FBackgroundColor := clWindow;
   FDefItemHeight := 20;
   FExpandSignType := tvestTheme;
   UpdateExpandSignSize;
@@ -3430,8 +3430,8 @@ end;
 
 procedure TCustomTreeView.SetBackgroundColor(Value: TColor);
 begin
-  if FBackgroundColor<>Value then begin
-    FBackgroundColor:=Value;
+  if Color<>Value then begin
+    Color:=Value;
     Invalidate;
   end;
 end;
@@ -4637,10 +4637,10 @@ begin
         SpaceRect.Top := Node.Top + Node.Height - FScrolledTop + BorderWidth;
       //if Node<>nil then DebugLn('BottomItem=',BottomItem.text) else DebugLn('NO BOTTOMITEM!!!!!!!!!');
       // TWinControl(Parent).InvalidateRect(Self,SpaceRect,true);
-      if (FBackgroundColor <> clNone) and (SpaceRect.Top < SpaceRect.Bottom) then
+      if (Color <> clNone) and (SpaceRect.Top < SpaceRect.Bottom) then
       begin
         //DebugLn('  SpaceRect=',SpaceRect.Left,',',SpaceRect.Top,',',SpaceRect.Right,',',SpaceRect.Bottom);
-        Brush.Color := FBackgroundColor;
+        Brush.Color := Color;
         FillRect(SpaceRect);
       end;
       // draw border
@@ -4978,31 +4978,36 @@ var
   procedure DrawBackground(IsSelected: Boolean; ARect: TRect);
   var
     Details: TThemedElementDetails;
-    CurBackgroundColor: TColor;
+    CurBackgroundColor,bclr: TColor;
   begin
-    if (tvoRowSelect in Options) and IsSelected then
-      if tvoThemedDraw in Options then
-      begin
-        if tvoFocusedPainting in FStates then
-          Details := ThemeServices.GetElementDetails(ttItemSelected)
-        else
-          Details := ThemeServices.GetElementDetails(ttItemSelectedNotFocus);
-        if ThemeServices.HasTransparentParts(Details) then
+    bclr:=Canvas.Brush.Color;
+    try
+      if (tvoRowSelect in Options) and IsSelected then
+        if tvoThemedDraw in Options then
         begin
-          Canvas.Brush.Color := BackgroundColor;
-          Canvas.FillRect(ARect);
-        end;
-        ThemeServices.DrawElement(Canvas.Handle, Details, ARect, nil);
-        Exit;
-      end
+          if tvoFocusedPainting in FStates then
+            Details := ThemeServices.GetElementDetails(ttItemSelected)
+          else
+            Details := ThemeServices.GetElementDetails(ttItemSelectedNotFocus);
+          if ThemeServices.HasTransparentParts(Details) then
+          begin
+            Canvas.Brush.Color := Color;
+            Canvas.FillRect(ARect);
+          end;
+          ThemeServices.DrawElement(Canvas.Handle, Details, ARect, nil);
+          Exit;
+        end
+        else
+          CurBackgroundColor := FSelectedColor
       else
-        CurBackgroundColor := FSelectedColor
-    else
-      CurBackgroundColor := FBackgroundColor;
-    if CurBackgroundColor <> clNone then
-    begin
-      Canvas.Brush.Color := CurBackgroundColor;
-      Canvas.FillRect(ARect);
+        CurBackgroundColor := Color;
+      if CurBackgroundColor <> clNone then
+      begin
+        Canvas.Brush.Color := CurBackgroundColor;
+        Canvas.FillRect(ARect);
+      end;
+    finally
+      Canvas.Brush.Color := bclr;
     end;
   end;
 
@@ -5055,6 +5060,7 @@ begin
     Exit;
   NodeSelected := (Node.Selected) or (Node.MultiSelected);
   Canvas.Font.Color := Font.Color;
+  Canvas.Brush.Color := Color;
   PaintImages := True;
   if IsCustomDrawn(dtItem, cdPrePaint) then
   begin

commit bbd1676580656e284129aa643eb5f13caad5c581

lcl: Fix TCustomTreeView BackgroundColor broken in r52515 #2beb2f65d0 (issue #30312)
git-svn-id: trunk@52578 -

lcl/comctrls.pp
@@ -3210,7 +3210,7 @@ type
   TCustomTreeView = class(TCustomControl)
   private
     FAccessibilityOn: Boolean;
-    FBackgroundColor: TColor;
+//    FBackgroundColor: TColor;
     FBottomItem: TTreeNode;
     FCallingOnChange: Boolean;
     FEditingItem: TTreeNode;
@@ -3280,6 +3280,7 @@ type
     FHintWnd: THintWindow;
     procedure CanvasChanged(Sender: TObject);
     function GetAutoExpand: boolean;
+    function GetBackgroundColor: TColor;
     function GetBottomItem: TTreeNode;
     function GetDropTarget: TTreeNode;
     function GetHideSelection: boolean;
@@ -3298,6 +3299,7 @@ type
     function GetShowSeparators: boolean;
     function GetToolTips: boolean;
     function GetTopItem: TTreeNode;
+    function IsStoredBackgroundColor: Boolean;
     procedure HintMouseLeave(Sender: TObject);
     procedure ImageListChange(Sender: TObject);
     procedure OnChangeTimer(Sender: TObject);
@@ -3526,8 +3528,9 @@ type
     procedure MoveHome(ASelect: Boolean = False);
     procedure MoveEnd(ASelect: Boolean = False);
   public
-    property BackgroundColor: TColor read FBackgroundColor write SetBackgroundColor default clWindow;
+    property BackgroundColor: TColor read GetBackgroundColor write SetBackgroundColor stored IsStoredBackgroundColor;
     property BorderWidth default 0;
+    property Color default clWindow;
     property BottomItem: TTreeNode read GetBottomItem write SetBottomItem;
     property DefaultItemHeight: integer read FDefItemHeight write SetDefaultItemHeight default 20;
     property DropTarget: TTreeNode read GetDropTarget write SetDropTarget;

lcl/include/treeview.inc
@@ -3155,11 +3155,13 @@ begin
                                + [csDisplayDragImage, csReflector];
   Width := 121;
   Height := 97;
+  Color := clWindow;
+
   FSBVertShowing:=-1;
   FSBHorzShowing:=-1;
   TabStop := True;
   ParentColor := False;
-  //FBackgroundColor := clWindow;
+//  FBackgroundColor := clWindow;
   FDefItemHeight := 20;
   FExpandSignType := tvestTheme;
   UpdateExpandSignSize;
@@ -4134,6 +4136,11 @@ begin
     FHintWnd.Hide;
 end;
 
+function TCustomTreeView.IsStoredBackgroundColor: Boolean;
+begin
+  result := Color <> clWindow;
+end;
+
 procedure TCustomTreeView.SetTopItem(Value: TTreeNode);
 begin
   if HandleAllocated and (Value <> nil) then
@@ -4388,6 +4395,11 @@ end;
 function TCustomTreeView.GetAutoExpand: boolean;
 begin
   Result:=(tvoAutoExpand in FOptions);
+end;
+
+function TCustomTreeView.GetBackgroundColor: TColor;
+begin
+  Result := Color;
 end;
 
 function TCustomTreeView.GetBottomItem: TTreeNode;