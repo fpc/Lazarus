6d5267d15a763e57dc4db78fccf21b479064f3d1
mattias <nc-gaertnma@netcologne.de>
2016-08-02 08:23:56 +0000

    IDE: fixed updating key/command relations, bug #30436
    git-svn-id: branches/fixes_1_6@52775 -

--- components/ideintf/idecommands.pas
+++ components/ideintf/idecommands.pas
@@ -502,7 +502,7 @@ type
   end;
   PIDEShortCut = ^TIDEShortCut;
 
-  { TIDECommandCategory
+  { TIDECommandCategory - list of TKeyCommandRelation
     TIDECommandCategory is used to divide the commands in handy packets }
     
   TIDECommandCategory = class(TList)
