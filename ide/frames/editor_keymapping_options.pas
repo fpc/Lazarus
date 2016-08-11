fpc/Lazarus

commit 0eaf0516c9316f942657f4d0fa83013a9095c8fb

LCL: Change ItemData parameter in FilterItem events from TObject to Pointer. Add "const" to ACaption: string.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@52745 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/frames/editor_keymapping_options.pas
@@ -60,7 +60,7 @@ type
     procedure ChooseSchemeButtonClick(Sender: TObject);
     procedure ClearButtonClick(Sender: TObject);
     procedure FilterEditAfterFilter(Sender: TObject);
-    function FilterEditFilterItem(Item: TObject; out Done: Boolean): Boolean;
+    function FilterEditFilterItem(ItemData: Pointer; out Done: Boolean): Boolean;
     procedure FilterEditKeyPress(Sender: TObject; var {%H-}Key: char);
     procedure FindKeyButtonClick(Sender: TObject);
     procedure KeyMapSplitterMoved(Sender: TObject);
@@ -222,21 +222,23 @@ begin
   ClearCommandMapping(TreeView.Selected)
 end;
 
-function TEditorKeymappingOptionsFrame.FilterEditFilterItem(Item: TObject; out Done: Boolean): Boolean;
+function TEditorKeymappingOptionsFrame.FilterEditFilterItem(ItemData: Pointer;
+  out Done: Boolean): Boolean;
 var
   KeyRel: TKeyCommandRelation;
 begin
   Done:=True;
   Result:=False;
-  if Item is TKeyCommandRelation then begin
-    KeyRel:=TKeyCommandRelation(Item);        // Tree item is actual key command.
-    Done:=False;
-    Result:=KeyMapKeyFilter.Key1<>VK_UNKNOWN;
-    if Result then begin                      // Key filter is defined
-      Done:=True;
-      Result:=(CompareIDEShortCutKey1s(@KeyMapKeyFilter,@KeyRel.ShortcutA)=0)
-           or (CompareIDEShortCutKey1s(@KeyMapKeyFilter,@KeyRel.ShortcutB)=0);
-    end;
+  if ItemData=nil then Exit;
+  Assert(TObject(ItemData) is TKeyCommandRelation,
+    'TEditorKeymappingOptionsFrame.FilterEditFilterItem: ItemData is not TKeyCommandRelation.');
+  KeyRel:=TKeyCommandRelation(ItemData);      // Tree item is actual key command.
+  Done:=False;
+  Result:=KeyMapKeyFilter.Key1<>VK_UNKNOWN;
+  if Result then begin                        // Key filter is defined.
+    Done:=True;
+    Result:=(CompareIDEShortCutKey1s(@KeyMapKeyFilter,@KeyRel.ShortcutA)=0)
+         or (CompareIDEShortCutKey1s(@KeyMapKeyFilter,@KeyRel.ShortcutB)=0);
   end;
 end;
 

ide/ideoptionsdlg.pas
@@ -71,7 +71,7 @@ type
     procedure CategoryTreeCollapsed(Sender: TObject; Node: TTreeNode);
     procedure CategoryTreeExpanded(Sender: TObject; Node: TTreeNode);
     procedure CategoryTreeKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-    function FilterEditFilterItem(Item: TObject; out Done: Boolean): Boolean;
+    function FilterEditFilterItem(ItemData: Pointer; out Done: Boolean): Boolean;
     procedure FormShow(Sender: TObject);
     procedure HelpButtonClick(Sender: TObject);
     procedure OkButtonClick(Sender: TObject);
@@ -283,14 +283,14 @@ begin
   end;
 end;
 
-function TIDEOptionsDialog.FilterEditFilterItem(Item: TObject; out Done: Boolean): Boolean;
+function TIDEOptionsDialog.FilterEditFilterItem(ItemData: Pointer; out Done: Boolean): Boolean;
 var
   OptEditor: TAbstractIDEOptionsEditor;
 begin
   Result:=False;
   Done:=False;                        // Filter will use also the node caption.
-  if Item=nil then Exit;
-  OptEditor:=TAbstractIDEOptionsEditor(Item);
+  if ItemData=nil then Exit;
+  OptEditor:=TAbstractIDEOptionsEditor(ItemData);
   OptEditor.RememberDefaultStyles;
   Result:=OptEditor.ContainsTextInCaption(FilterEdit.Filter);
 end;

lcl/editbtn.pas
@@ -188,8 +188,8 @@ type
   // Called when an item is filtered. Returns true if the item passes the filter.
   // Done=False means the data should also be filtered by its title string.
   // Done=True means no other filtering is needed.
-  TFilterItemEvent = function (ItemData: TObject; out Done: Boolean): Boolean of object;
-  TFilterItemExEvent = function (ACaption: string; ItemData: TObject;
+  TFilterItemEvent = function (ItemData: Pointer; out Done: Boolean): Boolean of object;
+  TFilterItemExEvent = function (const ACaption: string; ItemData: Pointer;
                                  out Done: Boolean): Boolean of object;
 
   // Can be used only for items that have a checkbox. Returns true if checked.

commit e06fda9bf24faec3b086a136cf668f31e2482cb2

IDE: Fix TEditorKeymappingOptionsFrame.FilterEditFilterItem, was broken by r52745 #85628cd5f9. Issue #30461.
git-svn-id: trunk@52794 -

ide/frames/editor_keymapping_options.pas
@@ -229,16 +229,16 @@ var
 begin
   Done:=True;
   Result:=False;
-  if ItemData=nil then Exit;
-  Assert(TObject(ItemData) is TKeyCommandRelation,
-    'TEditorKeymappingOptionsFrame.FilterEditFilterItem: ItemData is not TKeyCommandRelation.');
-  KeyRel:=TKeyCommandRelation(ItemData);      // Tree item is actual key command.
-  Done:=False;
-  Result:=KeyMapKeyFilter.Key1<>VK_UNKNOWN;
-  if Result then begin                        // Key filter is defined.
-    Done:=True;
-    Result:=(CompareIDEShortCutKey1s(@KeyMapKeyFilter,@KeyRel.ShortcutA)=0)
-         or (CompareIDEShortCutKey1s(@KeyMapKeyFilter,@KeyRel.ShortcutB)=0);
+  if TObject(ItemData) is TKeyCommandRelation then
+  begin
+    KeyRel:=TKeyCommandRelation(ItemData);      // Tree item is actual key command.
+    Done:=False;
+    Result:=KeyMapKeyFilter.Key1<>VK_UNKNOWN;
+    if Result then begin                        // Key filter is defined.
+      Done:=True;
+      Result:=(CompareIDEShortCutKey1s(@KeyMapKeyFilter,@KeyRel.ShortcutA)=0)
+           or (CompareIDEShortCutKey1s(@KeyMapKeyFilter,@KeyRel.ShortcutB)=0);
+    end;
   end;
 end;
 