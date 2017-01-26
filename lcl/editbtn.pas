fpc/Lazarus

commit 48eb046eecde8d7a92740335b12e2dc2b183e3e3

LCL: - Allow NullDate in TDateEdit when user clears the text. - Always set Text to a proper value when leaving the control. Issue #0031217
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@53938 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/editbtn.pas
@@ -565,6 +565,7 @@ type
     function GetDefaultGlyphName: String; override;
     procedure ButtonClick; override;
     procedure EditDblClick; override;
+    procedure EditEditingDone; override;
     procedure SetDirectInput(AValue: Boolean); override;
     procedure RealSetText(const AValue: TCaption); override;
     procedure SetDateMask; virtual;
@@ -1625,11 +1626,25 @@ begin
   if FocusOnButtonClick then FocusAndMaybeSelectAll;
 end;
 
+
 procedure TDateEdit.EditDblClick;
 begin
   inherited EditDblClick;
   if not ReadOnly then
     ButtonClick;
+end;
+
+procedure TDateEdit.EditEditingDone;
+var
+  AText: String;
+begin
+  inherited EditingDone;
+  if DirectInput then
+  begin
+    AText := DateToText(GetDate);
+    if AText <> Text then //avoid unneccesary recalculation FDate
+      Text := AText;
+  end;
 end;
 
 procedure TDateEdit.SetDirectInput(AValue: Boolean);
@@ -1967,10 +1982,20 @@ begin
     Def := FDate;
   ADate := Trim(Text);
   //if not DirectInput then FDate matches the Text, so no need to parse it
-  if (ADate <> '') and DirectInput then
+  if {(ADate <> '') and} DirectInput then
   begin
-    Result := TextToDate(ADate, Def);
-    FDate := Result;
+    if (ADate = '') then
+    begin
+      if FDefaultToday then
+        Result := SysUtils.Date
+      else
+        Result := NullDate;
+    end
+    else
+    begin
+      Result := TextToDate(ADate, Def);
+      FDate := Result;
+    end;
   end
   else
     Result := Def;

commit dd81ed3150dde3fbb5a0986e61cc0c059647c80f

LCL: TDateEdit: fix not calling OnEditingDone (bug introduced in r53938 #afa3922372 by me). Issue #0031278.
git-svn-id: trunk@54010 -

lcl/editbtn.pas
@@ -1638,7 +1638,7 @@ procedure TDateEdit.EditEditingDone;
 var
   AText: String;
 begin
-  inherited EditingDone;
+  inherited EditEditingDone;
   if DirectInput then
   begin
     AText := DateToText(GetDate);