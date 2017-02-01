2310edb3ee525ee01f134c25f3cc39e8705a2c89
michl <michl@lazarus-ide.org>
2017-02-01 19:40:48 +0000

    LCL: TComboBox: Make key input Delphi compatible with AutoDropDown = True. Issue #31247
    git-svn-id: branches/fixes_1_6@54058 -

--- lcl/stdctrls.pp
+++ lcl/stdctrls.pp
@@ -267,6 +267,7 @@ type
     FCanvas: TCanvas;
     FDropDownCount: Integer;
     FDroppedDown: boolean;
+    FDroppingDown: Boolean;
     FItemHeight: integer;
     FItemIndex: integer;
     FItemWidth: integer;
@@ -372,6 +373,7 @@ type
     procedure ClearSelection; //override;
     property CharCase: TEditCharCase read FCharCase write SetCharCase default ecNormal;
     property DroppedDown: Boolean read GetDroppedDown write SetDroppedDown;
+    property DroppingDown: Boolean read FDroppingDown write FDroppingDown;
     procedure SelectAll;
     property AutoComplete: boolean
       read GetAutoComplete write SetAutoComplete default False;
