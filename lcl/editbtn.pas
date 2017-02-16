fpc/Lazarus

commit 48eb046eecde8d7a92740335b12e2dc2b183e3e3

LCL: - Allow NullDate in TDateEdit when user clears the text. - Always set Text to a proper value when leaving the control. Issue #0031217
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@53938 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/editbtn.pas
@@ -565,6 +565,7 @@ type
     function GetDefaultGlyphName: String; override;
     procedure ButtonClick; override;
     procedure EditDblClick; override;
+    procedure EditEditingDone; override;
     procedure SetDirectInput(AValue: Boolean); override;
     procedure RealSetText(const AValue: TCaption); override;
     procedure SetDateMask; virtual;
@@ -1625,11 +1626,25 @@ begin
   if FocusOnButtonClick then FocusAndMaybeSelectAll;
 end;
 
+
 procedure TDateEdit.EditDblClick;
 begin
   inherited EditDblClick;
   if not ReadOnly then
     ButtonClick;
+end;
+
+procedure TDateEdit.EditEditingDone;
+var
+  AText: String;
+begin
+  inherited EditingDone;
+  if DirectInput then
+  begin
+    AText := DateToText(GetDate);
+    if AText <> Text then //avoid unneccesary recalculation FDate
+      Text := AText;
+  end;
 end;
 
 procedure TDateEdit.SetDirectInput(AValue: Boolean);
@@ -1967,10 +1982,20 @@ begin
     Def := FDate;
   ADate := Trim(Text);
   //if not DirectInput then FDate matches the Text, so no need to parse it
-  if (ADate <> '') and DirectInput then
+  if {(ADate <> '') and} DirectInput then
   begin
-    Result := TextToDate(ADate, Def);
-    FDate := Result;
+    if (ADate = '') then
+    begin
+      if FDefaultToday then
+        Result := SysUtils.Date
+      else
+        Result := NullDate;
+    end
+    else
+    begin
+      Result := TextToDate(ADate, Def);
+      FDate := Result;
+    end;
   end
   else
     Result := Def;

commit 4386a6db3375b68858d7f11ed36cb8e174175715

Merged revision(s) 54010 #dd81ed3150, 54025 #739663d456, 54070-54072 #8ce29506c5-#8ce29506c5 from trunk: LCL: TDateEdit: fix not calling OnEditingDone (bug introduced in r53938 #afa3922372 by me). Issue #0031278. ........ FileListBox: - do not display normal files if ftNormal is not specified in FileType (Delphi compatibility) - do not display filenames if FileType only includes ftDirectory or ftVolumeID. Issue #0031281 ........ TDateEdit: Don't change empty Text when toggling DirectInput in designer. Part of Issue #0030425. ........ TStaticText: publish properties Enabled, ParentShowHint and PopupMenu. Issues #0031313, #0031315 and #0031333. ........ TTimeEdit: publish properties ButtonCaption, ButtonCursor and ButtonHint. Issue #0031338. ........
git-svn-id: branches/fixes_1_6@54179 -

lcl/editbtn.pas
@@ -786,6 +786,7 @@ type
     procedure SetDirectInput(AValue: Boolean); override;
     procedure SetText(AValue: TCaption); override;
     procedure SetDateMask; virtual;
+    procedure Loaded; override;
   public
     constructor Create(AOwner: TComponent); override;
     function GetDateFormat: string;
@@ -905,6 +906,9 @@ type
       property OnAcceptTime: TAcceptTimeEvent read FOnAcceptTime write FOnAcceptTime;
       property OnCustomTime: TCustomTimeEvent read FOnCustomTime write FOnCustomTime;
       property ReadOnly;
+      property ButtonCaption;
+      property ButtonCursor;
+      property ButtonHint;
       property ButtonOnlyWhenFocused;
       property ButtonWidth;
       property Action;
@@ -2558,7 +2562,7 @@ procedure TDateEdit.EditEditingDone;
 var
   AText: String;
 begin
-  inherited EditingDone;
+  inherited EditEditingDone;
   if DirectInput then
   begin
     AText := DateToText(GetDate);
@@ -2572,9 +2576,11 @@ var
   Def: TDateTime;
 begin
   inherited SetDirectInput(AValue);
-  //Synchronize FDate and force valid text
+  //Synchronize FDate
   FDate := TextToDate(Text, NullDate);
-  SetDate(FDate);
+  //Force a valid date in the control, but not if Text was empty in designmode
+  if not ((csDesigning in ComponentState) and FDefaultToday and (FDate = NullDate)) then
+    SetDate(FDate);
 end;
 
 procedure TDateEdit.SetText(AValue: TCaption);
@@ -2626,6 +2632,14 @@ begin
   D:=GetDate;
   EditMask:=S;
   SetDate(D);
+end;
+
+procedure TDateEdit.Loaded;
+begin
+  inherited Loaded;
+  //Forces a valid Text in the control
+  if not (csDesigning in ComponentState) then
+    SetDate(FDate);
 end;
 
 Function ParseDate(S : String; Order : TDateOrder; Def: TDateTime) : TDateTime;

lcl/filectrl.pp
@@ -291,14 +291,17 @@ end;
 
 procedure TCustomFileListBox.UpdateFileList;
 const
-  AttrNotNormal = faReadOnly or
+  FileTypeFilesOnly = [ftReadOnly, ftHidden, ftSystem, ftArchive, ftNormal];
+  {AttrNotNormal = faReadOnly or
                   faHidden or
                   faSysFile or
                   faVolumeID or
-                  faDirectory or
-                  faArchive;
+                  faDirectory{ or
+                  faArchive};
+  }
 var
   Info: TSearchRec;
+  FileAttr: LongInt;
 
   function FileTypeToFileAttribute(FileType: TFileType): LongInt;
   const
@@ -327,19 +330,23 @@ begin
   Clear;
   if FileType <> [] then
   begin
+    FileAttr := FileTypeToFileAttribute(FileType);
     if FindFirstUTF8(
       IncludeTrailingPathDelimiter(FDirectory)+AllDirectoryEntriesMask,
-      FileTypeToFileAttribute(FileType), Info) = 0
+      FileAttr, Info) = 0
     then
       repeat
         if MatchesMaskList(Info.Name,Mask) then
         begin
-          if (ftNormal in FileType) or ((Info.Attr and AttrNotNormal) > 0) then
+          if (ftNormal in FileType) or ((Info.Attr and FileAttr {AttrNotNormal}) > 0) then
           begin
             if (Info.Attr and faDirectory) > 0 then
               Items.Add('['+Info.Name+']')
             else
-              Items.Add(Info.Name);
+            begin
+              if (FileType * FileTypeFilesOnly <> []) then //don't add files if no file attribute is specified
+                Items.Add(Info.Name);
+            end;
           end;
         end;
       until FindNextUTF8(Info) <> 0;

lcl/stdctrls.pp
@@ -1054,6 +1054,7 @@ type
     property DragCursor;
     property DragKind;
     property DragMode;
+    property Enabled;
     property FocusControl;
     property Font;
     property OnChangeBounds;
@@ -1076,6 +1077,8 @@ type
     property ParentBidiMode;
     property ParentFont;
     property ParentColor;
+    property ParentShowHint;
+    property PopupMenu;
     property ShowAccelChar;
     property ShowHint;
     property TabOrder;