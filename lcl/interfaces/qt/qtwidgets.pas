fpc/Lazarus

commit 346f43ad8a4ec687700d0e421f112278ee488aac

Qt, Qt5: fixed triggering of selection change in TCheckListBox. issue #31697
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@54696 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/qt/qtwidgets.pas
@@ -13848,6 +13848,23 @@ function TQtCheckListBox.EventFilter(Sender: QObjectH; Event: QEventH
   ): Boolean; cdecl;
 begin
   Result := False;
+  if (QEvent_type(Event) = QEventKeyPress) and
+    (QtVersionMajor = 4) and (QtVersionMinor >= 8) and
+     ((QKeyEvent_key(QKeyEventH(Event)) = QtKey_Up) or
+     (QKeyEvent_key(QKeyEventH(Event)) = QtKey_Down)) then
+  begin
+    {issue #31697}
+    Result:=inherited EventFilter(Sender, Event);
+    if ItemCount > 0 then
+    begin
+      if (getSelCount = 0) then
+      begin
+        Selected[0] := True;
+        Result := True;
+      end;
+      inherited signalSelectionChanged();
+    end;
+  end else
   if (QEvent_type(Event) = QEventMouseButtonDblClick) then
     // issue #25089
   else
@@ -13905,7 +13922,10 @@ begin
             if Assigned(Item) and (currentItem <> Item) then
             begin
               // DebugLn('TQtCheckListBox forced item change');
-              Self.setCurrentItem(Item, True);
+              // Self.setCurrentItem(Item, True);
+              {issue #31697}
+              QListWidget_setCurrentItem(QListWidgetH(Widget), Item, QItemSelectionModelSelectCurrent);
+              inherited signalSelectionChanged();
             end;
           end;
         end;
@@ -13959,6 +13979,11 @@ begin
   Msg.Msg := LM_CHANGED;
   Msg.WParam := PtrInt(QListWidget_row(QListWidgetH(Widget), Item));
   DeliverMessage(Msg);
+  if (QtVersionMajor = 4) and (QtVersionMinor >= 8) then
+  begin
+    QListWidget_setCurrentItem(QListWidgetH(Widget), Item, QItemSelectionModelSelectCurrent);
+    inherited signalSelectionChanged();
+  end;
 end;
 
 function TQtCheckListBox.GetItemCheckState(AIndex: Integer): QtCheckState;

lcl/interfaces/qt5/qtwidgets.pas
@@ -13824,6 +13824,22 @@ function TQtCheckListBox.EventFilter(Sender: QObjectH; Event: QEventH
   ): Boolean; cdecl;
 begin
   Result := False;
+  if (QEvent_type(Event) = QEventKeyPress) and
+     ((QKeyEvent_key(QKeyEventH(Event)) = QtKey_Up) or
+     (QKeyEvent_key(QKeyEventH(Event)) = QtKey_Down)) then
+  begin
+    {issue #31697}
+    Result:=inherited EventFilter(Sender, Event);
+    if ItemCount > 0 then
+    begin
+      if (getSelCount = 0) then
+      begin
+        Selected[0] := True;
+        Result := True;
+      end;
+      inherited signalSelectionChanged();
+    end;
+  end else
   if (QEvent_type(Event) = QEventMouseButtonDblClick) then
     // issue #25089
   else
@@ -13882,7 +13898,9 @@ begin
             if Assigned(Item) and (currentItem <> Item) then
             begin
               // DebugLn('TQtCheckListBox forced item change');
-              Self.setCurrentItem(Item, True);
+              {issue #31697}
+              QListWidget_setCurrentItem(QListWidgetH(Widget), Item, QItemSelectionModelSelectCurrent);
+              inherited signalSelectionChanged();
             end;
           end;
         end;
@@ -13936,6 +13954,8 @@ begin
   Msg.Msg := LM_CHANGED;
   Msg.WParam := PtrInt(QListWidget_row(QListWidgetH(Widget), Item));
   DeliverMessage(Msg);
+  QListWidget_setCurrentItem(QListWidgetH(Widget), Item, QItemSelectionModelSelectCurrent);
+  inherited signalSelectionChanged();
 end;
 
 function TQtCheckListBox.GetItemCheckState(AIndex: Integer): QtCheckState;

commit 8a85928884f7984dd5deadf4db941a601e9864a1

Merged revision(s) 54802 #ef647d8160 from trunk: Qt,Qt5: fixed regression from r54696 #d76aa4f29a. issue #31697 ........
git-svn-id: branches/fixes_1_8@54805 -

lcl/interfaces/qt/qtwidgets.pas
@@ -13999,11 +13999,6 @@ begin
   Msg.Msg := LM_CHANGED;
   Msg.WParam := PtrInt(QListWidget_row(QListWidgetH(Widget), Item));
   DeliverMessage(Msg);
-  if (QtVersionMajor = 4) and (QtVersionMinor >= 8) then
-  begin
-    QListWidget_setCurrentItem(QListWidgetH(Widget), Item, QItemSelectionModelSelectCurrent);
-    inherited signalSelectionChanged();
-  end;
 end;
 
 function TQtCheckListBox.GetItemCheckState(AIndex: Integer): QtCheckState;

lcl/interfaces/qt5/qtwidgets.pas
@@ -13971,8 +13971,6 @@ begin
   Msg.Msg := LM_CHANGED;
   Msg.WParam := PtrInt(QListWidget_row(QListWidgetH(Widget), Item));
   DeliverMessage(Msg);
-  QListWidget_setCurrentItem(QListWidgetH(Widget), Item, QItemSelectionModelSelectCurrent);
-  inherited signalSelectionChanged();
 end;
 
 function TQtCheckListBox.GetItemCheckState(AIndex: Integer): QtCheckState;