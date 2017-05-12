c5cf5937973aa4a0f1fb62ba015dbbd4f523f14d
wp <wp.no-reply@lazarus-ide-org>
2017-05-12 16:19:10 +0000

    Examples: Fix crash of demo "propstorage" (http://forum.lazarus.freepascal.org/index.php/topic,36862.msg246019/topicseen.html).
    git-svn-id: trunk@54881 -

--- examples/propstorage/unit1.pas
+++ examples/propstorage/unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Forms, StdCtrls, XMLPropStorage, IniPropStorage;
+  Forms, StdCtrls, XMLPropStorage, IniPropStorage, Classes;
 
 type
 
@@ -15,6 +15,7 @@ type
     IniPropStorage1: TIniPropStorage;
     Memo1: TMemo;
     XMLPropStorage1: TXMLPropStorage;
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -27,6 +28,15 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Memo1.Lines.Add('Enter some text here.');
+  Memo1.Lines.Add('At exit, it will be stored by TIniPropStorage and TXmlPropStorage');
+  Memo1.Lines.Add('along with the form''s width and height.');
+end;
 
 initialization
 
