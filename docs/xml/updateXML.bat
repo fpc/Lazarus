4ff46702898cb0590761dad9956ea96554fcda73
mattias <nc-gaertnma@netcologne.de>
2017-06-02 20:43:25 +0000

    docs: build_lcl_docs: --footer
    git-svn-id: branches/fixes_1_8@55170 -

--- docs/xml/updateXML.bat
+++ docs/xml/updateXML.bat
@@ -1,3 +1,4 @@
+rem BEWARE: makeskel 3.1.1 cannot update ndoes. The --update flag is ignored and always creates all nodes.
 rem Call makeskel for the given LCL module <%1> . <%2>
 rem PATH=\repos\fpc-2.5.trunk\utils\fpdoc
 makeskel --package=lcl --input="..\..\lcl\%1.%2 -Fi..\..\lcl\include" --descr=lcl\%1.xml --update --output=lcl\%1.upd.xml
