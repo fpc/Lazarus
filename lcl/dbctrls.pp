fpc/Lazarus

commit 1e99b3e312e62e09c38fe214bc20d1d1869a6454

LCL: Update DataLink better when TDbComboBox item changes. Issue #32383, patch from Stephanie.
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@55894 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/include/customdbcombobox.inc
@@ -35,7 +35,14 @@ end;
 procedure TCustomDBComboBox.Change;
 begin
   FDataLink.Modified;
-  inherited Change;
+  try
+    if FDataLink.CanModify then begin
+      FDataLink.Field.AsString := Text;
+      FDatalink.Modified;
+    end;
+  finally
+    inherited Change;
+  end;
 end;
 
 function TCustomDBComboBox.GetReadOnly: Boolean;

commit 1644bea3b3a1573009ea6332cb2b0e8ebccf264d

LCL: Fix a regression in TDBLookupComboBox caused by r55894 #14e5f42ff3. Issue #32463, patch from Michal Gawrycki.
git-svn-id: trunk@55912 -

lcl/dbctrls.pp
@@ -707,6 +707,7 @@ Type
 
   TDBComboBox = class(TCustomDBComboBox)
   protected
+    procedure Change; override;
     procedure DataChange(Sender: TObject); override;
     procedure KeyDown(var Key: Word; Shift: TShiftState); override;
     procedure KeyPress(var Key: char); override;

lcl/include/customdbcombobox.inc
@@ -34,15 +34,9 @@ end;
 
 procedure TCustomDBComboBox.Change;
 begin
-  FDataLink.Modified;
-  try
-    if FDataLink.CanModify then begin
-      FDataLink.Field.AsString := Text;
-      FDatalink.Modified;
-    end;
-  finally
-    inherited Change;
-  end;
+  if FDataLink.CanModify then
+    FDataLink.Modified;
+  inherited Change;
 end;
 
 function TCustomDBComboBox.GetReadOnly: Boolean;

lcl/include/dbcombobox.inc
@@ -17,6 +17,18 @@ begin
   FDataLink.Field.Text := Text;
 end;
 
+procedure TDBComboBox.Change;
+begin
+  try
+    if FDataLink.CanModify then begin
+      FDataLink.Field.AsString := Text;
+      FDatalink.Modified;
+    end;
+  finally
+    inherited Change;
+  end;
+end;
+
 procedure TDBComboBox.DataChange(Sender: TObject);
 var
   DataLinkField: TField;