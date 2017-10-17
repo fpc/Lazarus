56b0d79860562c992e34a0851d6dc6cb51c6e654
mattias <nc-gaertnma@netcologne.de>
2017-10-17 16:37:23 +0000

    leakview: fixed layout
    git-svn-id: branches/fixes_1_8@56111 -

--- components/leakview/heaptrcview.pas
+++ components/leakview/heaptrcview.pas
@@ -5,14 +5,14 @@ unit HeapTrcView;
 interface
 
 uses
-  Classes, SysUtils, XMLConf, contnrs, Clipbrd, LCLProc,
+  Classes, SysUtils, XMLConf, contnrs, Clipbrd, LCLProc, LCLType,
   LResources, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls, ExtCtrls,
   // LazUtils
   FileUtil, LazFileUtils,
   // IDEIntf
-  LazIDEIntf, MenuIntf,
+  LazIDEIntf, MenuIntf, ToolBarIntf, IDECommands,
   // LeakView
-  LeakInfo, IDECommands, ToolBarIntf, LCLType;
+  LeakInfo;
 
 type
   TJumpProc = procedure (Sender: TObject; const SourceName: string;
