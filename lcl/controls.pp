8b3759c88e135747824dbeb3ab8c2198dbc61d1f
juha <juha@lazarus-ide.org>
2017-10-18 10:14:55 +0000

    LCL-GTK2: Support 5 button mice. Issue #32562, patch from accorp.
    git-svn-id: trunk@56122 -

--- lcl/controls.pp
+++ lcl/controls.pp
@@ -3107,6 +3107,7 @@ const
 var
   IsMultiClick: boolean;
   TargetControl: TControl;
+  Button: Byte;
 begin
   Result := LM_NULL;
 
@@ -3159,10 +3160,16 @@ begin
   end;
   LastMouse.Down := AMouseDown;
 
-  if AMouseDown then
-    Result := MSGKINDDOWN[AButton][LastMouse.ClickCount]
+  // mouse buttons 4,5 share same messages
+  if AButton = 5 then
+    Button := 4
   else
-    Result := MSGKINDUP[AButton];
+    Button := AButton;
+
+  if AMouseDown then
+    Result := MSGKINDDOWN[Button][LastMouse.ClickCount]
+  else
+    Result := MSGKINDUP[Button];
 end;
 
 function GetKeyShiftState: TShiftState;
