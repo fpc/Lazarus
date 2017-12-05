82c8f6744571a2268b415b0a9027c1f8a37ab7f2
mattias <nc-gaertnma@netcologne.de>
2017-12-05 22:06:36 +0000

    pas2js: added pas2jsdsgn.lpk
    git-svn-id: trunk@56646 -

---
+++ components/pas2js/fpmake.pp
@@ -0,0 +1,59 @@
+{
+   File generated automatically by Lazarus Package Manager
+
+   fpmake.pp for Pas2JSDsgn 1.0.1
+
+   This file was generated on 05.12.2017
+}
+
+{$ifndef ALLPACKAGES} 
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+{$endif ALLPACKAGES}
+
+procedure add_Pas2JSDsgn(const ADirectory: string);
+
+var
+  P : TPackage;
+  T : TTarget;
+
+begin
+  with Installer do
+    begin
+    P:=AddPackage('pas2jsdsgn');
+    P.Version:='1.0.1';
+
+    P.Directory:=ADirectory;
+
+    P.Flags.Add('LazarusDsgnPkg');
+
+    P.Dependencies.Add('ideintf');
+    P.Options.Add('-MObjFPC');
+    P.Options.Add('-Schi');
+    P.Options.Add('-O1');
+    P.Options.Add('-g');
+    P.Options.Add('-gl');
+    P.Options.Add('-l');
+    P.Options.Add('-vewnhibq');
+    P.Options.Add('-dLCL');
+    P.Options.Add('-dLCL$(LCLWidgetType)');
+    P.UnitPath.Add('.');
+    T:=P.Targets.AddUnit('pas2jsdsgn.pas');
+    t.Dependencies.AddUnit('pjsdsgnregister');
+
+    T:=P.Targets.AddUnit('pjsdsgnregister.pas');
+
+    // copy the compiled file, so the IDE knows how the package was compiled
+    P.InstallFiles.Add('Pas2JSDsgn.compiled',AllOSes,'$(unitinstalldir)');
+
+    end;
+end;
+
+{$ifndef ALLPACKAGES}
+begin
+  add_Pas2JSDsgn('');
+  Installer.Run;
+end.
+{$endif ALLPACKAGES}
---
+++ components/pas2js/pjsdsgnregister.pas
@@ -0,0 +1,206 @@
+unit PJSDsgnRegister;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, ProjectIntf, CompOptsIntf, LazIDEIntf, Forms, Controls;
+
+const
+  ProjDescNamePas2JSWebApp = 'Web Application';
+  ProjDescNamePas2JSNodeJSApp = 'NodeJS Application';
+
+resourcestring
+  pjsdWebApplication = 'Web Application';
+  pjsdWebAppDescription = 'A pas2js program running in the browser';
+  pjsdNodeJSApplication = 'Node.js Application';
+  pjsdNodeJSAppDescription = 'A pas2js program running in node.js';
+
+type
+
+  { TProjectPas2JSWebApp }
+
+  TProjectPas2JSWebApp = class(TProjectDescriptor)
+  public
+    constructor Create; override;
+    function GetLocalizedName: string; override;
+    function GetLocalizedDescription: string; override;
+    function InitProject(AProject: TLazProject): TModalResult; override;
+    function CreateStartFiles(AProject: TLazProject): TModalResult; override;
+  end;
+
+  { TProjectPas2JSNodeJSApp }
+
+  TProjectPas2JSNodeJSApp = class(TProjectDescriptor)
+  public
+    constructor Create; override;
+    function GetLocalizedName: string; override;
+    function GetLocalizedDescription: string; override;
+    function InitProject(AProject: TLazProject): TModalResult; override;
+    function CreateStartFiles(AProject: TLazProject): TModalResult; override;
+  end;
+
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+  RegisterProjectDescriptor(TProjectPas2JSWebApp.Create);
+  RegisterProjectDescriptor(TProjectPas2JSNodeJSApp.Create);
+end;
+
+{ TProjectPas2JSNodeJSApp }
+
+constructor TProjectPas2JSNodeJSApp.Create;
+begin
+  inherited Create;
+  Name:=ProjDescNamePas2JSWebApp;
+  Flags:=DefaultProjectNoApplicationFlags-[pfRunnable];
+end;
+
+function TProjectPas2JSNodeJSApp.GetLocalizedName: string;
+begin
+  Result:=pjsdNodeJSApplication;
+end;
+
+function TProjectPas2JSNodeJSApp.GetLocalizedDescription: string;
+begin
+  Result:=pjsdNodeJSAppDescription;
+end;
+
+function TProjectPas2JSNodeJSApp.InitProject(AProject: TLazProject
+  ): TModalResult;
+var
+  MainFile: TLazProjectFile;
+  CompOpts: TLazCompilerOptions;
+  NewSource: String;
+begin
+  Result:=inherited InitProject(AProject);
+
+  MainFile:=AProject.CreateProjectFile('project1.lpr');
+  MainFile.IsPartOfProject:=true;
+  AProject.AddFile(MainFile,false);
+  AProject.MainFileID:=0;
+
+  CompOpts:=AProject.LazCompilerOptions;
+  CompOpts.TargetFilename:='program1';
+  CompOpts.Win32GraphicApp:=false;
+  CompOpts.UnitOutputDirectory:='js';
+  CompOpts.TargetFilename:='project1';
+  CompOpts.SetAlternativeCompile(
+    '$MakeExe(pas2js) -Jc -Jminclude -Tnodejs "-Fu$(ProjUnitPath)" $Name($(ProjFile))',true);
+
+  // create program source
+  NewSource:='program Project1;'+LineEnding
+    +LineEnding
+    +'{$mode objfpc}'+LineEnding
+    +LineEnding
+    +'uses'+LineEnding
+    +'  NodeJS, JS, Classes, SysUtils;'+LineEnding
+    +LineEnding
+    +'begin'+LineEnding
+    +'end.'+LineEnding
+    +LineEnding;
+  AProject.MainFile.SetSourceText(NewSource,true);
+
+  AProject.AddPackageDependency('pas2js_rtl');
+end;
+
+function TProjectPas2JSNodeJSApp.CreateStartFiles(AProject: TLazProject
+  ): TModalResult;
+begin
+  Result:=LazarusIDE.DoOpenEditorFile(AProject.MainFile.Filename,-1,-1,
+                                      [ofProjectLoading,ofRegularFile]);
+end;
+
+{ TProjectPas2JSWebApp }
+
+constructor TProjectPas2JSWebApp.Create;
+begin
+  inherited Create;
+  Name:=ProjDescNamePas2JSWebApp;
+  Flags:=DefaultProjectNoApplicationFlags-[pfRunnable];
+end;
+
+function TProjectPas2JSWebApp.GetLocalizedName: string;
+begin
+  Result:=pjsdWebApplication;
+end;
+
+function TProjectPas2JSWebApp.GetLocalizedDescription: string;
+begin
+  Result:=pjsdWebAppDescription;
+end;
+
+function TProjectPas2JSWebApp.InitProject(AProject: TLazProject): TModalResult;
+var
+  MainFile, HTMLFile: TLazProjectFile;
+  CompOpts: TLazCompilerOptions;
+  NewSource: String;
+begin
+  Result:=inherited InitProject(AProject);
+
+  MainFile:=AProject.CreateProjectFile('project1.lpr');
+  MainFile.IsPartOfProject:=true;
+  AProject.AddFile(MainFile,false);
+  AProject.MainFileID:=0;
+
+  CompOpts:=AProject.LazCompilerOptions;
+  CompOpts.TargetFilename:='program1';
+  CompOpts.Win32GraphicApp:=false;
+  CompOpts.UnitOutputDirectory:='js';
+  CompOpts.TargetFilename:='project1';
+  CompOpts.SetAlternativeCompile(
+    '$MakeExe(pas2js) -Jc -Jminclude -Tbrowser "-Fu$(ProjUnitPath)" $Name($(ProjFile))',true);
+
+  // create program source
+  NewSource:='program Project1;'+LineEnding
+    +LineEnding
+    +'{$mode objfpc}'+LineEnding
+    +LineEnding
+    +'uses'+LineEnding
+    +'  JS, Classes, SysUtils, Web;'+LineEnding
+    +LineEnding
+    +'begin'+LineEnding
+    +'end.'+LineEnding
+    +LineEnding;
+  AProject.MainFile.SetSourceText(NewSource,true);
+
+  HTMLFile:=AProject.CreateProjectFile('project1.html');
+  HTMLFile.IsPartOfProject:=true;
+  AProject.AddFile(HTMLFile,false);
+  // create html source
+  NewSource:=
+     '<!doctype html>'+LineEnding
+    +'<html lang="en">'+LineEnding
+    +'<head>'+LineEnding
+    +'  <meta charset="utf-8">'+LineEnding
+    +'  <title>Project1</title>'+LineEnding
+    +'<style>'+LineEnding
+    +'<script src="project1.js"></script>'+LineEnding
+    +'</head>'+LineEnding
+    +'<div>Project1</div>'+LineEnding
+    +'<script>'+LineEnding
+    +'  rtl.run();'+LineEnding
+    +'</script>'+LineEnding
+    +'<body>'+LineEnding;
+  HTMLFile.SetSourceText(NewSource);
+
+  AProject.AddPackageDependency('pas2js_rtl');
+end;
+
+function TProjectPas2JSWebApp.CreateStartFiles(AProject: TLazProject
+  ): TModalResult;
+begin
+  Result:=LazarusIDE.DoOpenEditorFile(AProject.MainFile.Filename,-1,-1,
+                                      [ofProjectLoading,ofRegularFile]);
+  if Result<>mrOK then exit;
+  Result:=LazarusIDE.DoOpenEditorFile('project1.html',-1,-1,
+                                      [ofProjectLoading,ofRegularFile]);
+end;
+
+end.
+
