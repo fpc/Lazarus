d6ff18f2830caa925f4638df56c3127e8fd7c1db
michael <michael@lazarus-ide.org>
2017-12-17 14:09:26 +0000

    * Add dialog for node js project options
    git-svn-id: trunk@56768 -

---
+++ components/pas2js/frmpas2jsnodejsprojectoptions.pp
@@ -0,0 +1,26 @@
+unit frmpas2jsnodejsprojectoptions;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm2 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
