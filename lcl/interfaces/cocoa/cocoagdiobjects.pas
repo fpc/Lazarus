32000402a9adcd9f1dc6325bff72618b88b06a3a
dmitry <dmitry@lazarus-ide.org>
2018-01-02 23:38:16 +0000

    cocoa: simplified 1 pixel drawn code
    git-svn-id: trunk@56926 -

--- lcl/interfaces/cocoa/cocoagdiobjects.pas
+++ lcl/interfaces/cocoa/cocoagdiobjects.pas
@@ -1719,8 +1719,8 @@ begin
   if (absDeltaX<=1) and (absDeltaY<=1) then
   begin
     // special case for 1-pixel lines
-    tx := bx + 0.05 * sign(deltaX) * absDeltaX;
-    ty := by + 0.05 * sign(deltay) * absDeltaY;
+    tx := bx + 0.05 * deltaX;
+    ty := by + 0.05 * deltay;
   end
   else
   begin
