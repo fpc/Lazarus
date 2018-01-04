c1697fc2eff62386a7940f85089a2fcb95087ad8
ondrej <ondrej@lazarus-ide.org>
2018-01-04 05:06:10 +0000

    win32: support teTrackBar in TWin32ThemeServices.GetDetailSize. Issue #32437, patch by Lagunov Aleksey
    git-svn-id: trunk@56945 -

--- lcl/interfaces/win32/win32themes.pas
+++ lcl/interfaces/win32/win32themes.pas
@@ -174,7 +174,8 @@ begin
        Result.cx := MulDiv(12, ScreenInfo.PixelsPerInchX, 96)
     else
     if ((Details.Element = teTreeview) and (Details.Part in [TVP_GLYPH, TVP_HOTGLYPH])) or
-       ((Details.Element = teWindow) and (Details.Part in [WP_SMALLCLOSEBUTTON])) then
+       ((Details.Element = teWindow) and (Details.Part in [WP_SMALLCLOSEBUTTON])) or
+       (Details.Element = teTrackBar) then
     begin
       R := Rect(0, 0, 800, 800);
       GetThemePartSize(GetTheme(Details.Element), 0, Details.Part, Details.State, @R, TS_TRUE, Result);
