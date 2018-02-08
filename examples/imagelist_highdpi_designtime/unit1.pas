0e38359be503029dc081e2267e00ab2fe4a3fa6b
wp <wp.no-reply@lazarus-ide-org>
2018-02-08 15:32:42 +0000

    Examples: Extend imagelist_hidpi_designtime sample to switch between small and large images usage in the listview.
    git-svn-id: trunk@57273 -

--- examples/imagelist_highdpi_designtime/unit1.pas
+++ examples/imagelist_highdpi_designtime/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ImgList,
-  Menus;
+  Menus, ExtCtrls;
 
 type
 
@@ -21,6 +21,7 @@ type
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
     PageControl1: TPageControl;
+    RadioGroup1: TRadioGroup;
     TabSheet1: TTabSheet;
     ToolBar1: TToolBar;
     ToolButton1: TToolButton;
@@ -28,6 +29,7 @@ type
     TreeView1: TTreeView;
     procedure ImageList1GetWidthForPPI(Sender: TCustomImageList; AImageWidth,
       APPI: Integer; var AResultWidth: Integer);
+    procedure RadioGroup1Click(Sender: TObject);
   private
 
   public
@@ -43,12 +45,21 @@ implementation
 
 { TForm1 }
 
+// Sharing the same ImageList for SmallImages and LargeImages of a ListView
 procedure TForm1.ImageList1GetWidthForPPI(Sender: TCustomImageList;
   AImageWidth, APPI: Integer; var AResultWidth: Integer);
 begin
   case AResultWidth of
     20..24: AResultWidth:=22;
     30..36: AResultWidth:=32;
+  end;
+end;
+
+procedure TForm1.RadioGroup1Click(Sender: TObject);
+begin
+  case Radiogroup1.ItemIndex of
+    0: Listview1.ViewStyle := vsIcon;
+    1: Listview1.ViewStyle := vsSmallIcon;
   end;
 end;
 
