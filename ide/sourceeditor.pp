584fd52fcd96152e33c4cad55c6cbbf925f0b639
ondrej <ondrej@lazarus-ide.org>
2018-03-22 16:24:04 +0000

    IDE: identifier completion window: fix system colors
    git-svn-id: trunk@57554 -

--- ide/sourceeditor.pp
+++ ide/sourceeditor.pp
@@ -2405,11 +2405,11 @@ begin
     end;
     Font.Style:=[];
   end;
-  Colors.BackgroundColor := FActiveEditBackgroundColor;
-  Colors.BackgroundSelectedColor := FActiveEditBackgroundSelectedColor;
-  Colors.TextColor := FActiveEditTextColor;
-  Colors.TextSelectedColor := FActiveEditTextSelectedColor;
-  Colors.TextHighLightColor := FActiveEditTextHighLightColor;
+  Colors.BackgroundColor := ColorToRGB(FActiveEditBackgroundColor);
+  Colors.BackgroundSelectedColor := ColorToRGB(FActiveEditBackgroundSelectedColor);
+  Colors.TextColor := ColorToRGB(FActiveEditTextColor);
+  Colors.TextSelectedColor := ColorToRGB(FActiveEditTextSelectedColor);
+  Colors.TextHighLightColor := ColorToRGB(FActiveEditTextHighLightColor);
   MaxX:=TheForm.ClientWidth;
   t:=CurrentCompletionType;
   if Manager.ActiveCompletionPlugin<>nil then
