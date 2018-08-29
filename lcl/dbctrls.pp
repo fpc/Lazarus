fpc/Lazarus

commit 27684af084b1d9d3aae93de7671c9c4a99c022bb

LCL: fix dbcombobox editing, issue #33164
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@58686 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/dbctrls.pp
@@ -34,7 +34,7 @@ uses
   // LazUtils
   LazTracer, LazUtilities,
   // LCL
-  LCLStrConsts, LMessages, LCLType, LResources, GraphType, Controls, Graphics,
+  LCLStrConsts, LMessages, LCLType, LCLIntf, LResources, GraphType, Controls, Graphics,
   Dialogs, StdCtrls, Buttons, MaskEdit, ExtCtrls, Calendar, ImgList;
 
 Type
@@ -714,6 +714,7 @@ Type
   TCustomDBComboBox = class(TCustomComboBox)
   private
     FDataLink: TFieldDataLink;
+    FDetectedEvents: Word;
     function GetDataField: string;
     function GetDataSource: TDataSource;
     function GetField: TField;
@@ -723,7 +724,12 @@ Type
     procedure SetReadOnly(const AValue: Boolean);
     procedure CMGetDataLink(var Message: TLMessage); message CM_GETDATALINK;
   protected
+    function DoEdit: boolean;
+    procedure DoOnCloseUp;
+    procedure LMDeferredEdit(var Message: TLMessage); message LM_DEFERREDEDIT;
+  protected
     procedure CloseUp; override;
+    Procedure Select; override;
     procedure DataChange(Sender: TObject); virtual; abstract;
     procedure Notification(AComponent: TComponent; Operation: TOperation); override;
     procedure Change; override;
@@ -748,11 +754,9 @@ Type
 
   TDBComboBox = class(TCustomDBComboBox)
   protected
-    procedure Change; override;
     procedure DataChange(Sender: TObject); override;
     procedure KeyDown(var Key: Word; Shift: TShiftState); override;
     procedure KeyPress(var Key: char); override;
-    procedure Select; override;
     procedure UpdateData(Sender: TObject); override;
   published
     property Align;

lcl/include/customdbcombobox.inc
@@ -15,6 +15,11 @@
 
 // included by dbctrls.pp
 
+CONST
+  DBCBEVENT_CHANGE   = 1;   // Detected change event
+  DBCBEVENT_SELECT   = 2;   // Detected select event
+  DBCBEVENT_CLOSEUP  = 4;   // Detected closeup event
+
 {TCustomDBComboBox}
 
 function TCustomDBComboBox.GetDataField: string;
@@ -34,13 +39,76 @@ end;
 
 procedure TCustomDBComboBox.Change;
 begin
-  FDataLink.Modified;
-  inherited Change;
+  FDetectedEvents := FDetectedEvents or DBCBEVENT_CHANGE;
+  PostMessage(Handle, LM_DEFERREDEDIT, 0, 0);
 end;
 
 function TCustomDBComboBox.GetReadOnly: Boolean;
 begin
   Result:=FDataLink.ReadOnly;
+end;
+
+function TCustomDBComboBox.DoEdit: boolean;
+var
+  oldDataChange: TNotifyEvent;
+
+  procedure RestoreDataChange;
+  begin
+    FDataLink.OnDataChange := oldDataChange;
+    if not result then begin
+      FDatalink.Reset;
+      SelectAll;
+    end;
+  end;
+
+begin
+
+  oldDataChange := FDataLink.OnDataChange;
+  FDataLink.OnDataChange := nil;
+
+  try
+    try
+
+      result := FDatalink.Edit;
+      RestoreDataChange;
+
+      if result then begin
+        FDatalink.Modified;
+        if FDetectedEvents and DBCBEVENT_CHANGE <> 0 then
+          inherited Change;
+        if FDetectedEvents and DBCBEVENT_SELECT <> 0 then
+          inherited Select;
+      end;
+      if FDetectedEvents and DBCBEVENT_CLOSEUP <> 0 then
+        DoOnCloseUp;
+
+    except
+      on E: Exception do begin
+
+        result := false;
+        RestoreDataChange;
+        if FDetectedEvents and DBCBEVENT_CLOSEUP <> 0 then
+          DoOnCloseUp;
+
+        raise;
+      end;
+    end;
+
+  finally
+    FDetectedEvents := 0;
+  end;
+end;
+
+procedure TCustomDBComboBox.DoOnCloseUp;
+begin
+  if Assigned(OnCloseUp) then OnCloseUp(Self);
+  if AutoSelect then
+  begin
+    SelectAll;
+    if (SelText = Text) then
+      AutoSelected := True;
+  end;//End if FAutoSelect
+  FDetectedEvents := 0;   // reset because closeup may occur without editing
 end;
 
 procedure TCustomDBComboBox.SetDataField(const AValue: string);
@@ -64,12 +132,23 @@ begin
   Message.Result := PtrUInt(FDataLink);
 end;
 
+procedure TCustomDBComboBox.LMDeferredEdit(var Message: TLMessage);
+begin
+  DoEdit;
+end;
+
 procedure TCustomDBComboBox.CloseUp;
 begin
   if [csLoading,csDestroying,csDesigning]*ComponentState<>[] then exit;
-  if not ReadOnly then
-    EditingDone;
- inherited CloseUp;
+  if FDetectedEvents and DBCBEVENT_CHANGE = 0 then
+    DoOnCloseUp
+  else
+    FDetectedEvents := FDetectedEvents or DBCBEVENT_CLOSEUP;
+end;
+
+procedure TCustomDBComboBox.Select;
+begin
+  FDetectedEvents := FDetectedEvents or DBCBEVENT_SELECT;
 end;
 
 procedure TCustomDBComboBox.Notification(AComponent: TComponent; Operation: TOperation);
@@ -83,7 +162,8 @@ end;
 
 procedure TCustomDBComboBox.EditingDone;
 begin
-  FDataLink.UpdateRecord;
+  if FDatalink.Editing and FDatalink.IsModified then
+    FDataLink.UpdateRecord;
   inherited EditingDone;
 end;
 
@@ -95,16 +175,7 @@ begin
     LM_PASTE:
       begin
         if FDataLink.CanModify then
-        begin
-          //LCL changes the Text before LM_PASTE is called and not after like Delphi. Issue 20330
-          //When Edit is called the Text property is reset to the previous value
-          //Add a workaround while bug is not fixed
-          FDataLink.OnDataChange := nil;
-          FDatalink.Edit;
-          FDataLink.Modified;
-          FDataLink.OnDataChange := @DataChange;
-          inherited WndProc(Message);
-        end
+          inherited WndProc(Message)
         else
           Message.Result := 1; // prevent calling default window proc
       end;

lcl/include/dbcombobox.inc
@@ -17,22 +17,6 @@ begin
   FDataLink.Field.Text := Text;
 end;
 
-procedure TDBComboBox.Change;
-begin
-  try
-    if FDataLink.CanModify then begin
-      FDataLink.OnDataChange := nil;
-      if FDataLink.Edit then begin
-        FDataLink.Field.AsString := Text;
-        FDatalink.Modified;
-      end;
-      FDataLink.OnDataChange := @Datachange;
-    end;
-  finally
-    inherited Change;
-  end;
-end;
-
 procedure TDBComboBox.DataChange(Sender: TObject);
 var
   DataLinkField: TField;
@@ -52,10 +36,6 @@ begin
     FDataLink.Reset;
     SelectAll;
     Key := VK_UNKNOWN;
-  end else
-  if Key in [VK_DELETE, VK_BACK] then begin
-    if not FDataLink.Edit then
-      Key := VK_UNKNOWN;
   end;
 end;
 
@@ -63,34 +43,9 @@ procedure TDBComboBox.KeyPress(var Key: char);
 begin
   inherited KeyPress(Key);
   case Key of
-    #8: // special keys
-      if not FDatalink.Edit then
-        Key:=#0;
-
     #32..#255: //standard keys
-      if not FieldCanAcceptKey(FDataLink.Field, Key) or not FDatalink.Edit then
+      if not FieldCanAcceptKey(FDataLink.Field, Key) then
         Key:=#0;
   end;//case
 end;
 
-procedure TDBComboBox.Select;
-begin
-  //avoid reseting text when calling select
-  FDataLink.OnDataChange := nil;
-  try
-    if FDataLink.Edit then
-    begin
-       FDataLink.Modified;
-       FDataLink.UpdateData;
-       inherited Select;
-    end
-    else
-    begin
-       // if cannot modify, let it reset
-       FDatalink.Reset;
-       DataChange(Self);
-    end;
-  finally
-    FDataLink.OnDataChange := @DataChange;
-  end;
-end;

lcl/lmessages.pp
@@ -90,6 +90,7 @@ const
 
   LM_GRABFOCUS      = LM_LCL + 80;
   LM_DRAWLISTITEM   = LM_LCL + 81;
+  LM_DEFERREDEDIT   = LM_LCL + 82;            // used in customdbcombobox
 
   // these IDs are reserved for internal messages in the interfaces
   LM_INTERFACEFIRST = LM_LCL + 99;

commit 6a5bc59a4d1fd7ea5ee40d243f556e2c70b9ebff

LCL, fix unbound DbLookupCombobox regression caused by r58686 #8ff1532ca9, issue #33164
git-svn-id: trunk@58799 -

lcl/dbctrls.pp
@@ -724,9 +724,11 @@ Type
     procedure SetReadOnly(const AValue: Boolean);
     procedure CMGetDataLink(var Message: TLMessage); message CM_GETDATALINK;
   protected
-    function DoEdit: boolean;
-    procedure DoOnCloseUp;
+    function DoEdit: boolean; virtual;
+    procedure DoOnCloseUp; virtual;
+    procedure DoOnSelect; virtual;
     procedure LMDeferredEdit(var Message: TLMessage); message LM_DEFERREDEDIT;
+    property DetectedEvents: Word read FDetectedEvents;
   protected
     procedure CloseUp; override;
     Procedure Select; override;
@@ -831,6 +833,8 @@ Type
   { TDBLookupComboBox }
 
   TDBLookupComboBox = class(TCustomDBComboBox)
+  protected
+    function DoEdit: boolean; override;
   private
     FLookup: TDBLookup;
     FScrollListDataset: Boolean;
@@ -851,7 +855,6 @@ Type
     procedure SetNullValueKey(const AValue: TShortCut);
     procedure UpdateLookup;
   protected
-    procedure CloseUp; override;
     procedure InitializeWnd; override;
     procedure DestroyWnd; override;
     procedure KeyDown(var Key: Word; Shift: TShiftState); override;
@@ -859,7 +862,6 @@ Type
     procedure Loaded; override;
     procedure UpdateData(Sender: TObject); override;
     procedure DataChange(Sender: TObject); override;
-    procedure Select; override;
   public
     constructor Create(AOwner: TComponent); override;
     property KeyValue: variant read GetKeyValue write SetKeyValue;
@@ -1854,6 +1856,11 @@ begin
 
   IsModified := False;
 end;
+
+CONST
+  DBCBEVENT_CHANGE   = 1;   // CustomDBBoxCombobox Detected change event
+  DBCBEVENT_SELECT   = 2;   // CustomDBBoxCombobox Detected select event
+  DBCBEVENT_CLOSEUP  = 4;   // CustomDBBoxCombobox Detected closeup event
 
 {$Include dblookup.inc}
 {$Include dbedit.inc}

lcl/include/customdbcombobox.inc
@@ -15,11 +15,6 @@
 
 // included by dbctrls.pp
 
-CONST
-  DBCBEVENT_CHANGE   = 1;   // Detected change event
-  DBCBEVENT_SELECT   = 2;   // Detected select event
-  DBCBEVENT_CLOSEUP  = 4;   // Detected closeup event
-
 {TCustomDBComboBox}
 
 function TCustomDBComboBox.GetDataField: string;
@@ -77,7 +72,7 @@ begin
         if FDetectedEvents and DBCBEVENT_CHANGE <> 0 then
           inherited Change;
         if FDetectedEvents and DBCBEVENT_SELECT <> 0 then
-          inherited Select;
+          DoOnSelect;
       end;
       if FDetectedEvents and DBCBEVENT_CLOSEUP <> 0 then
         DoOnCloseUp;
@@ -109,6 +104,11 @@ begin
       AutoSelected := True;
   end;//End if FAutoSelect
   FDetectedEvents := 0;   // reset because closeup may occur without editing
+end;
+
+procedure TCustomDBComboBox.DoOnSelect;
+begin
+  inherited Select;
 end;
 
 procedure TCustomDBComboBox.SetDataField(const AValue: string);

lcl/include/dblookupcombobox.inc
@@ -38,6 +38,20 @@ begin
     Text := '';
 end;
 
+function TDBLookupComboBox.DoEdit: boolean;
+begin
+  if (FDataLink.DataSource=nil) or (DataField='') then begin
+    if DetectedEvents and DBCBEVENT_SELECT <>0 then begin
+      UpdateData(self);
+      DoOnSelect;
+    end;
+    if DetectedEvents and DBCBEVENT_CLOSEUP <>0 then
+      DoOnCloseUp;
+  end
+  else
+    Result := inherited DoEdit;
+end;
+
 procedure TDBLookupComboBox.ActiveChange(Sender: TObject);
 begin
   if FDataLink.Active then
@@ -45,16 +59,8 @@ begin
 end;
 
 procedure TDBLookupComboBox.DataChange(Sender: TObject);
-var
-  i: Integer;
 begin
-  if FDatalink.Active then
-    i := FLookup.GetKeyIndex
-  else
-    i := -1;
-  ItemIndex := i;
-  if i = -1 then
-    Text := '';
+  UpdateLookup;
 end;
 
 procedure TDBLookupComboBox.DestroyWnd;
@@ -62,32 +68,6 @@ begin
   inherited;
   //after handle destroy Items address changes
   FLookup.ControlItems := Items;
-end;
-
-procedure TDBLookupComboBox.Select;
-begin
-  FDataLink.OnDataChange := nil;
-  try
-    if FDataLink.Edit then
-    begin
-       FDataLink.Modified;
-       FDataLink.UpdateData;
-       inherited Select;
-    end
-    else
-    begin
-       // if cannot modify, let it reset
-       if ReadOnly then begin
-         FDatalink.Reset;
-         DataChange(Self);
-       end else begin
-         FDataLink.Modified;
-         inherited Select;
-       end;
-    end;
-  finally
-    FDataLink.OnDataChange := @DataChange;
-  end;
 end;
 
 procedure TDBLookupComboBox.KeyDown(var Key: Word; Shift: TShiftState);
@@ -208,12 +188,5 @@ begin
     if i = -1 then
       Text := '';
   end;
-end;
-
-procedure TDBLookupComboBox.CloseUp;
-begin
-  if Style = csDropDownList then
-    FDataLink.UpdateRecord;
-  inherited CloseUp;
 end;
 