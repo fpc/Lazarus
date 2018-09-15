12ce8a3854eff2fcec4002be8d325d783f64ee83
mattias <nc-gaertnma@netcologne.de>
2018-09-15 19:46:38 +0000

    set trunk version to 2.1.0
    git-svn-id: trunk@59010 -

--- components/lazutils/lazversion.pas
+++ components/lazutils/lazversion.pas
@@ -21,11 +21,11 @@ interface
 
 const
   laz_major = 2;
-  laz_minor = 0;
+  laz_minor = 1;
   laz_release = 0;
   laz_patch = 0;
   laz_fullversion = ((laz_major *  100 + laz_minor) * 100 + laz_release) * 100 + laz_patch;
-  laz_version = '2.0.0.0';
+  laz_version = '2.1.0.0';
 
 implementation
 
