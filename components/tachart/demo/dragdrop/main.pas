a0a5e9d8cd90de1635aaa46170bfbf2c1efcf15f
wp <wp.no-reply@lazarus-ide-org>
2018-10-03 16:20:57 +0000

    TAChart: Extend dragdrop demo to show horizontal dragging of bars.
    git-svn-id: trunk@59243 -

--- components/tachart/demo/dragdrop/main.pas
+++ components/tachart/demo/dragdrop/main.pas
@@ -14,6 +14,7 @@ type
 
   TForm1 = class(TForm)
     cbSorted: TCheckBox;
+    cbConstBarWidth: TCheckBox;
     chPoints: TChart;
     chBarsBarSeries1: TBarSeries;
     chPointsLineSeries1: TLineSeries;
@@ -26,21 +27,24 @@ type
     ctPointsDataPointHintTool1: TDataPointHintTool;
     PageControl1: TPageControl;
     Panel1: TPanel;
+    Panel2: TPanel;
+    rbDragY: TRadioButton;
+    rbDragX: TRadioButton;
     RandomChartSource1: TRandomChartSource;
     tsPoints: TTabSheet;
     tsBars: TTabSheet;
+    procedure cbConstBarWidthChange(Sender: TObject);
     procedure cbSortedChange(Sender: TObject);
     procedure chPointsLineSeries1GetMark(out AFormattedMark: String;
       AIndex: Integer);
     procedure ctBarsDataPointDragTool1Drag(ASender: TDataPointDragTool;
-      var AGraphPoint: TDoublePoint);
-    procedure ctBarsDataPointDragTool1DragStart(ASender: TDataPointDragTool;
       var AGraphPoint: TDoublePoint);
     procedure ctPointsDataPointClickTool1PointClick(ATool: TChartTool;
       APoint: TPoint);
     procedure ctPointsDataPointHintTool1Hint(ATool: TDataPointHintTool;
       const APoint: TPoint; var AHint: String);
     procedure FormCreate(Sender: TObject);
+    procedure rbDragYChange(Sender: TObject);
   end;
 
 var
@@ -60,6 +64,14 @@ begin
   chPointsLineSeries1.ListSource.Sorted := cbSorted.Checked;
 end;
 
+procedure TForm1.cbConstBarWidthChange(Sender: TObject);
+begin
+  if cbConstBarWidth.Checked then
+    chBarsBarSeries1.BarWidthStyle := bwPercentMin
+  else
+    chBarsBarSeries1.BarWidthStyle := bwPercent;
+end;
+
 procedure TForm1.chPointsLineSeries1GetMark(
   out AFormattedMark: String; AIndex: Integer);
 begin
@@ -74,20 +86,16 @@ end;
 procedure TForm1.ctBarsDataPointDragTool1Drag(ASender: TDataPointDragTool;
   var AGraphPoint: TDoublePoint);
 begin
-  // Only allow vertical dragging.
-  AGraphPoint.X := ASender.Origin.X;
-end;
-
-procedure TForm1.ctBarsDataPointDragTool1DragStart(ASender: TDataPointDragTool;
-  var AGraphPoint: TDoublePoint);
-const
-  Y_TOLERANCE = 3;
-var
-  f: TGraphToImageFunc;
-begin
-  f := @chBars.YGraphToImage;
-  if Abs(f(ASender.Origin.Y) - f(AGraphPoint.Y)) > Y_TOLERANCE then
-    ASender.Handled;
+  if rbDragY.Checked then begin
+    // Only allow vertical dragging.
+    AGraphPoint.X := ASender.Origin.X;
+    ctBarsDataPointDragTool1.ActiveCursor := crSizeNS;
+  end else
+  if rbDragX.Checked then begin
+    // Only allow horizontal dragging
+    AGraphPoint.Y := ASender.Origin.Y;
+    ctBarsDataPointDragTool1.ActiveCursor := crSizeWE;
+  end;
 end;
 
 procedure TForm1.ctPointsDataPointClickTool1PointClick(
@@ -116,6 +124,17 @@ begin
   for i := 1 to 10 do
     chPointsLineSeries1.AddXY(i, Random(20) - 10);
   chBarsBarSeries1.ListSource.CopyFrom(RandomChartSource1);
+end;
+
+procedure TForm1.rbDragYChange(Sender: TObject);
+begin
+  if rbDragY.Checked then begin
+    chBarsBarSeries1.ToolTargets := chBarsBarSeries1.ToolTargets - [nptCustom];
+    ctBarsDataPointDragTool1.Targets := ctBarsDataPointDragTool1.Targets - [nptCustom];
+  end else begin
+    chBarsBarSeries1.ToolTargets := chBarsBarSeries1.ToolTargets + [nptCustom];
+    ctBarsDataPointDragTool1.Targets := ctBarsDataPointDragTool1.Targets + [nptCustom];
+  end;
 end;
 
 end.
