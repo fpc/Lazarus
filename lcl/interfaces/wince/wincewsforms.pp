9e965ff41764d3882c7abc8c47189533e6cb02f8
lacak <lacak@lazarus-ide.org>
2018-10-22 08:34:01 +0000

    LCL: wince: Forms are not correctly centered, when used Position=poScreenCenter or poDesktopCenter (patch aligns WinCE implementation to Win32 implementation). Bug #33957
    git-svn-id: trunk@59337 -

--- lcl/interfaces/wince/wincewsforms.pp
+++ lcl/interfaces/wince/wincewsforms.pp
@@ -120,6 +120,10 @@ implementation
 
 uses WinCEInt, WinCEWSMenus;
 
+type
+  TWinControlAccess = class(TWinControl)
+  end;
+
 { TWinCEWSScrollBox }
 
 class function TWinCEWSScrollBox.CreateHandle(const AWinControl: TWinControl;
@@ -364,15 +368,14 @@ end;
 class procedure TWinCEWSCustomForm.SetBounds(const AWinControl: TWinControl;
     const ALeft, ATop, AWidth, AHeight: Integer);
 var
-  SizeRect: Windows.RECT;
+  AForm: TCustomForm absolute AWinControl;
+  SizeRect, WR, CurRect: Windows.RECT;
   BorderStyle: TFormBorderStyle;
-  WR: Windows.RECT;
+  L, T, W, H: integer;
 begin
-  { User selected LCL window size }
-  SizeRect.Top := ATop;
-  SizeRect.Left := ALeft;
-  SizeRect.Bottom := ATop + AHeight;
-  SizeRect.Right := ALeft + AWidth;
+  // the LCL defines the size of a form without border, wince with.
+  // -> adjust size according to BorderStyle
+  SizeRect := Bounds(ALeft, ATop, AWidth, AHeight);
 
   BorderStyle := TCustomForm(AWinControl).BorderStyle;
 
@@ -402,9 +405,33 @@ begin
   Windows.AdjustWindowRectEx(@SizeRect, BorderStyleToWinAPIFlags(
       BorderStyle), false, BorderStyleToWinAPIFlagsEx(TCustomForm(AWinControl), BorderStyle));
 
+  L := ALeft;
+  T := ATop;
+  W := SizeRect.Right - SizeRect.Left;
+  H := SizeRect.Bottom - SizeRect.Top;
+
+  // we are calling setbounds in TWinControl.Initialize
+  // if position is default it will be changed to designed. We do not want this.
+  if wcfInitializing in TWinControlAccess(AWinControl).FWinControlFlags then
+  begin
+    if GetWindowRect(AForm.Handle, CurRect) then
+    begin
+      if AForm.Position in [poDefault, poDefaultPosOnly] then
+      begin
+        L := CurRect.Left;
+        T := CurRect.Top;
+      end;
+
+      if AForm.Position in [poDefault, poDefaultSizeOnly] then
+      begin
+        W := CurRect.Right - CurRect.Left;
+        H := CurRect.Bottom - CurRect.Top;
+      end;
+    end;
+  end;
+
   // rect adjusted, pass to inherited to do real work
-  TWinCEWSWinControl.SetBounds(AWinControl, SizeRect.Left, SizeRect.Top,
-    SizeRect.Right - SizeRect.Left, SizeRect.Bottom - SizeRect.Top);
+  TWinCEWSWinControl.SetBounds(AWinControl, L, T, W, H);
 
   {$IFDEF VerboseSizeMsg}
   DebugLn(
