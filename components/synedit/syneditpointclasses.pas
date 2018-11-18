1d51dbaf1089782b811b0f3078e1b0b7cc5d124f
zeljko <zeljko@lazarus-ide.org>
2018-11-18 11:56:49 +0000

    SynEdit: removed workaround for Qt/Qt5. issue #34563
    git-svn-id: trunk@59591 -

--- components/synedit/syneditpointclasses.pas
+++ components/synedit/syneditpointclasses.pas
@@ -2799,11 +2799,6 @@ begin
     exit;
   inherited ShowCaret;
   Result := LCLIntf.ShowCaret(Handle);
-  {$IFDEF LCLQt}
-  // workaround for issue: https://bugs.freepascal.org/view.php?id=34563
-  // assume that if ShowCaret works, the cared has been created ok
-  FCreated := FCreated or Result;
-  {$ENDIF}
 end;
 
 function TSynEditScreenCaretPainterSystem.SetCaretPosEx(x, y: Integer): Boolean;
