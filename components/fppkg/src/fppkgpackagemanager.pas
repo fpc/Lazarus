8cba1e0f8832946078c9f36790b3a49671f99d03
joost <joost@lazarus-ide.org>
2019-01-01 16:38:21 +0000

    Fppkg: fppkg_initializeoptionsfrm.pas added to project
    git-svn-id: trunk@59966 -

--- components/fppkg/src/fppkgpackagemanager.pas
+++ components/fppkg/src/fppkgpackagemanager.pas
@@ -10,7 +10,7 @@ interface
 uses
   lazfppkgmanagerintf, fppkg_const, fppkg_details, fppkg_mainfrm, fppkg_optionsfrm, laz_pkgrepos, 
   FppkgWorkerThread, Fppkg_EnvironmentOptions, Fppkg_Interface, fppkg_packageoptionsfrm, 
-  fppkg_packagevariant, fppkg_packagefileoptionsfrm, LazarusPackageIntf;
+  fppkg_packagevariant, fppkg_packagefileoptionsfrm, fppkg_initializeoptionsfrm, LazarusPackageIntf;
 
 implementation
 
