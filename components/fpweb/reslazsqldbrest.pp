f2708bf3954595fb36296e828f72a6fdc0b4f3a3
michael <michael@lazarus-ide.org>
2019-02-25 20:30:52 +0000

    * All resource strings moved to separate unit
    git-svn-id: trunk@60506 -

---
+++ components/fpweb/reslazsqldbrest.pp
@@ -0,0 +1,24 @@
+unit reslazsqldbrest;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+Resourcestring
+  SSQLDBRESTModule = 'SQLDB REST Bridge Module';
+  SSQLDBRESTModuleDesc = 'SQLDB REST Bridge: A Web datamodule for producing strict HTML.';
+  SExposeAConnection = 'Expose a connection';
+  SSaveSettingsToIni = 'Save settings to INI file';
+  SLoadSettingsFromIni = 'Load settings from INI file';
+  SJSONFilesFilter = 'JSON Files|*.json|All files|%s';
+  SSaveSchemaToJSONFile = 'Save schema to JSON file';
+  SLoadSchemaFromJSONFile = 'Load schema from JSON file';
+
+
+implementation
+
+end.
+
