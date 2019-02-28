fpc/Lazarus

commit 37a8c2f9c7ed25eb6940700f5a4032e40e59cee7

TAChart: Increase width of Text column in DataPoints editor.
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@60512 4005530d-fff6-0310-9dd1-cebe43e6787f

components/tachart/tadatapointseditor.pas
@@ -155,16 +155,18 @@ begin
 
   // Adjust column widths
   w := sgData.Canvas.TextWidth('$000000') + 3*varCellPadding + sgData.DefaultRowHeight;
-  for i := 0 to sgData.Columns.Count-1 do
+  for i := 0 to sgData.Columns.Count-2 do
     sgData.Columns[i].Width := w;
+  sgData.Columns[sgData.Columns.Count-1].Width := 3*w;
 
-{$IFDEF WINDOWS}
-  Width := sgData.ColWidths[0] + 1 + sgData.Columns.Count * w + sgData.Left * 2 +
-    IfThen(sgData.BorderStyle = bsNone, 0, 3);
-{$ELSE}
-  Width := sgData.ColWidths[0] + sgData.Columns.Count * w + sgData.Left * 2 +
-    sgData.GridLineWidth * (sgData.Columns.Count-1);
-{$ENDIF}
+  w := sgData.ColWidths[0] + sgData.Left * 2;
+  for i := 0 to sgData.Columns.Count-1 do
+    inc(w, sgData.Columns[i].Width);
+ {$IFDEF WINDOWS}
+  Width := Min(Screen.Width, w + 1 + IfThen(sgData.BorderStyle = bsNone, 0, 3));
+ {$ELSE}
+  Width := Min(Screen.Width, w + sgData.GridLinedWidth * (sgData.Columns.Count-1));
+ {$ENDIF}
 end;
 
 procedure TDataPointsEditorForm.miDeleteRowClick(Sender: TObject);

components/tachart/tadatapointseditor.lfm
@@ -68,6 +68,7 @@ object DataPointsEditorForm: TDataPointsEditorForm
     CancelButton.Name = 'CancelButton'
     CancelButton.DefaultCaption = True
     TabOrder = 1
+    DefaultButton = pbHelp
     ShowButtons = [pbOK, pbCancel]
   end
   object pmRows: TPopupMenu

commit 319386da3f9d7e732cfc95d866b26382003bc89f

TAChart: Fix compilation on Linux (broken by r60512 #6e6af28591). Patch by Lagunov Aleksey, issue #35167).
git-svn-id: trunk@60532 -

components/tachart/tadatapointseditor.pas
@@ -199,7 +199,7 @@ begin
  {$IFDEF WINDOWS}
   Width := Min(Screen.Width, w + 1 + IfThen(sgData.BorderStyle = bsNone, 0, 3));
  {$ELSE}
-  Width := Min(Screen.Width, w + sgData.GridLinedWidth * (sgData.Columns.Count-1));
+  Width := Min(Screen.Width, w + sgData.GridLineWidth * (sgData.Columns.Count-1));
  {$ENDIF}
 end;
 