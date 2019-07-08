312cafc8cbda50fcb3a929cc9d050c6a18ba7584
martin <laz.git@mfriebe.de>
2019-07-08 19:11:18 +0000

    IDE: Fix crash on accessing nil (in MainIdeBar). Happens if the InitialSetupDialog is closed (OS close button) and the IDE exits.
    git-svn-id: trunk@61545 -

--- ide/main.pp
+++ ide/main.pp
@@ -3607,10 +3607,11 @@ begin
   if FWaitForClose and (ToolStatus = itNone) then
   begin
     FWaitForClose := False;
-    MainIDEBar.Close;
+    if MainIDEBar <> nil then
+      MainIDEBar.Close;
   end;
 
-  if not IDEIsClosing and MainIDEBar.HandleAllocated then
+  if (MainIDEBar <> nil) and not IDEIsClosing and MainIDEBar.HandleAllocated then
   begin
     if (ToolStatus = itDebugger) then
       EnvironmentOptions.EnableDebugDesktop
