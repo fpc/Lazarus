da14845df31436c4ec74960fcdf9e63f6b71a1b2
maxim <maxim@lazarus-ide.org>
2019-07-19 23:09:25 +0000

    LCL: fixed license header in lcltranslator.pas file and removed license header from defaulttranslator.pas file (it has no copyrightable code)
    git-svn-id: trunk@61606 -

--- lcl/defaulttranslator.pas
+++ lcl/defaulttranslator.pas
@@ -1,21 +1,5 @@
 unit DefaultTranslator;
 
-{ Copyright (C) 2015 Lazarus Developers Team
-
-  This library is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Library General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
-
-  This program is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
-  for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; if not, write to the Free Software Foundation,
-  Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1335, USA.
-}
 {
 If you need standard translation, just use this unit in your project and enable
 i18n in project options. It will translate your project automatically.
