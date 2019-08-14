390c48d82030dddc5d3969ed7d2f4325dc9cc00d
dmitry <dmitry@lazarus-ide.org>
2019-08-14 20:46:17 +0000

    cocoa: adding support for ownerData checkbox values on TListView
    git-svn-id: trunk@61695 -

--- lcl/interfaces/cocoa/cocoawscomctrls.pas
+++ lcl/interfaces/cocoa/cocoawscomctrls.pas
@@ -115,6 +115,7 @@ type
     isSetTextFromWS: Integer; // allows to suppress the notifation about text change
                               // when initiated by Cocoa itself.
     checkedIdx : NSMutableIndexSet;
+    ownerData  : Boolean;
 
     constructor Create(AOwner: NSObject; ATarget: TWinControl; AHandleView: NSView); override;
     destructor Destroy; override;
@@ -192,7 +193,7 @@ type
     //carbon//class procedure SetHoverTime(const ALV: TCustomListView; const AValue: Integer); override;
     class procedure SetImageList(const ALV: TCustomListView; const {%H-}AList: TListViewImageList; const {%H-}AValue: TCustomImageListResolution); override;
     class procedure SetItemsCount(const ALV: TCustomListView; const Avalue: Integer); override;
-    (*class procedure SetOwnerData(const ALV: TCustomListView; const {%H-}AValue: Boolean); override;*)
+    class procedure SetOwnerData(const ALV: TCustomListView; const {%H-}AValue: Boolean); override;
     class procedure SetProperty(const ALV: TCustomListView; const AProp: TListViewProperty; const AIsSet: Boolean); override;
     class procedure SetProperties(const ALV: TCustomListView; const AProps: TListViewProperties); override;
     class procedure SetScrollBars(const ALV: TCustomListView; const AValue: TScrollStyle); override;
@@ -1196,7 +1197,7 @@ begin
     Result := false;
     Exit;
   end;
-  Result:=lclcb.checkedIdx.containsIndex(AIndex);
+  Result := lclcb.checkedIdx.containsIndex(AIndex);
 end;
 
 class function TCocoaWSCustomListView.ItemGetPosition(
@@ -1442,6 +1443,18 @@ begin
   lTableLV.noteNumberOfRowsChanged();
 end;
 
+class procedure TCocoaWSCustomListView.SetOwnerData(const ALV: TCustomListView;
+  const AValue: Boolean);
+var
+  lCocoaLV : TCocoaListView;
+  lTableLV : TCocoaTableListView;
+  cb       : TLCLListViewCallback;
+begin
+  if not CheckParamsCb(lCocoaLV, lTableLV, cb, ALV) then Exit;
+  cb.ownerData := AValue;
+  if cb.ownerData then cb.checkedIdx.removeAllIndexes; // releasing memory
+end;
+
 class procedure TCocoaWSCustomListView.SetProperty(const ALV: TCustomListView;
   const AProp: TListViewProperty; const AIsSet: Boolean);
 var
@@ -1638,7 +1651,10 @@ function TLCLListViewCallback.GetItemCheckedAt(ARow, ACol: Integer;
 var
   BoolState : array [Boolean] of Integer = (NSOffState, NSOnState);
 begin
-  IsChecked := BoolState[checkedIdx.containsIndex(ARow)];
+  if ownerData and Assigned(listView) and (ARow>=0) and (ARow < listView.Items.Count) then
+    IsChecked := BoolState[listView.Items[ARow].Checked]
+  else
+    IsChecked := BoolState[checkedIdx.containsIndex(ARow)];
   Result := true;
 end;
 
