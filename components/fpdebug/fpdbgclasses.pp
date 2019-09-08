fpc/Lazarus

commit 32e36addfe1504f5ca90cdb0593af1ef9080cb6d

FpDebug: Keep the loop running, if internal stepping causes deBreakPoint without actually hitting a breakpoint. Required because now there can be several such singlesteps in one iteration of the loop.
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@61840 4005530d-fff6-0310-9dd1-cebe43e6787f

components/fpdebug/fpdbgclasses.pp
@@ -1250,17 +1250,6 @@ begin
 
   if result = deBreakpoint then
   begin
-    if assigned(FCurrentBreakpoint) then
-    begin
-      // When a breakpoint has been hit, the debugger always continues with
-      // a single-step to jump over the breakpoint.
-      if not AThread.NextIsSingleStep then
-        // In this case the debugger has to continue. The debugger did only
-        // stop to be able to reset the breakpoint again. It was not a 'normal'
-        // singlestep.
-        result := deInternalContinue;
-    end;
-
     // Determine the address where the execution has stopped
     CurrentAddr:=AThread.GetInstructionPointerRegisterValue;
     FCurrentWatchpoint:=AThread.DetectHardwareWatchpoint;

components/fpdebug/fpdbgcontroller.pas
@@ -843,10 +843,12 @@ begin
        CurrentThread is then destroyed in the next call to continue....
     *)
     FPDEvent:=FCurrentProcess.ResolveDebugEvent(FCurrentThread);
-    DebugLn(DBG_VERBOSE, 'Process stopped with event %s. IP=%s, SP=%s, BSP=%s.', [FPDEventNames[FPDEvent],
-                                                                FCurrentProcess.FormatAddress(FCurrentThread.GetInstructionPointerRegisterValue),
-                                                                FCurrentProcess.FormatAddress(FCurrentThread.GetStackPointerRegisterValue),
-                                                                FCurrentProcess.FormatAddress(FCurrentThread.GetStackBasePointerRegisterValue)]);
+    DebugLn(DBG_VERBOSE, 'Process stopped with event %s. IP=%s, SP=%s, BSP=%s. HasBreak: %s',
+                         [FPDEventNames[FPDEvent],
+                         FCurrentProcess.FormatAddress(FCurrentThread.GetInstructionPointerRegisterValue),
+                         FCurrentProcess.FormatAddress(FCurrentThread.GetStackPointerRegisterValue),
+                         FCurrentProcess.FormatAddress(FCurrentThread.GetStackBasePointerRegisterValue),
+                         dbgs(CurrentProcess.CurrentBreakpoint<>nil)]);
     if assigned(FCommand) then
       begin
       FCommand.ResolveEvent(FPDEvent, IsHandled, IsFinished);
@@ -863,6 +865,7 @@ begin
     begin
      case FPDEvent of
        deInternalContinue: AExit := False;
+       deBreakpoint: AExit := FCurrentProcess.CurrentBreakpoint <> nil; // no breakpoint? continue
 {        deLoadLibrary :
           begin
             if FCurrentProcess.GetLib(FCurrentProcess.LastEventProcessIdentifier, ALib)

commit 304f6e7be94a9ca2ffbaf181668a0492d12a05d7

FpDebug: Fix requesting process to pause, broken in r61840 #ece9967721
git-svn-id: trunk@61845 -

components/fpdebug/fpdbgclasses.pp
@@ -339,6 +339,8 @@ type
     FProcessID: Integer;
     FThreadID: Integer;
 
+    function GetPauseRequested: boolean;
+    procedure SetPauseRequested(AValue: boolean);
     procedure ThreadDestroyed(const AThread: TDbgThread);
   protected
     FCurrentBreakpoint: TFpInternalBreakpoint;  // set if we are executing the code at the break
@@ -353,6 +355,7 @@ type
     FLibMap: TMap;    // map LibAddr -> LibObject
     FBreakMap: TBreakLocationMap;  // map BreakAddr -> BreakObject
     FTmpRemovedBreaks: array of TDBGPtr;
+    FPauseRequested: longint;
 
     FMainThread: TDbgThread;
     function GetHandle: THandle; virtual;
@@ -423,6 +426,8 @@ type
     property ExitCode: DWord read FExitCode;
     property CurrentBreakpoint: TFpInternalBreakpoint read FCurrentBreakpoint;
     property CurrentWatchpoint: integer read FCurrentWatchpoint;
+    property PauseRequested: boolean read GetPauseRequested write SetPauseRequested;
+    function GetAndClearPauseRequested: Boolean;
 
     // Properties valid when last event was an deException
     property ExceptionMessage: string read FExceptionMessage write FExceptionMessage;
@@ -1419,6 +1424,21 @@ procedure TDbgProcess.ThreadDestroyed(const AThread: TDbgThread);
 begin
   if AThread = FMainThread
   then FMainThread := nil;
+end;
+
+function TDbgProcess.GetPauseRequested: boolean;
+begin
+  Result := Boolean(InterLockedExchangeAdd(FPauseRequested, 0));
+end;
+
+function TDbgProcess.GetAndClearPauseRequested: Boolean;
+begin
+  Result := Boolean(InterLockedExchange(FPauseRequested, ord(False)));
+end;
+
+procedure TDbgProcess.SetPauseRequested(AValue: boolean);
+begin
+  InterLockedExchange(FPauseRequested, ord(AValue));
 end;
 
 procedure TDbgProcess.LoadInfo;

components/fpdebug/fpdbgcontroller.pas
@@ -865,7 +865,8 @@ begin
     begin
      case FPDEvent of
        deInternalContinue: AExit := False;
-       deBreakpoint: AExit := FCurrentProcess.CurrentBreakpoint <> nil; // no breakpoint? continue
+       deBreakpoint: AExit := (FCurrentProcess.CurrentBreakpoint <> nil) or // no breakpoint? continue
+                              (FCurrentProcess.GetAndClearPauseRequested);
 {        deLoadLibrary :
           begin
             if FCurrentProcess.GetLib(FCurrentProcess.LastEventProcessIdentifier, ALib)

components/fpdebug/fpdbgdarwinclasses.pas
@@ -924,6 +924,7 @@ end;
 function TDbgDarwinProcess.Pause: boolean;
 begin
   result := FpKill(ProcessID, SIGTRAP)=0;
+  PauseRequested:=true;
 end;
 
 function TDbgDarwinProcess.AnalyseDebugEvent(AThread: TDbgThread): TFPDEvent;

components/fpdebug/fpdbglinuxclasses.pas
@@ -990,6 +990,7 @@ end;
 function TDbgLinuxProcess.Pause: boolean;
 begin
   result := fpkill(ProcessID, SIGTRAP)=0;
+  PauseRequested:=true;
   if not result then
     begin
     DebugLn(DBG_WARNINGS, 'Failed to send SIGTRAP to process %d. Errno: %d',[ProcessID, errno]);

components/fpdebug/fpdbgwinclasses.pas
@@ -161,7 +161,6 @@ type
   TDbgWinProcess = class(TDbgProcess)
   private
     FInfo: TCreateProcessDebugInfo;
-    FPauseRequested: boolean;
     FProcProcess: TProcessUTF8;
     FJustStarted: boolean;
     function GetFullProcessImageName(AProcessHandle: THandle): string;
@@ -1145,7 +1144,7 @@ var
 begin
   //hndl := OpenProcess(PROCESS_CREATE_THREAD or PROCESS_QUERY_INFORMATION or PROCESS_VM_OPERATION or PROCESS_VM_WRITE or PROCESS_VM_READ, False, TargetPID);
   hndl := OpenProcess(PROCESS_ALL_ACCESS, false, ProcessID);
-  FPauseRequested:=true;
+  PauseRequested:=true;
   result := DebugBreakProcess(hndl);
   if not Result then begin
     DebugLn(DBG_WARNINGS, ['pause failed(1) ', GetLastError]);