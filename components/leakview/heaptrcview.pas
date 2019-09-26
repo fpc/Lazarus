d98120d75f5026f59aa5ea35c77463354f4d7a9b
martin <laz.git@mfriebe.de>
2019-09-26 16:37:28 +0000

    LeakView: fix crash due to double free.
    git-svn-id: trunk@61926 -

--- components/leakview/heaptrcview.pas
+++ components/leakview/heaptrcview.pas
@@ -272,12 +272,13 @@ begin
     // removing duplicates
     for i := 0 to fItems.Count - 1 do begin
       trace := TStackTrace(fItems[i]);
+      if trace = nil then
+        continue;
       s := GetHashString(trace);
       hashed := TStackTrace(hash.Items[s]);
       if Assigned(hashed) then begin
         inc(hashed.LeakCount);
-        trace.Free; // remove from list
-        fItems[i] := nil;
+        fItems[i] := nil; // this call destroy on the old trace object
       end else
         hash.Add(s, trace)
     end;
