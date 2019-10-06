fpc/Lazarus

commit 8f05bb2cb8f8515d8f1bb6c952348e6dfd8890dc

Designer: Support Undo for properties of subcomponents like 'LabeledEdit1.SubLabel'. Issue #36071.
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@61974 4005530d-fff6-0310-9dd1-cebe43e6787f

designer/designer.pp
@@ -1438,13 +1438,15 @@ procedure TDesigner.ExecuteUndoItem(IsActUndo: boolean);
     end;
 
     if FForm.Name <> tmpCompN then
-      tmpObj := TObject(FForm.FindComponent(tmpCompN))
+      tmpObj := TObject(FForm.FindSubComponent(tmpCompN))
     else
       tmpObj := TObject(FForm);
 
     if VarIsError(AVal) or VarIsEmpty(AVal) or VarIsNull(AVal) then
       ShowMessage('error: invalid var type');
     tmpStr := VarToStr(AVal);
+    //DebugLn(['TDesigner.ExecuteUndoItem: FForm=', FForm.Name, ', CompName=', tmpCompN,
+    //  ', FieldName=', tmpFieldN, ', tmpObj=', tmpObj, ', tmpStr=', tmpStr, ', IsActUndo=', IsActUndo]);
 
     if FUndoList[FUndoCurr].propInfo<>nil then
     begin
@@ -1801,6 +1803,7 @@ var
   SaveControlSelection: TControlSelection;
   AStream: TStringStream;
   APropInfo: PPropInfo;
+  Comp: TComponent;
 begin
   Result := (FUndoLock = 0);
   if not Result then Exit;
@@ -1856,9 +1859,12 @@ begin
       compName := '';
       parentName := '';
       if aPersistent is TComponent then begin
-        compName := TComponent(aPersistent).Name;
-        if TComponent(aPersistent).HasParent then
-          parentName := TComponent(aPersistent).GetParentComponent.Name;
+        Comp := TComponent(aPersistent);
+        compName := Comp.Name;
+        if Comp.Owner <> LookupRoot then                // This is a subcomponent.
+          compName := Comp.Owner.Name + '.' + compName; // Add owner to the name.
+        if Comp.HasParent then
+          parentName := Comp.GetParentComponent.Name;
       end;
       opType := aOpType;
       isValid := true;

lcl/controls.pp
@@ -1629,6 +1629,7 @@ type
     function GetParentComponent: TComponent; override;
     function IsParentOf(AControl: TControl): boolean; virtual;
     function GetTopParent: TControl;
+    function FindSubComponent(AName: string): TComponent;
     function IsVisible: Boolean; virtual;// checks parents too
     function IsControlVisible: Boolean; virtual;// does not check parents
     function IsEnabled: Boolean; // checks parent too

lcl/include/control.inc
@@ -5244,10 +5244,6 @@ begin
   Result := Parent;
 end;
 
-{------------------------------------------------------------------------------
-  function TControl.IsParentOf(AControl: TControl): boolean;
-
- ------------------------------------------------------------------------------}
 function TControl.IsParentOf(AControl: TControl): boolean;
 begin
   Result := False;
@@ -5264,6 +5260,25 @@ begin
   Result := Self;
   while Assigned(Result.Parent) do
     Result := Result.Parent;
+end;
+
+function TControl.FindSubComponent(AName: string): TComponent;
+// Like TComponent.FindComponent but finds also a subcomponent which name is
+//  separated by a dot. For example 'LabeledEdit1.SubLabel'.
+var
+  i: Integer;
+  SubName: String;
+begin
+  i := Pos('.', AName);
+  if i > 0 then begin
+    SubName := Copy(AName, i+1, Length(AName));
+    Delete(AName, i, Length(AName));
+  end
+  else
+    SubName := '';
+  Result := FindComponent(AName);
+  if Assigned(Result) and (SubName<>'') then
+    Result := Result.FindComponent(SubName);
 end;
 
 {------------------------------------------------------------------------------

commit 6362b3d71af68516e2f9b403f4850ac532b4585d

Designer: Fix a crash bug caused by r61974 #dcb8bd49fb. Top form has no Owner.
git-svn-id: trunk@62001 -

designer/designer.pp
@@ -1878,8 +1878,9 @@ begin
       if aPersistent is TComponent then begin
         Comp := TComponent(aPersistent);
         compName := Comp.Name;
-        if Comp.Owner <> LookupRoot then                // This is a subcomponent.
-          compName := Comp.Owner.Name + '.' + compName; // Add owner to the name.
+        // Add owner to the name of a subcomponent.
+        if Assigned(Comp.Owner) and (Comp.Owner <> LookupRoot) then
+          compName := Comp.Owner.Name + '.' + compName;
         if Comp.HasParent then
           parentName := Comp.GetParentComponent.Name;
       end;