e216e6cfecb6063c7f345c50d8d26be24d69d272
wp <wp.no-reply@lazarus-ide-org>
2019-10-17 22:08:42 +0000

    TAChart: Replace r62069 #40d59fec0b by better solution.
    git-svn-id: trunk@62070 -

--- components/tachart/tacustomseries.pas
+++ components/tachart/tacustomseries.pas
@@ -1862,10 +1862,11 @@ begin
   Unused(APointIdx);
   Unused(AXIdx, AYIdx);
   if IsNaN(AGraphPt) then
-    pt := Point(10000, 10000)
-  else
+    exit(MaxInt)
+  else begin
     pt := ParentChart.GraphToImage(AGraphPt);
-  Result := AParams.FDistFunc(AParams.FPoint, pt);
+    Result := AParams.FDistFunc(AParams.FPoint, pt);
+  end;
 end;
 
 // AIndex refers to the index into YList here.
