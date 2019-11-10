d35e17fbdd400f6ea8cfed410935801491e85b55
wp <wp.no-reply@lazarus-ide-org>
2019-11-10 21:58:05 +0000

    TAChart: Update tutorial "background" to avoid the deprecated background drawing events.
    git-svn-id: trunk@62229 -

--- components/tachart/tutorials/background/main.pas
+++ components/tachart/tutorials/background/main.pas
@@ -5,7 +5,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, TAGraph, TASeries, TASources, Forms, Controls,
-  Graphics, Dialogs;
+  Graphics, Dialogs, TADrawUtils;
 
 type
 
@@ -15,10 +15,12 @@ type
     Chart1: TChart;
     Chart1BarSeries1: TBarSeries;
     ListChartSource1: TListChartSource;
-    procedure Chart1BeforeDrawBackground(ASender: TChart; ACanvas: TCanvas;
-      const ARect: TRect; var ADoDefaultDrawing: Boolean);
-    procedure Chart1BeforeDrawBackWall(ASender: TChart; ACanvas: TCanvas;
-      const ARect: TRect; var ADoDefaultDrawing: Boolean);
+    procedure Chart1BeforeCustomDrawBackground(ASender: TChart;
+      ADrawer: IChartDrawer; const ARect: TRect; var ADoDefaultDrawing: Boolean
+      );
+    procedure Chart1BeforeCustomDrawBackWall(ASender: TChart;
+      ADrawer: IChartDrawer; const ARect: TRect; var ADoDefaultDrawing: Boolean
+      );
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
@@ -35,6 +37,9 @@ implementation
 
 {$R *.lfm}
 
+uses
+  IntfGraphics;
+
 //{$R splash_logo.res}
 
 { TForm1 }
@@ -47,22 +52,72 @@ begin
 
   // or, use resources:
   //FBackImage.LoadFromResourceName(HInstance, 'splash_logo');
-  // Don't forget to active the "{$R" directive above.
+  // Don't forget to activate the "{$R" directive above.
 end;
 
+procedure TForm1.Chart1BeforeCustomDrawBackWall(ASender: TChart;
+  ADrawer: IChartDrawer; const ARect: TRect; var ADoDefaultDrawing: Boolean);
+var
+  bmp: TBitmap;
+  img: TLazIntfImage;
+begin
+  img := TLazIntfImage.Create(0, 0);
+  try
+    bmp := TBitmap.Create;
+    try
+      bmp.SetSize(ARect.Right - ARect.Left, ARect.Bottom - ARect.Top);
+      bmp.Canvas.StretchDraw(Rect(0, 0, bmp.Width, bmp.Height), FBackImage.Graphic);
+      img.LoadFromBitmap(bmp.Handle, bmp.MaskHandle);
+    finally
+      bmp.Free;
+    end;
+    ADrawer.PutImage(ARect.Left, ARect.Top, img);
+    ADoDefaultDrawing := false;
+  finally
+    img.Free;
+  end;
+end;
+
+(* --------------- deprecated, was in use before Laz 2.1 -----------------------
 procedure TForm1.Chart1BeforeDrawBackWall(ASender: TChart; ACanvas: TCanvas;
   const ARect: TRect; var ADoDefaultDrawing: Boolean);
 begin
   ACanvas.StretchDraw(ARect, FBackImage.Graphic);
   ADoDefaultDrawing := false;
 end;
+----------------------------------------------------------------------------- *)
 
+procedure TForm1.Chart1BeforeCustomDrawBackground(ASender: TChart;
+  ADrawer: IChartDrawer; const ARect: TRect; var ADoDefaultDrawing: Boolean);
+var
+  bmp: TBitmap;
+  img: TLazIntfImage;
+begin
+  img := TLazIntfImage.Create(0, 0);
+  try
+    bmp := TBitmap.Create;
+    try
+      bmp.SetSize(ARect.Right - ARect.Left, ARect.Bottom - ARect.Top);
+      bmp.Canvas.GradientFill(Rect(0, 0, bmp.Width, bmp.Height), clSkyBlue, clWhite, gdVertical);
+      img.LoadFromBitmap(bmp.Handle, bmp.MaskHandle);
+    finally
+      bmp.Free;
+    end;
+    ADrawer.PutImage(ARect.Left, ARect.Top, img);
+    ADoDefaultDrawing := false;
+  finally
+    img.Free;
+  end;
+end;
+
+(* --------------- deprecated, was in use before Laz 2.1 -----------------------
 procedure TForm1.Chart1BeforeDrawBackground(ASender: TChart; ACanvas: TCanvas;
   const ARect: TRect; var ADoDefaultDrawing: Boolean);
 begin
   ACanvas.GradientFill(ARect, clSkyBlue, clWhite, gdVertical);
   ADoDefaultDrawing := false;
 end;
+----------------------------------------------------------------------------- *)
 
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
