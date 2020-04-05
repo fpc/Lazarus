651a6436a2f9ad11ed26fd31926d18847828b6db
juha <juha@lazarus-ide.org>
2020-04-05 11:35:01 +0000

    Support Haiku build. Makefiles are generated from Makefile.fpc files using proper tools. Issue #36316, patch from Alfred.
    git-svn-id: trunk@62890 -

--- components/fpdebug/fpdbgcommon.pas
+++ components/fpdebug/fpdbgcommon.pas
@@ -6,11 +6,11 @@ interface
 
 type
 // Target information, could be different from host debugger
-  TMachineType = (mtNone, mtSPARC, mt386, mt68K, mtPPC, mtPPC64, mtARM,
+  TMachineType = (mtNone, mtSPARC, mt386, mt68K, mtPPC, mtPPC64, mtARM, mtARM64,
                   mtOLD_ALPHA, mtIA_64, mtX86_64, mtAVR8, mtALPHA);
   TBitness = (bNone, b32, b64);
   TByteOrder = (boNone, boLSB, boMSB);
-  TOperatingSystem = (osNone, osBSD, osDarwin, osEmbedded, osLinux, osMac, osWindows);
+  TOperatingSystem = (osNone, osBSD, osDarwin, osEmbedded, osLinux, osUnix, osMac, osWindows);
 
   TTargetDescriptor = record
     machineType: TMachineType;
@@ -32,6 +32,7 @@ begin
     // TODO: Expand list when debugger support updated for other targets
     machineType := {$if defined(CPU386) or defined(CPUI386)} mt386
                    {$elseif defined(CPUX86_64) or defined(CPUAMD64) or defined(CPUX64)} mtX86_64
+                   {$elseif defined(CPUAARCH64)} mtARM64
                    {$elseif defined(CPUARM)} mtARM
                    {$elseif defined(CPUPOWERPC)} mtPPC
                    {$endif};
@@ -40,7 +41,10 @@ begin
     byteorder   := {$ifdef ENDIAN_LITTLE} boLSB {$else} boMSB {$endif};
 
     OS          := {$if defined(DARWIN)} osDarwin
+                   {$elseif defined(EMBEDDED)} osEmbedded
                    {$elseif defined(LINUX)} osLinux
+                   {$elseif defined(BSD)} osBSD
+                   {$elseif defined(UNIX)} osUnix
                    {$elseif defined(MSWINDOWS)} osWindows {$endif};
   end;
 end;
