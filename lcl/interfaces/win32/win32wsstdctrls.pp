fpc/Lazarus

commit 37ba646571bd57a0a3958380a86055098042893e

LCL, Win32: Fix TScrollBar, keep enabled=false when updating pos/min/max. Issue 0036429 (With comments by BrunoK)
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@62613 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/win32/win32wsstdctrls.pp
@@ -501,7 +501,10 @@ begin
     ScrollInfo.nPage := PageSize;
     ScrollInfo.nPos := Position;
 
-    SendMessage(Handle, SBM_SETSCROLLINFO, WParam(True), LParam(@ScrollInfo));
+    { ~bk 2019.12.11
+       https://docs.microsoft.com/en-us/windows/win32/controls/sbm-setscrollinfo
+       says that "they should use the SetScrollInfo function".}
+    SetScrollInfo(Handle, SB_CTL, ScrollInfo, IsEnabled);
     case Kind of
       sbHorizontal:
         SetWindowLong(Handle, GWL_STYLE, GetWindowLong(Handle, GWL_STYLE) or SBS_HORZ);

lcl/interfaces/win32/win32winapi.inc
@@ -3375,7 +3375,9 @@ begin
   ScrollInfo.cbSize:=sizeof(ScrollInfo);
   if (ScrollInfo.fMask and SIF_Range > 0) then
     ScrollInfo.nMax := Max(ScrollInfo.nMin, ScrollInfo.nMax - 1);
-  Result := Windows.SetScrollInfo(Handle, SBStyle, @ScrollInfo, BRedraw);
+  { ~bk passing BRedraw True/False might change the windows visual enabled state
+        that would not be reflected in TScollBar.Enabled property }
+  Result := Windows.SetScrollInfo(Handle, SBStyle, @ScrollInfo, BRedraw and WidgetSet.IsWindowEnabled(Handle));
 end;
 
 {------------------------------------------------------------------------------

commit 53bb4453cf543c7d28f23a470365914482815201

TScrollBar: Fix positioning on user action / regression from r62613 #59a33a8f2b
git-svn-id: trunk@63040 -

lcl/interfaces/win32/win32wsstdctrls.pp
@@ -503,7 +503,14 @@ begin
     { ~bk 2019.12.11
        https://docs.microsoft.com/en-us/windows/win32/controls/sbm-setscrollinfo
        says that "they should use the SetScrollInfo function".}
-    SetScrollInfo(Handle, SB_CTL, ScrollInfo, IsEnabled);
+    { However, this is sent while processing an incoming notification on user
+      action. SetScrollInfo acts immediately, and misplaces the scrollbar.
+      If it is not enabled this can not happen. And using SetScrollInfo we can
+      avoid enabling it by accident. }
+    if IsEnabled then
+      SendMessage(Handle, SBM_SETSCROLLINFO, WParam(True), LParam(@ScrollInfo))
+    else
+      SetScrollInfo(Handle, SB_CTL, ScrollInfo, IsEnabled);;
     case Kind of
       sbHorizontal:
         SetWindowLong(Handle, GWL_STYLE, GetWindowLong(Handle, GWL_STYLE) or SBS_HORZ);