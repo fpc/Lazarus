fpc/Lazarus

commit 9aa017c86101f5be6b798eba53fdb0df9f300b1c

IDE, SourceEditor: implemented identifier-completion activates on typing. Forget to test for cancelled
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@63316 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/sourceeditor.pp
@@ -4008,7 +4008,8 @@ begin
         inc(SourceCompletionCaretXY.x,length(AChar));
 
         AttrName := GetCodeAttributeName(p);
-        ok := (FEditor.MultiCaret.CaretsCount = 0) and
+        ok := (FCodeCompletionState.State <> ccsCancelled) and
+              (FEditor.MultiCaret.CaretsCount = 0) and
               (AttrName <> SYNS_XML_AttrComment) and
               (AttrName <> SYNS_XML_AttrDirective) and
               (AttrName <> SYNS_XML_AttrString);

commit 79ba6dbb06b81b1cdacfb07827acc362ad4fea79

Fix crash, introduced in r63316 #000ea7c493 "auto complete on typing". Issue #37184
git-svn-id: trunk@63322 -

ide/sourceeditor.pp
@@ -2142,7 +2142,9 @@ begin
     FAutoHideHintTimer.Enabled := False;
   if AutoStartCompletionBoxTimer<>nil then
     AutoStartCompletionBoxTimer.Enabled:=false;
-  if FManager.ActiveEditor.FCodeCompletionState.State in [ccsDot, ccsOnTyping] then
+  if (FManager.ActiveEditor <> nil) and
+     (FManager.ActiveEditor.FCodeCompletionState.State in [ccsDot, ccsOnTyping])
+  then
     FManager.ActiveEditor.FCodeCompletionState.State := ccsReady;
   if FAutoShown then
     HideHint;