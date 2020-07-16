fpc/Lazarus

commit 2e14ca84ec324874c0e018ece9202f46baba6e4e

LCL: Prevent a crach when closing a form with an active in-place cell editor of a grid. Issue #37202.
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@63541 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/grids.pas
@@ -8412,19 +8412,22 @@ var
 begin
   if not EditorLocked and (Editor<>nil) and Editor.Visible then
   begin
-    WasFocused := Editor.Focused;
-    FEditorMode:=False;
+    FEditorMode := False;
     FGridState := gsNormal;
-    {$ifdef dbgGrid}DebugLnEnter('EditorHide [',Editor.ClassName,'] INIT FCol=',IntToStr(FCol),' FRow=',IntToStr(FRow));{$endif}
-    LockEditor;
-    try
-      DoEditorHide;
-    finally
-      if WasFocused then
-        SetFocus;
-      UnLockEditor;
+    if Editor.Owner<>nil then  // May be nil when the form is closing.
+    begin
+      WasFocused := Editor.Focused;
+      {$ifdef dbgGrid}DebugLnEnter('EditorHide [',Editor.ClassName,'] INIT FCol=',IntToStr(FCol),' FRow=',IntToStr(FRow));{$endif}
+      LockEditor;
+      try
+        DoEditorHide;
+      finally
+        if WasFocused then
+          SetFocus;
+        UnLockEditor;
+      end;
+      {$ifdef dbgGrid}DebugLnExit('EditorHide END');{$endif}
     end;
-    {$ifdef dbgGrid}DebugLnExit('EditorHide END');{$endif}
   end;
 end;
 

commit e2840b9665e2591e93f648b559888761e5332eb4

LCL: Fix a regression in TSTringGrid caused by r63541 #1f0ff07dc2. Test for Editor.Parent instead of Editor.Owner.
git-svn-id: trunk@63571 -

lcl/grids.pas
@@ -8414,7 +8414,7 @@ begin
   begin
     FEditorMode := False;
     FGridState := gsNormal;
-    if Editor.Owner<>nil then  // May be nil when the form is closing.
+    if Editor.Parent<>nil then  // May be nil when the form is closing.
     begin
       WasFocused := Editor.Focused;
       {$ifdef dbgGrid}DebugLnEnter('EditorHide [',Editor.ClassName,'] INIT FCol=',IntToStr(FCol),' FRow=',IntToStr(FRow));{$endif}