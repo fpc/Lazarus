fpc/Lazarus

commit 05545668e8cc4bc2978328ed7b65c17c413bcf8d

FpDebug: Renamed TFpDbgHardcodedContext to TFpDbgHardcodedScope
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@63899 4005530d-fff6-0310-9dd1-cebe43e6787f

components/fpdebug/fpdbgdwarfdataclasses.pas
@@ -738,6 +738,7 @@ type
     procedure PushCopy(AFromIndex: Integer);
     procedure PushConst(const AVal: TDBGPtr);
     procedure PushTargetMem(const AVal: TDBGPtr);
+    procedure PushTargetRegister(const ARegNum: Cardinal);
     function  Peek: PFpDbgMemLocation;
     function  PeekForDeref: PFpDbgMemLocation;
     function  PeekKind: TFpDbgMemLocationType; // Can be called on empty stack
@@ -1874,6 +1875,15 @@ begin
   inc(FCount);
 end;
 
+procedure TDwarfLocationStack.PushTargetRegister(const ARegNum: Cardinal);
+begin
+  if Length(FList) <= FCount then
+    IncCapacity;
+  FList[FCount] := Default(TFpDbgMemLocation);
+  FList[FCount] := RegisterLoc(ARegNum);
+  inc(FCount);
+end;
+
 function TDwarfLocationStack.Peek: PFpDbgMemLocation;
 begin
   Assert(0 < FCount);
@@ -2089,11 +2099,12 @@ begin
           FStack.PushConst(NewValue);
         end;
       DW_OP_regx: begin
-          if not FMemManager.ReadRegister(ULEB128toOrdinal(CurData), NewValue, FContext) then begin
-            SetError;
-            exit;
-          end;
-          FStack.PushConst(NewValue);
+          //if not FMemManager.ReadRegister(ULEB128toOrdinal(CurData), NewValue, FContext) then begin
+          //  SetError;
+          //  exit;
+          //end;
+          //FStack.PushConst(NewValue);
+          FStack.PushTargetRegister(ULEB128toOrdinal(CurData));
         end;
 
       DW_OP_breg0..DW_OP_breg31: begin

components/fpdebug/fpdbginfo.pas
@@ -121,6 +121,11 @@ type
     function GetAsWideString: WideString; virtual;
     function GetAsFloat: Extended; virtual;
 
+    procedure SetAsCardinal(AValue: QWord); virtual;
+    procedure SetAsInteger(AValue: Int64); virtual;
+    procedure SetAsBool(AValue: Boolean); virtual;
+    procedure SetAsString(AValue: AnsiString); virtual;
+
     function GetAddress: TFpDbgMemLocation;  virtual;
     function DoGetSize(out ASize: TFpDbgValueSize): Boolean; virtual;
     function GetDataAddress: TFpDbgMemLocation;  virtual;
@@ -153,10 +158,10 @@ type
     property Kind: TDbgSymbolKind read GetKind;
     property FieldFlags: TFpValueFieldFlags read GetFieldFlags;
 
-    property AsInteger: Int64 read GetAsInteger;
-    property AsCardinal: QWord read GetAsCardinal;
-    property AsBool: Boolean read GetAsBool;
-    property AsString: AnsiString read GetAsString;
+    property AsInteger: Int64 read GetAsInteger write SetAsInteger;
+    property AsCardinal: QWord read GetAsCardinal write SetAsCardinal;
+    property AsBool: Boolean read GetAsBool write SetAsBool;
+    property AsString: AnsiString read GetAsString write SetAsString;
     property AsWideString: WideString read GetAsWideString;
     property AsFloat: Extended read GetAsFloat;
 
@@ -488,6 +493,7 @@ type
     function GetProcedureAtAddress: TFpValue; virtual;
     function GetMemManager: TFpDbgMemManager; virtual;
     function GetSizeOfAddress: Integer; virtual;
+    procedure DoReferenceReleased; override;
   public
     constructor Create(ALocationContext: TFpDbgLocationContext);
     destructor Destroy; override;
@@ -536,18 +542,19 @@ type
     function ReadMemory(AnAddress: TDbgPtr; ASize: Cardinal; ADest: Pointer; out ABytesRead: Cardinal): Boolean; override;
     function ReadMemoryEx(AnAddress, AnAddressSpace: TDbgPtr; ASize: Cardinal; ADest: Pointer): Boolean; override;
     function ReadRegister(ARegNum: Cardinal; out AValue: TDbgPtr; AContext: TFpDbgLocationContext): Boolean; override;
+    function WriteRegister(ARegNum: Cardinal; const AValue: TDbgPtr; AContext: TFpDbgLocationContext): Boolean; override;
     function RegisterSize(ARegNum: Cardinal): Integer; override;
     procedure SetRegisterValue(ARegNum: Cardinal; AValue: TDbgPtr);
   end;
 
-  { TFpDbgInfoCallContext }
+  { TFpDbgInfoAbstractCallContext }
 
   // This class is used to represent the context, just after the debugger made
   // the debugee call some function.
   // The special addition to make this work is that it is possible to set a
   // register-value by calling SetRegisterValue. Further this class is an empty
   // wrapper.
-  TFpDbgInfoCallContext = class(TFpDbgLocationContext)
+  TFpDbgAbstractCallContext = class(TFpDbgLocationContext)
   private
     FBaseContext: TFpDbgLocationContext;
     FMemManager: TFpDbgMemManager;
@@ -563,6 +570,9 @@ type
   public
     constructor Create(const ABaseContext: TFpDbgLocationContext; AMemReader: TFpDbgMemReaderBase; AMemConverter: TFpDbgMemConvertor);
     destructor Destroy; override;
+
+    function CreateParamSymbol(AParamIndex: Integer; ASymbolType: TFpSymbol): TFpValue; virtual; abstract;
+
     procedure SetRegisterValue(ARegNum: Cardinal; AValue: TDbgPtr);
     procedure SetError(const Message: string);
     property IsValid: Boolean read FIsValid;
@@ -658,9 +668,12 @@ begin
   FRegisterCache[ARegNum].Value := AValue;
 end;
 
-{ TFpDbgInfoCallContext }
+function TFpDbgCallMemReader.WriteRegister(ARegNum: Cardinal; const AValue: TDbgPtr; AContext: TFpDbgLocationContext): Boolean;
+begin
+  Result := FBaseMemReader.WriteRegister(ARegNum, AValue, AContext);
+end;
 
-constructor TFpDbgInfoCallContext.Create(
+constructor TFpDbgAbstractCallContext.Create(
   const ABaseContext: TFpDbgLocationContext; AMemReader: TFpDbgMemReaderBase;
   AMemConverter: TFpDbgMemConvertor);
 begin
@@ -675,7 +688,7 @@ begin
   Inherited Create;
 end;
 
-destructor TFpDbgInfoCallContext.Destroy;
+destructor TFpDbgAbstractCallContext.Destroy;
 begin
   FMemManager.Free;
   FMemReader.Free;
@@ -683,37 +696,37 @@ begin
   inherited Destroy;
 end;
 
-function TFpDbgInfoCallContext.GetAddress: TDbgPtr;
+function TFpDbgAbstractCallContext.GetAddress: TDbgPtr;
 begin
   Result := FBaseContext.Address;
 end;
 
-function TFpDbgInfoCallContext.GetMemManager: TFpDbgMemManager;
+function TFpDbgAbstractCallContext.GetMemManager: TFpDbgMemManager;
 begin
   Result := FMemManager;
 end;
 
-function TFpDbgInfoCallContext.GetSizeOfAddress: Integer;
+function TFpDbgAbstractCallContext.GetSizeOfAddress: Integer;
 begin
   Result := FBaseContext.SizeOfAddress;
 end;
 
-function TFpDbgInfoCallContext.GetStackFrame: Integer;
+function TFpDbgAbstractCallContext.GetStackFrame: Integer;
 begin
   Result := FBaseContext.StackFrame;
 end;
 
-function TFpDbgInfoCallContext.GetThreadId: Integer;
+function TFpDbgAbstractCallContext.GetThreadId: Integer;
 begin
   Result := FBaseContext.ThreadId;
 end;
 
-procedure TFpDbgInfoCallContext.SetRegisterValue(ARegNum: Cardinal; AValue: TDbgPtr);
+procedure TFpDbgAbstractCallContext.SetRegisterValue(ARegNum: Cardinal; AValue: TDbgPtr);
 begin
   FMemReader.SetRegisterValue(ARegNum, AValue);
 end;
 
-procedure TFpDbgInfoCallContext.SetError(const Message: string);
+procedure TFpDbgAbstractCallContext.SetError(const Message: string);
 begin
   FIsValid := False;
   FMessage := Message;
@@ -990,6 +1003,26 @@ begin
   Result := 0;
 end;
 
+procedure TFpValue.SetAsCardinal(AValue: QWord);
+begin
+  SetLastError(CreateError(fpErrChangeVariableNotSupported));
+end;
+
+procedure TFpValue.SetAsInteger(AValue: Int64);
+begin
+  SetLastError(CreateError(fpErrChangeVariableNotSupported));
+end;
+
+procedure TFpValue.SetAsBool(AValue: Boolean);
+begin
+  SetLastError(CreateError(fpErrChangeVariableNotSupported));
+end;
+
+procedure TFpValue.SetAsString(AValue: AnsiString);
+begin
+  SetLastError(CreateError(fpErrChangeVariableNotSupported));
+end;
+
 { TPasParserConstNumberSymbolValue }
 
 function TFpValueConstNumber.GetKind: TDbgSymbolKind;
@@ -1166,6 +1199,11 @@ end;
 function TFpDbgSymbolScope.FindSymbol(const AName: String): TFpValue;
 begin
   Result := nil;
+end;
+
+procedure TFpDbgSymbolScope.DoReferenceReleased;
+begin
+  inherited DoReferenceReleased;
 end;
 
 function TFpDbgSimpleLocationContext.GetMemManager: TFpDbgMemManager;

components/fpdebug/fperrormessages.pas
@@ -32,6 +32,7 @@ resourcestring
   MsgfpErrReadMemSizeLimit                        = 'Memory read size exceeds limit';
   MsgfpErrCanNotReadMemAtAddr             = 'Failed to read Mem at Address $%1:x';
   MsgfpErrFailedReadRegiseter             = 'Failed to read data from register';
+  MsgfpInternalErrFailedWriteMem          = 'Failed to write data';
   // 200 LocationParser
   MsgfpErrLocationParser                  = 'Internal Error: Cannot calculate location.';
   MsgfpErrLocationParserMemRead           = '%1:s (while calculating location)';          // Pass on nested error
@@ -56,6 +57,7 @@ const
   fpErrInvalidNumber                   = TFpErrorCode(28);
   fpErrCannotDereferenceType           = TFpErrorCode(29);
   fpErrTypeHasNoIndex                  = TFpErrorCode(30);
+  fpErrChangeVariableNotSupported      = TFpErrorCode(31);
 
   // 100 memreader error
   fpInternalErrFailedReadMem        = TFpErrorCode(100);
@@ -63,6 +65,9 @@ const
   fpErrReadMemSizeLimit             = TFpErrorCode(102);
   fpErrCanNotReadMemAtAddr          = TFpErrorCode(103);
   fpErrFailedReadRegister           = TFpErrorCode(104);
+  fpInternalErrCanNotWriteInvalidMem= TFpErrorCode(105);
+  fpErrFailedWriteMem               = TFpErrorCode(106);
+  fpErrCanNotWriteMemAtAddr         = TFpErrorCode(107);
 
   // 200 LocationParser
   fpErrLocationParser                 = TFpErrorCode(200);
@@ -200,6 +205,7 @@ begin
     fpInternalErrFailedReadMem:        Result := MsgfpInternalErrfpErrFailedReadMem;
     fpErrCanNotReadMemAtAddr:          Result := MsgfpErrCanNotReadMemAtAddr;
     fpErrFailedReadRegister:           Result := MsgfpErrFailedReadRegiseter;
+    fpErrFailedWriteMem:       Result := MsgfpInternalErrFailedWriteMem;
 
     fpErrLocationParser:                 Result := MsgfpErrLocationParser;
     fpErrLocationParserMemRead:          Result := MsgfpErrLocationParserMemRead;

components/fpdebug/languages/fperrormessages.pot
@@ -101,6 +101,10 @@ msgstr ""
 msgid "Internal error: Missing data location"
 msgstr ""
 
+#: fperrormessages.msgfpinternalerrfailedwritemem
+msgid "Failed to write data"
+msgstr ""
+
 #: fperrormessages.msgfpinternalerrfperrfailedreadmem
 msgid "Internal error: Failed to read data from memory"
 msgstr ""

components/fpdebug/languages/fperrormessages.pt_BR.po
@@ -112,6 +112,10 @@ msgstr "Impossível acessar elemento indexado na expressão %1:s"
 msgid "Internal error: Missing data location"
 msgstr "Erro interno: Localização dos dados faltando"
 
+#: fperrormessages.msgfpinternalerrfailedwritemem
+msgid "Failed to write data"
+msgstr ""
+
 #: fperrormessages.msgfpinternalerrfperrfailedreadmem
 msgid "Internal error: Failed to read data from memory"
 msgstr "Erro interno: Falha ao ler dados da memória"

components/fpdebug/languages/fperrormessages.ru.po
@@ -111,6 +111,10 @@ msgstr "Невозможно получить доступ к элементу �
 msgid "Internal error: Missing data location"
 msgstr "Внутренняя ошибка: неизвестно расположение данных"
 
+#: fperrormessages.msgfpinternalerrfailedwritemem
+msgid "Failed to write data"
+msgstr ""
+
 #: fperrormessages.msgfpinternalerrfperrfailedreadmem
 msgid "Internal error: Failed to read data from memory"
 msgstr "Внутренняя ошибка: не удалось прочитать данные из памяти"

components/fpdebug/fpdbgclasses.pp
@@ -136,6 +136,8 @@ type
     function ReadMemoryEx(AnAddress, AnAddressSpace: TDbgPtr; ASize: Cardinal; ADest: Pointer): Boolean; override;
     function ReadRegister(ARegNum: Cardinal; out AValue: TDbgPtr; AContext: TFpDbgLocationContext): Boolean; override;
     function RegisterSize(ARegNum: Cardinal): Integer; override;
+
+    function WriteRegister(ARegNum: Cardinal; const AValue: TDbgPtr; AContext: TFpDbgLocationContext): Boolean; override;
   end;
 
   { TDbgStackFrameInfo
@@ -1198,7 +1200,10 @@ var
   Process: TDbgProcess;
 begin
   Process := GetDbgProcess;
-  if not Process.GetThread(AContext.ThreadId, Result) then
+  // In fact, AContext should always be assigned, assuming that the main thread
+  // should be used is dangerous. But functions like TFpDbgMemManager.ReadSignedInt
+  // have a default value of nil for the context. Which is a lot of work to fix.
+  if not Assigned(AContext) or not Process.GetThread(AContext.ThreadId, Result) then
     Result := Process.MainThread;
 end;
 
@@ -1256,6 +1261,34 @@ begin
     AValue := ARegister.NumValue;
     result := true;
     end;
+end;
+
+function TDbgMemReader.WriteRegister(ARegNum: Cardinal; const AValue: TDbgPtr; AContext: TFpDbgLocationContext): Boolean;
+var
+  ARegister: TDbgRegisterValue;
+  StackFrame: Integer;
+  AFrame: TDbgCallstackEntry;
+  CtxThread: TDbgThread;
+begin
+  result := false;
+  CtxThread := GetDbgThread(AContext);
+  if CtxThread = nil then
+    exit;
+
+  if AContext <> nil then // TODO: Always true?
+    StackFrame := AContext.StackFrame
+  else
+    StackFrame := 0;
+  if StackFrame = 0 then
+    begin
+    ARegister:=CtxThread.RegisterValueList.FindRegisterByDwarfIndex(ARegNum);
+    if assigned(ARegister) then
+      begin
+      CtxThread.SetRegisterValue(ARegister.Name, AValue);
+      CtxThread.LoadRegisterValues;
+      result := true;
+      end;
+    end
 end;
 
 function TDbgMemReader.RegisterSize(ARegNum: Cardinal): Integer;

components/fpdebug/fpdbgcontroller.pas
@@ -11,8 +11,8 @@ uses
   Maps,
   LazLoggerBase, LazClasses,
   DbgIntfBaseTypes, DbgIntfDebuggerBase,
-  FpDbgDisasX86, FpDbgUtil,
-  FpDbgClasses,
+  FpDbgDisasX86,
+  FpDbgClasses, FpDbgCallContextInfo, FpDbgUtil,
   {$ifdef windows}  FpDbgWinClasses,  {$endif}
   {$ifdef darwin}  FpDbgDarwinClasses,  {$endif}
   {$ifdef linux}  FpDbgLinuxClasses,  {$endif}
@@ -425,6 +425,8 @@ begin
 
   FRoutineAddress := LocToAddr(ARoutineAddress);
   FCallContext := ACallContext;
+
+  StoreRegisters;
 end;
 
 procedure TDbgControllerCallRoutineCmd.Init;
@@ -433,7 +435,6 @@ begin
 
   FStep := sSingleStep;
   StoreInstructionPointer;
-  StoreRegisters;
   InsertCallInstructionCode;
 end;
 

commit 566610a9ba13f5c60e7a79081c3846e68fc7adf8

FpDebug: Cleanup of several issues introduced in r63899 #539c1f9a2b, in which a lot more was committed then planned. Among others utility functions to allow calling functions with parameters. The ability to set the value of (regvar) variables and some minor bug-fixes. As part of the parameter-support, addresses located in a register are not immediately read and converted to constants anymore. But the location of the actual register is stored. This might have unforeseen side-effects
git-svn-id: trunk@63900 -

.gitattributes
@@ -1460,6 +1460,7 @@ components/fpdebug/app/fpdserver/fpdserver.lpi svneol=native#text/plain
 components/fpdebug/app/fpdserver/fpdserver.lpr svneol=native#text/plain
 components/fpdebug/app/fpdserver/readme.txt svneol=native#text/plain
 components/fpdebug/fpdbgavrclasses.pas svneol=native#text/pascal
+components/fpdebug/fpdbgcallcontextinfo.pas svneol=native#text/pascal
 components/fpdebug/fpdbgclasses.pp svneol=native#text/pascal
 components/fpdebug/fpdbgcommon.pas svneol=native#text/pascal
 components/fpdebug/fpdbgcontroller.pas svneol=native#text/plain

components/fpdebug/fpdbgcallcontextinfo.pas
@@ -1,0 +1,44 @@
+unit FpDbgCallContextInfo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  DbgIntfBaseTypes,
+  FpDbgInfo,
+  FpdMemoryTools,
+  FpDbgDwarfDataClasses,
+  FpDbgDwarf;
+
+type
+  { TFpDbgInfoCallContext }
+
+  TFpDbgInfoCallContext = class(TFpDbgAbstractCallContext)
+  public
+    function CreateParamSymbol(AParamIndex: Integer; ASymbol: TFpSymbol): TFpValue; override;
+  end;
+
+implementation
+
+{ TFpDbgInfoCallContext }
+
+function TFpDbgInfoCallContext.CreateParamSymbol(AParamIndex: Integer; ASymbol: TFpSymbol): TFpValue;
+var
+  ParameterMemLocation: TFpDbgMemLocation;
+  TypeSymbol: TFpSymbol;
+  ParamSymbol: TFpSymbolDwarfFunctionResult;
+begin
+  ParameterMemLocation := RegisterLoc(5);
+  TypeSymbol := ASymbol.TypeInfo;
+  ParamSymbol := TFpSymbolDwarfFunctionResult.Create(ASymbol.Name, TDbgDwarfSymbolBase(ASymbol).InformationEntry, TypeSymbol.Kind, ParameterMemLocation);
+  try
+    Result := ParamSymbol.Value;
+  finally
+    ParamSymbol.ReleaseReference;
+  end;
+  TFpValueDwarf(Result).Context := Self;
+end;
+
+end.
+

components/fpdebug/fpdbgdwarfdataclasses.pas
@@ -2099,11 +2099,6 @@ begin
           FStack.PushConst(NewValue);
         end;
       DW_OP_regx: begin
-          //if not FMemManager.ReadRegister(ULEB128toOrdinal(CurData), NewValue, FContext) then begin
-          //  SetError;
-          //  exit;
-          //end;
-          //FStack.PushConst(NewValue);
           FStack.PushTargetRegister(ULEB128toOrdinal(CurData));
         end;
 

components/fpdebug/fpdbginfo.pas
@@ -493,7 +493,6 @@ type
     function GetProcedureAtAddress: TFpValue; virtual;
     function GetMemManager: TFpDbgMemManager; virtual;
     function GetSizeOfAddress: Integer; virtual;
-    procedure DoReferenceReleased; override;
   public
     constructor Create(ALocationContext: TFpDbgLocationContext);
     destructor Destroy; override;
@@ -1199,11 +1198,6 @@ end;
 function TFpDbgSymbolScope.FindSymbol(const AName: String): TFpValue;
 begin
   Result := nil;
-end;
-
-procedure TFpDbgSymbolScope.DoReferenceReleased;
-begin
-  inherited DoReferenceReleased;
 end;
 
 function TFpDbgSimpleLocationContext.GetMemManager: TFpDbgMemManager;

components/fpdebug/fperrormessages.pas
@@ -26,13 +26,17 @@ resourcestring
   MsgfpErrInvalidNumber                   = 'Cannot parse number: %1:s';
   MsgfpErrCannotDereferenceType           = 'Cannot dereference Expression "%1:s"';
   MsgfpErrTypeHasNoIndex                  = 'Cannot access indexed element in expression %1:s';
+  MsgfpErrChangeVariableNotSupported      = 'Changing the value of this variable is not supported';
   // 100 memreader error
   MsgfpInternalErrfpErrFailedReadMem              = 'Internal error: Failed to read data from memory';
   MsgfpInternalErrCanNotReadInvalidMem            = 'Internal error: Missing data location';
   MsgfpErrReadMemSizeLimit                        = 'Memory read size exceeds limit';
   MsgfpErrCanNotReadMemAtAddr             = 'Failed to read Mem at Address $%1:x';
   MsgfpErrFailedReadRegiseter             = 'Failed to read data from register';
-  MsgfpInternalErrFailedWriteMem          = 'Failed to write data';
+  MsgfpErrFailedWriteMem                  = 'Failed to write data';
+  MsgfpInternalErrCanNotWriteInvalidMem   = 'Internal error writing data: Missing data location';
+  MsgfpErrCanNotWriteMemAtAddr            = 'Failed to read Mem at Address $%1:x';
+
   // 200 LocationParser
   MsgfpErrLocationParser                  = 'Internal Error: Cannot calculate location.';
   MsgfpErrLocationParserMemRead           = '%1:s (while calculating location)';          // Pass on nested error
@@ -199,13 +203,16 @@ begin
     fpErrInvalidNumber:                   Result := MsgfpErrInvalidNumber;
     fpErrCannotDereferenceType:           Result := MsgfpErrCannotDereferenceType;
     fpErrTypeHasNoIndex: Result := MsgfpErrTypeHasNoIndex;
+    fpErrChangeVariableNotSupported:      Result := MsgfpErrChangeVariableNotSupported;
 
     fpInternalErrCanNotReadInvalidMem: Result := MsgfpInternalErrCanNotReadInvalidMem;
     fpErrReadMemSizeLimit:             Result := MsgfpErrReadMemSizeLimit;
     fpInternalErrFailedReadMem:        Result := MsgfpInternalErrfpErrFailedReadMem;
     fpErrCanNotReadMemAtAddr:          Result := MsgfpErrCanNotReadMemAtAddr;
     fpErrFailedReadRegister:           Result := MsgfpErrFailedReadRegiseter;
-    fpErrFailedWriteMem:       Result := MsgfpInternalErrFailedWriteMem;
+    fpInternalErrCanNotWriteInvalidMem:Result := MsgfpInternalErrCanNotWriteInvalidMem;
+    fpErrFailedWriteMem:               Result := MsgfpErrFailedWriteMem;
+    fpErrCanNotWriteMemAtAddr:         Result := MsgfpErrCanNotWriteMemAtAddr;
 
     fpErrLocationParser:                 Result := MsgfpErrLocationParser;
     fpErrLocationParserMemRead:          Result := MsgfpErrLocationParserMemRead;

components/fpdebug/languages/fperrormessages.pot
@@ -17,11 +17,27 @@ msgstr ""
 
 #: fperrormessages.msgfperrcannotreadmemataddr
 #, object-pascal-format
+msgctxt "fperrormessages.msgfperrcannotreadmemataddr"
 msgid "Failed to read Mem at Address $%1:x"
+msgstr ""
+
+#: fperrormessages.msgfperrcannotwritememataddr
+#, object-pascal-format
+msgctxt "fperrormessages.msgfperrcannotwritememataddr"
+msgid "Failed to read Mem at Address $%1:x"
+msgstr ""
+
+#: fperrormessages.msgfperrchangevariablenotsupported
+msgid "Changing the value of this variable is not supported"
 msgstr ""
 
 #: fperrormessages.msgfperrfailedreadregiseter
 msgid "Failed to read data from register"
+msgstr ""
+
+#: fperrormessages.msgfperrfailedwritemem
+msgctxt "fperrormessages.msgfperrfailedwritemem"
+msgid "Failed to write data"
 msgstr ""
 
 #: fperrormessages.msgfperrinvalidnumber
@@ -101,8 +117,8 @@ msgstr ""
 msgid "Internal error: Missing data location"
 msgstr ""
 
-#: fperrormessages.msgfpinternalerrfailedwritemem
-msgid "Failed to write data"
+#: fperrormessages.msgfpinternalerrcannotwriteinvalidmem
+msgid "Internal error writing data: Missing data location"
 msgstr ""
 
 #: fperrormessages.msgfpinternalerrfperrfailedreadmem

components/fpdebug/languages/fperrormessages.pt_BR.po
@@ -28,12 +28,28 @@ msgstr "Impossível desreferenciar Expressão \"%1:s\""
 
 #: fperrormessages.msgfperrcannotreadmemataddr
 #, object-pascal-format
+msgctxt "fperrormessages.msgfperrcannotreadmemataddr"
 msgid "Failed to read Mem at Address $%1:x"
 msgstr "Falha ao ler memória no endereço $%1:x"
+
+#: fperrormessages.msgfperrcannotwritememataddr
+#, object-pascal-format, fuzzy
+msgctxt "fperrormessages.msgfperrcannotwritememataddr"
+msgid "Failed to read Mem at Address $%1:x"
+msgstr "Falha ao ler memória no endereço $%1:x"
+
+#: fperrormessages.msgfperrchangevariablenotsupported
+msgid "Changing the value of this variable is not supported"
+msgstr ""
 
 #: fperrormessages.msgfperrfailedreadregiseter
 msgid "Failed to read data from register"
 msgstr "Falha ao ler dados do registrador"
+
+#: fperrormessages.msgfperrfailedwritemem
+msgctxt "fperrormessages.msgfperrfailedwritemem"
+msgid "Failed to write data"
+msgstr ""
 
 #: fperrormessages.msgfperrinvalidnumber
 #, object-pascal-format
@@ -112,8 +128,8 @@ msgstr "Impossível acessar elemento indexado na expressão %1:s"
 msgid "Internal error: Missing data location"
 msgstr "Erro interno: Localização dos dados faltando"
 
-#: fperrormessages.msgfpinternalerrfailedwritemem
-msgid "Failed to write data"
+#: fperrormessages.msgfpinternalerrcannotwriteinvalidmem
+msgid "Internal error writing data: Missing data location"
 msgstr ""
 
 #: fperrormessages.msgfpinternalerrfperrfailedreadmem

components/fpdebug/languages/fperrormessages.ru.po
@@ -27,12 +27,28 @@ msgstr "Невозможно разыменовать выражение \"%1:s\
 
 #: fperrormessages.msgfperrcannotreadmemataddr
 #, object-pascal-format
+msgctxt "fperrormessages.msgfperrcannotreadmemataddr"
 msgid "Failed to read Mem at Address $%1:x"
 msgstr "Не удалось прочитать память по адресу $%1:x"
+
+#: fperrormessages.msgfperrcannotwritememataddr
+#, object-pascal-format, fuzzy
+msgctxt "fperrormessages.msgfperrcannotwritememataddr"
+msgid "Failed to read Mem at Address $%1:x"
+msgstr "Не удалось прочитать память по адресу $%1:x"
+
+#: fperrormessages.msgfperrchangevariablenotsupported
+msgid "Changing the value of this variable is not supported"
+msgstr ""
 
 #: fperrormessages.msgfperrfailedreadregiseter
 msgid "Failed to read data from register"
 msgstr "Не удалось прочитать данные из регистра"
+
+#: fperrormessages.msgfperrfailedwritemem
+msgctxt "fperrormessages.msgfperrfailedwritemem"
+msgid "Failed to write data"
+msgstr ""
 
 #: fperrormessages.msgfperrinvalidnumber
 #, object-pascal-format
@@ -111,8 +127,8 @@ msgstr "Невозможно получить доступ к элементу �
 msgid "Internal error: Missing data location"
 msgstr "Внутренняя ошибка: неизвестно расположение данных"
 
-#: fperrormessages.msgfpinternalerrfailedwritemem
-msgid "Failed to write data"
+#: fperrormessages.msgfpinternalerrcannotwriteinvalidmem
+msgid "Internal error writing data: Missing data location"
 msgstr ""
 
 #: fperrormessages.msgfpinternalerrfperrfailedreadmem