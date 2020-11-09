ba08e77f4572a6cd1897d9c403797b4714827c09
mattias <nc-gaertnma@netcologne.de>
2020-11-09 18:03:36 +0000

    lcl: fixed compile with fpc 3.0.4
    git-svn-id: trunk@64119 -

--- lcl/lresources.pp
+++ lcl/lresources.pp
@@ -251,7 +251,7 @@ type
     procedure FlushStackToStream;
     procedure WriteToStream(const Buffer; Count: Longint);
   protected
-    procedure FlushBuffer; override;
+    procedure FlushBuffer; {$IF FPC_FULLVERSION >= 30200}override;{$ENDIF}
     procedure WriteValue(Value: TValueType);
     procedure WriteStr(const Value: String);
     procedure WriteIntegerContent(i: integer);
