13e0aa558a526eb3ece2c68cf15c9890ce017f43
martin <laz.git@mfriebe.de>
2021-02-10 18:16:38 +0000

    DebuggerTest: restore use resources
    git-svn-id: trunk@64520 -

--- components/lazdebuggers/lazdebugtestbase/testcommonsources.pas
+++ components/lazdebuggers/lazdebugtestbase/testcommonsources.pas
@@ -10,7 +10,7 @@ uses
   LazFileUtils, LazUTF8,
   TestOutputLogger;
 
-{.$R sources.rc}
+{$R sources.rc} // change to .res if you do not have windres (or fpcres). Ensure you have the latest .res pre-compiled resource
 
 type
 
