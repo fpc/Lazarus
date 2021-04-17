39035d597a42460a853ccffb07ffc0f9eabf0850
mattias <nc-gaertnma@netcologne.de>
2021-04-17 11:34:48 +0000

    IDEIntf: objectinspector: fixed frreing not used prop editors
    git-svn-id: trunk@65013 -

--- components/ideintf/objectinspector.pp
+++ components/ideintf/objectinspector.pp
@@ -2145,10 +2145,7 @@ var
 begin
   if not EditorFilter(PropEditor) then
   begin
-    // if some elements of a set is not being shown then free their editor
-    // to avoid memory leaks; sine only visible editors will be cleared.
-    if PropEditor.ClassType = TSetElementPropertyEditor then
-      PropEditor.Free;
+    PropEditor.Free;
     Exit;
   end;
 
