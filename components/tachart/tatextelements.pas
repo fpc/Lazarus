84d1a95877ecd98c93e89f8a1249d16eafc79dba
wp_xyz <wp_xxyyzzz@gmx.net>
2021-08-03 14:08:05 +0200

    TAChart: Fix usage of Margins in TChartTitle.Assign
    (cherry picked from commit cbbc6306a8a95b0ac243277fb81a40296bac9b34)

--- components/tachart/tatextelements.pas
+++ components/tachart/tatextelements.pas
@@ -606,6 +606,7 @@ begin
       Self.FFont.Assign(Font);
       Self.FFrame.Assign(Frame);
       Self.FText.Assign(Text);
+      Self.FMargin := Margin;
       Self.FWordWrap := WordWrap;
    end;
 
