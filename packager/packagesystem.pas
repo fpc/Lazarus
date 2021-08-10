5a9d725dfecf70fe9ef088d3d9a1629f96290edd
Sławek Załęcki <git@naty.pl>
2021-08-10 23:12:47 +0000

    Update packagesystem.pas - fix curly brackets to square brackets

--- packager/packagesystem.pas
+++ packager/packagesystem.pas
@@ -4169,7 +4169,7 @@ begin
       // check ambiguous units
       Result:=CheckAmbiguousPackageUnits(APackage);
       if Result<>mrOk then begin
-        DebugLn('Error: (lazarus) [TLazPackageGraph.CompilePackage} CheckAmbiguousPackageUnits failed: ',APackage.IDAsString);
+        DebugLn('Error: (lazarus) [TLazPackageGraph.CompilePackage] CheckAmbiguousPackageUnits failed: ',APackage.IDAsString);
         exit;
       end;
 
@@ -5317,7 +5317,7 @@ begin
   // delete ambiguous files
   Result:=DeleteAmbiguousFiles(SrcFilename);
   if Result=mrAbort then begin
-    DebugLn('Error: (lazarus) [TLazPackageGraph.SavePackageMainSource} DoDeleteAmbiguousFiles failed');
+    DebugLn('Error: (lazarus) [TLazPackageGraph.SavePackageMainSource] DoDeleteAmbiguousFiles failed');
     exit;
   end;
 
