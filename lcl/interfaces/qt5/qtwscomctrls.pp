1eaab5e736a44cad12ab4bfee572bdae5b4301ed
Željan Rikalo <zeljko@lazarus-ide.org>
2021-08-30 13:08:03 +0000

    Revert "Qt,Qt5: Fixed checkboxes states after sorting in TListView. issue #38137"
    This reverts commit 109c9f14ba46dd9b9dd200f81dda4d6d2bf16147

--- lcl/interfaces/qt5/qtwscomctrls.pp
+++ lcl/interfaces/qt5/qtwscomctrls.pp
@@ -165,8 +165,6 @@ type
     class function GetViewOrigin(const ALV: TCustomListView): TPoint; override;
     class function GetVisibleRowCount(const ALV: TCustomListView): Integer; override;
 
-    class function RestoreItemCheckedAfterSort(const ALV: TCustomListView): Boolean; override;
-
     class procedure SelectAll(const ALV: TCustomListView; const AIsSet: Boolean); override;
 
     class procedure SetAllocBy(const ALV: TCustomListView; const AValue: Integer); override;
@@ -2071,17 +2069,10 @@ begin
       QtTreeWidget.setItemData(Item, 0, AItem);
       if AList.Checkboxes then
       begin
-        if RestoreItemCheckedAfterSort(AList) then
-          // when true checkboxes are fixed by LCL, so we uncheck each item first
-          // look at TCustomListView.SortWithParams. issue #38137
-          QtTreeWidget.ItemChecked[i] := False
+        if AItem.Checked then
+          QTreeWidgetItem_setCheckState(Item, 0, QtChecked)
         else
-        begin
-          if AItem.Checked then
-            QTreeWidgetItem_setCheckState(Item, 0, QtChecked)
-          else
-            QTreeWidgetItem_setCheckState(Item, 0, QtUnChecked);
-        end;
+          QTreeWidgetItem_setCheckState(Item, 0, QtUnChecked);
       end;
 
       if ImgListRes.Valid and (ImgListRes.Count > 0) and
@@ -2231,12 +2222,6 @@ begin
   if not WSCheckHandleAllocated(ALV, 'GetVisibleRowCount') then
     Exit;
   Result := TQtAbstractItemView(ALV.Handle).getVisibleRowCount;
-end;
-
-class function TQtWSCustomListView.RestoreItemCheckedAfterSort(
-  const ALV: TCustomListView): Boolean;
-begin
-  Result := True;
 end;
 
 class procedure TQtWSCustomListView.SelectAll(const ALV: TCustomListView;
