8af9cd091878ffd38504bf536a549a5697ea9c79
dsiders <dsiders@lazarus-ide.org>
2021-10-28 02:55:57 +0100

    Docs: Modifies build_lcl_docs and scripts.Address an issue in #28500.
    Modified files:
    
    * docs/html/build_lcl_docs.lpi
    *  docs/html/build_lcl_docs.lpr
    Adds TFPDocRun.ChmTitle property for CHM title and text in the LHelp TOC navigation tree.
    Sets the footer file for each of the output formats generated in the program. Allows CHM to have links to the online HTML version, while HTML does not.
    Modifies logic used to generate links to external topics. Causes LazUtils (the smaller of the two outputs) to be created twice.
    
    * docs/html/build_chm.bat
    * docs/html/build_chm.sh
    Adds logic to generate distinct footer files for CHM vs HTML. Footer files include document title, version information, and generation date.
    
    * docs/html/build_html.bat
    * docs/html/build_html.sh
    Copies custom CSS file to the output directory.
    
    * docs/html/chmlazutilsfooter.xml
    * docs/html/chmlclfooter.xml
    * docs/html/locallazutilsfooter.xml
    * docs/html/locallclfooter.xml
    Standardized footer files for CHM and HTML output formats. A separate footer file is used for LCL and LazUtils for each of the output formats.
    
    * docs/html/fpdoc.css
    Adds font family fallbacks for common default fonts on various platforms.
    Modifies layout and borders for definition lists (DL, DT, DD tags). Removes the border around DL.
    Adds style rules for tags, classes used in file footers.

--- docs/html/build_html.sh
+++ docs/html/build_html.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# Builds all Lazarus docs and pack them in one .chm file
-#
-# Notes:
-#
-# Before running this file, first compile the project build_lcl_docs.lpi
-#
-# In order to link to RTL and FCL, place rtl.xct and fcl.xct in ../chm/
-#
-./build_lcl_docs --fpcdocs=../chm --outfmt html
 
+# run build_chm.sh first... it generates the footer files with version and revision info
 
+# change any path information used in this script for your system
+fpdocpath="c:/lazarus/fpc/3.2.0/bin/x86_64-win64"
+
+# build html output format using locallclfooter.xml, locallazutilsfooter.xml for footer content
+./build_lcl_docs.exe --outfmt=html --css-file=fpdoc.css --fpcdocs=../chm --footer locallclfooter.xml --fpdoc=$fpdocpath/fpdoc.exe --warnings --verbose 2>&1 | tee ./build_html.log
+
+# use custom css instead of the built in one
+cp lazutils/fpdoc.css lazutils/lazutils/
+cp lcl/fpdoc.css lcl/lcl/
