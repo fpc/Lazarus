a323f31a96399dfbb1a2eec7a332cfb821aab942
Martin <laz.git@mfriebe.de>
2021-11-01 17:36:25 +0100

    Revert unrelated change

--- components/fpdebug/fpimgreaderelf.pas
+++ components/fpdebug/fpimgreaderelf.pas
@@ -332,7 +332,7 @@ begin
     New(p);
     P^.Offs := fs.FileOfs;
     p^.Sect.Size := fs.Size;
-    p^.Sect.VirtualAddress := fs.Address; //0; // Todo? fs.Address - ImageBase
+    p^.Sect.VirtualAddress := 0; // Todo? fs.Address - ImageBase
     p^.Loaded := False;
     FSections.Objects[idx] := TObject(p);
   end;
