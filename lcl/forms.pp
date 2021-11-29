8438ed9d988f4d7eb33f997b7156bdacb39ccd71
Ondrej Pokorny <lazarus@kluug.net>
2021-11-29 22:05:19 +0100

    Revert "LCL: Add TCustomFrame.CreateParams. Somehow fixes issue #25124."
    This reverts commit 20c1e10aca898dc1f22b325c223be7260b49fa43.

--- lcl/forms.pp
+++ lcl/forms.pp
@@ -314,7 +314,6 @@ type
     procedure DefineProperties(Filer: TFiler); override;
     procedure CalculatePreferredSize(var PreferredWidth,
            PreferredHeight: integer; WithThemeSpace: Boolean); override;
-    procedure CreateParams(var Params: TCreateParams); override;
     procedure UpdateOpaque;
   public
     constructor Create(AOwner: TComponent); override;
