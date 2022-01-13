708715204e430f1ac42d3aae62a1004073c7a53a
Martin <laz.git@mfriebe.de>
2022-01-13 18:20:13 +0100

    FpDebug: Win32, Fix "Run (F9)" incorrectly triggered watch/stack updates while the app was still running (slowing execution).
    (cherry picked from commit a92db946ec220c40f3c959506c014b8319b87494)

--- components/lazdebuggers/lazdebuggerfp/fpdebugdebugger.pas
+++ components/lazdebuggers/lazdebuggerfp/fpdebugdebugger.pas
@@ -3070,6 +3070,7 @@ begin
     bplFpcSpecific, bplRtlRestoreContext, bplRtlUnwind,
     {$ENDIF}
     bplFpcExceptHandler ,bplFpcFinallyHandler, bplFpcLeaveHandler,
+    bplSehW32Except, bplSehW32Finally,
     {$ENDIF}
     bplStepOut]);
 
@@ -3079,6 +3080,7 @@ begin
       {$IFDEF WIN64} , bplRtlRestoreContext, bplFpcSpecific {$ENDIF}
       , bplFpcExceptHandler ,bplFpcFinallyHandler, bplFpcLeaveHandler
       {$ENDIF}
+      , bplSehW32Except, bplSehW32Finally
       ]);
 
   case st of
@@ -3091,6 +3093,7 @@ begin
           {$IFDEF MSWINDOWS}
           {$IFDEF WIN64} , bplFpcSpecific {$ENDIF}
           , bplFpcExceptHandler ,bplFpcFinallyHandler, bplFpcLeaveHandler
+          , bplSehW32Except, bplSehW32Finally
           {$ENDIF}
           ]);
       end
