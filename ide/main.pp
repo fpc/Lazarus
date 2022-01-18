f7d2d0a69094f47df78cea089b89febe39d38936
Juha <juha@lazarus-ide.org>
2022-01-18 11:55:27 +0200

    IDE: Use '-W' parameter for MacOS bundle on "Run without Debugging". Simplify the code. Issue #39567.

--- ide/main.pp
+++ ide/main.pp
@@ -7298,22 +7298,22 @@ var
   Process: TProcessUTF8;
   RunCmdLine, RunWorkingDirectory, ExeFile: string;
   Params: TStringList;
-  RunAppBundle, Handled: Boolean;
+  Handled: Boolean;
   ARunMode: TRunParamsOptionsMode;
 begin
   debugln(['Hint: (lazarus) [TMainIDE.DoRunProjectWithoutDebug] START']);
   if Project1=nil then
     Exit(mrNone);
 
-  Handled:=false;
-  Result:=DoCallRunWithoutDebugBuilding(Handled);
+  Handled := false;
+  Result := DoCallRunWithoutDebugBuilding(Handled);
   if Handled then exit;
 
   Result := DoBuildProject(crRun,[]);
   if Result <> mrOK then
     Exit;
 
-  Result:=DoCallRunWithoutDebugInit(Handled);
+  Result := DoCallRunWithoutDebugInit(Handled);
   if Handled then exit;
 
   RunCmdLine := MainBuildBoss.GetRunCommandLine;
@@ -7325,22 +7325,18 @@ begin
     Exit(mrNone);
   end;
 
-  Params:=TStringList.Create;
+  Params := TStringList.Create;
   Process := TProcessUTF8.Create(nil);
   try
-    RunAppBundle:={$IFDEF Darwin}true{$ELSE}false{$ENDIF};
-    RunAppBundle:=RunAppBundle and Project1.UseAppBundle;
-
     SplitCmdLineParams(RunCmdLine,Params);
     if Params.Count=0 then begin
       IDEMessageDialog(lisUnableToRun,
         Format(lisUnableToRun2, ['<project has no target file>']),
         mtError, [mbOK]);
       exit(mrCancel);
-    end else begin
-      ExeFile:=Params[0];
-      Params.Delete(0);
     end;
+    ExeFile := Params[0];
+    Params.Delete(0);
     //writeln('TMainIDE.DoRunProjectWithoutDebug ExeFile=',ExeFile);
     Process.Executable := ExeFile;
     Process.Parameters.Assign(Params);
@@ -7353,28 +7349,28 @@ begin
     if not GlobalMacroList.SubstituteStr(RunWorkingDirectory) then
       RunWorkingDirectory := '';
     if RunWorkingDirectory = '' then
-      RunWorkingDirectory := ExtractFilePath(Process.Executable);
+      RunWorkingDirectory := ExtractFilePath(ExeFile);
     Process.CurrentDirectory := RunWorkingDirectory;
 
-    if RunAppBundle
-        and (FileExistsUTF8(Process.Executable)
-        or DirectoryExistsUTF8(Process.Executable))
-        and FileExistsUTF8('/usr/bin/open') then
+    if MainBuildBoss.GetProjectUsesAppBundle
+    and (FileExistsUTF8(ExeFile) or DirectoryExistsUTF8(ExeFile))
+    and FileExistsUTF8('/usr/bin/open') then
     begin
       // run bundle via open
-      Process.Parameters.Insert(0,Process.Executable);
+      Process.Parameters.Insert(0,'-W');
+      Process.Parameters.Insert(1,ExeFile);
       Process.Executable := '/usr/bin/open';
-    end else if not FileIsExecutable(Process.Executable) then
+    end
+    else if not FileIsExecutable(ExeFile) then
     begin
       MainBuildBoss.WriteDebug_RunCommandLine;
       if (ARunMode<>nil) and ARunMode.UseLaunchingApplication then
         IDEMessageDialog(lisLaunchingApplicationInvalid,
           Format(lisTheLaunchingApplicationDoesNotExistsOrIsNotExecuta,
-                 [Process.Executable, LineEnding, LineEnding+LineEnding]),
+                 [ExeFile, LineEnding, LineEnding+LineEnding]),
           mtError, [mbOK])
       else
-        IDEMessageDialog(lisUnableToRun,
-          Format(lisUnableToRun2, [Process.Executable]),
+        IDEMessageDialog(lisUnableToRun, Format(lisUnableToRun2, [ExeFile]),
           mtError, [mbOK]);
       Exit(mrCancel);
     end;
