db27890d55e97710b668122b020ae8201bc60076
Željan Rikalo <zeljko@lazarus-ide.org>
2022-02-25 17:53:15 +0100

    Qt5: added QPixmapCache_clear(). part of issue #38844

--- lcl/interfaces/qt5/cbindings/Qt5Pas.pro
+++ lcl/interfaces/qt5/cbindings/Qt5Pas.pro
@@ -212,6 +212,7 @@ HEADERS +=  \
            qtextdocumentwriter_c.h \
            qicon_c.h \
            qpixmap_c.h \
+           qpixmapcache_c.h \
            qimage_c.h \
            qbitmap_c.h \
            qpicture_c.h \
@@ -556,6 +557,7 @@ SOURCES +=  \
            qtextdocumentwriter_c.cpp \
            qicon_c.cpp \
            qpixmap_c.cpp \
+           qpixmapcache_c.cpp \
            qimage_c.cpp \
            qbitmap_c.cpp \
            qpicture_c.cpp \
---
+++ lcl/interfaces/qt5/cbindings/src/qpixmapcache_c.h
@@ -0,0 +1,20 @@
+//******************************************************************************
+//  Copyright (c) 2014-2022 by Željan Rikalo
+//
+//  See the included file COPYING.TXT for details about the copyright.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//******************************************************************************
+
+
+#ifndef QPIXMAP_C_H
+#define QPIXMAP_C_H
+
+#include <QtGui>
+#include "pascalbind.h"
+
+C_EXPORT void QPixmapCache_Clear();
+
+#endif
