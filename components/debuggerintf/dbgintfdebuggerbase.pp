9e36e91b7d9f30069a35a7c89c47d3a152100d1f
Martin <laz.git@mfriebe.de>
2022-02-25 20:23:20 +0100

    Debugger: Registers, clear incorrectly cached values. Fix wrong value shown, if display-format is changed.
    (cherry picked from commit 448ccc8ca179ab6a97d2bebebf600e28b6280e1d)

--- components/debuggerintf/dbgintfdebuggerbase.pp
+++ components/debuggerintf/dbgintfdebuggerbase.pp
@@ -3304,6 +3304,8 @@ begin
   if FDataValidity = AValidity then exit;
   Old := FDataValidity;
   FDataValidity := AValidity;
+  if AValidity = ddsUnknown then
+    ClearDispValues;
   DoDataValidityChanged(Old);
 end;
 
