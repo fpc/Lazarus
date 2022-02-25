ed5a24948450edd1d41cd062a1aba64310407992
Željan Rikalo <zeljko@lazarus-ide.org>
2022-02-25 17:53:15 +0100

    Qt5: added QPixmapCache_clear(). part of issue #38844
    (cherry picked from commit db27890d55e97710b668122b020ae8201bc60076)

---
+++ lcl/interfaces/qt5/cbindings/src/qpixmapcache_c.cpp
@@ -0,0 +1,16 @@
+//******************************************************************************
+//  Copyright (c) 2014-2022 by Željan Rikalo
+//
+//  See the included file COPYING.TXT for details about the copyright.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//******************************************************************************
+
+#include "qpixmapcache_c.h"
+
+void QPixmapCache_Clear()
+{
+  QPixmapCache::clear();
+}
