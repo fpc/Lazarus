ae6d20b6a4fffeacf84b3f9ee43cbe450dbd20e8
Juha <juha@lazarus-ide.org>
2022-03-29 21:41:54 +0300

    Codetools: Add target processor names for SPARC CPU matching the names in FPC. Issue #39692.

--- components/codetools/definetemplates.pas
+++ components/codetools/definetemplates.pas
@@ -3848,9 +3848,9 @@ procedure GetTargetProcessors(const TargetCPU: string; aList: TStrings);
 
   procedure Sparc;
   begin
-    aList.Add('SPARC V7');
-    aList.Add('SPARC V8');
-    aList.Add('SPARC V9');
+    aList.Add('SPARCV7');
+    aList.Add('SPARCV8');
+    aList.Add('SPARCV9');
   end;
 
   procedure Mips;
