537f43754ca77e39f15839299b9f7059e39f90dd
mattias <nc-gaertnma@netcologne.de>
2022-05-15 09:51:43 +0200

    set fixes version to 2.2.2

--- components/lazutils/lazversion.pas
+++ components/lazutils/lazversion.pas
@@ -22,10 +22,10 @@ interface
 const
   laz_major = 2;
   laz_minor = 2;
-  laz_release = 0;
-  laz_patch = 4;
+  laz_release = 2;
+  laz_patch = 0;
   laz_fullversion = ((laz_major *  100 + laz_minor) * 100 + laz_release) * 100 + laz_patch;
-  laz_version = '2.2.0.4';
+  laz_version = '2.2.2.0';
 
 implementation
 
