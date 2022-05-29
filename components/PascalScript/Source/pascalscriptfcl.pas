2d84fa6e4a4a7fdab0a4cde772aef1016d3f221f
mattias <nc-gaertnma@netcologne.de>
2022-05-29 20:10:47 +0200

    pascalscript: added missing file

---
+++ components/PascalScript/Source/pascalscriptfcl.pas
@@ -0,0 +1,18 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit PascalScriptFCL;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  uPSRuntime, uPSC_classes, uPSC_dateutils, uPSC_DB, uPSC_dll, uPSC_std, 
+  uPSCompiler, uPSComponent, uPSComponent_DB, uPSComponent_Default, uPSUtils, 
+  uPSDebugger, uPSDisassembly, uPSPreProcessor, uPSR_classes, uPSR_dateutils, 
+  uPSR_DB, uPSR_dll, uPSR_std, uPSComponent_COM, uPSC_comobj, uPSR_comobj;
+
+implementation
+
+end.
