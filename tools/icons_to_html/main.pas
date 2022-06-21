61f77f3cbf1001c9af84e4b7f70eadf44f32cb77
wp_xyz <wp_xxyyzzz@gmx.net>
2022-06-21 23:18:22 +0200

    Tools/icons_to_html: Add mouse-over to see the images in dark-mode. Store parameters in ini file. By Roland Hahn.

--- tools/icons_to_html/main.pas
+++ tools/icons_to_html/main.pas
@@ -6,33 +6,43 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Dialogs, StdCtrls, EditBtn, FileUtil,
-  LazUTF8, LazFileUtils, LCLIntf, SynEdit, SynHighlighterHTML;
+  LazUTF8, LazFileUtils, LCLIntf, Buttons, Menus, IniFiles,
+  SynEdit, SynHighlighterHTML;
 
 type
 
   { TMainForm }
 
   TMainForm = class(TForm)
-    btnCreateHTML: TButton;
-    btnSave: TButton;
-    btnShow: TButton;
-    btnClose: TButton;
+    bbtnClose: TBitBtn;
+    bbtnCreateHTML: TBitBtn;
+    bbtnSave: TBitBtn;
+    bbtnShow: TBitBtn;
     cbDarkMode: TCheckBox;
     DirectoryEdit: TDirectoryEdit;
+    ImageList: TImageList;
+    popLastDirs: TPopupMenu;
+    sbtnLastDirs: TSpeedButton;
     SynEdit: TSynEdit;
     SynHTMLSyn: TSynHTMLSyn;
     TaskDialog: TTaskDialog;
+    procedure bbtnCloseClick(Sender: TObject);
+    procedure bbtnCreateHTMLClick(Sender: TObject);
+    procedure bbtnSaveClick(Sender: TObject);
+    procedure bbtnShowClick(Sender: TObject);
     procedure cbDarkModeChange(Sender: TObject);
-    procedure FormShow(Sender: TObject);
     procedure DirectoryEditChange(Sender: TObject);
-    procedure btnCreateHTMLClick(Sender: TObject);
-    procedure btnSaveClick(Sender: TObject);
-    procedure btnShowClick(Sender: TObject);
-    procedure btnCloseClick(Sender: TObject);
+    procedure DirectoryEditEditingDone(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure LastDirClick(Sender: TObject);
+    procedure sbtnLastDirsClick(Sender: TObject);
   private
     fn: String;
     ImgDir: String;
-    function GetDestFileName: String;
+    Config: TIniFile;
+    LastDirsMax: Integer;
     procedure InfoMsg(const AMsg: String);
     procedure ErrorMsg(const AMsg: String);
   public
@@ -48,29 +58,89 @@ implementation
 
 { TMainForm }
 
-procedure TMainForm.FormShow(Sender: TObject);
+procedure TMainForm.FormCreate(Sender: TObject);
+var
+  i: Integer;
+  MenItem: TMenuItem;
 begin
-  ImgDir := CleanAndExpandDirectory('../../images/general_purpose/');
-  if DirectoryExists(ImgDir) then
-    DirectoryEdit.Text := ImgDir
-  else
-    DirectoryEdit.Text := CleanAndExpandDirectory(GetCurrentDir);
+  LastDirsMax := 9;
+  for i := 0 to LastDirsMax do
+  begin
+    MenItem := TMenuItem.Create(popLastDirs);
+    MenItem.OnClick := @LastDirClick;
+    MenItem.ImageIndex := 6;
+    popLastDirs.Items.Add(MenItem);
+  end;
+end;
+
+procedure TMainForm.FormShow(Sender: TObject);
+var
+  i: Integer;
+begin
+  Config := TIniFile.Create('IconTableConfig.ini');
+  try
+    Top := Config.ReadInteger('Position', 'Top', 100);
+    Left := Config.ReadInteger('Position', 'Left', 100);
+    Width := Config.ReadInteger('Position', 'Width', 100);
+    Height := Config.ReadInteger('Position', 'Height', 100);
+    WindowState := wsNormal;
+    Application.ProcessMessages;
+    WindowState := TWindowState(Config.ReadInteger('Position', 'WindowState', 0));
+
+    for i := 0 to LastDirsMax do
+    begin
+      popLastDirs.Items[i].Caption := Config.ReadString('LastDirs', 'LastDir' + i.ToString, '');
+      popLastDirs.Items[i].Visible := popLastDirs.Items[i].Caption > '';
+    end;
+
+    cbDarkMode.Checked := Config.ReadBool('Options', 'DarkMode', False);
+  finally
+    Config.Free;
+  end;
+
+  if (popLastDirs.Items[0].Caption = '') and (DirectoryExists(CleanAndExpandDirectory('../../images/general_purpose/'))) then
+  begin
+    popLastDirs.Items[0].Caption := CleanAndExpandDirectory('../../images/general_purpose/');
+    popLastDirs.Items[0].Visible := popLastDirs.Items[0].Caption > '';
+  end;
+
+  if DirectoryExists(popLastDirs.Items[0].Caption) then
+    DirectoryEdit.Directory := popLastDirs.Items[0].Caption;
+
+  sbtnLastDirs.Enabled := popLastDirs.Items[0].Caption > '';
+end;
+
+procedure TMainForm.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+var
+  i: Integer;
+begin
+  if WindowState = wsMinimized then
+    WindowState := wsNormal;
+
+  Config := TIniFile.Create('IconTableConfig.ini');
+  try
+    Config.WriteInteger('Position', 'Top', RestoredTop);
+    Config.WriteInteger('Position', 'Left', RestoredLeft);
+    Config.WriteInteger('Position', 'Width', RestoredWidth);
+    Config.WriteInteger('Position', 'Height', RestoredHeight);
+    Config.WriteInteger('Position', 'WindowState', Integer(WindowState));
+
+    for i := 0 to LastDirsMax do
+      Config.WriteString('LastDirs', 'LastDir' + i.ToString, popLastDirs.Items[i].Caption);
+
+    Config.WriteBool('Options', 'DarkMode', cbDarkMode.Checked);
+  finally
+    Config.Free;
+  end;
 end;
 
 procedure TMainForm.cbDarkModeChange(Sender: TObject);
 begin
-  btnSave.Enabled := False;
-  btnShow.Enabled := False;
+  bbtnSave.Enabled := False;
+  bbtnShow.Enabled := False;
 end;
 
-procedure TMainForm.DirectoryEditChange(Sender: TObject);
-begin
-  btnCreateHTML.Enabled := (DirectoryEdit.Text <> '') and DirectoryExists(DirectoryEdit.Text);
-  if btnCreateHtml.Enabled then
-    btnCreateHTML.SetFocus;
-end;
-
-procedure TMainForm.btnCreateHTMLClick(Sender: TObject);
+procedure TMainForm.bbtnCreateHTMLClick(Sender: TObject);
 var
   AllFileList: TStringList;
   IcoFileList: TStringList;
@@ -90,7 +160,10 @@ var
   IconGroups: Integer = 0;
   BodyFontColor: String = ' color: #000000;';
   BodyBackColor: String = ' background-color: #ffffff;';
+  InfoFontColor: String = ' color: #000000;';
   InfoBackColor: String = ' background-color: #ffffe0;';
+  HoverFontColor: String = ' color: #ffffff;';
+  HoverBackColor: String = ' background-color: #303030;';
 begin
   try
     AllFileList := TStringList.Create;
@@ -100,8 +173,6 @@ begin
     PixSizeList := TStringList.Create;
     Screen.BeginWaitCursor;
     SynEdit.Lines.BeginUpdate;
-
-    ImgDir := CleanAndExpandDirectory(DirectoryEdit.Text);
 
     FindAllFiles(AllFileList, ImgDir, '*.png', False);
 
@@ -141,7 +212,10 @@ begin
     begin
       BodyFontColor := ' color: #ffffff;';
       BodyBackColor := ' background-color: #303030;';
+      InfoFontColor := ' color: #ffffff;';
       InfoBackColor := ' background-color: #000000;';
+      HoverFontColor := ' color: #000000;';
+      HoverBackColor := ' background-color: #ffffff;';
     end;
 
     SynEdit.Lines.Clear;
@@ -152,11 +226,12 @@ begin
     SynEdit.Lines.Add('<meta charset="UTF-8">');
     SynEdit.Lines.Add('<style media="all">');
     SynEdit.Lines.Add('  body {font-family: sans-serif; font-size: 16px; font-weight: 400; margin: 0 auto; padding: 30px 0px 80px 0px;' + BodyBackColor + BodyFontColor + '}');
-    SynEdit.Lines.Add('  td.topleft {border-bottom: 5px solid #ddd; padding: 15px; text-align: left;' + InfoBackColor + '}');
-    SynEdit.Lines.Add('  td.topcenter {border-bottom: 5px solid #ddd; padding: 15px; text-align: center;' + InfoBackColor + '}');
     SynEdit.Lines.Add('  table {border-collapse: collapse; margin-left: auto; margin-right: auto;}');
+    SynEdit.Lines.Add('  tr:hover {' + HoverBackColor + HoverFontColor + '}');
     SynEdit.Lines.Add('  td {border-bottom: 1px solid #ddd; padding: 15px; text-align: left;}');
-    SynEdit.Lines.Add('  .info_container {margin: 0 auto; width: 500px; box-shadow: 0px 0px 5px 3px rgba(192, 192, 192, 0.37); padding: 15px; margin-top: 30px;' + InfoBackColor + '}');
+    SynEdit.Lines.Add('  td.topleft {border-bottom: 5px solid #ddd; padding: 15px; text-align: left;' + InfoBackColor + InfoFontColor + '}');
+    SynEdit.Lines.Add('  td.topcenter {border-bottom: 5px solid #ddd; padding: 15px; text-align: center;' + InfoBackColor + InfoFontColor + '}');
+    SynEdit.Lines.Add('  .info_container {margin: 0 auto; width: 500px; box-shadow: 0px 0px 5px 3px rgba(192, 192, 192, 0.37); padding: 15px; margin-top: 30px;' + InfoBackColor + InfoFontColor + '}');
     SynEdit.Lines.Add('</style>');
     SynEdit.Lines.Add('</head>');
     SynEdit.Lines.Add('<body>');
@@ -211,9 +286,9 @@ begin
     SynEdit.Lines.Add('</body>');
     SynEdit.Lines.Add('</html>');
 
-    btnSave.Enabled := True;
-    btnSave.SetFocus;
-    btnShow.Enabled := False;
+    bbtnSave.Enabled := True;
+    bbtnSave.SetFocus;
+    bbtnShow.Enabled := False;
   finally
     AllFileList.Free;
     IcoFileList.Free;
@@ -225,35 +300,73 @@ begin
   end;
 end;
 
-procedure TMainForm.btnSaveClick(Sender: TObject);
+procedure TMainForm.bbtnSaveClick(Sender: TObject);
 begin
-  fn := GetDestFileName;
+  fn := ImgDir + 'IconTable.html';
   try
     SynEdit.Lines.SaveToFile(fn);
     InfoMsg('Saved as: ' + fn);
-    btnShow.Enabled := True;
-    btnShow.SetFocus;
+    bbtnShow.Enabled := True;
+    bbtnShow.SetFocus;
   except
     ErrorMsg('The file could not be saved as: ' + fn);
   end;
 end;
 
-procedure TMainForm.btnShowClick(Sender: TObject);
+procedure TMainForm.bbtnShowClick(Sender: TObject);
 begin
-  fn := GetDestFileName;
+  fn := ImgDir + 'IconTable.html';
   if FileExists(fn) then
     OpenURL(fn);
-  btnClose.SetFocus;
+  bbtnCreateHTML.SetFocus;
 end;
 
-procedure TMainForm.btnCloseClick(Sender: TObject);
+procedure TMainForm.bbtnCloseClick(Sender: TObject);
 begin
   Close;
 end;
 
-function TMainForm.GetDestFileName: String;
+procedure TMainForm.DirectoryEditEditingDone(Sender: TObject);
+var
+  i: Integer;
 begin
-  Result := AppendPathDelim(DirectoryEdit.Text) + 'IconTable.html';
+  if DirectoryExists(DirectoryEdit.Directory) then
+  begin
+    ImgDir := CleanAndExpandDirectory(DirectoryEdit.Directory);
+    bbtnCreateHTML.Enabled := True;
+
+    for i := 0 to LastDirsMax do
+      if ImgDir = popLastDirs.Items[i].Caption then
+      begin
+        popLastDirs.Items[i].MenuIndex := 0;
+        Exit;
+      end;
+
+    popLastDirs.Items[LastDirsMax].Caption := ImgDir;
+    popLastDirs.Items[LastDirsMax].MenuIndex := 0;
+  end;
+end;
+
+procedure TMainForm.DirectoryEditChange(Sender: TObject);
+begin
+  bbtnCreateHTML.Enabled := DirectoryExists(DirectoryEdit.Directory);
+end;
+
+procedure TMainForm.LastDirClick(Sender: TObject);
+begin
+  if DirectoryExists(TMenuItem(Sender).Caption) then
+  begin
+    TMenuItem(Sender).MenuIndex := 0;
+    DirectoryEdit.Directory := TMenuItem(Sender).Caption;
+  end;
+end;
+
+procedure TMainForm.sbtnLastDirsClick(Sender: TObject);
+var
+  pt: TPoint;
+begin
+  pt := sbtnLastDirs.ClientToScreen(Point(sbtnLastDirs.Width, sbtnLastDirs.Height));
+  popLastDirs.PopUp(pt.X, pt.Y);
 end;
 
 procedure TMainForm.InfoMsg(const AMsg: String);
