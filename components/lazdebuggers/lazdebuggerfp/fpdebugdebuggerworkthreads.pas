9f70423e351412adafecbd80dc97dc57d0b1c206
Martin <laz.git@mfriebe.de>
2022-09-06 21:26:07 +0200

    LazDebuggerFp: fix getting full Callstack.Count - Issue #39886
    (cherry picked from commit 61e5fb272ae172886c042c306bbd5e6db695dbff)

--- components/lazdebuggers/lazdebuggerfp/fpdebugdebuggerworkthreads.pas
+++ components/lazdebuggers/lazdebuggerfp/fpdebugdebuggerworkthreads.pas
@@ -547,7 +547,8 @@ begin
   FDebugger.FLockList.GetLockFor(ThreadCallStack);
   try
     CurCnt := ThreadCallStack.Count;
-    while (not StopRequested) and (FRequiredMinCount > CurCnt) and
+    while (not StopRequested) and
+          ( (FRequiredMinCount > CurCnt) or (FRequiredMinCount < 0) ) and
           (not ThreadCallStack.HasReadAllAvailableFrames)
     do begin
       ReqCnt := Min(CurCnt + 5, FRequiredMinCount);
