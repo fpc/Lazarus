e31f4333cff304dabbf62b3918750404cdad5085
rich2014 <rich2014.git@outlook.com>
2022-10-14 23:23:31 +0800

    FIX: COCOA: TCocoaWSCustomEdit.SetText: detect and remove line-break when text is changed programmatically

--- lcl/interfaces/cocoa/cocoawsstdctrls.pas
+++ lcl/interfaces/cocoa/cocoawsstdctrls.pas
@@ -1213,16 +1213,18 @@ end;
 class procedure TCocoaWSCustomEdit.SetText(const AWinControl: TWinControl;
   const AText: String);
 var
-  txt : string;
-  mxl : Integer;
+  txt : NSString;              // NSString for AText
+  txtWithLineBreak: NSString;  // need not release
+  field: TCocoaTextField;
 begin
   if (AWinControl.HandleAllocated) then
   begin
-    txt := AText;
-    mxl := TCustomEdit(AWinControl).MaxLength;
-    if (mxl > 0) and (UTF8Length(txt) > mxl) then
-      txt := UTF8Copy(txt, 1, mxl);
-    ControlSetTextWithChangeEvent(NSControl(AWinControl.Handle), txt);
+    field:= TCocoaTextField(AWinControl.Handle);
+    txt:= NSStringUtf8(AText);
+    txtWithLineBreak := NSStringRemoveLineBreak(txt);
+    field.setStringValue(txtWithLineBreak);
+    field.textDidChange(nil); // check maxLength and calls controls callback (if any)
+    txt.release;
   end;
 end;
 
