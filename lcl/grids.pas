50b04807a4189049ed90d380328e267a87b821a7
wp_xyz <wp_xxyyzzz@gmx.net>
2022-11-12 00:34:43 +0100

    LCL/Grids: Erase grid col/row drag indicator when no col/row swap is performed. Patch by Joeny Ang, issue #39995
    (cherry picked from commit a1f01e19761bdfd7ae3e2911dfda79c2ec754fa7)

--- lcl/grids.pas
+++ lcl/grids.pas
@@ -6532,7 +6532,10 @@ var
   aColRow: Integer;
 begin
   if FromIndex=ToIndex then
+  begin
+    VisualChange;
     exit;
+  end;
 
   CheckIndex(IsColumn, FromIndex);
   CheckIndex(IsColumn, ToIndex);
