3e36732de66950848622b4dd529904a2ed21b996
Željan Rikalo <zeljko@lazarus-ide.org>
2022-12-21 13:49:32 +0100

    laz.virtualtrees: fixed crash with qt widgetsets on windows. issue #40056

--- components/virtualtreeview/laz.virtualtrees.pas
+++ components/virtualtreeview/laz.virtualtrees.pas
@@ -6,6 +6,9 @@ unit laz.VirtualTrees;
 {$define PACKARRAYPASCAL}
 {$endif}
 
+{$if defined(LCLQt) or defined(LCLQt5) or defined(LCLQt6)}
+{$undef windows}
+{$endif}
 // The contents of this file are subject to the Mozilla Public License
 // Version 1.1 (the "License"); you may not use this file except in compliance
 // with the License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
