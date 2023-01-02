ce2948da4f925d1f5239df0b27cf3675ee263c39
mattias <nc-gaertnma@netcologne.de>
2023-01-02 15:39:52 +0100

    ide: comments

--- ide/frames/packagelisteditordlg.pas
+++ ide/frames/packagelisteditordlg.pas
@@ -1,7 +1,7 @@
 {
  /***************************************************************************
-                          packageslisteditordlg.pas
-                          -------------------------
+                          packagelisteditordlg.pas
+                          ------------------------
 
  ***************************************************************************/
 
@@ -11,7 +11,7 @@
  *****************************************************************************
 
  Abstract:
-   Defines the TPckListEditorDialog, which is a form to edit search lists of packages
+   Defines the TPkgListEditorDialog, which is a form to edit search lists of packages
  
 }
 unit PackageListEditorDlg;
