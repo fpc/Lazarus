7511fdb89b18a6c4df8384ced6e73c8f4bfe70e1
Maxim Ganetsky <maxim@lazarus-ide.org>
2023-01-11 01:21:20 +0300

    IDE: cleaned up unneeded calls of TranslateUnitResourceStrings for LazarusCommonStrConst and IdeDebuggerStringConstants units

--- ide/idetranslations.pas
+++ ide/idetranslations.pas
@@ -488,9 +488,7 @@ end;
   Params: none
   Result: none
 
-  Translates all resourcestrings of the resource string files:
-    - lazarusidestrconsts.pas
-    - LazarusCommonStrConst
+  Translates IDE and LCL resource strings
 -------------------------------------------------------------------------------}
 procedure TranslateResourceStrings(const LazarusDir, CustomLang: string);
 const
@@ -513,11 +511,6 @@ begin
   // IDE
   TranslateUnitResourceStrings('LazarusIDEStrConsts',
     Dir+'languages/lazaruside'+Ext,Lang,FallbackLang);
-  TranslateUnitResourceStrings('LazarusCommonStrConst',
-    Dir+'components/ideintf/languages/lazaruscommonstrconst'+Ext,Lang,FallbackLang);
-  // Debugger GUI
-  TranslateUnitResourceStrings('IdeDebuggerStringConstants',
-    Dir+'ide/packages/idedebugger/languages/idedebuggerstringconstants'+Ext,Lang,FallbackLang);
   // LCL (needed to translate button captions in a dialog about config directory belonging to another Lazarus instance)
   TranslateUnitResourceStrings('LCLStrConsts',
     Dir+'lcl/languages/lclstrconsts'+Ext,Lang,FallbackLang);
