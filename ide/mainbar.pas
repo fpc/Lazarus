1945f6863bc002ce697f77d377de92024818ec56
Željan Rikalo <zeljko@lazarus-ide.org>
2023-02-01 16:14:26 +0100

    IDE: qt5 does not need workaround for main IDE height since issue #40071 is fixed

--- ide/mainbar.pas
+++ ide/mainbar.pas
@@ -511,7 +511,7 @@ begin
 end;
 
 function TMainIDEBar.CalcNonClientHeight: Integer;
-{$IF DEFINED(LCLGtk) OR DEFINED(LCLGtk2) OR DEFINED(LCLQt) OR DEFINED(LCLQt5) OR DEFINED(LCLQt6)}
+{$IF DEFINED(LCLGtk) OR DEFINED(LCLGtk2) OR DEFINED(LCLQt) OR DEFINED(LCLQt6)}
 var
   WindowRect, WindowClientRect: TRect;
 {$ENDIF}
@@ -532,7 +532,7 @@ begin
   if not Showing then
     Exit(0);
 
-  {$IF DEFINED(LCLGtk) OR DEFINED(LCLGtk2) OR DEFINED(LCLQt) OR DEFINED(LCLQt5) OR DEFINED(LCLQt6)}
+  {$IF DEFINED(LCLGtk) OR DEFINED(LCLGtk2) OR DEFINED(LCLQt) OR DEFINED(LCLQt6)}
   //Gtk2 + Qt
   //retrieve real main menu height because
   // - Gtk2, Qt:  SM_CYMENU does not work
