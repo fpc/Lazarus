8e4eb138d3837ab90977640522dab63204141dd4
dsiders <dsiders@lazarus-ide.org>
2023-02-05 06:11:24 +0000

    Docs: RttiControls. Adds log file output to build script.

--- components/rtticontrols/fpdoc/make-docs.sh
+++ components/rtticontrols/fpdoc/make-docs.sh
@@ -5,20 +5,25 @@
 docdir="../../../docs"
 
 # path to fpdoc executable
-#fpcdir="../../../fpc/3.2.2/bin/x86_64-win64"
-fpcdir="../../../../fpc331/fpc/bin/x86_64-win64"
+fpcdir="../../../fpc/3.2.2/bin/x86_64-win64"
+#fpcdir="../../../../fpc331/fpc/bin/x86_64-win64"
 
-echo "RTTIControls package"
+# info for footers... not used atm
+#verno="2.3.0"
+#commit=`git describe --long | cut -d "-" -f 4 | cut -b "2-"`
+dt=`date +"%Y-%m-%d"`
 
-# generate chm format without footers
+echo "(RttiControls) RTTI Controls Package"
+
+# generate chm format with date footer
 # imports done manually to set the correct prefix for the output format
 # output to current directory
-$fpcdir/fpdoc.exe --project=rtticontrols-project.xml --format=chm --import="$docdir/chm/rtl.xct,ms-its:rtl.chm::/" --import="$docdir/chm/fcl.xct,ms-its:fcl.chm::/" --import="$docdir/chm/lcl.xct,ms-its:lcl.chm::/" --import="$docdir/chm/lazutils.xct,ms-its:lazutils.chm::/"
+$fpcdir/fpdoc --project=rtticontrols-project.xml --format=chm --footer-date="yyyy-mm-dd" --import="$docdir/chm/rtl.xct,ms-its:rtl.chm::/" --import="$docdir/chm/fcl.xct,ms-its:fcl.chm::/" --import="$docdir/chm/lcl.xct,ms-its:lcl.chm::/" --import="$docdir/chm/lazutils.xct,ms-its:lazutils.chm::/" 2>&1 | tee ./build_chm.log
+
+# generate html format with date footer
+# imports done manually to set the correct prefix for the output format
+# html written to rtticontrols directory
+$fpcdir/fpdoc --project=rtticontrols-project.xml --format=html --footer-date="yyyy-mm-dd" --import="$docdir/chm/rtl.xct,../rtl/" --import="$docdir/chm/fcl.xct,../fcl/" --import="$docdir/chm/lcl.xct,../lcl/" --import="$docdir/chm/lazutils.xct,../lazutils/" --output=rtticontrols 2>&1 | tee ./build_html.log
 
 # copy generated chm help to lazarus docs directory
-cp -v runtimetypeinfocontrols.{chm,xct} $docdir/chm/
-
-# generate html format without footers
-# imports done manually to set the correct prefix for the output format
-# html written to rtticontrols sub-directory
-$fpcdir/fpdoc.exe --project=rtticontrols-project.xml --format=html --import="$docdir/chm/rtl.xct,../rtl/" --import="$docdir/chm/fcl.xct,../fcl/" --import="$docdir/chm/lcl.xct,../lcl/" --import="$docdir/chm/lazutils.xct,../lazutils/" --output=rtticontrols
+#cp -v runtimetypeinfocontrols.{chm,xct} $docdir/chm/
