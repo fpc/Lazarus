bfde9bed13fc44d3dd3b846261949ce4e8d9fd06
Michaël Van Canneyt <michael@freepascal.org>
2023-03-18 23:50:26 +0100

    * Re-added erroneously removed units

--- ide/lazarus.pp
+++ ide/lazarus.pp
@@ -62,6 +62,7 @@ uses
   Splash,
   Main,
   AboutFrm,
+  LazDebuggerFpLldb, LazDebuggerFp, laz.virtualtreeview_package,
   // use the custom IDE static packages AFTER 'main'
   {$IFDEF AddStaticPkgs}
   {$I staticpackages.inc}
