fc7ddbfe42bcc890132388f49396709056eecde1
Maxim Ganetsky <maxim@lazarus-ide.org>
2023-04-24 01:30:15 +0300

    IDE: cleaned up unused resource string

--- ide/lazarusidestrconsts.pas
+++ ide/lazarusidestrconsts.pas
@@ -3552,7 +3552,6 @@ resourcestring
   lisCleanUp = 'Clean up';
   lisLazBuildBuildMany = 'Build Many';
   lisAutomatically = 'Automatically';
-  lisCleanCommonFiles = 'Clean common files';
   lisCleanAll = 'Clean all';
   lisCleanOnlyOnce = 'Switch after building to automatically';
   lisAfterCleaningUpSwitchToAutomaticClean = 'After cleaning up (clean all or clean common files), switch to '
