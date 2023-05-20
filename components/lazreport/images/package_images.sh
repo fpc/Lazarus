10da471513c5a2c5bc53d46cfa62ac8647362c57
Jesus Reyes A <jesusrmx@gmail.com>
2023-05-20 03:25:15 -0600

    Revert "LazReport: updated bullets.res with a png file instead of a bmp file"
    This reverts commit 2543e7340d7824db04a4aa729787367510e03f5b.

--- components/lazreport/images/package_images.sh
+++ components/lazreport/images/package_images.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ../../../tools/lazres ../source/lr_register.res @Package_images.txt
-../../../tools/lazres ../source/bullets.res bitmaps/bulletgray.png bitmaps/bulletgreen.png designer/lrd_ins_fields.png
+../../../tools/lazres ../source/bullets.res bitmaps/bulletgray.png bitmaps/bulletgreen.png designer/lrd_ins_fields.bmp
