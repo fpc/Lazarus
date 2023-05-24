1b6b95b3b5bd4ba9e5d0e4a3d88eb5f748ebdfef
DomingoGP <dgalmesp@gmail.com>
2023-05-25 01:01:43 +0200

    Jedi code format: new jcfbase.lpk package and command line jcf using that package.

---
+++ components/jcf2/jcfbase.pas
@@ -0,0 +1,42 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit jcfbase;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  AsmKeywords, BuildParseTree, BuildTokenList, ParseError, ParseTreeNode, 
+  ParseTreeNodeType, SourceToken, SourceTokenList, Tokens, TokenUtils, 
+  PreProcessorExpressionParser, PreProcessorExpressionTokenise, 
+  PreProcessorExpressionTokens, PreProcessorParseTree, AllProcesses, 
+  BaseVisitor, FormatFlags, Nesting, RemoveEmptyComment, SwitchableVisitor, 
+  TreeWalker, VisitSetNesting, VisitSetXY, VisitStripEmptySpace, AlignAssign, 
+  AlignBase, AlignComment, AlignConst, AlignField, AlignTypedef, AlignVars, 
+  Capitalisation, IdentifierCaps, SpecificWordCaps, UnitNameCaps, 
+  IndentAsmParam, Indenter, BasicStats, FixCase, RebreakLines, 
+  ReduceWhiteSpace, RemoveBlankLine, RemoveComment, 
+  RemoveConsecutiveWhiteSpace, RemoveReturn, RemoveUnneededWhiteSpace, 
+  MozComment, BlockStyles, LongLineBreaker, NoReturnAfter, NoReturnBefore, 
+  PropertyOnOneLine, RemoveBlankLinesAfterProcHeader, RemoveBlankLinesInVars, 
+  RemoveConsecutiveReturns, RemoveReturnsAfter, RemoveReturnsAfterBegin, 
+  RemoveReturnsBeforeEnd, ReturnAfter, ReturnBefore, ReturnChars, 
+  ReturnsAfterFinalEnd, MaxSpaces, MoveSpaceToBeforeColon, NoSpaceAfter, 
+  NoSpaceBefore, RemoveSpaceAtLineEnd, SingleSpaceAfter, SingleSpaceBefore, 
+  SpaceBeforeColon, SpaceToTab, TabToSpace, AddBeginEnd, AddBlockEndSemicolon, 
+  FindReplace, SortUses, SortUsesData, UsesClauseFindReplace, 
+  UsesClauseInsert, UsesClauseRemove, WarnAssignToFunctionName, 
+  WarnCaseNoElse, WarnDestroy, WarnEmptyBlock, WarnImbalancedComment, Warning, 
+  WarnRealType, WarnUnusedParam, Converter, ConvertTypes, FileConverter, 
+  StringsConverter, StringsWriter, JcfSetBase, SetAlign, SetAsm, SetCaps, 
+  SetClarify, SetComments, SetFile, SetIndent, SetObfuscate, SetPreProcessor, 
+  SetReplace, SetReturns, SetSpaces, SettingsTypes, SetTransform, SetUses, 
+  SetWordList, RegistrySettings, SettingsStream, JcfRegistrySettings, 
+  JcfSettings, JcfUiTools, IntList, JcfLog, JcfMiscFunctions, JcfStringUtils, 
+  JcfUnicodeFiles, JcfVersionConsts, JcfUIConsts;
+
+implementation
+
+end.
