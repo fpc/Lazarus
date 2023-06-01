c972cd7491fe5e7c23fbdce818599ff838aa8e01
Abou Al Montacir <abou.almontacir@sfr.fr>
2023-06-01 23:01:12 +0200

    GTK3: Updated GModule2 unit generated with new gir2pas version.
    Removed manually functions that are not present in Debian 11.

--- lcl/interfaces/gtk3/gtk3bindings/lazgmodule2.pas
+++ lcl/interfaces/gtk3/gtk3bindings/lazgmodule2.pas
@@ -21,17 +21,28 @@ const
   {$endif}
 
 type
-  TGModuleFlags = Integer;
-const
-  { GModuleFlags }
-  G_MODULE_BIND_LAZY: TGModuleFlags = 1;
-  G_MODULE_BIND_LOCAL: TGModuleFlags = 2;
-  G_MODULE_BIND_MASK: TGModuleFlags = 3;
+  TGModuleFlags = (
+    TGModuleFlagsMinValue = -$7FFFFFFF,
+    G_MODULE_BIND_LAZY = 1,
+    G_MODULE_BIND_LOCAL = 2,
+    G_MODULE_BIND_MASK = 3,
+    TGModuleFlagsMaxValue = $7FFFFFFF
+  );
+  TGModuleError = (
+    TGModuleErrorMinValue = -$7FFFFFFF,
+    G_MODULE_ERROR_FAILED = 0,
+    G_MODULE_ERROR_CHECK_FAILED = 1,
+    TGModuleErrorMaxValue = $7FFFFFFF
+  );
 type
 
+
+  { TGModule }
   PPGModule = ^PGModule;
   PGModule = ^TGModule;
 
+
+  { TGModuleFlags }
   PPGModuleFlags = ^PGModuleFlags;
   PGModuleFlags = ^TGModuleFlags;
   TGModule = object
@@ -39,15 +50,18 @@ type
     procedure make_resident; cdecl; inline;
     function name: Pgchar; cdecl; inline;
     function symbol(symbol_name: Pgchar; symbol: Pgpointer): gboolean; cdecl; inline;
-    function build_path(directory: Pgchar; module_name: Pgchar): Pgchar; cdecl; inline; static;
     function error: Pgchar; cdecl; inline; static;
     function open(file_name: Pgchar; flags: TGModuleFlags): PGModule; cdecl; inline; static;
     function supported: gboolean; cdecl; inline; static;
   end;
   TGModuleCheckInit = function(module: PGModule): Pgchar; cdecl;
+
+
+  { TGModuleError }
+  PPGModuleError = ^PGModuleError;
+  PGModuleError = ^TGModuleError;
   TGModuleUnload = procedure(module: PGModule); cdecl;
 
-function g_module_build_path(directory: Pgchar; module_name: Pgchar): Pgchar; cdecl; external;
 function g_module_close(module: PGModule): gboolean; cdecl; external;
 function g_module_error: Pgchar; cdecl; external;
 function g_module_name(module: PGModule): Pgchar; cdecl; external;
@@ -74,11 +88,6 @@ end;
 function TGModule.symbol(symbol_name: Pgchar; symbol: Pgpointer): gboolean; cdecl;
 begin
   Result := LazGModule2.g_module_symbol(@self, symbol_name, symbol);
-end;
-
-function TGModule.build_path(directory: Pgchar; module_name: Pgchar): Pgchar; cdecl;
-begin
-  Result := LazGModule2.g_module_build_path(directory, module_name);
 end;
 
 function TGModule.error: Pgchar; cdecl;
