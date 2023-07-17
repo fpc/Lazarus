df338fd8677d18c573ab809fc5b335787fef1f43
Bart <9132501-flyingsheep@users.noreply.gitlab.com>
2023-07-17 12:48:02 +0200

    LCL: add TaskDlgEmulation unit (skeleton).

---
+++ lcl/taskdlgemulation.pp
@@ -0,0 +1,23 @@
+unit taskdlgemulation;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,
+  LazUTF8,
+  LCLType, LCLStrConsts, LCLIntf, InterfaceBase, ImgList,
+  LResources, Menus, Graphics, Forms, Controls, StdCtrls, ExtCtrls, Buttons, Dialogs, DialogRes;
+
+function ExecuteLCLTaskDialog(const ADlg: TTaskDialog): Boolean;
+
+implementation
+
+function ExecuteLCLTaskDialog(const ADlg: TTaskDialog): Boolean;
+begin
+  Result := False;
+end;
+
+end.
+
