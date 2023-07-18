fpc/Lazarus

commit b184a37dbe93bfa9a670b870eca53ba77a8bb100

Cocoa: fix the issue that Modal Form may disappear when switching back from other apps
the issue fixed:
1. run your app, open Modal Form
2. switch to other apps
3. in the dock, right click your app ico and select "Show All Window", and select a window
4. the Modal Form is disappear (actually ordered behind the selected Form)

lcl/interfaces/cocoa/cocoawindows.pas
@@ -122,6 +122,8 @@ type
   private
     // for the reentrancy of makeFirstResponder()
     makeFirstResponderCount: Integer;
+  private
+    procedure DoWindowDidBecomeKey(); message 'DoWindowDidBecomeKey';
   protected
     fieldEditor: TCocoaFieldEditor;
     firedMouseEvent: Boolean;
@@ -234,6 +236,9 @@ type
   end;
 
 implementation
+
+uses
+  CocoaInt;
 
 { TCocoaDesignOverlay }
 
@@ -756,6 +761,13 @@ begin
     callback.Close;
 end;
 
+procedure TCocoaWindow.DoWindowDidBecomeKey();
+begin
+  if Assigned(CocoaWidgetSet.CurModalForm) then
+    CocoaWidgetSet.CurModalForm.orderFront(nil);
+  CursorHelper.SetCursorOnActive();
+end;
+
 procedure TCocoaWindow.windowDidBecomeKey(notification: NSNotification);
 begin
   // forcing to keep the level as all other LCL windows
@@ -780,7 +792,7 @@ begin
     and (contentView.isKindOfClass(TCocoaWindowContent)) then
     self.makeFirstResponder( TCocoaWindowContent(contentView).documentView );
 
-  CursorHelper.SetCursorOnActive;
+  performSelector_withObject_afterDelay( ObjCSelector('DoWindowDidBecomeKey'), nil, 0.1 );
 end;
 
 procedure TCocoaWindow.windowDidResignKey(notification: NSNotification);

commit 3542056197e5c16beca782033f4234320d9277eb

Cocoa: fix the issue be introduced by b184a37
Window/Control Active Message Lost

lcl/interfaces/cocoa/cocoawindows.pas
@@ -763,8 +763,8 @@ end;
 
 procedure TCocoaWindow.DoWindowDidBecomeKey();
 begin
-  if Assigned(CocoaWidgetSet.CurModalForm) then
-    CocoaWidgetSet.CurModalForm.orderFront(nil);
+  if CocoaWidgetSet.isModalSession then
+    self.orderFront(nil);
   CursorHelper.SetCursorOnActive();
 end;
 
@@ -1040,6 +1040,11 @@ begin
     dl := {%H-}NSObject( NSText(newResponder).delegate );
     if Assigned(dl) and (dl.isKindOfClass(NSView)) then
       newResponder := NSResponder(dl);
+  end
+  else
+  if newResponder.isKindOfClass(NSWindow) or newResponder.isKindOfClass(TCocoaWindowContentDocument) then
+  begin
+    newResponder := nil;
   end
   else
   begin