1bf9eac4dc927d3de84e841388203971d7f0e875
dsiders <dsiders@lazarus-ide.org>
2023-07-20 05:37:33 +0100

    Docs: Updates CHM and HTML build scripts to use fpdoc 3.2.2 and adds log files.
    * These changes are temporary.
    * fpdoc version 3.3.1-13340-g757f65d0e2 [2023-07-18] has output issues.

--- docs/html/build_html.sh
+++ docs/html/build_html.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-#
-# Builds  Lazarus documentaiton for LCL and LazUtils in HTML format.
+# Builds Lazarus documentaiton for LCL and LazUtils in HTML format.
 # Requires a $(LazarusDir)/docs/chm directory which contains rtl.xct and fcl.xct.
 # Process fails if either the directory or the files are missing.
 # Process creates ./lcl and ./lazutils subdirectories for HTML output.
 # Update the content in locallclfooter.xml if needed before starting the script.
-#
 
 # Path to the fpdoc program
 # The trunk (main) version of fpdoc is recommended.
 # CHANGE THIS TO A PATH VALID FOR YOUR SYSTEM AND FPDOC VERSION.
-#fpdocpath="../../fpc/3.2.2/bin/x86_64-win64"
-fpdocpath="../../../fpc331/fpc/bin/x86_64-win64"
+fpdocpath="../../fpc/3.2.2/bin/x86_64-win64"
+# fpdoc in version 3.3.1-13340-g757f65d0e2 [2023/07/18] has output issues.
+#fpdocpath="../../../fpc331/fpc/bin/x86_64-win64"
 
 # rebuild if needed
 #../../lazbuild build_lcl_docs.lpi
 
 # build html output without footers
-./build_lcl_docs --outfmt=html --fpcdocs=../chm --fpdoc=$fpdocpath/fpdoc
+#./build_lcl_docs --outfmt=html --fpcdocs=../chm --fpdoc=$fpdocpath/fpdoc
+
+# build chm output with date in footer
+#./build_lcl_docs --outfmt=html --fpcdocs=../chm --fpdoc=$fpdocpath/fpdoc --arg="--footer-date"
 
 # For FPDoc 3.2.X: build html output with footers in locallclfooter.xml
-#./build_lcl_docs --outfmt=html --fpcdocs=../chm --fpdoc=$fpdocpath/fpdoc --footer=locallclfooter.xml
+./build_lcl_docs --outfmt=html --fpcdocs=../chm --fpdoc=$fpdocpath/fpdoc --footer=locallclfooter.xml 2>&1 | tee -a ./build_html.log
 
 # For FPDoc 3.3.X: build html output with footers in locallclfooter.xml
-#./build_lcl_docs --outfmt=html --fpcdocs=../chm --fpdoc=$fpdocpath/fpdoc --footer=@locallclfooter.xml
+#./build_lcl_docs --outfmt=html --fpcdocs=../chm --fpdoc=$fpdocpath/fpdoc --footer=@locallclfooter.xml 2>&1 | tee -a ./build_html.log
 
 # For FPDoc 3.3.X: build html output with footer text
 #./build_lcl_docs --outfmt=html --fpcdocs=../chm --fpdoc=$fpdocpath/fpdoc --footer="(c) Copyright 2022. All rights reserved."
