70a68d6bea80c0e1e76c9775f6d0c1fb791f9c90
wp_xyz <wp_xxyyzzz@gmx.net>
2023-08-29 18:00:27 +0200

    TAChart: Add rounding-error-tolerant comparison functions to TAMath.
    (cherry picked from commit ac7dc351627603627ffee95eee540c42ce4bda43)

--- components/tachart/tamath.pas
+++ components/tachart/tamath.pas
@@ -24,7 +24,11 @@ procedure EnsureOrder(var A, B: Double); overload; inline;
 
 procedure ExpandRange(var ALo, AHi: Double; ACoeff: Double);
 
+function GreaterEqual(X, Y: Double; AEpsilon: Double = 0.0): boolean;
+function GreaterThan(X, Y: Double; AEpsilon: Double = 0.0): boolean;
 function InRangeUlps(AX, ALo, AHi: Double; AMaxUlps: Word): Boolean;
+function LessEqual(X, Y: Double; AEpsilon: Double = 0.0): boolean;
+function LessThan(X, Y: Double; AEpsilon: Double = 0.0): boolean;
 
 function SafeInfinity: Double; inline;
 function SafeInRange(AValue, ABound1, ABound2: Double): Boolean;
@@ -143,9 +147,29 @@ begin
   AHi += d * ACoeff;
 end;
 
+function GreaterEqual(X, Y: Double; AEpsilon: Double = 0.0): boolean;
+begin
+  Result := (X > Y) or SameValue(X, Y, AEpsilon);
+end;
+
+function GreaterThan(X, Y: Double; AEpsilon: Double = 0.0): boolean;
+begin
+  Result := (X > Y) and not SameValue(X, Y, AEpsilon);
+end;
+
 function InRangeUlps(AX, ALo, AHi: Double; AMaxUlps: Word): Boolean;
 begin
   Result := InRange(Ulps(AX), Ulps(ALo) - AMaxUlps, Ulps(AHi) + AMaxUlps);
+end;
+
+function LessEqual(X, Y: Double; AEpsilon: Double = 0.0): boolean;
+begin
+  Result := (X < Y) or SameValue(X, Y, AEpsilon);
+end;
+
+function LessThan(X, Y: Double; AEpsilon: Double = 0.0): boolean;
+begin
+  Result := (X < Y) and not SameValue(X, Y, AEpsilon);
 end;
 
 function SafeInfinity: Double;
