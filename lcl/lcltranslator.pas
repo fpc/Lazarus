fpc/Lazarus

commit 36c7ef29edbdeb7d8fea99555e17f33d82fcafc1

LCL, fix DBgrid does not accept translations in main form only, issue #35202
git-svn-id: trunk@60667 -

lcl/lcltranslator.pas
@@ -350,7 +350,7 @@ begin
       APropInfo:=APropList^[i];
       if Assigned(PPropInfo(APropInfo)^.GetProc) and
          Assigned(APropInfo^.PropType) and
-         IsStoredProp(AnInstance, APropInfo) then
+         Assigned(PPropInfo(APropInfo)^.SetProc) then
         case APropInfo^.PropType^.Kind of
           tkSString,
           tkLString,

commit ddbcec7bcc83f0e5bf9a6e6a9c5436489a61de3e

LCL, LCLTranslator: Do not check if a property has a setter before even attempting to read what it is and thus ignoring classes (with translatable properties) returned by properties without setters. Check if a property setter is present only just before actually writing to it.
Fixes translation of TLabeledEdit.EditLabel.Caption (EditLabel is a property without setter there).

It was a regression after commit 36c7ef29edbdeb7d8fea99555e17f33d82fcafc1 which fixed issue #35202.

lcl/lcltranslator.pas
@@ -347,13 +347,12 @@ begin
       begin
       APropInfo:=APropList^[i];
       if Assigned(PPropInfo(APropInfo)^.GetProc) and
-         Assigned(APropInfo^.PropType) and
-         Assigned(PPropInfo(APropInfo)^.SetProc) then
+         Assigned(APropInfo^.PropType) then
         case APropInfo^.PropType^.Kind of
           tkSString,
           tkLString,
           tkAString:
-            if APropInfo^.PropType=TypeInfo(TTranslateString) then
+            if (Assigned(PPropInfo(APropInfo)^.SetProc)) and (APropInfo^.PropType=TypeInfo(TTranslateString)) then
             begin
               TmpStr := GetStrProp(AnInstance, APropInfo);
               {$IFDEF VerbosePOTranslator}