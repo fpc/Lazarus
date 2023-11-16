6a1faf14191000e71b9e5836f9dbb76fdf138fbb
Maxim Ganetsky <maxim@lazarus-ide.org>
2023-11-17 01:44:57 +0300

    IDE: added clarifying comment in LoadCustomKeySchemasInDir procedure of keymapping.pp unit

--- ide/keymapping.pp
+++ ide/keymapping.pp
@@ -4414,6 +4414,7 @@ begin
             exp.DefineCommandCategories; // default Relations
             exp.LoadFromXMLConfig(xml, 'KeyMapping/', false);
             dst.AddObject(nm, exp);
+            //now exp is owned by dst, don't free it in this procedure
             exp := nil;
           end;
         finally
