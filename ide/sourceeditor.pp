476704c28780ab47d885972b0659bc164d30ae08
rich2014 <rich2014.git@outlook.com>
2023-11-24 01:03:21 +0800

    IDE: prevent the tab itself from becoming the focusi n SourceEditor

--- ide/sourceeditor.pp
+++ ide/sourceeditor.pp
@@ -6968,6 +6968,7 @@ Begin
     debugln('[TSourceNotebook.CreateNotebook] C');
     {$ENDIF}
     EditorOpts.ApplyTabFontSettingsTo(FNotebook);
+    TabStop := false;
     Align := alClient;
     APage:=TTabSheet.Create(FNotebook);
     APage.Caption:='unit1';
