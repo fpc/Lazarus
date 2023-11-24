5c5693e6bee1dae26af6ba9e8f0bcc1dbca410a5
Maxim Ganetsky <maxim@lazarus-ide.org>
2023-11-24 18:41:06 +0300

    IDE: save/restore size of "Select profiles to build" window of "Build Lazarus" dialog

--- ide/buildlazdialog.pas
+++ ide/buildlazdialog.pas
@@ -1470,6 +1470,7 @@ begin
       if fProfiles.Selected.IndexOf(fProfiles[i].Name)>-1 then
         EditForm.CheckListBox1.Checked[ind]:=True;
     end;
+    IDEDialogLayoutList.ApplyLayout(EditForm);
     // Show the form.
     EditForm.ShowModal;
     if EditForm.ModalResult in [mrOK, mrYes] then begin
@@ -1483,6 +1484,7 @@ begin
     if EditForm.ModalResult=mrYes then
       ModalResult:=mrAll;
   finally
+    IDEDialogLayoutList.SaveLayout(EditForm);
     EditForm.Free;
   end;
 end;
@@ -1556,6 +1558,7 @@ end;
 
 procedure TConfigureBuildLazarusDlg.CleanRadioButtonClick(Sender: TObject);
 begin
+  //
 end;
 
 end.
