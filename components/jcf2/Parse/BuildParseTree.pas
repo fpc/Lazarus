88eb9d53cdd1b44942770c854f356aca500742b5
DomingoGP <dgalmesp@gmail.com>
2023-11-25 12:05:23 +0100

    Jedi code format:  recognize "sealed" / "abstract" for inheriting objects.
    (cherry picked from commit 3ff5d50aaaf9f8f1a8c448741fdc5ad1f0902297)

--- components/jcf2/Parse/BuildParseTree.pas
+++ components/jcf2/Parse/BuildParseTree.pas
@@ -4594,6 +4594,12 @@ begin
 
   Recognise(ttObject);
 
+  if fcTokenList.FirstSolidTokenType = ttSealed then
+    Recognise(ttSealed);
+
+  if fcTokenList.FirstSolidTokenType = ttAbstract then
+    Recognise(ttAbstract);
+
   if fcTokenList.FirstSolidTokenType = ttOpenBracket then
     RecogniseObjHeritage;
 
