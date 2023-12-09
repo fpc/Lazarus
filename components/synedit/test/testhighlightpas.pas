fpc/Lazarus

commit cbf59cc75ae3f3d7cc32345c32f99916989c94a8

SynEdit: fix class-section (and strict). Only highlight in classes/objects. Fixed look-ahead for "strict". Issue #40614

components/synedit/synhighlighterpas.pp
@@ -81,6 +81,7 @@ type
 
     // we need to detect:    type TFoo = procedure; // must not fold
     //                       var  foo: procedure;   // must not fold
+    rsAfterSemiColon,   // very first word after ";"
     rsAfterEqualOrColon,   // very first word after "=" or ":"
     rsAfterEqual,          // between "=" and ";" (or block end) // a ^ means ctrl-char, not pointer to type
 
@@ -1693,12 +1694,19 @@ begin
 end;
 
 function TSynPasSyn.Func63: TtkTokenKind;
+var
+  tbf: TPascalCodeFoldBlockType;
 begin
   if KeyComp('Public') then begin
-    Result := tkKey;
-    fRange := fRange - [rsAfterClassMembers, rsVarTypeInSpecification];
-    if (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) then begin
-      if (TopPascalCodeFoldBlockType=cfbtClassSection) then
+    tbf := TopPascalCodeFoldBlockType;
+    if (tbf in [cfbtClass, cfbtClassSection, cfbtRecord]) and
+       (fRange * [rsInProcHeader, rsAfterEqual, rsAfterEqualOrColon, rsVarTypeInSpecification] = []) and
+       (fRange * [rsAfterSemiColon, rsAfterClass] <> [])
+    then begin
+      Result := tkKey;
+      fRange := fRange - [rsAfterClassMembers, rsVarTypeInSpecification] + [rsAfterSemiColon];
+      FOldRange := FOldRange - [rsAfterSemiColon];
+      if (tbf=cfbtClassSection) then
         EndPascalCodeFoldBlockLastLine;
       StartPascalCodeFoldBlock(cfbtClassSection);
     end
@@ -1706,9 +1714,11 @@ begin
     // outside class: procedure foo; public name 'abc';
     if (PasCodeFoldRange.BracketNestLevel = 0) and
        (fRange * [rsInProcHeader, rsProperty, rsAfterEqualOrColon, rsWasInProcHeader] = [rsWasInProcHeader]) and
-       (TopPascalCodeFoldBlockType in ProcModifierAllowed)
+       (tbf in ProcModifierAllowed - [cfbtClass, cfbtClassSection, cfbtRecord])
     then
-      FRange := FRange + [rsInProcHeader];
+      FRange := FRange + [rsInProcHeader]
+    else
+      Result := tkIdentifier;
   end
   else if KeyComp('Record') then begin
     StartPascalCodeFoldBlock(cfbtRecord);
@@ -2083,9 +2093,11 @@ function TSynPasSyn.Func89: TtkTokenKind;
     FndPos, FndLen: integer;
   begin
     Result := ScanAheadForNextToken(7, FndLine, FndPos, FndLen);
-    if Result and (FndLine[FndPos] in ['p', 'P']) then
-      Result := ( (FndLen = 7) and KeyCompEx(@FndLine[FndPos + 1], PChar('rivate'), 6) ) or
-                ( (FndLen = 9) and KeyCompEx(@FndLine[FndPos + 1], PChar('rotected'), 8) );
+    if Result then
+      Result := (FndLine[FndPos] in ['p', 'P']) and
+                ( ( (FndLen = 7) and KeyCompEx(@FndLine[FndPos + 1], PChar('rivate'), 6) ) or
+                  ( (FndLen = 9) and KeyCompEx(@FndLine[FndPos + 1], PChar('rotected'), 8) )
+                );
   end;
 
 begin
@@ -2099,31 +2111,36 @@ begin
     end;
   end
   else
+  if KeyComp('strict') and
+     (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) and
+     (fRange * [rsInProcHeader, rsAfterEqual, rsAfterEqualOrColon, rsVarTypeInSpecification] = []) and
+     (fRange * [rsAfterSemiColon, rsAfterClass] <> []) and
+     ScanForClassSection
+  then begin
+    fRange := fRange + [rsAfterSemiColon]; // flag for private/proctected (must be next)
+    FOldRange := FOldRange - [rsAfterSemiColon];
+    Result := tkKey;
+  end
+  else
     Result := tkIdentifier;
-  // Structural Scan / Quick
-  if FIsInNextToEOL and not IsCollectingNodeInfo then
-    exit;
-  // Scanning for display / Look ahead
-  if KeyComp('strict') then
-  begin
-    if (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) then
-      if ScanForClassSection then
-        Result := tkKey;
-  end;
 end;
 
 function TSynPasSyn.Func91: TtkTokenKind;
 begin
   if KeyComp('Downto') then
     Result := tkKey
-  else if KeyComp('Private') then begin
+  else
+  if KeyComp('Private') and
+     (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) and
+     (fRange * [rsInProcHeader, rsAfterEqual, rsAfterEqualOrColon, rsVarTypeInSpecification] = []) and
+     (fRange * [rsAfterSemiColon, rsAfterClass] <> [])
+  then begin
     Result := tkKey;
-    fRange := fRange - [rsAfterClassMembers, rsVarTypeInSpecification];
-    if (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) then begin
-      if (TopPascalCodeFoldBlockType=cfbtClassSection) then
-        EndPascalCodeFoldBlockLastLine;
-      StartPascalCodeFoldBlock(cfbtClassSection);
-    end;
+    fRange := fRange - [rsAfterClassMembers, rsVarTypeInSpecification] + [rsAfterSemiColon];
+    FOldRange := FOldRange - [rsAfterSemiColon];
+    if (TopPascalCodeFoldBlockType=cfbtClassSection) then
+      EndPascalCodeFoldBlockLastLine;
+    StartPascalCodeFoldBlock(cfbtClassSection);
   end
   else
     Result := tkIdentifier;
@@ -2184,17 +2201,20 @@ end;
 
 function TSynPasSyn.Func96: TtkTokenKind;
 begin
-  if KeyComp('Published') then begin
+  if KeyComp('Published') and
+     (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) and
+     (fRange * [rsInProcHeader, rsAfterEqual, rsAfterEqualOrColon, rsVarTypeInSpecification] = []) and
+     (fRange * [rsAfterSemiColon, rsAfterClass] <> [])
+  then begin
     Result := tkKey;
-    fRange := fRange - [rsAfterClassMembers, rsVarTypeInSpecification];
-    if (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) then begin
-      if (TopPascalCodeFoldBlockType=cfbtClassSection) then
-        EndPascalCodeFoldBlockLastLine;
-      StartPascalCodeFoldBlock(cfbtClassSection);
-    end;
+    fRange := fRange - [rsAfterClassMembers, rsVarTypeInSpecification] + [rsAfterSemiColon];
+    FOldRange := FOldRange - [rsAfterSemiColon];
+    if (TopPascalCodeFoldBlockType=cfbtClassSection) then
+      EndPascalCodeFoldBlockLastLine;
+    StartPascalCodeFoldBlock(cfbtClassSection);
   end
   else
-  if (PasCodeFoldRange.BracketNestLevel = 0) and
+  if (PasCodeFoldRange.BracketNestLevel = 0) and  // TODO nested record
      (fRange * [rsInProcHeader, rsProperty, rsAfterEqualOrColon, rsWasInProcHeader] = [rsWasInProcHeader]) and
      (TopPascalCodeFoldBlockType in ProcModifierAllowed) and
      KeyComp('Override')
@@ -2405,14 +2425,17 @@ end;
 
 function TSynPasSyn.Func106: TtkTokenKind;
 begin
-  if KeyComp('Protected') then begin
+  if KeyComp('Protected') and
+     (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) and
+     (fRange * [rsInProcHeader, rsAfterEqual, rsAfterEqualOrColon, rsVarTypeInSpecification] = []) and
+     (fRange * [rsAfterSemiColon, rsAfterClass] <> [])
+  then begin
     Result := tkKey;
-    fRange := fRange - [rsAfterClassMembers, rsVarTypeInSpecification];
-    if (TopPascalCodeFoldBlockType in [cfbtClass, cfbtClassSection, cfbtRecord]) then begin
-      if (TopPascalCodeFoldBlockType=cfbtClassSection) then
-        EndPascalCodeFoldBlockLastLine;
-      StartPascalCodeFoldBlock(cfbtClassSection);
-    end;
+    fRange := fRange - [rsAfterClassMembers, rsVarTypeInSpecification] + [rsAfterSemiColon];
+    FOldRange := FOldRange - [rsAfterSemiColon];
+    if (TopPascalCodeFoldBlockType=cfbtClassSection) then
+      EndPascalCodeFoldBlockLastLine;
+    StartPascalCodeFoldBlock(cfbtClassSection);
   end
   else Result := tkIdentifier;
 end;
@@ -3776,7 +3799,8 @@ begin
       fRange := fRange + [rsWasInProcHeader];
     fRange := fRange - [rsProperty, rsInProcHeader];
   end;
-  fRange := fRange - [rsVarTypeInSpecification, rsAfterEqual];
+  fRange := fRange - [rsVarTypeInSpecification, rsAfterEqual] + [rsAfterSemiColon];
+  FOldRange := FOldRange - [rsAfterSemiColon];
 end;
 
 procedure TSynPasSyn.SlashProc;
@@ -3995,7 +4019,8 @@ begin
             fRange := fRange - [rsAfterClass];
 
           fRange := fRange -
-            (FOldRange * [rsAfterEqualOrColon, rsAtPropertyOrReadWrite, rsAfterClassField,
+            (FOldRange * [rsAfterEqualOrColon, rsAfterSemiColon,
+                          rsAtPropertyOrReadWrite, rsAfterClassField,
                           rsAfterIdentifierOrValue, rsAfterEqualThenType,
                           rsWasInProcHeader, rsAtProcName, rsAfterProcName]
             ) -

components/synedit/test/testhighlightpas.pas
@@ -61,6 +61,7 @@ type
     procedure TestContextForProcedureNameAttr;
     procedure TestContextForInterface;
     procedure TestContextForDeprecated;
+    procedure TestContextForClassSection;
     procedure TestContextForClassModifier; // Sealed abstract
     procedure TestContextForClassHelper;
     procedure TestContextForTypeHelper;
@@ -1456,6 +1457,123 @@ begin
     SubTest('unimplemented', AFolds);
     SubTest('experimental' , AFolds);
     SubTest('platform'     , AFolds);
+  end;
+end;
+
+procedure TTestHighlighterPas.TestContextForClassSection;
+var
+  rc, lead1, lead2, cm, s1, s2, v: string;
+  strict1, strict2: Boolean;
+  cmod, sp1, sp2: Integer;
+begin
+  for rc in ['class ', 'object'] do
+  for lead1 in ['', '  '] do
+  for lead2 in ['', '  '] do
+  for cm  in ['                ', ' sealed abstract', ' sealed         ', ' abstract       '] do
+  for s1 in ['strict private  ', 'strict protected', 'private          ', 'protected        ', 'public           ', 'published        '] do
+  for s2 in ['strict private  ', 'strict protected', 'private          ', 'protected        ', 'public           ', 'published        '] do
+  for v  in ['private          ', 'protected        ', 'public           ', 'published        '] do
+  begin
+    sp1 := ord(tkSpace);
+    if lead1 = '' then sp1 := TK_SKIP;
+    sp2 := ord(tkSpace);
+    if lead2 = '' then sp2 := TK_SKIP;
+
+    strict1 := s1[1] = 's';
+    strict2 := s2[1] = 's';
+    cmod := 0;
+    if cm[2] <> ' ' then
+    case cm[9] of
+      ' ': cmod := 1;
+      'a': cmod := 2;
+      't': cmod := 1;
+    end;
+
+    if (rc <> 'class ') and ( (cmod <> 0) or strict1 or  strict2 )
+    then
+      continue;
+
+    ReCreateEdit;
+    SetLines
+      ([ 'Unit A; interface',  // 0
+         'type',
+         'TFoo='+rc+cm ,  // 2  class sealed abstract
+         lead1+trim(s1),
+         lead2+trim(s2),
+           'a,'+trim(v)+':'+trim(v)+';', // 5
+         lead1+trim(s2),
+         lead2+trim(s1),
+           'function '+trim(v)+'('+trim(v)+':'+trim(v)+';'+trim(v)+','+trim(v)+':'+trim(v)+'):'+trim(v)+';', // 8
+         lead1+trim(s1),
+         lead2+trim(s2), //10
+         'end;',
+         lead1+trim(v)+'='+trim(v)+';', // 12
+         lead2+trim(v)+'='+trim(v)+';',
+         'var',
+         lead1+trim(v)+':'+trim(v)+';', // 15
+         lead2+trim(v)+':'+trim(v)+';',
+         ''
+      ]);
+
+    case cmod of
+      0: CheckTokensForLine('TFoo=class',  2, [ tkIdentifier, tkSymbol, tkKey ]);
+      1: CheckTokensForLine('TFoo=class',  2, [ tkIdentifier, tkSymbol, tkKey, tkSpace, tkKey, tkSpace ]);
+      2: CheckTokensForLine('TFoo=class',  2, [ tkIdentifier, tkSymbol, tkKey, tkSpace, tkKey, tkSpace, tkKey ]);
+    end;
+
+    case strict1 of
+      False: CheckTokensForLine('public',          3, [ sp1, tkKey ]);
+      True:  CheckTokensForLine('strict private',  3, [ sp1, tkKey, tkSpace, tkKey ]);
+    end;
+    case strict2 of
+      False: CheckTokensForLine('public',          4, [ sp2, tkKey ]);
+      True:  CheckTokensForLine('strict private',  4, [ sp2, tkKey, tkSpace, tkKey ]);
+    end;
+
+    CheckTokensForLine('a,public:public;',  5,
+      [ tkIdentifier {a}, tkSymbol{,}, tkIdentifier {public}, tkSymbol{:},
+        tkIdentifier, tkSymbol{;} ]);
+
+    case strict2 of
+      False: CheckTokensForLine('public',          6, [ sp1, tkKey ]);
+      True:  CheckTokensForLine('strict private',  6, [ sp1, tkKey, tkSpace, tkKey ]);
+    end;
+    case strict1 of
+      False: CheckTokensForLine('public',          7, [ sp2, tkKey ]);
+      True:  CheckTokensForLine('strict private',  7, [ sp2, tkKey, tkSpace, tkKey ]);
+    end;
+
+
+    CheckTokensForLine('function ...',  8,
+      [ tkKey {function}, tkSpace, tkIdentifier + FAttrProcName{public},tkSymbol{(},
+        tkIdentifier {public}, tkSymbol{:}, tkIdentifier, tkSymbol{;},
+        tkIdentifier {public}, tkSymbol{,}, tkIdentifier {public}, tkSymbol{:}, tkIdentifier, tkSymbol{;},
+        tkSymbol{)}, tkIdentifier, tkSymbol{;}
+      ]);
+
+    case strict1 of
+      False: CheckTokensForLine('public',          9, [ sp1, tkKey ]);
+      True:  CheckTokensForLine('strict private',  9, [ sp1, tkKey, tkSpace, tkKey ]);
+    end;
+    case strict2 of
+      False: CheckTokensForLine('public',         10, [ sp2, tkKey ]);
+      True:  CheckTokensForLine('strict private', 10, [ sp2, tkKey, tkSpace, tkKey ]);
+    end;
+
+    CheckTokensForLine('end',          11, [ tkKey, tkSymbol ]);
+
+    CheckTokensForLine('public=public;',  12,
+      [ sp1, tkIdentifier {public}, tkSymbol{=}, tkIdentifier, tkSymbol{;} ]);
+    CheckTokensForLine('public=public;',  13,
+      [ sp2, tkIdentifier {public}, tkSymbol{=}, tkIdentifier, tkSymbol{;} ]);
+
+    CheckTokensForLine('var',          14, [ tkKey ]);
+
+    CheckTokensForLine('public:public;',  15,
+      [ sp1, tkIdentifier {public}, tkSymbol{:}, tkIdentifier, tkSymbol{;} ]);
+    CheckTokensForLine('public:public;',  16,
+      [ sp2, tkIdentifier {public}, tkSymbol{:}, tkIdentifier, tkSymbol{;} ]);
+
   end;
 end;
 

components/synedit/test/testhighlightfoldbase.pas
@@ -19,6 +19,8 @@ type
 
   function ExpVLine(ALine: Integer; AExp: Array of integer): TTestExpValuesForLine;
 
+const
+  TK_SKIP = -1;
 type
 
   TExpTokenInfo = record
@@ -212,10 +214,14 @@ begin
   c := 0;
   while not FTheHighLighter.GetEol do begin
     e := ExpTokens[c];
+    if e.ExpKind = TK_SKIP then begin
+      inc(c);
+      continue;
+    end;
     //DebugLn([FTheHighLighter.GetToken,' (',FTheHighLighter.GetTokenKind ,') at ', FTheHighLighter.GetTokenPos]);
 
     if etiKind in e.Flags then
-      AssertEquals(Name + ' Kind @ TokenId Line='+IntToStr(LineIdx)+' pos='+IntToStr(c)+' Src='+FTheHighLighter.GetToken+' @'+IntToStr(FTheHighLighter.GetTokenPos),
+      AssertEquals(Name + ' ASSERT token-kind @ TokenId Line='+IntToStr(LineIdx)+' pos='+IntToStr(c)+' Src='+FTheHighLighter.GetToken+' @'+IntToStr(FTheHighLighter.GetTokenPos),
         e.ExpKind, FTheHighLighter.GetTokenKind);
 
     GotAttr := FTheHighLighter.GetTokenAttribute;

components/synedit/test/testsyntextarea.pas
@@ -69,6 +69,7 @@ end;
 procedure TTestSynTextArea.TearDown;
 begin
   FTokenBreaker.Free;
+  FreeAndNil(FTheHighLighter);
   inherited TearDown;
 end;
 

commit b9f865d16d08628799992a278f49b967d09d8363

SynEdit: PasHighLighter, fix class sections (private/public/...). Issue #40614 / broken in commit cbf59cc75ae3f3d7cc32345c32f99916989c94a8

components/synedit/synhighlighterpas.pp
@@ -92,10 +92,10 @@ type
     rsAtClass,
     rsInObjcProtocol,
     rsAfterClass,
+    rsInTypeHelper,
     rsAfterIdentifierOrValue, // anywhere where a ^ deref can happen "foo^", "foo^^", "foo()^", "foo[]^"
     rsAfterIdentifierOrValueAdd,
 
-    rsAtClosingBracket,   // ')'
     rsAtCaseLabel,
     rsAtProcName,    // after a procedure/function/... keyword, when the name is expected (not for types)
                      // also after "unit unitname" to detect "deprecated"
@@ -1417,7 +1417,12 @@ begin
   if KeyComp('For') then begin
     Result := tkKey;
     if TopPascalCodeFoldBlockType in PascalStatementBlocks then
-      StartPascalCodeFoldBlock(cfbtForDo);
+      StartPascalCodeFoldBlock(cfbtForDo)
+    else
+    if rsInTypeHelper in FOldRange then begin
+      fRange := fRange + [rsInTypeHelper];
+      FOldRange := FOldRange - [rsInTypeHelper];
+    end;
   end
   else
   if KeyComp('Shl') then begin
@@ -1722,9 +1727,10 @@ begin
   end
   else if KeyComp('Record') then begin
     StartPascalCodeFoldBlock(cfbtRecord);
-    fRange := fRange - [rsVarTypeInSpecification];
+    fRange := fRange - [rsVarTypeInSpecification, rsAfterEqual, rsAfterEqualOrColon] + [rsAfterSemiColon];
+    FOldRange := FOldRange - [rsAfterSemiColon];
     if CompilerMode = pcmDelphi then
-      fRange := fRange + [rsAtClass] - [rsAfterEqual, rsAfterEqualOrColon]; // highlight helper
+      fRange := fRange + [rsAtClass]; // highlight helper
     Result := tkKey;
   end
   else if KeyComp('Array') then Result := tkKey
@@ -1763,12 +1769,14 @@ begin
   // TODO: "class helper" fold at "class", but "type helper" fold at "helper"
   else if KeyComp('helper') then begin
     if (rsAtClass in fRange) and (PasCodeFoldRange.BracketNestLevel = 0)
-    then
-      Result := tkKey
+    then begin
+      Result := tkKey;
+      fRange := fRange - [rsVarTypeInSpecification, rsAfterEqual] + [rsInTypeHelper];
+    end
     else
     if (rsAfterEqualThenType in fRange) and TypeHelpers then begin
       Result := tkKey;
-      fRange := fRange - [rsVarTypeInSpecification, rsAfterEqual];
+      fRange := fRange - [rsVarTypeInSpecification, rsAfterEqual] + [rsInTypeHelper];
       StartPascalCodeFoldBlock(cfbtClass); // type helper
     end
     else
@@ -3712,10 +3720,6 @@ begin
   fTokenID := tkSymbol;
   fRange := fRange + [rsAfterIdentifierOrValueAdd];
   PasCodeFoldRange.DecBracketNestLevel;
-  if (PasCodeFoldRange.BracketNestLevel = 0) then begin
-    if (fRange * [rsAfterClass] <> []) then
-      fRange := fRange + [rsAtClosingBracket];
-  end;
 end;
 
 procedure TSynPasSyn.SquareOpenProc;
@@ -3960,6 +3964,7 @@ end;
 procedure TSynPasSyn.Next;
 var
   IsAtCaseLabel: Boolean;
+  OldNestLevel: Integer;
 begin
   fAsmStart := False;
   FIsPasDocKey := False;
@@ -3989,6 +3994,7 @@ begin
         SlashContinueProc
       else begin
         FOldRange := fRange;
+        OldNestLevel := PasCodeFoldRange.BracketNestLevel;
         if (PasCodeFoldRange.BracketNestLevel = 1) then // procedure foo; [attr...]
           FOldRange := FOldRange - [rsWasInProcHeader];
         FTokenFlags := [];
@@ -4014,17 +4020,24 @@ begin
 
         if not (FTokenID in [tkSpace, tkComment, tkIDEDirective, tkDirective]) then begin
           if (PasCodeFoldRange.BracketNestLevel = 0) and
-             not(rsAtClosingBracket in fRange)
+             (OldNestLevel = 0)
           then
             fRange := fRange - [rsAfterClass];
+          if (PasCodeFoldRange.BracketNestLevel > 0) or
+             (OldNestLevel > 0)
+          then
+            FOldRange := FOldRange - [rsInTypeHelper];
 
           fRange := fRange -
             (FOldRange * [rsAfterEqualOrColon, rsAfterSemiColon,
                           rsAtPropertyOrReadWrite, rsAfterClassField,
                           rsAfterIdentifierOrValue, rsAfterEqualThenType,
-                          rsWasInProcHeader, rsAtProcName, rsAfterProcName]
-            ) -
-            [rsAtClosingBracket];
+                          rsWasInProcHeader, rsAtProcName, rsAfterProcName,
+                          rsInTypeHelper]
+            );
+
+          if (FTokenID = tkIdentifier) and (rsInTypeHelper in FOldRange) then
+            fRange := fRange + [rsAfterSemiColon];
 
           if rsAtClass in fRange then begin
             if FOldRange * [rsAtClass, rsAfterClass] <> [] then
@@ -4034,7 +4047,6 @@ begin
           end
         end
         else begin
-          fRange := fRange - [rsAtClosingBracket];
           if rsAtClass in fRange then
             fRange := fRange + [rsAfterClass];
         end;

components/synedit/test/testhighlightpas.pas
@@ -1715,9 +1715,12 @@ begin
       ([ 'Unit A; interface',
          'type',
          'TFoo = class helper for TBar',
-           'helper, sealed, abstract: Integer;',
+           'helper, sealed, abstract, public: Integer;',
            'procedure Foo; abstract;',
           'end;',
+         'TFoo = class helper for TBar',
+         'protected',
+         'end;',
          ''
       ]);
     CheckTokensForLine('class declaration"',  2,
@@ -1726,11 +1729,21 @@ begin
         tkSpace, tkIdentifier
       ]);
     CheckTokensForLine('var in class "',  3,
-      [ tkIdentifier, tkSymbol, tkSpace,  tkIdentifier, tkSymbol, tkSpace,  tkIdentifier, tkSymbol,
+      [ tkIdentifier, tkSymbol, tkSpace,  tkIdentifier, tkSymbol, tkSpace,
+        tkIdentifier, tkSymbol, tkSpace,  tkIdentifier, tkSymbol,
         tkSpace, tkIdentifier, tkSymbol
       ]);
     CheckTokensForLine('procedure in class "',  4,
       [ tkKey, tkSpace, tkIdentifier + FAttrProcName,  tkSymbol, tkSpace, tkKey,  tkSymbol ]);
+    CheckTokensForLine('end',  5,
+      [ tkKey, tkSymbol ]);
+    CheckTokensForLine('class declaration"',  6,
+      [ tkIdentifier, tkSpace, tkSymbol, tkSpace,
+        tkKey {class}, tkSpace, tkKey {helper}, tkSpace, tkKey {for},
+        tkSpace, tkIdentifier
+      ]);
+    CheckTokensForLine('class section',  7,
+      [ tkKey ]);
 
 
     ReCreateEdit;
@@ -1742,6 +1755,8 @@ begin
            'helper, sealed, abstract: Integer;',
            'procedure Foo; abstract;',
           'end;',
+         'TFoo = class helper(helper) for helper',
+         'protected',
          ''
       ]);
     CheckTokensForLine('class declaration"',  2,
@@ -1756,6 +1771,8 @@ begin
       ]);
     CheckTokensForLine('procedure in class "',  4,
       [ tkKey, tkSpace, tkIdentifier + FAttrProcName,  tkSymbol, tkSpace, tkKey,  tkSymbol ]);
+    CheckTokensForLine('class section',  7,
+      [ tkKey ]);
   end;
 end;
 
@@ -1785,6 +1802,11 @@ procedure TTestHighlighterPas.TestContextForTypeHelper;
         tkKey {type}, tkSpace, tkIdentifier {helper}, tkSpace, tkKey {for}, tkSpace, tkIdentifier, tkSymbol
       ]);
     AssertEquals('not a helper, switched off / no fold', 0, PasHighLighter.FoldOpenCount(11));
+
+    CheckTokensForLine('class section',  14,
+      [ tkKey ]);
+    CheckTokensForLine('NOT class section',  18,
+      [ tkIdentifier ]);
   end;
 
 var
@@ -1812,6 +1834,13 @@ begin
          'type',
          '{$modeswitch typehelpers-}',
          'helper = type helper for helper;',
+         '{$modeswitch typehelpers}',
+         'helper = type helper for helper',
+         'protected',
+         'end;',
+         '{$modeswitch typehelpers-}',
+         'helper = type helper for helper',
+         'protected',
          '{$modeswitch typehelpers}',
          ''
       ]);
@@ -1884,6 +1913,9 @@ begin
          'TFoo = record helper for TBar',
            'helper, sealed, abstract: Integer;',
           'end;',
+         'TFoo = record helper for TBar',
+          'protected;',
+          'end;',
          ''
       ]);
     CheckTokensForLine('record declaration"',  2,
@@ -1895,6 +1927,8 @@ begin
       [ tkIdentifier, tkSymbol, tkSpace,  tkIdentifier, tkSymbol, tkSpace,  tkIdentifier, tkSymbol,
         tkSpace, tkIdentifier, tkSymbol
       ]);
+    CheckTokensForLine('class section',  6,
+      [ tkKey ]);
 
 
     ReCreateEdit;