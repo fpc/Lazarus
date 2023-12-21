fpc/Lazarus

commit 9db0c4e54e0e15d8395a25157531d209b811ddd3

SynEdit: PasHighLighter, fix parsing "=" after "generic foo<_A>" (without spaces, looks like ">=")

components/synedit/synhighlighterpas.pp
@@ -1803,7 +1803,9 @@ begin
     if (PasCodeFoldRange.BracketNestLevel = 0)
        and (TopPascalCodeFoldBlockType in
         [cfbtVarType, cfbtLocalVarType, cfbtNone, cfbtProcedure, cfbtAnonymousProcedure, cfbtProgram,
-         cfbtUnit, cfbtUnitSection])
+         cfbtUnit, cfbtUnitSection,
+         cfbtClass, cfbtClassSection, cfbtRecord // if inside a type section in class/record
+        ])
     then begin
       if (rsAfterEqualOrColon in fRange) then begin
         if TypeHelpers then
@@ -3483,8 +3485,11 @@ procedure TSynPasSyn.GreaterProc;
 begin
   fTokenID := tkSymbol;
   inc(Run);
-  if fLine[Run] = '=' then
+  if fLine[Run] = '=' then begin
     inc(Run);
+    if (rsInTypeBlock in fRange) then // generic TFoo<..>= // no space between > and =
+      fRange := fRange + [rsAfterEqual, rsAfterEqualOrColon];
+  end;
   if fRange * [rsProperty, rsVarTypeInSpecification] = [rsProperty] then
     fRange := fRange + [rsAtPropertyOrReadWrite];
 end;

components/synedit/test/testhighlightpas.pas
@@ -61,6 +61,7 @@ type
     procedure TestContextForProcedureNameAttr;
     procedure TestContextForInterface;
     procedure TestContextForDeprecated;
+    procedure TestContextForClassObjRecHelp;
     procedure TestContextForClassSection;
     procedure TestContextForClassModifier; // Sealed abstract
     procedure TestContextForClassHelper;
@@ -1457,6 +1458,104 @@ begin
     SubTest('unimplemented', AFolds);
     SubTest('experimental' , AFolds);
     SubTest('platform'     , AFolds);
+  end;
+end;
+
+procedure TTestHighlighterPas.TestContextForClassObjRecHelp;
+var
+  i0, i1, i2, i3, i4: Integer;
+  s0, s1, s2: String;
+begin
+  ReCreateEdit;
+  EnableFolds([cfbtClass, cfbtRecord], []);
+
+  for i0 := 0 to 9 do
+  for i1 := 0 to 5 do
+  for i2 := 0 to 1 do
+  for i3 := 0 to 1 do
+  for i4 := 0 to 3 do
+  begin
+    if (i1 = 3) and (i2 = 1) then // type type helper
+      continue;
+
+    case i0 of
+      0: s0 := '';
+      1: s0 := 'TSome = class type';
+      2: s0 := 'TSome = class public type';
+      3: s0 := 'TSome = class var a: integer; type';
+      4: s0 := 'TSome = object type';
+      5: s0 := 'TSome = object public type';
+      6: s0 := 'TSome = object var a: integer; type';
+      7: s0 := 'TSome = record type';
+      8: s0 := 'TSome = record public type';
+      9: s0 := 'TSome = record var a: integer; type';
+// TODO: nested in record-case
+      //10: s0 := 'TSome = record case integer of 1: ( a: record b: integer; type'; // BracketNestLevel inside record-case
+    end;
+
+    case i4 of
+      0: s1 := 'TFoo = ';
+      1: s1 := 'TFoo=';
+      2: s1 := 'generic TFoo<A> = ';
+      3: s1 := 'generic TFoo<A>=';
+    end;
+
+    case i1 of
+      0: s2 := 'class';
+      1: s2 := 'record';
+      2: s2 := 'object';
+      3: s2 := 'type helper for integer';
+      4: s2 := 'class helper for TFoo';
+      5: s2 := 'record helper for TBar';
+    end;
+    if i2 = 1 then s2 := 'type '+s2;
+    if i3 = 1 then s2 := ' '+s2; // leading space
+
+    SetLines
+      ([ 'Unit A; interface {$modeswitch advancedrecords}{$modeswitch typehelpers}',  // 0
+         'type',
+         s0,
+         '',
+         s1,
+         s2,  // 5
+         'public',
+         'end;',
+         ''
+      ]);
+
+    AssertEquals(1, FTheHighLighter.FoldOpenCount(5));  // fold opens for class/record/...
+    AssertEquals(7, FTheHighLighter.FoldEndLine(5, 0));  // fold end for class/record/...
+  end;
+
+
+
+  for i2 := 0 to 1 do
+  for i3 := 0 to 1 do
+  for i4 := 0 to 1 do
+  begin
+
+    case i4 of
+      0: s1 := 'TFoo : ';
+      1: s1 := 'TFoo:';
+    end;
+
+    s2 := 'record';
+    if i2 = 1 then s2 := 'type '+s2;
+    if i3 = 1 then s2 := ' '+s2; // leading space
+    SetLines
+      ([ 'Unit A; interface {$modeswitch advancedrecords}{$modeswitch typehelpers}',  // 0
+         'var',
+         '',
+         '',
+         s1,
+         s2,  // 5
+         'public',
+         'end;',
+         ''
+      ]);
+
+    AssertEquals(1, FTheHighLighter.FoldOpenCount(5));  // fold opens for class/record/...
+    AssertEquals(7, FTheHighLighter.FoldEndLine(5, 0));  // fold end for class/record/...
   end;
 end;
 

commit cca9327230bfef6b62c05bd05ab1a543da612b83

SynEdit: PasHighLighter, fix "type" in "class" blocks. Broken in 9db0c4e54e0e15d8395a25157531d209b811ddd3

components/synedit/synhighlighterpas.pp
@@ -1798,10 +1798,13 @@ begin
 end;
 
 function TSynPasSyn.Func66: TtkTokenKind;
+var
+  tfb: TPascalCodeFoldBlockType;
 begin
   if KeyComp('Type') then begin
+    tfb := TopPascalCodeFoldBlockType;
     if (PasCodeFoldRange.BracketNestLevel = 0)
-       and (TopPascalCodeFoldBlockType in
+       and (tfb in
         [cfbtVarType, cfbtLocalVarType, cfbtNone, cfbtProcedure, cfbtAnonymousProcedure, cfbtProgram,
          cfbtUnit, cfbtUnitSection,
          cfbtClass, cfbtClassSection, cfbtRecord // if inside a type section in class/record
@@ -1812,11 +1815,14 @@ begin
           fRange := fRange + [rsAfterEqualThenType];
       end
       else begin
-        if TopPascalCodeFoldBlockType in [cfbtVarType, cfbtLocalVarType] then
+        if tfb in [cfbtVarType, cfbtLocalVarType] then begin
           EndPascalCodeFoldBlockLastLine;
-        if TopPascalCodeFoldBlockType in [cfbtProcedure, cfbtAnonymousProcedure]
-        then StartPascalCodeFoldBlock(cfbtLocalVarType)
-        else StartPascalCodeFoldBlock(cfbtVarType);
+          tfb := TopPascalCodeFoldBlockType;
+        end;
+        if not(tfb in [cfbtClass, cfbtClassSection, cfbtRecord]) then
+          if tfb in [cfbtProcedure, cfbtAnonymousProcedure]
+          then StartPascalCodeFoldBlock(cfbtLocalVarType)
+          else StartPascalCodeFoldBlock(cfbtVarType);
         fRange := fRange + [rsInTypeBlock];
       end;
     end;