fpc/Lazarus

commit fad67e71ecd08a4c926e221f6553adadb9498aea

SynEdit: Improve GetPhysicalCharWidths in paint.

components/synedit/lazsyntextarea.pp
@@ -54,7 +54,7 @@ type
     FLinesView:  TSynEditStrings;
     FMarkupManager: TSynEditMarkupManager;
 
-    FCharWidths: TPhysicalCharWidths;
+    FCharWidths, FCharWidths2: TPhysicalCharWidths;
     FCharWidthsLen: Integer;
     FCurTxtLineIdx : Integer;
     FCurLineByteLen: Integer;
@@ -314,6 +314,7 @@ end;
 procedure TLazSynPaintTokenBreaker.Finish;
 begin
   FCharWidths := nil;
+  FCharWidths2 := nil;
 end;
 
 procedure TLazSynPaintTokenBreaker.SetHighlighterTokensLine(ALine: TLineIdx; out
@@ -322,9 +323,17 @@ var
   LogLeftPos: Integer;
 begin
   FDisplayView.SetHighlighterTokensLine(ALine, ARealLine, LogLeftPos, FCurLineByteLen);
-  FCharWidths := FLinesView.GetPhysicalCharWidths(ARealLine);
-  FCharWidthsLen := Length(FCharWidths);
-  FLinesView.GetPhysicalCharWidths(ARealLine, FCharWidths, FCharWidthsLen);
+  if FLinesView.LogPhysConvertor.CurrentLine = ARealLine then begin
+    FCharWidths2 := FCharWidths;
+    FCharWidths :=FLinesView.LogPhysConvertor.CurrentWidthsDirect;
+  end
+  else begin
+    if FCharWidths2 <> nil then begin
+      FCharWidths := FCharWidths2;
+      FCharWidths2 := nil;
+    end;
+    FLinesView.GetPhysicalCharWidths(ARealLine, FCharWidths, FCharWidthsLen);
+  end;
   FCurLineByteLen := FCurLineByteLen + LogLeftPos - 1;
 
   FCurViewToken.TokenLength     := 0;
@@ -1790,6 +1799,7 @@ var
       rcLine.Left := DrawLeft;
       LineBufferRtlLogPos := -1;
 
+      CharWidths := nil; // keep refcnt = 1 -- in case they get resized
       FTokenBreaker.SetHighlighterTokensLine(TV + CurLine, CurTextIndex);
       CharWidths := FTokenBreaker.CharWidths;
       fMarkupManager.PrepareMarkupForRow(CurTextIndex+1);

commit 0a3d7d4e280663e2818aec1570b721c7f534bfa9

SynEdit: fix charwidths issue in paint. Broken in fad67e71ecd08a4c926e221f6553adadb9498aea Issue #40716

components/synedit/lazsyntextarea.pp
@@ -326,6 +326,7 @@ begin
   if FLinesView.LogPhysConvertor.CurrentLine = ARealLine then begin
     FCharWidths2 := FCharWidths;
     FCharWidths :=FLinesView.LogPhysConvertor.CurrentWidthsDirect;
+    FCharWidthsLen :=FLinesView.LogPhysConvertor.CurrentWidthsCount;
   end
   else begin
     if FCharWidths2 <> nil then begin