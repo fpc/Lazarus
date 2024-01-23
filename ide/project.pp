145bfa9c0c58c279a697cf2a4ff3c576093d75e3
Martin <laz.git@mfriebe.de>
2024-01-23 12:33:11 +0100

    IDE, SourceEditor: Restore saving (to session) the current active tab of *all* open SourceWindows. (Undone from 907d1a29da06488ba0861e872dd32120b276eaa0)

--- ide/project.pp
+++ ide/project.pp
@@ -3954,7 +3954,8 @@ var
 begin
   for i := 0 to AllEditorsInfoCount - 1 do
     with AllEditorsInfo[i] do
-      IsVisibleTab := (WindowID = AWindowID) and (EditorComponent = AnEditor);
+      if AllEditorsInfo[i].WindowID = AWindowID then
+        IsVisibleTab := (EditorComponent = AnEditor);
   AnEditorInfo := EditorInfoWithEditorComponent(AnEditor);
   if AnEditorInfo = nil then Exit(nil);
   Result := AnEditorInfo.UnitInfo;
