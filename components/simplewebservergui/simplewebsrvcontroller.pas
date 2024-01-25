4103cb5c760ce193206386b0b7211d53d3d0ebc7
mattias <nc-gaertnma@netcologne.de>
2024-01-25 13:31:45 +0100

    pas2js: added default browser paths for windows

--- components/simplewebservergui/simplewebsrvcontroller.pas
+++ components/simplewebservergui/simplewebsrvcontroller.pas
@@ -2038,6 +2038,7 @@ function TSimpleWebServerController.GetBrowserChrome(URL: string;
 begin
   Result := FindBrowserPath([
     {$IFDEF Darwin}'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',{$ENDIF}
+    {$IFDEF MSWindows}'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe',{$ENDIF}
     'google-chrome'],URL,Params);
 end;
 
@@ -2046,6 +2047,7 @@ function TSimpleWebServerController.GetBrowserFirefox(URL: string;
 begin
   Result := FindBrowserPath([
     {$IFDEF Darwin}'/Applications/Firefox.app/Contents/MacOS/firefox',{$ENDIF}
+    {$IFDEF MSWindows}'C:\Program Files\Mozilla Firefox\firefox.exe',{$ENDIF}
     'firefox','mozilla'],URL,Params);
 end;
 
@@ -2077,7 +2079,9 @@ end;
 function TSimpleWebServerController.GetBrowserEdge(URL: string; Params: TStrings
   ): string;
 begin
-  Result := FindBrowserPath(['edge'],URL,Params);
+  Result := FindBrowserPath([
+    'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe',
+    'edge'],URL,Params);
 end;
 {$ENDIF}
 
