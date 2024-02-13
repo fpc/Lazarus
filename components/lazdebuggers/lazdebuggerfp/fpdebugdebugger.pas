972e988e00b4ee0c361d882ec5f19abf9e246e33
Martin <laz.git@mfriebe.de>
2024-02-13 22:31:11 +0100

    FpDebug: further fix reading pty for console window.
    (cherry picked from commit e9099ab0e82f4f2f8a426791e3383948af9dbd47)

--- components/lazdebuggers/lazdebuggerfp/fpdebugdebugger.pas
+++ components/lazdebuggers/lazdebuggerfp/fpdebugdebugger.pas
@@ -3424,8 +3424,9 @@ begin
     AThread := TFpWaitForConsoleOutputThread(FConsoleOutputThread);
     FConsoleOutputThread := nil;
     AThread.Terminate;
-    AThread.DoHasConsoleOutput(0);
     AThread.WaitFor;
+    sleep(50);
+    AThread.DoHasConsoleOutput(0);
     AThread.Free;
     end;
 
