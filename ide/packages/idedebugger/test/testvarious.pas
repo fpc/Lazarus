3324959e82b6151eda67bd83934cd04be6bbbaa8
Martin <laz.git@mfriebe.de>
2024-03-03 20:48:18 +0100

    Debugger: fix parsing for array indexes (expanding array in watches / replace .. idx)
    (cherry picked from commit 45a6759c3d633b346edc21a04539441bd320b7b0)

--- ide/packages/idedebugger/test/testvarious.pas
+++ ide/packages/idedebugger/test/testvarious.pas
@@ -25,6 +25,11 @@ implementation
 { TTestIdeDebuggerVarious }
 
 procedure TTestIdeDebuggerVarious.TestExpressionForArrayElement;
+  procedure Test(ABefore: string; AIdx: Integer; AnAfter: string);
+  begin
+    AssertEquals(ABefore+' @ ' + IntToStr(AIdx) + ' => '+AnAfter,
+                 AnAfter, GetExpressionForArrayElement(ABefore, AIdx));
+  end;
 begin
   AssertEquals('a',    'a[11]',    GetExpressionForArrayElement('a', 11));
   AssertEquals('(a)',  '(a)[11]',  GetExpressionForArrayElement('(a)', 11));
@@ -45,6 +50,30 @@ begin
     'PCellProps((TFpList((StringGrid1.FGrid.FCellArr.FCols.FList)^[2]).FList)^[0..3])^',
     GetExpressionForArrayElement('PCellProps((TFpList((StringGrid1.FGrid.FCellArr.FCols.FList)^[0..3]).FList)^[0..3])^', 2)
   );
+
+  // nested goes first
+  Test('Foo(a[x[2..14]..99])',      11, 'Foo(a[x[11]..99])');
+  Test('Foo(a[1+x[2..14]..99])',    11, 'Foo(a[1+x[11]..99])');
+  Test('Foo(a[(1+x[2..14])..99])',  11, 'Foo(a[(1+x[11])..99])');
+  Test('Foo(a[0..x[2..14]])',       11, 'Foo(a[0..x[11]])');
+  Test('Foo(a[0..1+x[2..14]])',     11, 'Foo(a[0..1+x[11]])');
+  Test('Foo(a[0..(1+x[2..14])])',   11, 'Foo(a[0..(1+x[11])])');
+
+  // not in set
+  Test('Foo(a[x in [1..3]])',  11, 'Foo(a[x in [1..3]])[11]'); // no ..
+  Test('Foo(a[x in [1..3]..True])',  11, 'Foo(a[11])');
+
+  Test('Foo(a[b[2..22]])',     11, 'Foo(a[b[11]])');
+  Test('Foo(a[(1+b[2..22])])', 11, 'Foo(a[(1+b[11])])');
+
+  // ","
+  Test('Foo(a[2..22,55..66])', 11, 'Foo(a[11,55..66])');
+  Test('Foo(a[99,2..22])',     11, 'Foo(a[99,11])');
+
+  // set from array
+  Test('e1 in [x[1..15]]',     11, 'e1 in [x[11]]');
+  Test('e1 in [x[1..15]..x[33..35]]',     11, 'e1 in [x[11]..x[33..35]]');
+
 end;
 
 initialization
