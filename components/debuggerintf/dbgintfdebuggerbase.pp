aeffa71e22a9cb54b31c68a7e0efe7450c170b16
Martin <laz.git@mfriebe.de>
2024-03-14 15:14:19 +0100

    Debugger: fixed crash in disassembler (dangling pointer, due to wrong object in free notify)

--- components/debuggerintf/dbgintfdebuggerbase.pp
+++ components/debuggerintf/dbgintfdebuggerbase.pp
@@ -4971,7 +4971,7 @@ begin
     OldId := MergeRng.RangeStartAddr;
     MergeRng.Merge(ARange);
     if assigned(FOnMerge)
-    then FOnMerge(ARange, MergeRng);
+    then FOnMerge(MergeRng, ARange);
     FFreeItemLock := True; // prevent destruction of MergeRng
     Delete(OldId);
     FFreeItemLock := False;
