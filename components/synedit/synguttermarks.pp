9e4f31e4525c9e0f136cb467cb16a0401322e586
Martin <laz.git@mfriebe.de>
2024-04-20 14:26:36 +0200

    SynEdit: Marks-gutter, use all columns before icons get out of visible range. Issue #40914
    (cherry picked from commit 8bcc16401918ed63fb0b777216f8e25f95563daa)

--- components/synedit/synguttermarks.pp
+++ components/synedit/synguttermarks.pp
@@ -145,9 +145,6 @@ var
   iRange: TLineRange;
 begin
   Result := False;
-  aFirstCustomColumnIdx := 0;
-  if FBookMarkOpt.DrawBookmarksFirst then
-    aFirstCustomColumnIdx := 1;
   aScreenLine := aScreenLine + ToIdx(GutterArea.TextArea.TopLine);
   j := ViewedTextBuffer.DisplayView.ViewToTextIndexEx(aScreenLine, iRange);
   if aScreenLine <> iRange.Top then
@@ -162,6 +159,10 @@ begin
     MLine.Sort(smsoBookmarkFirst, smsoPriority)
   else
     MLine.Sort(smsoBookMarkLast, smsoPriority);
+
+  aFirstCustomColumnIdx := 0;
+  if (FBookMarkOpt.DrawBookmarksFirst) and (MLine.Count < ColumnCount) then
+    aFirstCustomColumnIdx := 1;
 
   LineHeight := SynEdit.LineHeight;
   //Gutter.Paint always supplies AClip.Left = GutterPart.Left
@@ -180,7 +181,7 @@ begin
       continue;
 
     if (MLine[j].IsBookmark <> LastMarkIsBookmark) and
-       (j = 0) and (FColumnCount > 1)
+       (j = 0) and (aFirstCustomColumnIdx >= 1)
     then begin
       // leave one column empty
       MarkRect.Left := MarkRect.Right;
