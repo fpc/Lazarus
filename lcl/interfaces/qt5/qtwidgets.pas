9e4dc74ca5ac356b051ad9f739a1b54c5b6a195d
Željan Rikalo <zeljko@holobit.hr>
2024-05-05 14:27:17 +0000

    Qt5,Qt6: fixed readOnly status for TQtTextEdit on FreeBSD. issue #40930
    (cherry picked from commit 0a7ff2fb3dd2136de63cec6d2dbbbef83e826bab)
    
    Co-authored-by: Željan Rikalo <zeljko@lazarus-ide.org>

--- lcl/interfaces/qt5/qtwidgets.pas
+++ lcl/interfaces/qt5/qtwidgets.pas
@@ -10213,7 +10213,7 @@ end;
 
 procedure TQtTextEdit.setReadOnly(const AReadOnly: Boolean);
 begin
-  {$IFDEF DARWIN} // issue #40246
+  {$IF DEFINED(DARWIN) OR DEFINED(FREEBSD)} // issue #40246
   if AReadOnly <> QTextEdit_isReadOnly(QTextEditH(Widget)) then
   {$ENDIF}
     QTextEdit_setReadOnly(QTextEditH(Widget), AReadOnly);
