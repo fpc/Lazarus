71526ba83e1d432c71fdaa3e19a9df04892454b4
Ondrej Pokorny <ondrej@kluug.net>
2024-05-23 11:16:01 +0200

    Lazarus IDE: better comment about MainFormOnTaskBar := False, issue #39654

--- ide/lazarus.pp
+++ ide/lazarus.pp
@@ -115,11 +115,10 @@ begin
   OnGetApplicationName:=@GetLazarusApplicationName;
 
   {$IFDEF Windows}
-  // on windows when MainFormOnTaskBar = True the main form becomes
+  // On windows when MainFormOnTaskBar = True the main form becomes
   // the parent of all other forms and therefore it always shows under
-  // other forms. For Lazarus the main form is the component palette form 
-  // and it is not a desired behavior to see it always under other windows.
-  // So until we have a good docking solution let's have a dummy taskbar windows on windows.
+  // other forms including the design-mode forms.
+  // It can be changed to MainFormOnTaskBar := True once we have a working inline form designer.
   Application.{%H-}MainFormOnTaskBar := False;
   {$ENDIF}
 
