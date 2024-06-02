a9137b980f50089f6528896888495251fee23781
DomingoGP <dgalmesp@gmail.com>
2024-06-02 15:18:52 +0200

    Jedi code format: Fix issue #40981 inherited function calls.
    (cherry picked from commit b37e92de41605c20d55c22d1c018f05732679c8a)

--- components/jcf2/Parse/BuildParseTree.pas
+++ components/jcf2/Parse/BuildParseTree.pas
@@ -2876,7 +2876,7 @@ begin
   begin
     Recognise(ttInherited);
 
-    if not (fcTokenList.FirstSolidTokenType in Operators + [ttSemicolon]) then
+    if not (fcTokenList.FirstSolidTokenType in Operators + BlockExitWords + [ttSemicolon, ttCloseBracket, ttComma]) then
     begin
       RecogniseDesignator;
       if fcTokenList.FirstSolidTokenType = ttOpenBracket then
