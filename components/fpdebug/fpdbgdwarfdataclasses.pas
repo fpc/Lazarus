48c96f03fc8dd2a1f05bd9b196da8fba6a6890a1
Martin <laz.git@mfriebe.de>
2024-07-05 10:28:07 +0200

    FpDebug: improve compile error msg

--- components/fpdebug/fpdbgdwarfdataclasses.pas
+++ components/fpdebug/fpdbgdwarfdataclasses.pas
@@ -35,7 +35,7 @@
 unit FpDbgDwarfDataClasses;
 
 {$mode objfpc}{$H+}
-{$IFOPT T+}{$ERROR Typedaddress not allowed}{$ENDIF}
+{$IFOPT T+}{$ERROR Typedaddress (-Sy) not allowed}{$ENDIF}
 {$ModeSwitch advancedrecords}
 {$IFDEF INLINE_OFF}{$INLINE OFF}{$ENDIF}
 {$IF FPC_Fullversion=30202}{$Optimization NOPEEPHOLE}{$ENDIF}
