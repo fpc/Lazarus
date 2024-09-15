f8ef2a93e655ab84c71252ef6db198fcbabaced5
rich2014 <rich2014.git@outlook.com>
2024-09-15 23:44:51 +0800

    Cocoa/ListView: in vsReport, support tips to expand and display text when the column is too narrow

--- lcl/interfaces/cocoa/cocoatables.pas
+++ lcl/interfaces/cocoa/cocoatables.pas
@@ -1204,6 +1204,7 @@ begin
   fieldControl.setDrawsBackground( False );
   fieldControl.setEditable( False );
   fieldControl.setLineBreakMode( NSLineBreakByTruncatingTail );
+  fieldControl.setAllowsExpansionToolTips(True);
   self.setTextField( fieldControl );
   self.addSubview( fieldControl );
 end;
