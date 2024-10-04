e78f9e84ad6e594cd7af4e8412ea5bb347b62cd0
rich2014 <rich2014.git@outlook.com>
2024-10-05 00:11:04 +0800

    Cocoa: fix memory leak in LegacyScroller #41171

--- lcl/interfaces/cocoa/cocoascrollers.pas
+++ lcl/interfaces/cocoa/cocoascrollers.pas
@@ -511,6 +511,7 @@ begin
   scrollBar:= TCocoaScrollBar(TCocoaScrollBar.alloc);
   manager:= TCocoaScrollStyleManagerLegacy.createForScrollBar;
   scrollBar.setManager( manager );
+  scrollBar.autorelease;
 end;
 
 function allocScroller(parent: TCocoaManualScrollView; dst: NSRect; aVisible: Boolean)
