62084108c076336abb4e7c7e9721471e2c069f50
Ondrej Pokorny <ondrej@kluug.net>
2024-10-14 14:20:06 +0200

    PackageTabs: Improve visibility of selected editor tab by disabling button transparency. Issue #41132

--- components/packagetabs/packagetabs_impl.pas
+++ components/packagetabs/packagetabs_impl.pas
@@ -500,7 +500,7 @@ constructor TPackageTabButton.Create(aOwner: TComponent);
 begin
   inherited Create(aOwner);
 
-  Transparent := True;
+  Transparent := False;
   Flat := True;
   AutoSize := True;
   GroupIndex := 1;
