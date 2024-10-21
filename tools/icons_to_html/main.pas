fpc/Lazarus

commit 9bd8fbdd2d4027d72d05c50dc0eebf6bb0662097

Icons_to_html: restored resource strings after 464d4133ccae215e94a2e7e654b67bd6107962fc

tools/icons_to_html/main.pas
@@ -74,13 +74,13 @@ const
 resourcestring
   rsInformation = 'Information';
   rsError = 'Error';
-  rsTheConfigurationCouldNotBeSaved = 'The configuration file could not be saved.';
-  rsTheTempFileCouldNotBeDeleted = 'The temp file could not be deleted.';
+  rsTheConfigurationCouldNotBeSaved = 'Configuration file could not be saved.';
+  rsTheTempFileCouldNotBeDeleted = 'Temporary file could not be deleted.';
   rsTheFileCouldNotBeSavedAs = 'The file could not be saved as: %s';
   rsSavedAs = 'Saved as: %s';
-  rsNoPngImageFilesFoundIn = 'No png image files found in %s';
-  rsNoMatchingPngImageFilesFoundIn = 'No matching png image files found in %s';
-  rsTheFolderDoesNotExist = 'The folder [%s] does not exist or is currently not available.'#13#13'Should it be removed from the list?';
+  rsNoPngImageFilesFoundIn = 'No PNG image files found in %s';
+  rsNoMatchingPngImageFilesFoundIn = 'No matching PNG image files found in %s';
+  rsTheFolderDoesNotExist = 'Folder "%s" does not exist or is currently not available.'+LineEnding+LineEnding+'Should it be removed from the list?';
   rsThisFolderContains = ThisFolderContains;
   rsSize = 'Size';
   rsName = 'Name';

images/general_purpose/IconTable.html
@@ -21,7 +21,7 @@
 <table>
   <tr class="no_border">
     <td class="colorset1 right_border"></td>
-    <td class="colorset2 text_center" colspan="6">Größe</td>
+    <td class="colorset2 text_center" colspan="6">Size</td>
   </tr>
   <tr>
     <td class="colorset1 right_border">Name</td>
@@ -5641,7 +5641,7 @@
   </tr>
 </table>
 <div class="infobox colorset2">
-Dieser Ordner enthält 3738 Icons in 623 Icon-Gruppen mit 6 Icon-Größen.
+This folder contains 3738 icons in 623 icon groups with 6 icon sizes.
 <hr>
 The images in this folder can be used in Lazarus applications as toolbar or button icons.<br>
 <br>

tools/icons_to_html/languages/icontable.de.po
@@ -50,13 +50,15 @@ msgid "Name"
 msgstr "Name"
 
 #: main.rsnomatchingpngimagefilesfoundin
-#, object-pascal-format
-msgid "No matching png image files found in %s"
+#, object-pascal-format, fuzzy
+#| msgid "No matching png image files found in %s"
+msgid "No matching PNG image files found in %s"
 msgstr "Keine passenden png-Bilddateien gefunden in %s"
 
 #: main.rsnopngimagefilesfoundin
-#, object-pascal-format
-msgid "No png image files found in %s"
+#, object-pascal-format, fuzzy
+#| msgid "No png image files found in %s"
+msgid "No PNG image files found in %s"
 msgstr "Keine png-Bilddateien gefunden in %s"
 
 #: main.rssavedas
@@ -81,7 +83,9 @@ msgid "16x16, 24x24 and 32x32 pixels for \"small\" images,"
 msgstr "16x16, 24x24 und 32x32 Pixel für \"kleine\" Bilder,"
 
 #: main.rstheconfigurationcouldnotbesaved
-msgid "The configuration file could not be saved."
+#, fuzzy
+#| msgid "The configuration file could not be saved."
+msgid "Configuration file could not be saved."
 msgstr "Die Konfiguration konnte nicht gespeichert werden."
 
 #: main.rsthefilecouldnotbesavedas
@@ -90,9 +94,13 @@ msgid "The file could not be saved as: %s"
 msgstr "Die Datei konnte nicht gespeichert werden als: %s"
 
 #: main.rsthefolderdoesnotexist
-#, object-pascal-format
+#, object-pascal-format, fuzzy
+#| msgid ""
+#| "The folder [%s] does not exist or is currently not available.\n"
+#| "\n"
+#| "Should it be removed from the list?"
 msgid ""
-"The folder [%s] does not exist or is currently not available.\n"
+"Folder \"%s\" does not exist or is currently not available.\n"
 "\n"
 "Should it be removed from the list?"
 msgstr ""
@@ -101,7 +109,9 @@ msgstr ""
 "Soll er von der Liste gestrichen werden?"
 
 #: main.rsthetempfilecouldnotbedeleted
-msgid "The temp file could not be deleted."
+#, fuzzy
+#| msgid "The temp file could not be deleted."
+msgid "Temporary file could not be deleted."
 msgstr "Die Temporär-Datei konnte nicht gelöscht werden."
 
 #: main.rsthisfoldercontains
@@ -164,3 +174,4 @@ msgstr "Ordner mit den Bildern, für die der HTML-Code erzeugt werden soll"
 #: tmainform.sbtnlastdirs.hint
 msgid "Last used"
 msgstr "Zuletzt benutzt"
+

tools/icons_to_html/languages/icontable.pot
@@ -39,12 +39,12 @@ msgstr ""
 
 #: main.rsnomatchingpngimagefilesfoundin
 #, object-pascal-format
-msgid "No matching png image files found in %s"
+msgid "No matching PNG image files found in %s"
 msgstr ""
 
 #: main.rsnopngimagefilesfoundin
 #, object-pascal-format
-msgid "No png image files found in %s"
+msgid "No PNG image files found in %s"
 msgstr ""
 
 #: main.rssavedas
@@ -69,7 +69,7 @@ msgid "16x16, 24x24 and 32x32 pixels for \"small\" images,"
 msgstr ""
 
 #: main.rstheconfigurationcouldnotbesaved
-msgid "The configuration file could not be saved."
+msgid "Configuration file could not be saved."
 msgstr ""
 
 #: main.rsthefilecouldnotbesavedas
@@ -80,13 +80,13 @@ msgstr ""
 #: main.rsthefolderdoesnotexist
 #, object-pascal-format
 msgid ""
-"The folder [%s] does not exist or is currently not available.\n"
+"Folder \"%s\" does not exist or is currently not available.\n"
 "\n"
 "Should it be removed from the list?"
 msgstr ""
 
 #: main.rsthetempfilecouldnotbedeleted
-msgid "The temp file could not be deleted."
+msgid "Temporary file could not be deleted."
 msgstr ""
 
 #: main.rsthisfoldercontains

tools/icons_to_html/languages/icontable.ru.po
@@ -48,15 +48,13 @@ msgid "Name"
 msgstr "Имя"
 
 #: main.rsnomatchingpngimagefilesfoundin
-#, object-pascal-format, fuzzy
-#| msgid "No matching PNG image files found in %s"
-msgid "No matching png image files found in %s"
+#, object-pascal-format
+msgid "No matching PNG image files found in %s"
 msgstr "Не найдено соответствующих файлов изображений PNG в %s"
 
 #: main.rsnopngimagefilesfoundin
-#, object-pascal-format, fuzzy
-#| msgid "No PNG image files found in %s"
-msgid "No png image files found in %s"
+#, object-pascal-format
+msgid "No PNG image files found in %s"
 msgstr "Не найдено файлов изображений PNG в %s"
 
 #: main.rssavedas
@@ -81,9 +79,7 @@ msgid "16x16, 24x24 and 32x32 pixels for \"small\" images,"
 msgstr ""
 
 #: main.rstheconfigurationcouldnotbesaved
-#, fuzzy
-#| msgid "Configuration file could not be saved."
-msgid "The configuration file could not be saved."
+msgid "Configuration file could not be saved."
 msgstr "Невозможно сохранить файл настройки."
 
 #: main.rsthefilecouldnotbesavedas
@@ -92,13 +88,9 @@ msgid "The file could not be saved as: %s"
 msgstr "Невозможно сохранить файл %s"
 
 #: main.rsthefolderdoesnotexist
-#, object-pascal-format, fuzzy
-#| msgid ""
-#| "Folder \"%s\" does not exist or is currently not available.\n"
-#| "\n"
-#| "Should it be removed from the list?"
+#, object-pascal-format
 msgid ""
-"The folder [%s] does not exist or is currently not available.\n"
+"Folder \"%s\" does not exist or is currently not available.\n"
 "\n"
 "Should it be removed from the list?"
 msgstr ""
@@ -107,9 +99,7 @@ msgstr ""
 "Удалить его из списка?"
 
 #: main.rsthetempfilecouldnotbedeleted
-#, fuzzy
-#| msgid "Temporary file could not be deleted."
-msgid "The temp file could not be deleted."
+msgid "Temporary file could not be deleted."
 msgstr "Невозможно удалить временный файл."
 
 #: main.rsthisfoldercontains

commit 38ce55d2e39381cd408953d380312eaf60b894a0

icons_to_html: Restored German translation broken after 9bd8fbdd2d4027d72d05c50dc0eebf6bb0662097

tools/icons_to_html/main.pas
@@ -419,31 +419,33 @@ begin
       IcoFileList.Count, IconGroups, PixSizeList.Count
     ]));
 
-    if cbTranslatedHTML.Checked then
-    begin
-      HTMLLines.Add('<hr>');
-      HTMLLines.Add('<p>' + rsInfoText1 + '</p>');
-      HTMLLines.Add('<p>' + rsInfoText2 + '</p>');
-      HTMLLines.Add('<ul>');
-      HTMLLines.Add('  <li>' + rsSizeInfoSmall + '</li>');
-      HTMLLines.Add('  <li>' + rsSizeInfoMedium + '</li>');
-      HTMLLines.Add('  <li>' + rsSizeInfoLarge + '</li>');
-      HTMLLines.Add('</ul>');
-      HTMLLines.Add('<p>' + Format(rsImagesWereProvidedBy, [Link(URL_RolandHahn, URL_RolandHahn)]) + '</p>');
-      HTMLLines.Add('<p>' + Format(rsLicense, [License_CC0]) + '<br>');
-      HTMLLines.Add(Link(URL_CC0, URL_CC0) + '<br>');
-      HTMLLines.Add(rsFreelyAvailable + '</p>');
-    end else
     if FileExists(ImgDirectory + InfoTextFileName) then
     begin
-      try
-        InfoTxtList := TStringList.Create;
-        InfoTxtList.LoadFromFile(ImgDirectory + InfoTextFileName);
+      if cbTranslatedHTML.Checked then
+      begin
         HTMLLines.Add('<hr>');
-        for i := 0 to InfoTxtList.Count - 1 do
-          HTMLLines.Add(InfoTxtList[i] + '<br>');
-      finally
-        InfoTxtList.Free;
+        HTMLLines.Add('<p>' + rsInfoText1 + '</p>');
+        HTMLLines.Add('<p>' + rsInfoText2 + '</p>');
+        HTMLLines.Add('<ul>');
+        HTMLLines.Add('  <li>' + rsSizeInfoSmall + '</li>');
+        HTMLLines.Add('  <li>' + rsSizeInfoMedium + '</li>');
+        HTMLLines.Add('  <li>' + rsSizeInfoLarge + '</li>');
+        HTMLLines.Add('</ul>');
+        HTMLLines.Add('<p>' + Format(rsImagesWereProvidedBy, [Link(URL_RolandHahn, URL_RolandHahn)]) + '</p>');
+        HTMLLines.Add('<p>' + Format(rsLicense, [License_CC0]) + '<br>');
+        HTMLLines.Add(Link(URL_CC0, URL_CC0) + '<br>');
+        HTMLLines.Add(rsFreelyAvailable + '</p>');
+      end else
+      begin
+        InfoTxtList := TStringList.Create;
+        try
+          InfoTxtList.LoadFromFile(ImgDirectory + InfoTextFileName);
+          HTMLLines.Add('<hr>');
+          for i := 0 to InfoTxtList.Count - 1 do
+            HTMLLines.Add(InfoTxtList[i] + '<br>');
+        finally
+          InfoTxtList.Free;
+        end;
       end;
     end;
     HTMLLines.Add('</div>');