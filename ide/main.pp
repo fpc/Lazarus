e3cf97497dfa0f70d22d71e230193f6f796ac27c
Juha <juha@lazarus-ide.org>
2024-12-07 07:49:09 +0200

    IDE: In ComponentList show Designer when selecting component or clicking on selection button. Merge request !399 by n7800.
    (cherry picked from commit ca8d8b9b84d236e1c207ee2a4a76f1143ea55bf3)

--- ide/main.pp
+++ ide/main.pp
@@ -6165,6 +6165,7 @@ begin
        State=iwgfDisabled,OwningComponent);
     ComponentListForm.OnOpenPackage:=@PkgBoss.IDEComponentPaletteOpenPackage;
     ComponentListForm.OnOpenUnit:=@PkgBoss.IDEComponentPaletteOpenUnit;
+    ComponentListForm.OnClassSelected:=@ComponentPaletteClassSelected;
   end else if State=iwgfDisabled then
     ComponentListForm.DisableAutoSizing{$IFDEF DebugDisableAutoSizing}('TMainIDE.DoShowComponentList'){$ENDIF};
   if State>=iwgfShow then
