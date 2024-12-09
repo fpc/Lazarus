fb6edd5b0657b2ddf78fc6ca5365837a8e7550c2
Martin <laz.git@mfriebe.de>
2024-12-09 20:16:32 +0100

    IDE: SourceEditor change Highlighter, don't show internal dummy HL.
    (cherry picked from commit 046947520c7072f7cb423903cde502a7fcb6c45e)

--- ide/sourceeditor.pp
+++ ide/sourceeditor.pp
@@ -7231,7 +7231,7 @@ begin
   if SrcEdit=nil then exit;
   if Sender is TIDEMenuItem then begin
     IDEMenuItem:=TIDEMenuItem(Sender);
-    i:=IDEMenuItem.SectionIndex;
+    i:=IDEMenuItem.Tag;
     if (i>=0) and (i<EditorOpts.HighlighterList.Count) then begin
       SrcEdit.SyntaxHighlighterId:=i;
       SrcEdit.UpdateProjectFile([sepuChangedHighlighter]);
@@ -7690,6 +7690,8 @@ var
 begin
   SrcEditSubMenuHighlighter.ChildrenAsSubMenu:=true;
   for i := 0 to EditorOpts.HighlighterList.Count - 1 do begin
+    if EditorOpts.HighlighterList.SharedSynInstances[i] is TNonSrcIDEHighlighter then
+      continue;
     CurName:='Highlighter'+IntToStr(i);
     CurCaption:= EditorOpts.HighlighterList.Captions[i];
     if SrcEditSubMenuHighlighter.Count=i then begin
@@ -7699,6 +7701,7 @@ begin
     end else begin
       IDEMenuItem:=SrcEditSubMenuHighlighter[i];
       IDEMenuItem.Caption:=CurCaption;
+      IDEMenuItem.Tag:=i;
       IDEMenuItem.OnClick:=@HighlighterClicked;
     end;
     if IDEMenuItem is TIDEMenuCommand then
