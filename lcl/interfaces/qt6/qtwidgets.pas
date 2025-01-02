f7bc18cc5199725873624050fbc66e9f81b23bb4
Željan Rikalo <zeljko@holobit.hr>
2025-01-02 17:01:41 +0000

    Qt6: do not activate THintWindow on show.
    (cherry picked from commit ae5e5d4d6e1806f8a43860d8d50f1c529e75448f)
    
    Co-authored-by: zeljan1 <zeljko@holobit.hr>

--- lcl/interfaces/qt6/qtwidgets.pas
+++ lcl/interfaces/qt6/qtwidgets.pas
@@ -18604,6 +18604,9 @@ begin
   Result := QWidget_create(Parent, QtTool or QtFramelessWindowHint);
   FDeleteLater := True;
   FMenuBar := nil;
+  {$IFDEF DARWIN}
+  QWidget_setAttribute(Result, QtWA_ShowWithoutActivating);
+  {$ENDIF}
   {$IFDEF QTSCROLLABLEFORMS}
   ScrollArea := nil;
   {$ENDIF}
