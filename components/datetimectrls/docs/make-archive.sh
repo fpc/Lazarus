2a59941a5971254bea10361ee0b77fe1bc72c8ce
dsiders <dsiders@lazarus-ide.org>
2025-01-14 04:57:33 -0500

    Docs: DateTimeCtrls. Syncs CSS, build scripts.

--- components/datetimectrls/docs/make-archive.sh
+++ components/datetimectrls/docs/make-archive.sh
@@ -5,14 +5,10 @@ echo "Create archive for DateTimeCtrls html documentation"
 echo ""
 
 # convert to unix line endings expected by git
-cd datetimectrls
-dos2unix *.{html,css}
-dos2unix **/*.html
-cd ..
 
 # create an archive for the generated html content
 dt=`date +"%Y-%m-%d"`
 7z a -t7z -r -mx9 docs-html-datetimectrls-$dt.7z ./datetimectrls/
 
-# move .7z file to another directory for permanent storage
+cp -v docs-html-datetimectrls-$dt.7z ../../../docs/build/html/
 # the .7z file is removed in clean-files.sh
