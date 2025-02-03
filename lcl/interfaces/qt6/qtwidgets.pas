241e9e966a7707eebad374774f1052c1a28dfb7f
Željan Rikalo <zeljko@holobit.hr>
2025-02-03 17:48:25 +0000

    Qt6: do not allow THintWindow to grab focus under wayland.
    (cherry picked from commit d931804a5189721fd55ca48c10f761d04e959abf)
    
    Co-authored-by: zeljan1 <zeljko@holobit.hr>

--- lcl/interfaces/qt6/qtwidgets.pas
+++ lcl/interfaces/qt6/qtwidgets.pas
@@ -18601,7 +18601,12 @@ begin
     Parent := QApplication_activeModalWidget
   else
     Parent := nil;
-  Result := QWidget_create(Parent, QtTool or QtFramelessWindowHint);
+  {$IFDEF HASX11}
+  if IsWayland then
+    Result := QWidget_create(Parent, QtToolTip or QtFramelessWindowHint)
+  else
+  {$ENDIF}
+    Result := QWidget_create(Parent, QtTool or QtFramelessWindowHint);
   FDeleteLater := True;
   FMenuBar := nil;
   {$IFDEF UNIX}
