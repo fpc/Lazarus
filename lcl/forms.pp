fpc/Lazarus

commit 4891f1f0e1aebe5903daeff49bd6360b34e09eea

TScrollBox: fix flicker when Form.AutoSizeAll is called. Do not call ShowScrollBar when nothing changed.

lcl/forms.pp
@@ -107,6 +107,7 @@ type
     FTracking: Boolean;
     FVisible: Boolean;
     FOldScrollInfo: TScrollInfo;
+    FOldVisible: Boolean;
     FOldScrollInfoValid: Boolean;
   protected
     FControl: TWinControl;

lcl/include/controlscrollbar.inc
@@ -230,14 +230,18 @@ begin
     if (not FOldScrollInfoValid) or (not CompareMem(@ScrollInfo, @FOldScrollInfo, SizeOf(TScrollInfo))) then
     begin
       FOldScrollInfo := ScrollInfo;
-      FOldScrollInfoValid := True;
       SetScrollInfo(ControlHandle, IntfBarKind[Kind], ScrollInfo, NewVisible);
       // update policy too
       ScrollInfo.fMask := SIF_UPDATEPOLICY;
       ScrollInfo.nTrackPos := TrackToPolicyMap[FTracking];
       SetScrollInfo(ControlHandle, IntfBarKind[Kind], ScrollInfo, NewVisible);
-    end;
-    ShowScrollBar(ControlHandle, IntfBarKind[Kind], NewVisible);
+      ShowScrollBar(ControlHandle, IntfBarKind[Kind], NewVisible);
+    end
+    else
+    if (not FOldScrollInfoValid) or (FOldVisible <> NewVisible) then
+      ShowScrollBar(ControlHandle, IntfBarKind[Kind], NewVisible);
+    FOldVisible := NewVisible;
+    FOldScrollInfoValid := True;
     {$IFDEF VerboseScrollingWinControl}
     //if DebugCondition then
       DebugLn(['TControlScrollBar.UpdateScrollBar ',DbgSName(FControl),' ',DbgSName(Self),' ',dbgs(Kind),' FVisible=',FVisible,' Range=',FRange,' FPosition=',FPosition,' FPage=',FPage,' FAutoRange=',FAutoRange,' ShouldVisible=',NewVisible,' IsVisible=',IsScrollBarVisible]);

commit be89c477d9d872e5d60ea6010bc6a65b69f182ad

LCL: fix TControlScrollBar needs update if owner's handle changes.  Issue #40794 introduced in 4891f1f0e1aebe5903daeff49bd6360b34e09eea

lcl/forms.pp
@@ -180,6 +180,7 @@ type
     function AutoScrollEnabled: Boolean; virtual;
     procedure CalculateAutoRanges; virtual;
     procedure CreateWnd; override;
+    procedure DestroyWnd; override;
     function GetClientScrollOffset: TPoint; override;
     function GetLogicalClientRect: TRect; override;// logical size of client area
     procedure DoOnResize; override;

lcl/include/custommemo.inc
@@ -142,6 +142,8 @@ begin
     DebugLn('[TCustomMemo.FinalizeWnd] END ',DbgSName(Self),' ',FLines.ClassName,' FLines.Count=',dbgs(FLines.Count));
     {$endif}
   end;
+  if FHorzScrollBar <> nil then FHorzScrollBar.InvalidateScrollInfo;
+  if FVertScrollBar <> nil then FVertScrollBar.InvalidateScrollInfo;
   inherited FinalizeWnd;
 end;
 

lcl/include/scrollingwincontrol.inc
@@ -35,6 +35,13 @@ begin
   end;
 end;
 
+procedure TScrollingWinControl.DestroyWnd;
+begin
+  inherited DestroyWnd;
+  if FHorzScrollBar <> nil then FHorzScrollBar.InvalidateScrollInfo;
+  if FVertScrollBar <> nil then FVertScrollBar.InvalidateScrollInfo;
+end;
+
 function TScrollingWinControl.GetClientScrollOffset: TPoint;
 begin
   if (HorzScrollBar <> nil) and (VertScrollBar <> nil) then
@@ -75,8 +82,7 @@ begin
   //debugln(['TScrollingWinControl.DoOnResize ',DbgSName(Self),' ',dbgs(BoundsRect),' ',dbgs(ClientRect),' ',dbgs(GetLogicalClientRect)]);
 end;
 
-procedure TScrollingWinControl.GetPreferredSizeClientFrame(out aWidth,
-  aHeight: integer);
+procedure TScrollingWinControl.GetPreferredSizeClientFrame(out aWidth, aHeight: Integer);
 // return frame width independent of scrollbars (i.e. as if scrollbars not shown)
 begin
   if AutoScroll and (VertScrollBar<>nil) then