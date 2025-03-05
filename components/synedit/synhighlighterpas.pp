36891eb9fddc479ec52c9a54cef693e6884f4b58
Martin <laz.git@mfriebe.de>
2025-03-05 11:38:30 +0100

    SynEdit: PasHighLighter, fix "record helper" in "mode objfpc" with modeswitches.
    (cherry picked from commit e393919040541686f19304f711857336aa2ec40d)

--- components/synedit/synhighlighterpas.pp
+++ components/synedit/synhighlighterpas.pp
@@ -1855,7 +1855,7 @@ begin
     StartPascalCodeFoldBlock(cfbtRecord);
     fRange := fRange - [rsVarTypeInSpecification, rsAfterEqual, rsAfterEqualOrColon] + [rsAfterSemiColon];
     FOldRange := FOldRange - [rsAfterSemiColon];
-    if CompilerMode = pcmDelphi then
+    if (CompilerMode = pcmDelphi) or (TypeHelpers {and adv_record}) then
       fRange := fRange + [rsAtClass]; // highlight helper
     Result := tkKey;
   end
