5c7fcf1fc01b88f2b20cd907f0309c4cfc357ca6
Bart <9132501-flyingsheep@users.noreply.gitlab.com>
2025-04-07 22:30:41 +0200

    LazUtf8: correct a StartCharIndex < 1 in Utf8Insert. Reason: consistent with System.Insert().
    (cherry picked from commit 913b749fa96f4a7d9dc0b850c06306f126e412a0)

--- components/lazutils/lazutf8.pas
+++ components/lazutils/lazutf8.pas
@@ -1197,6 +1197,8 @@ procedure UTF8Insert(const source: String; var s: String; StartCharIndex: PtrInt
 var
   StartBytePos: PChar;
 begin
+  if StartCharIndex < 1 then
+    StartCharIndex := 1;  //Insert() does this correction too
   StartBytePos:=UTF8CodepointStart(PChar(s),length(s),StartCharIndex-1);
   if StartBytePos <> nil then
     Insert(source, s, StartBytePos-PChar(s)+1);
