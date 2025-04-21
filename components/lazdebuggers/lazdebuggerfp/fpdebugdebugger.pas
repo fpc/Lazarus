9285940e2b2f06b3778dbd7bc5e94395bbd7a4a6
Martin <laz.git@mfriebe.de>
2025-04-21 12:27:36 +0200

    FpDebug: Additional security for b1a29e754676bd62c1ee4bf9bb875d3a3eec468b "race condition during exit". Defer the state, till after the ProcessMessages

--- components/lazdebuggers/lazdebuggerfp/fpdebugdebugger.pas
+++ components/lazdebuggers/lazdebuggerfp/fpdebugdebugger.pas
@@ -3565,9 +3565,9 @@ begin
   {$POP}
   LockRelease;
   try
-    SetState(dsStop);
     StopAllWorkers;
     FreeDebugThread;
+    SetState(dsStop); // after FreeDebugThread, which does ProcessMessages
   finally
     UnlockRelease;
   end;
