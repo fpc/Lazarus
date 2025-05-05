fpc/Lazarus

commit 3185572fdfa1b4efacb375b8b8781f1c1d8b6a7e

LCL-GTK2: Prevent a crash in OpenBSD, related to tooltips. Issue #41244, patch by Alfred Glänzer.

lcl/interfaces/gtk2/gtk2proc.inc
@@ -8132,13 +8132,11 @@ begin
     else
       If CompareText(WName,LazGtkStyleNames[lgsStatusBar])=0 then begin
         lgs:=lgsStatusBar;
-        AddToStyleWindow:=true;
         StyleObject^.Widget := gtk_statusbar_new;
       end
     else
       If CompareText(WName,LazGtkStyleNames[lgsCalendar])=0 then begin
         lgs:=lgsCalendar;
-        AddToStyleWindow:=true;
         StyleObject^.Widget := gtk_calendar_new;
       end
     else
@@ -8185,6 +8183,7 @@ begin
         WidgetName := 'gtk-tooltip-lcl';
         StyleObject^.Owner := Tp;
         Tp := nil;
+        AddToStyleWindow:=false;
       end
     else
       If CompareText(WName,LazGtkStyleNames[lgsHScale])=0 then begin

commit d611e0c2a3c1bee380dc2d34f01aacf95b1d2698

LCL-Gtk2: Avoid `Gtk-CRITICAL` warnings on Linux, issue #41611.
Avoid warnings like:
(project1:153785): Gtk-CRITICAL **: 09:35:29.899:
IA__gtk_paint_flat_box: assertion 'style->depth ==
gdk_drawable_get_depth (window)' failed

This is a regression after commit
3185572fdfa1b4efacb375b8b8781f1c1d8b6a7e, which fixed crash on OpenBSD
(issue #41244).

Now the change from the above commit is active only for the OpenBSD
platform.

lcl/interfaces/gtk2/gtk2proc.inc
@@ -8185,7 +8185,11 @@ begin
         WidgetName := 'gtk-tooltip-lcl';
         StyleObject^.Owner := Tp;
         Tp := nil;
+        // The following line causes `Gtk-CRITICAL` warning on Linux, issue #41611.
+        // It seems to be only needed on OpenBSD, issue #41244.
+        {$IF DEFINED(OPENBSD)}
         AddToStyleWindow:=false;
+        {$ENDIF}
       end
     else
       If CompareText(WName,LazGtkStyleNames[lgsHScale])=0 then begin

lcl/interfaces/gtk2/gtk2themes.pas
@@ -263,6 +263,7 @@ begin
           BP_PUSHBUTTON:
             begin
               Result.Widget := GetStyleWidget(lgsButton);
+              // style is always overwritten to lgsButton to avoid problems with TSpeedButton color, issue #22901
               Result.Style := GetStyle(lgsButton);
               Result.State := GtkButtonMap[Details.State];
               if Details.State = PBS_PRESSED then
@@ -482,6 +483,7 @@ begin
       end;
     teToolTip:
       begin
+        // style is always overwritten to avoid problems with theming, issue #41611
         Result.Style := GetStyle(lgsTooltip);
         Result.Widget := GetStyleWidget(lgsTooltip);
         Result.State := GTK_STATE_NORMAL;