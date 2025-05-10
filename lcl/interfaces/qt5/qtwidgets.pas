54902552e21306cba014f7c29b22bdc94ec34d10
Željan Rikalo <zeljko@lazarus-ide.org>
2025-05-10 12:32:11 +0200

    Qt5: fix potential crash when push button is destroyed or recreated inside mouseRelease event (potential because qt5 still uses deleteLater())
    (cherry picked from commit 7fe4213c44ebbc50315154366b03ed1744c1b961)
    
    Co-authored-by: zeljan1 <zeljko@holobit.hr>

--- lcl/interfaces/qt5/qtwidgets.pas
+++ lcl/interfaces/qt5/qtwidgets.pas
@@ -6340,13 +6340,18 @@ begin
   case QEvent_type(Event) of
     QEventMouseButtonRelease:
       begin
-        Result := SlotMouse(Sender, Event);
-        if not QWidget_hasFocus(Widget) and
-          (QMouseEvent_button(QMouseEventH(Event)) = QtLeftButton) and
-          (QWidget_focusPolicy(Widget) > QtNoFocus) then
-        begin
-          QObject_blockSignals(Sender, True);
-          Application.QueueAsyncCall(@PushButtonUnblock, PtrInt(Self));
+        BeginEventProcessing;
+        try
+          Result := SlotMouse(Sender, Event);
+          if not QWidget_hasFocus(Widget) and
+            (QMouseEvent_button(QMouseEventH(Event)) = QtLeftButton) and
+            (QWidget_focusPolicy(Widget) > QtNoFocus) then
+          begin
+            QObject_blockSignals(Sender, True);
+            Application.QueueAsyncCall(@PushButtonUnblock, PtrInt(Self));
+          end;
+        finally
+          EndEventProcessing;
         end;
       end;
     else
