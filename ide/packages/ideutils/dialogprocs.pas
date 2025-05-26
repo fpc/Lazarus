e9d7d8bd404dcae4cf562cba11106841c94251c0
mattias <nc-gaertnma@netcologne.de>
2025-05-26 13:51:40 +0200

    ideutils: DeleteFileInteractive: dumpstack on wrong params

--- ide/packages/ideutils/dialogprocs.pas
+++ ide/packages/ideutils/dialogprocs.pas
@@ -598,8 +598,12 @@ var
   CurFilename: String;
   Found: Boolean;
 begin
-  if FileName='' then
+  if (Filename='') or not FilenameIsAbsolute(FileName) then
+  begin
+    debugln(['Error: DeleteFileInteractive File="',Filename,'"']);
+    DumpStack;
     exit(mrAbort);  // Serious bug on the calling side, so abort.
+  end;
   CurFilename:=Filename;
   repeat
     Result:=mrOk;
