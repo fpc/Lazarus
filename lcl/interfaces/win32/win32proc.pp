c25eb6858d04df41259895a8172636d9f6822442
wp_xyz <wp_xxyyzzz@gmx.net>
2025-06-19 09:57:06 +0200

    LCL/buttons: Fix incorrect measuring of text width in win32 buttons (single-line case), issue #41696
    (cherry picked from commit 630b9e7fe53bb5038c1d035b2ebdea0d7266d6a0)

--- lcl/interfaces/win32/win32proc.pp
+++ lcl/interfaces/win32/win32proc.pp
@@ -1074,7 +1074,7 @@ begin
   end else
   begin
     DeleteAmpersands(Text);
-    Result := Windows.GetTextExtentPoint32(canvasHandle, PChar(Text), Length(Text), textSize);
+    Result := LCLIntf.GetTextExtentPoint32(canvasHandle, PChar(Text), Length(Text), textSize);
     if Result then
     begin
       Width := textSize.cx;
