c400834a6ff6129dc43f0ce0f2b66615230304f9
Martin <laz.git@mfriebe.de>
2025-06-25 22:05:27 +0200

    SynEdit: Fix Issue with missing updates (markup not updated). Only wait for Autosize if in/after CreateHandle, otherwise it may never happen (locked, but no size change). In the IDE, revert/reload (after disk change) did not update user-defined-colors.
    (cherry picked from commit bf6a42159bff9fb0693c1d34e8a5c235fea0ec1a)

--- components/synedit/synedit.pp
+++ components/synedit/synedit.pp
@@ -4968,7 +4968,8 @@ end;
 
 function TCustomSynEdit.WaitingForInitialSize: boolean;
 begin
-  Result := (sfAfterHandleCreatedNeeded in fStateFlags) or AutoSizeDelayed or (not HandleAllocated);
+  Result := ((sfAfterHandleCreatedNeeded in fStateFlags) and AutoSizeDelayed) or
+            (not HandleAllocated);
 end;
 
 procedure TCustomSynEdit.DoHandleInitialSizeFinished;
