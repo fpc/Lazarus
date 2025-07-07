b47a19209abcc01317ab9ff116dabc895f82d913
n7800 <14154601-n7800@users.noreply.gitlab.com>
2025-07-07 05:41:32 +0500

    AnchorDocking/Options: Disable "Flat header buttons" checkbox if "Show headers" is unchecked

--- components/anchordocking/anchordockoptionsdlg.pas
+++ components/anchordocking/anchordockoptionsdlg.pas
@@ -306,6 +306,7 @@ begin
   HeaderStyleComboBox.Enabled:=HasHeaders;
   HighlightFocusedCheckBox.Enabled:=HasHeaders;
   DockSitesCanBeMinimized.Enabled:=HasHeaders;
+  FlatHeadersButtons.Enabled:=HasHeaders;
 end;
 
 constructor TAnchorDockOptionsFrame.Create(TheOwner: TComponent);
