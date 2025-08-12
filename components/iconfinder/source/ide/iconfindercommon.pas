7caafb4d1608026b7c1f1e4fccb9a251692bd092
wp_xyz <wp_xxyyzzz@gmx.net>
2025-08-13 01:08:04 +0200

    IconFinder: Update list of icon folders registered by default.

--- components/iconfinder/source/ide/iconfindercommon.pas
+++ components/iconfinder/source/ide/iconfindercommon.pas
@@ -18,12 +18,12 @@ uses
 const
   ICONFINDER_CONFIG_FILENAME = 'iconfindercfg.xml';
   IMAGE_DIR = 'images/';
-  DEFAULT_IMAGE_FOLDERS: array[0..14] of String = (
+  DEFAULT_IMAGE_FOLDERS: array[0..18] of String = (
     'images/general_purpose/',  // only this one will be active by default
     'images/actions/',
     'images/codecompletion/',
     'images/codeexplorer/',
-    //'images/codetoolsdefines/',
+    'images/codetoolsdefines/',
     'images/components/',
     'images/componenttreeview/',
     'images/debugger/',
@@ -36,7 +36,10 @@ const
     'images/states/',
     'components/chmhelp/lhelp/images/',
     'components/datetimectrls/pictures/',
-    'components/lazcontrols/images/'
+    'components/iconfinder/images/',
+    'components/lazcontrols/images/',
+    'components/onlinepackagemanager/images/Tree_Buttons/',
+    'components/projectgroups/images/'
   );
 
 
