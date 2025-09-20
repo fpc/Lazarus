abc6a61c1e74cbde47537a7cb97bc705eb3fd7ab
wp_xyz <wp_xxyyzzz@gmx.net>
2025-09-21 00:01:04 +0200

    OPM: Add Laz v4.2 to compatibility list.

--- components/onlinepackagemanager/opkman_common.pas
+++ components/onlinepackagemanager/opkman_common.pas
@@ -113,13 +113,13 @@ const
     'Other',
     'Games and Game Engines');
 
-  MaxLazVersions = 10;
+  MaxLazVersions = 11;
   LazVersions: array [0..MaxLazVersions - 1] of String = (
     '2.0.x', '2.2.0', '2.2.4',                                       // order from old to young
     '3.0.0', '3.2.0', '3.4.0', '3.6.0', '3.8.0',
-    '4.0.0',
+    '4.0.0', '4.2.0',
     'Trunk');
-  LazDefVersions = '4.0.0, 3.8.0, 3.6.0, 3.4.0, 3.2.0, 3.0.0';
+  LazDefVersions = '4.2.0, 4.0.0, 3.8.0, 3.6.0, 3.4.0, 3.2.0, 3.0.0';
   LazTrunk = '4.99.0';
 
   MaxFPCVersions = 7;
