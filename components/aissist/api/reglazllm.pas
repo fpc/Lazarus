2e067b1186f564be11f94de6be540429f535a0e2
Michaël Van Canneyt <michael@freepascal.org>
2025-10-11 22:31:23 +0200

    * Set value for protocol will add correct unit to uses clause

--- components/aissist/api/reglazllm.pas
+++ components/aissist/api/reglazllm.pas
@@ -5,7 +5,7 @@ unit reglazllm;
 interface
 
 uses
-  Classes, SysUtils, fpwebclient, fphttpwebclient, llm.Client, PropEdits;
+  Classes, SysUtils, fpwebclient, fphttpwebclient, llm.Client, FormEditingIntf, PropEdits;
 
 type
 
@@ -14,13 +14,22 @@ type
   TProtocolEditor = class(TStringPropertyEditor)
     procedure GetValues(Proc: TGetStrProc); override;
     function GetAttributes: TPropertyAttributes; override;
+    procedure SetValue(const NewValue: ansistring); override;
+  private
+    procedure AddUnitToFormUses(const aUnitName: string);
   end;
 
 procedure register;
 
 implementation
 
-uses LResources, llm.claude,llm.chatgpt,llm.gemini,llm.ollama;
+uses
+  LResources, llm.claude,llm.chatgpt,llm.gemini,llm.ollama,
+  IDEMsgIntf,
+  LazIDEIntf, IDEExternToolIntf, ComponentEditors, ProjectIntf, CodeCache, CodeToolManager;
+
+resourcestring
+  SErrFailedToAddUnit = 'Failed to add unit %s to uses clause: %s';
 
 procedure register;
 begin
@@ -35,6 +44,50 @@ end;
 function TProtocolEditor.GetAttributes: TPropertyAttributes;
 begin
   Result := [paMultiSelect, paValueList, paRevertable];
+end;
+
+procedure TProtocolEditor.AddUnitToFormUses(const aUnitName : string);
+var
+  lLookupRoot: TPersistent;
+  lRoot: TComponent absolute lLookupRoot;
+  lDesigner: TComponentEditorDesigner;
+  lFile: TLazProjectFile;
+  Code: TCodeBuffer;
+  lMsg : String;
+begin
+  lLookupRoot:=PropertyHook.LookupRoot;
+  if not (lLookupRoot is TComponent) then
+    exit;
+  lDesigner := TComponentEditorDesigner(FormEditingHook.GetDesignerByComponent(lRoot));
+  if lDesigner=nil then
+    exit;
+  // get unit. Requires a recent lazarus.
+  lFile:=lDesigner.ProjectFile;
+  if lFile=nil then
+    exit;
+  Code:=CodeToolBoss.FindFile(lFile.Filename);
+  // save changes in source editor to codetools
+  LazarusIDE.SaveSourceEditorChangesToCodeCache(Nil); // -1: commit all source editors
+  // add unit to interface uses
+  if CodeToolBoss.AddUnitToMainUsesSectionIfNeeded(Code, aUnitName, '') then
+    begin
+    lMsg:=CodeToolBoss.ErrorMessage;
+    if lMsg<>'' then
+      begin
+      lMsg:=Format(SErrFailedToAddUNit,[aUnitName,lMsg]);
+      AddIDEMessage(mluError,lMsg,lFile.Filename);
+      end;
+    end;
+end;
+
+procedure TProtocolEditor.SetValue(const NewValue: ansistring);
+var
+  C : TLLMProtocolClass;
+begin
+  inherited SetValue(NewValue);
+  C:=TCustomLLMClient.FindProtocolClass(NewValue);
+  if assigned(C) then
+    AddUnitToFormUses(C.UnitName);
 end;
 
 procedure TProtocolEditor.GetValues(Proc: TGetStrProc);
