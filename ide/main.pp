f30529d95e1e5213c3d6d32d3a4ea0ae02a214d0
Juha <juha@lazarus-ide.org>
2025-11-09 14:34:46 +0200

    IDE: Add missing handling for the ecRestart command. Merge request !584 by n7800.

--- ide/main.pp
+++ ide/main.pp
@@ -2993,6 +2993,7 @@ begin
     itmFileSaveAll.Command:=GetIdeCmdRegToolBtn(ecSaveAll);
     itmFileClose.Command:=GetIdeCmdRegToolBtn(ecClose);
     itmFileCleanDirectory.Command:=GetIdeCmdRegToolBtn(ecCleanDirectory);
+    itmFileRestart.Command:=GetIdeCmdRegToolBtn(ecRestart);
     itmFileQuit.Command:=GetIdeCmdRegToolBtn(ecQuit);
 
     // edit menu
@@ -3630,6 +3631,7 @@ begin
   ecOpen:                     mnuOpenClicked(Self);
   ecOpenUnit:                 DoUseUnitDlg(udOpenUnit);
   ecSaveAll:                  DoSaveAll([sfCheckAmbiguousFiles,sfSaveNonProjectFiles]);
+  ecRestart:                  mnuRestartClicked(Self);
   ecQuit:                     mnuQuitClicked(Self);
   ecRun:
     begin
