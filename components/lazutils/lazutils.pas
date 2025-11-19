4e9fd3cdb183370743fa6162c230a99389220293
Maxim Ganetsky <maxim@lazarus-ide.org>
2025-11-20 01:38:25 +0300

    LazUtils: committed automatically changed file

--- components/lazutils/lazutils.pas
+++ components/lazutils/lazutils.pas
@@ -13,14 +13,14 @@ uses
   FPCAdds, GraphMath, GraphType, HTML2TextRender, IntegerList, Laz2_DOM, 
   Laz2_XMLCfg, Laz2_XMLRead, Laz2_XMLUtils, Laz2_XMLWrite, Laz2_XPath, 
   Laz_DOM, Laz_XMLCfg, Laz_XMLRead, Laz_XMLStreaming, Laz_XMLWrite, 
-  LazClasses, lazCollections, LazConfigStorage, LazDbgLog, LazFglHash, 
+  LazClasses, LazCollections, LazConfigStorage, LazDbgLog, LazFglHash, 
   LazFileCache, LazFileUtils, LazLinkedList, LazListClasses, LazLogger, 
   LazLoggerBase, LazLoggerDummy, LazLoggerProfiling, LazMethodList, 
   LazPasReadUtil, LazStringUtils, LazSysUtils, LazTracer, LazUnicode, 
   LazUTF16, LazUTF8, LazUtilities, LazUtilsStrConsts, LazVersion, 
   LConvEncoding, LCSVUtils, LookupStringList, Maps, Masks, ObjectLists, 
   StringHashList, TextStrings, Translations, UTF8Process, PList2JSon, 
-  LazarusPackageIntf;
+  LazListClassesBase, LazarusPackageIntf;
 
 implementation
 
