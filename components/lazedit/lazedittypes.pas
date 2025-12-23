6816011ba50c69a321046debd4da620d8179bdfd
Martin <laz.git@mfriebe.de>
2025-12-23 12:39:06 +0100

    LazEdit: missing unit

---
+++ components/lazedit/lazedittypes.pas
@@ -0,0 +1,16 @@
+unit LazEditTypes;
+
+{$mode objfpc}{$H+}
+
+interface
+
+type
+  TLinePos = type integer; // 1..high(Integer);
+  TLineIdx = type integer; // 0..high(Integer);
+  IntPos = type integer; // 1..high(Integer);
+  IntIdx = type integer; // 0..high(Integer);
+
+implementation
+
+end.
+
