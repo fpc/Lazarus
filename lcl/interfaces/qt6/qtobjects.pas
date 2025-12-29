7d17b4f04f4108b0f8bfcad2c3321049d937e221
zeljan1 <zeljko@holobit.hr>
2025-12-29 17:11:20 +0100

    Qt6: proper usage of TMap.

--- lcl/interfaces/qt6/qtobjects.pas
+++ lcl/interfaces/qt6/qtobjects.pas
@@ -5430,10 +5430,17 @@ begin
 end;
 
 procedure TQtGDIObjects.AddGDIObject(AObject: TObject);
+var
+  Key: PtrUInt;
+  Obj: TObject;
 begin
-  if not FSavedHandlesList.HasId(AObject) then
+  if AObject = nil then
+    exit;
+  Key := PtrUInt(AObject);
+  Obj := AObject;
+  if not FSavedHandlesList.HasId(Key) then
   begin
-    FSavedHandlesList.Add(AObject, AObject);
+    FSavedHandlesList.Add(Key, Obj);
     inc(FCount);
     {$IFDEF DebugQTGDIObjects}
     if FMaxCount < FCount then
@@ -5443,10 +5450,15 @@ begin
 end;
 
 procedure TQtGDIObjects.RemoveGDIObject(AObject: TObject);
+var
+  Key: PtrUInt;
 begin
-  if FSavedHandlesList.HasId(AObject) then
+  if AObject = nil then
+    exit;
+  Key := PtrUInt(AObject);
+  if FSavedHandlesList.HasId(Key) then
   begin
-    FSavedHandlesList.Delete(AObject);
+    FSavedHandlesList.Delete(Key);
     dec(FCount);
   end;
 end;
@@ -5454,8 +5466,8 @@ end;
 function TQtGDIObjects.IsValidGDIObject(AGDIObject: PtrUInt): Boolean;
 begin
   if (AGDIObject = 0) then
-    Exit(False);
-  Result := FSavedHandlesList.HasId(TObject(AGDIObject));
+    exit(False);
+  Result := FSavedHandlesList.HasId(AGDIObject);
   {$IFDEF DebugQTGDIObjects}
   if not Result then
   begin
