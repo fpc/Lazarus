b35b29d64724d90f2e4aaf1020a3d363478c893a
rich2014 <rich2014.git@outlook.com>
2026-01-19 18:14:24 +0800

    LCL/Cocoa: fix the minor issue related to FileDialog, Merge branch 'filedialog'

--- lcl/interfaces/cocoa/cocoaconfig.pas
+++ lcl/interfaces/cocoa/cocoaconfig.pas
@@ -176,13 +176,20 @@ type
   TCocoaConfigFileDialogAccessoryView = record
     showsFilePackagesSwitch: Boolean;
     minWidth: Double;
+    baseY: Double;
     horzSpacing: Double;
     vertSpacing: Double;
   end;
 
-  TCocoaConfigFileDialog = record
+  TCocoaConfigFileDialogCommon = record
     allowsFilePackagesContents: Boolean;
     accessoryView: TCocoaConfigFileDialogAccessoryView;
+  end;
+
+  TCocoaConfigFileDialog = record
+    open: TCocoaConfigFileDialogCommon;
+    save: TCocoaConfigFileDialogCommon;
+    selectDirectory: TCocoaConfigFileDialogCommon;
   end;
 
 type
