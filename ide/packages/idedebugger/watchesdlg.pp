fpc/Lazarus

commit feddf603d4a0180eba4e066f70cdf1819d95feee

IDE, Debugger: Watches, improve Drag/drop

ide/packages/idedebugger/watchesdlg.pp
@@ -163,6 +163,7 @@ type
     procedure FormShow(Sender: TObject);
     procedure tbWordWrapClick(Sender: TObject);
     procedure tvWatchesChange(Sender: TBaseVirtualTree; Node: PVirtualNode);
+    procedure MoveWatchToBelow(var AWatch: TIdeWatch; var AWatchNode: PVirtualNode; ADestNode: PVirtualNode);
     procedure tvWatchesDragDrop(Sender: TBaseVirtualTree; Source: TObject;
       DataObject: IDataObject; Formats: TFormatArray; Shift: TShiftState;
       const Pt: TPoint; var Effect: LongWord; Mode: TDropMode);
@@ -598,6 +599,37 @@ begin
   UpdateInspectPane;
 end;
 
+procedure TWatchesDlg.MoveWatchToBelow(var AWatch: TIdeWatch; var AWatchNode: PVirtualNode;
+  ADestNode: PVirtualNode);
+var
+  ADestWatch: TIdeWatch;
+  NewIdx: Integer;
+begin
+  if AWatch = nil then
+    AWatch := TIdeWatch(tvWatches.NodeItem[AWatchNode])
+  else
+  if AWatchNode = nil then
+    AWatchNode := tvWatches.FindNodeForItem(AWatch);
+
+  assert((AWatch<>nil) and (AWatchNode<>nil), 'TWatchesDlg.MoveWatchToBelow: (AWatch<>nil) and (AWatchNode<>nil)');
+
+  if ADestNode = nil then begin
+    // move to top
+    tvWatches.MoveTo(AWatchNode, PVirtualNode(nil), amAddChildFirst, False);
+    AWatch.Index := 0;
+  end
+  else begin
+    ADestWatch := TIdeWatch(tvWatches.NodeItem[ADestNode]);
+    assert(ADestWatch <> nil, 'TWatchesDlg.MoveWatchToBelow: ADestWatch <> nil');
+    NewIdx := ADestWatch.Index;
+    if NewIdx < AWatch.Index then
+      inc(NewIdx);
+
+    tvWatches.MoveTo(AWatchNode, ADestNode, amInsertAfter, False);
+    ADestWatch.Index := NewIdx;
+  end;
+end;
+
 procedure TWatchesDlg.tvWatchesNodeDblClick(Sender: TBaseVirtualTree;
   const HitInfo: THitInfo);
 begin
@@ -613,98 +645,77 @@ procedure TWatchesDlg.tvWatchesDragDrop(Sender: TBaseVirtualTree;
   Shift: TShiftState; const Pt: TPoint; var Effect: LongWord; Mode: TDropMode);
 var
   s: String;
-  NewWatch: TCurrentWatch;
   Nodes: TNodeArray;
   Target, N, NTarget, NewNode: PVirtualNode;
-  AWatch, ASourceWatch, ATargetWatch: TIdeWatch;
-  NewIdx: Integer;
+  AWatch, ASourceWatch: TIdeWatch;
 begin
+  Target := tvWatches.DropTargetNode;
+
+  if Target <> nil then begin
+    NTarget := tvWatches.NodeParent[Target];
+    while NTarget <> nil do begin
+      Target := NTarget;
+      NTarget := tvWatches.NodeParent[Target];
+    end;
+  end;
+
   if Source = tvWatches then begin
     if (not (FWatchesInView is TCurrentWatches)) or (GetSelectedSnapshot <> nil) then
       exit;
 
-    Nodes := tvWatches.GetSortedSelection(True);
-    Target := tvWatches.GetNodeAt(Pt);
     if (Target = nil) then
       exit;
 
-    if Mode = dmAbove then begin
-      // Insert above
-      if tvWatches.Selected[Target] then begin
-        NTarget := tvWatches.GetPreviousSiblingNoInit(Target);
-        while (NTarget <> nil) and tvWatches.Selected[NTarget] do begin
-          Target := NTarget;
-          NTarget := tvWatches.GetPreviousSiblingNoInit(Target);
-        end;
-      end;
-      if Target <> nil then
-        Target := tvWatches.GetPreviousSiblingNoInit(Target);
-    end
-    else
-    if tvWatches.Selected[Target] then begin
-      // Insert below
-      NTarget := tvWatches.GetNextSiblingNoInit(Target);
-      while (NTarget <> nil) and tvWatches.Selected[NTarget] do begin
-        Target := NTarget;
-        NTarget := tvWatches.GetNextSiblingNoInit(Target);
-      end;
-    end;
-    if Target = nil then
-      NTarget := tvWatches.GetFirstChildNoInit(nil)
-    else
-      NTarget := tvWatches.GetNextSiblingNoInit(Target);
+    Nodes := tvWatches.GetSortedSelection(True);
+    if Length(Nodes) = 0 then
+      exit;
+
+    if (Mode = dmAbove) and (Target <> nil) then
+      Target := tvWatches.GetPreviousSiblingNoInit(Target);
 
     BeginUpdate;
     try
-
-    for N in Nodes do begin
-      if tvWatches.NodeParent[N] = nil then begin
-        // Move top/outer node
-        if (N = Target) or (N = NTarget) then
-          continue; // already in place
-
-        AWatch := TIdeWatch(tvWatches.NodeItem[N]);
-        NewNode := N;
-      end
-      else begin
-        // Copy child node
-        ASourceWatch := TIdeWatch(tvWatches.NodeItem[N]);
-        assert(ASourceWatch <> nil, 'TWatchesDlg.tvWatchesDragDrop: ASourceWatch <> nil');
-        if ASourceWatch = nil then
-          Continue;
-
-        AWatch := FWatchesInView.Add(ASourceWatch.Expression);
-        AWatch.Assign(ASourceWatch);
-
-        NewNode := tvWatches.FindNodeForItem(AWatch);
-      end;
-
       if Target = nil then
-        tvWatches.MoveTo(NewNode, Target, amAddChildFirst, False)
+        NTarget := tvWatches.GetFirstChildNoInit(nil)
       else
-        tvWatches.MoveTo(NewNode, Target, amInsertAfter, False);
+        NTarget := tvWatches.GetNextSiblingNoInit(Target);
 
-      assert(AWatch <> nil, 'TWatchesDlg.tvWatchesDragDrop: AWatch <> nil');
-      if AWatch = nil then
-        Continue;
-      NewIdx := 0;
-      if Target <> nil then begin
-        ATargetWatch := TIdeWatch(tvWatches.NodeItem[Target]);
-        assert(ATargetWatch <> nil, 'TWatchesDlg.tvWatchesDragDrop: ATargetWatch <> nil');
-        if ATargetWatch <> nil then begin
-          NewIdx := ATargetWatch.Index;
-          if NewIdx < AWatch.Index then
-            inc(NewIdx);
+      for N in Nodes do begin
+        if tvWatches.NodeParent[N] = nil then begin
+          // Move top/outer node
+          if (N = Target) or (N = NTarget) then begin
+            // already in place
+            if (N = NTarget) then begin
+              Target := NTarget;
+              NTarget := tvWatches.GetNextSiblingNoInit(Target);
+            end;
+            continue;
+          end;
+
+          AWatch := TCurrentWatch(tvWatches.NodeItem[N]);
+          NewNode := N;
+        end
+        else begin
+          // Copy child node
+          ASourceWatch := TIdeWatch(tvWatches.NodeItem[N]);
+          assert(ASourceWatch <> nil, 'TWatchesDlg.tvWatchesDragDrop: ASourceWatch <> nil');
+          if ASourceWatch = nil then
+            Continue;
+
+          AWatch := FWatchesInView.Add(ASourceWatch.Expression);
+          AWatch.Assign(ASourceWatch);
+
+          NewNode := tvWatches.FindNodeForItem(AWatch);
         end;
+
+        MoveWatchToBelow(AWatch, NewNode, Target);
+        AWatch.DisplayName := '';
+
+        Target := NewNode;
+        NTarget := tvWatches.GetNextSiblingNoInit(Target);
       end;
-      AWatch.Index := NewIdx;
-      AWatch.DisplayName := '';
 
-      Target := NewNode;
-      NTarget := tvWatches.GetNextSiblingNoInit(Target);
-    end;
-
-    DebugBoss.Watches.DoModified;
+      DebugBoss.Watches.DoModified;
     finally
       EndUpdate;
     end;
@@ -717,15 +728,27 @@ begin
   if (Source is TCustomEdit) then s := TCustomEdit(Source).SelText;
 
   if s <> '' then begin
+    if (GetSelectedSnapshot <> nil) then
+      Target := nil; // add at end
+
+    BeginUpdate;
     DebugBoss.Watches.CurrentWatches.BeginUpdate;
     try
-      NewWatch := DebugBoss.Watches.CurrentWatches.Add(s);
-      NewWatch.DisplayFormat := DefaultWatchDisplayFormat;
-      NewWatch.Enabled       := True;
+      AWatch := DebugBoss.Watches.CurrentWatches.Add(s);
+      AWatch.DisplayFormat := DefaultWatchDisplayFormat;
+      AWatch.Enabled       := True;
       if EnvironmentDebugOpts.DebuggerAutoSetInstanceFromClass then
-        NewWatch.EvaluateFlags := [defClassAutoCast];
+        AWatch.EvaluateFlags := [defClassAutoCast];
+
+      if Target <> nil then begin
+        NewNode := nil;
+        if (Mode = dmAbove) then
+          Target := tvWatches.GetPreviousSiblingNoInit(Target);
+        MoveWatchToBelow(AWatch, NewNode, Target);
+      end;
     finally
       DebugBoss.Watches.CurrentWatches.EndUpdate;
+      EndUpdate;
     end;
   end;
 end;
@@ -736,6 +759,10 @@ procedure TWatchesDlg.tvWatchesDragOver(Sender: TBaseVirtualTree;
 var
   N, Target: PVirtualNode;
 begin
+  Accept := (Mode in [dmAbove, dmBelow]) and
+            (tvWatches.DropTargetNode <> nil);
+  if not Accept then
+    exit;
   Accept := ( (Source is TSynEdit) and (TSynEdit(Source).SelAvail) ) or
             ( (Source is TCustomEdit) and (TCustomEdit(Source).SelText <> '') ) or
             ( (Source = tvWatches) and (tvWatches.SelectedCount > 0) and
@@ -743,18 +770,22 @@ begin
             )
             ;
 
-  if Accept and (Source = tvWatches) then begin
-    Target := tvWatches.GetNodeAt(Pt);
-    Accept := (Target <> nil) and (tvWatches.NodeParent[Target] = nil);
-    if Accept then
-      case Mode of
-        dmAbove: ;
-        dmBelow: Accept := not tvWatches.Expanded[Target];
-        else     Accept := false;
-      end;
-    if not Accept then
-      exit;
+  if Accept then begin
+    case Mode of
+      dmAbove: Accept := (tvWatches.NodeParent[tvWatches.DropTargetNode] = nil);
+      dmBelow: begin
+          Target := tvWatches.GetNextVisibleNoInit(tvWatches.DropTargetNode);
+          Accept := (Target = nil) or
+                    (tvWatches.NodeParent[Target] = nil)
+        end;
+    end;
+  end;
 
+  if Accept and (Source = tvWatches) then begin
+    // TODO: if one single continuous selected block
+    //Accept := not tvWatches.Selected[tvWatches.DropTargetNode];
+    //if not Accept then
+    //  exit;
 
     for N in tvWatches.SelectedNodes(True) do begin
       if tvWatches.NodeItem[N] = nil then begin

ide/packages/idedebugger/breakpointsdlg.pp
@@ -1274,6 +1274,9 @@ begin
      (FDraggingGroupHeader)
   then
     DropMode := dmNowhere;
+
+  if (HitInfo.HitNode <> nil) and (GetGroupFrame(HitInfo.HitNode) <> nil) then // header node
+    DropMode := dmNowhere
 end;
 
 procedure TBreakPointsDlg.tvBreakPointsFocusChanged(Sender: TBaseVirtualTree;

ide/packages/idedebugger/debuggertreeview.pas
@@ -964,6 +964,12 @@ end;
 function TDbgTreeView.DetermineDropMode(const P: TPoint; var HitInfo: THitInfo;
   var NodeRect: TRect): TDropMode;
 begin
+  if not Assigned(HitInfo.HitNode) and (HitInfo.HitPositions * [hiAbove,hiBelow] = []) then begin
+    HitInfo.HitNode := GetLastVisibleNoInit;
+    NodeRect := GetDisplayRect(HitInfo.HitNode, HitInfo.HitColumn, False);
+    Result := dmBelow;
+  end
+  else
   if Assigned(HitInfo.HitNode) then
   begin
     if ((NodeRect.Top + NodeRect.Bottom) div 2) > P.Y then

commit c8c97f7406c1b018e154a74c6b78ecef984dd00a

IDE, Debugger: fix re-ordering watches (drag/drop). Broken in feddf603d4a0180eba4e066f70cdf1819d95feee

ide/packages/idedebugger/watchesdlg.pp
@@ -632,7 +632,7 @@ begin
       inc(NewIdx);
 
     tvWatches.MoveTo(AWatchNode, ADestNode, amInsertAfter, False);
-    ADestWatch.Index := NewIdx;
+    AWatch.Index := NewIdx;
   end;
 end;
 