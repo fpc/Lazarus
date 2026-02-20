ea0e03a16c8fcf2f219d2d349a456b7ca2bc187c
mattias <nc-gaertnma@netcologne.de>
2026-02-20 21:08:38 +0100

    set fixes version to 4.6

--- components/lazutils/lazversion.pas
+++ components/lazutils/lazversion.pas
@@ -24,11 +24,11 @@ type
 
 const
   laz_major = 4;
-  laz_minor = 5;
+  laz_minor = 6;
   laz_release = 0;
   laz_patch = 0;
   laz_fullversion = ((laz_major *  100 + laz_minor) * 100 + laz_release) * 100 + laz_patch;
-  laz_version = '4.5.0.0';
+  laz_version = '4.6.0.0';
 
 var
   OnLCLWidgetTypeName: TLCLWidgetTypeNameEvent;  // Set by LCL
