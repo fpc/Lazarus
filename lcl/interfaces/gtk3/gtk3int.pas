1ae3cae51d7b657c8d9b81abbf73e024dee9a982
zeljan1 <zeljko@holobit.hr>
2026-03-08 17:25:19 +0100

    Gt3k: implemented procedure TGtk3WidgetSet.DrawDefaultDockImage()

--- lcl/interfaces/gtk3/gtk3int.pas
+++ lcl/interfaces/gtk3/gtk3int.pas
@@ -106,6 +106,8 @@ type
     // tmp
     cssProvider:PGtkCssProvider;
 
+    FDockImage: PGtkWidget;
+
     FDragImageList: PGtkWidget;
     FDragImageListIcon: PGtkImage;
     FDragHotSpot: TPoint;
