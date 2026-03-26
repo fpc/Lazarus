f8280a8a0533d0c44ecf6bff5c01bbcf3ca213b5
rich2014 <rich2014.git@outlook.com>
2026-03-26 15:44:07 +0800

    Cocoa: add Event related in Cocoa_Extra

--- lcl/interfaces/cocoa/cocoa_extra.pas
+++ lcl/interfaces/cocoa/cocoa_extra.pas
@@ -474,6 +474,42 @@ type
     function scrollingDeltaY: CGFloat; message 'scrollingDeltaY';
   end;
 
+const
+  NX_SUBTYPE_AUX_CONTROL_BUTTONS = 8;
+
+  NX_KEYTYPE_SOUND_UP = 0;
+  NX_KEYTYPE_SOUND_DOWN = 1;
+  NX_KEYTYPE_BRIGHTNESS_UP = 2;
+  NX_KEYTYPE_BRIGHTNESS_DOWN = 3;
+  NX_KEYTYPE_CAPS_LOCK = 4;
+  NX_KEYTYPE_HELP = 5;
+  NX_POWER_KEY =  6;
+  NX_KEYTYPE_MUTE =  7;
+  NX_UP_ARROW_KEY =  8;
+  NX_DOWN_ARROW_KEY = 9;
+  NX_KEYTYPE_NUM_LOCK = 10;
+
+  NX_KEYTYPE_CONTRAST_UP = 11;
+  NX_KEYTYPE_CONTRAST_DOWN = 12;
+  NX_KEYTYPE_LAUNCH_PANEL = 13;
+  NX_KEYTYPE_EJECT = 14;
+  NX_KEYTYPE_VIDMIRROR = 15;
+
+  NX_KEYTYPE_PLAY =  16;
+  NX_KEYTYPE_NEXT =  17;
+  NX_KEYTYPE_PREVIOUS = 18;
+  NX_KEYTYPE_FAST =  19;
+  NX_KEYTYPE_REWIND = 20;
+
+  NX_KEYTYPE_ILLUMINATION_UP = 21;
+  NX_KEYTYPE_ILLUMINATION_DOWN = 22;
+  NX_KEYTYPE_ILLUMINATION_TOGGLE = 23;
+
+var
+  kAXTrustedCheckOptionPrompt: CFStringRef; cvar; external;
+
+  function AXIsProcessTrustedWithOptions(options: CFDictionaryRef): Boolean; cdecl; external;
+
 type
   NSTitlebarSeparatorStyle = NSInteger;     { available in 11.0 }
 const
