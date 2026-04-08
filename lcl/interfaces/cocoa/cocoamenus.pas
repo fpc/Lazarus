fpc/Lazarus

commit 40faf7fb97a38da981a41caaddd421807919349b

Cocoa: better support for Quiting APP (CMD+Q/[X]Button/System Shutdown), issue #20955 related

lcl/interfaces/cocoa/cocoamenus.pas
@@ -512,6 +512,8 @@ begin
 end;
 
 procedure TCocoaMenuItem_Quit.lclItemSelected(sender: id);
+var
+  canClose: Boolean;
 begin
   {$ifdef COCOALOOPHIJACK}
   // see bug #36265. if hot-key (Cmd+Q) is used the menu item
@@ -519,17 +521,15 @@ begin
   // The following if statement prevents "double" form close
   if LoopHiJackEnded then Exit;
   {$endif}
-  // Should be used instead of Application.Terminate when possible
-  // to allow events to be sent, see bug 32148
-  if Assigned(CocoaConfigApplication.events.onQuitApp) then
-    { Don't call directly since key/mouse events need to unwind in case
-      OnQuitApp frees the caller }
-    Application.QueueAsyncCall(
-      TDataEvent(CocoaConfigApplication.events.onQuitApp), PtrInt(nil))
-  else if Assigned(Application.MainForm) then
-    Application.MainForm.Close
-  else
-    Application.Terminate;
+
+  canClose:= True;
+  if Assigned(Application.MainForm) then
+    canClose:= Application.MainForm.CloseQuery;
+
+  if canClose then begin
+    if NOT Application.Terminated then
+      Application.Terminate;
+  end;
 end;
 
 { TCocoaMenuUtil }

lcl/interfaces/cocoa/cocoaapplication.pas
@@ -247,31 +247,27 @@ var
   Cancel: Boolean;
   Reason: NSAppleEventDescriptor;
 begin
-  Cancel := False;
+  Cancel:= False;
   // Check if it's a system-wide event
-  Reason := event.attributeDescriptorForKeyword(kEventParamReason);
-  if (Reason <> nil) and
-     ((Reason.typeCodeValue = kAEQuitAll) or
+  Reason := event.paramDescriptorForKeyword(kEventParamReason);
+  if (Reason = nil) or
+     (((Reason.typeCodeValue = kAEQuitAll) or
       (reason.typeCodeValue = kAEReallyLogOut) or
       (reason.typeCodeValue = kAERestart) or
-      (reason.typeCodeValue = kAEShutDown)) then
+      (reason.typeCodeValue = kAEShutDown))) then
   begin
     Application.IntfQueryEndSession(Cancel);
     if not Cancel then
       Application.IntfEndSession;
   end;
-  // Try to quit
-  if not Cancel then
-  begin
-    if Assigned(CocoaConfigApplication.events.onQuitApp) then
-      CocoaConfigApplication.events.onQuitApp(PtrInt(nil))
-    else if Assigned(Application.MainForm) then
-      Application.MainForm.Close
-    else
+
+  if NOT Cancel then begin
+    if NOT Application.Terminated then
       Application.Terminate;
     if Assigned(WakeMainThread) then
       WakeMainThread(nil);
   end;
+
   // Let caller know if the shutdown was cancelled
   if (not Application.Terminated) and (replyEvent.descriptorType <> typeNull) then
     replyEvent.setParamDescriptor_forKeyword(NSAppleEventDescriptor.descriptorWithInt32(userCanceledErr), keyErrorNumber);

lcl/interfaces/cocoa/cocoaconfig.inc
@@ -46,8 +46,6 @@ var
     events: (
       // highest priority event handler
       highestHandler: nil;
-      // Event to call when there is no MainForm with MainForm.close
-      onQuitApp: nil;
       // Platform key Event modification function
       keyEventToVK: nil;
     );

lcl/interfaces/cocoa/cocoaconfig.pas
@@ -150,14 +150,11 @@ type
 type
   // return True for event Handled, bypass post system processing
   TCocoaApplicationEventHandler = function( event: NSEvent ): Boolean of object;
-  // Need to pass to QueueAsyncCall which takes TDataEvent
-  TCocoaApplicationOnAppQuitEvent = TDataEvent;
   // Platform specific NSEvent key translation to VK Keycode function
   TCocoaApplicationEventToVK = function(AEvent: NSEvent): Word;
 
   TCocoaConfigApplicationEvents = record
     highestHandler: TCocoaApplicationEventHandler;
-    onQuitApp: TCocoaApplicationOnAppQuitEvent;
     keyEventToVK: TCocoaApplicationEventToVK;
   end;
 

commit fb2435392ece394183e6ece24be1d1236e46ecd8

Cocoa: fix the CMD+Q issue introduced in 40faf7f

lcl/interfaces/cocoa/cocoamenus.pas
@@ -513,6 +513,7 @@ end;
 
 procedure TCocoaMenuItem_Quit.lclItemSelected(sender: id);
 var
+  mainForm: TCustomForm;
   canClose: Boolean;
 begin
   {$ifdef COCOALOOPHIJACK}
@@ -522,13 +523,18 @@ begin
   if CocoaWidgetSetState.LoopHiJackEnded then Exit;
   {$endif}
 
+  mainForm:= Application.MainForm;
   canClose:= True;
-  if Assigned(Application.MainForm) then
-    canClose:= Application.MainForm.CloseQuery;
+  if Assigned(mainForm) then
+    canClose:= mainForm.CloseQuery;
 
-  if canClose then begin
-    if NOT Application.Terminated then
-      Application.Terminate;
+  if NOT canClose then
+    Exit;
+
+  if NOT Application.Terminated then begin
+    if Assigned(mainForm) then
+      mainForm.Close;
+    Application.Terminate;
   end;
 end;
 