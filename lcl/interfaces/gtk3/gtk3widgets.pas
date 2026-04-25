fpc/Lazarus

commit 0ff3d3fd82f6fd06f8e44dc7c8f1ea73466b32f7

Gtk3: rely on preferredSize, not on tricks from overriden class. issue #41696, issue #42230

lcl/interfaces/gtk3/gtk3lclbutton.inc
@@ -32,27 +32,6 @@ begin
   end;
 
   ParentClass^.get_preferred_width(widget, min_width, nat_width);
-
-  if AControl.LCLObject.AutoSize then
-  begin
-    if (AControl.LCLWidth > 0) and not Gtk3WidgetSet.IsWayland then
-    begin
-      nat_width^ := AControl.LCLWidth;
-      min_width^ := AControl.LCLWidth;
-    end;
-    exit;
-  end;
-  // Custom width handling
-  if AControl.LCLWidth = 0 then
-  begin
-    min_width^ := Max(min_width^ div 2, AControl.LCLObject.Width);
-    nat_width^ := Max(min_width^, AControl.LCLObject.Width);
-  end else
-  begin
-    min_width^ := Max(min_width^ div 2, AControl.LCLWidth);
-    nat_width^ := Max(min_width^, AControl.LCLWidth);
-  end;
-
 end;
 
 procedure LCLGtkButtonGetPreferredHeight(widget: PGtkWidget; min_height, nat_height: Pgint); cdecl;
@@ -87,28 +66,6 @@ begin
   end;
 
   ParentClass^.get_preferred_height(widget, min_height, nat_height);
-
-  if AControl.LCLObject.AutoSize then
-  begin
-    if (AControl.LCLHeight > 0) and not Gtk3WidgetSet.IsWayland then
-    begin
-      nat_height^ := AControl.LCLHeight;
-      min_height^ := AControl.LCLHeight;
-    end;
-    exit;
-  end;
-
-  // Custom height handling
-  if AControl.LCLHeight = 0 then
-  begin
-    min_height^ := Max(min_height^ div 2, AControl.LCLObject.Height);
-    nat_height^ := Max(min_height^, AControl.LCLObject.Height);
-  end else
-  begin
-    min_height^ := Max(min_height^ div 2, AControl.LCLHeight);
-    nat_height^ := Max(min_height^, AControl.LCLHeight);
-  end;
-
 end;
 
 procedure LCLGtkButtonClassInit(klass: PGTypeClass; {%H-}data: Pointer); cdecl;

lcl/interfaces/gtk3/gtk3widgets.pas
@@ -11465,14 +11465,21 @@ procedure TGtk3Button.preferredSize(var PreferredWidth,
 var
   AWidgetClass: PGtkWidgetClass;
   AMinW, AMinH: gint;
+  SavedW, SavedH: gint;
 begin
   if not IsWidgetOk then
     exit;
-  AWidgetClass := PGtkWidgetClass(FWidget^.g_type_instance.g_class);
-  if Assigned(AWidgetClass) and Assigned(AWidgetClass^.get_preferred_width) then
-    AWidgetClass^.get_preferred_width(FWidget, @AMinW, @PreferredWidth);
-  if Assigned(AWidgetClass) and Assigned(AWidgetClass^.get_preferred_height) then
-    AWidgetClass^.get_preferred_height(FWidget, @AMinH, @PreferredHeight);
+  FWidget^.get_size_request(@SavedW, @SavedH);
+  FWidget^.set_size_request(-1, -1);
+  try
+    AWidgetClass := PGtkWidgetClass(FWidget^.g_type_instance.g_class);
+    if Assigned(AWidgetClass) and Assigned(AWidgetClass^.get_preferred_width) then
+      AWidgetClass^.get_preferred_width(FWidget, @AMinW, @PreferredWidth);
+    if Assigned(AWidgetClass) and Assigned(AWidgetClass^.get_preferred_height) then
+      AWidgetClass^.get_preferred_height(FWidget, @AMinH, @PreferredHeight);
+  finally
+    FWidget^.set_size_request(SavedW, SavedH);
+  end;
 end;
 
 { TGtk3ToggleButton }

commit e67376d295ce95a481aadb4b3c7b8077e8cca142

Gtk3: fixed regression from https://gitlab.com/freepascal.org/lazarus/lazarus/-/commit/0ff3d3fd82f6fd06f8e44dc7c8f1ea73466b32f7, issue #42250

lcl/interfaces/gtk3/gtk3lclbutton.inc
@@ -4,6 +4,7 @@ procedure LCLGtkButtonGetPreferredWidth(widget: PGtkWidget; min_width, nat_width
 var
   AControl: TGtk3Widget;
   ParentClass: PGtkWidgetClass;
+  LCLW: Integer;
 begin
   if not Assigned(min_width) or not Assigned(nat_width) then
   begin
@@ -24,20 +25,33 @@ begin
     Exit;
   end;
 
-  AControl := TGtk3Widget(HwndFromGtkWidget(widget));
-  if not Assigned(AControl) then
-  begin
-    DebugLn('Error: cannot get TGtk3Widget for widget parameter.');
-    Exit;
-  end;
-
   ParentClass^.get_preferred_width(widget, min_width, nat_width);
+
+  AControl := TGtk3Widget(HwndFromGtkWidget(widget));
+  if not Assigned(AControl) or not Assigned(AControl.LCLObject) then
+    Exit;
+
+  if AControl.LCLObject.AutoSize then
+    Exit;
+
+  if AControl.LCLWidth > 0 then
+    LCLW := AControl.LCLWidth
+  else
+    LCLW := AControl.LCLObject.Width;
+
+  if LCLW > 0 then
+  begin
+    if min_width^ > LCLW then
+      min_width^ := LCLW;
+    nat_width^ := LCLW;
+  end;
 end;
 
 procedure LCLGtkButtonGetPreferredHeight(widget: PGtkWidget; min_height, nat_height: Pgint); cdecl;
 var
   AControl: TGtk3Widget;
   ParentClass: PGtkWidgetClass;
+  LCLH: Integer;
 begin
   if not Assigned(min_height) or not Assigned(nat_height) then
   begin
@@ -58,14 +72,26 @@ begin
     Exit;
   end;
 
-  AControl := TGtk3Widget(HwndFromGtkWidget(widget));
-  if not Assigned(AControl) then
-  begin
-    DebugLn('Error: cannot get TGtk3Widget for widget parameter.');
-    Exit;
-  end;
-
   ParentClass^.get_preferred_height(widget, min_height, nat_height);
+
+  AControl := TGtk3Widget(HwndFromGtkWidget(widget));
+  if not Assigned(AControl) or not Assigned(AControl.LCLObject) then
+    Exit;
+
+  if AControl.LCLObject.AutoSize then
+    Exit;
+
+  if AControl.LCLHeight > 0 then
+    LCLH := AControl.LCLHeight
+  else
+    LCLH := AControl.LCLObject.Height;
+
+  if LCLH > 0 then
+  begin
+    if min_height^ > LCLH then
+      min_height^ := LCLH;
+    nat_height^ := LCLH;
+  end;
 end;
 
 procedure LCLGtkButtonClassInit(klass: PGTypeClass; {%H-}data: Pointer); cdecl;

lcl/interfaces/gtk3/gtk3widgets.pas
@@ -11659,7 +11659,7 @@ end;
 procedure TGtk3Button.preferredSize(var PreferredWidth,
   PreferredHeight: integer; WithThemeSpace: Boolean);
 var
-  AWidgetClass: PGtkWidgetClass;
+  AWidgetClass, AQueryClass: PGtkWidgetClass;
   AMinW, AMinH: gint;
   SavedW, SavedH: gint;
 begin
@@ -11669,10 +11669,16 @@ begin
   FWidget^.set_size_request(-1, -1);
   try
     AWidgetClass := PGtkWidgetClass(FWidget^.g_type_instance.g_class);
-    if Assigned(AWidgetClass) and Assigned(AWidgetClass^.get_preferred_width) then
-      AWidgetClass^.get_preferred_width(FWidget, @AMinW, @PreferredWidth);
-    if Assigned(AWidgetClass) and Assigned(AWidgetClass^.get_preferred_height) then
-      AWidgetClass^.get_preferred_height(FWidget, @AMinH, @PreferredHeight);
+    if g_type_check_instance_is_a(PGTypeInstance(FWidget), LCLGtkButtonGetType()) then
+      AQueryClass := PGtkWidgetClass(g_type_class_peek_parent(PGTypeClass(AWidgetClass)))
+    else
+      AQueryClass := AWidgetClass;
+    if not Assigned(AQueryClass) then
+      AQueryClass := AWidgetClass;
+    if Assigned(AQueryClass^.get_preferred_width) then
+      AQueryClass^.get_preferred_width(FWidget, @AMinW, @PreferredWidth);
+    if Assigned(AQueryClass^.get_preferred_height) then
+      AQueryClass^.get_preferred_height(FWidget, @AMinH, @PreferredHeight);
   finally
     FWidget^.set_size_request(SavedW, SavedH);
   end;