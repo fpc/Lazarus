160fd8b1782719e9188eb75580d57dacb9055518
Martin <laz.git@mfriebe.de>
2026-05-22 19:24:19 +0200

    FpDebug: Dwarf, fix Version handling for line info

--- components/fpdebug/fpdbgdwarfdataclasses.pas
+++ components/fpdebug/fpdbgdwarfdataclasses.pas
@@ -5644,6 +5644,9 @@ end;
 
 constructor TDwarfCompilationUnit.Create(AOwner: TFpDwarfInfo; ADebugFile: PDwarfDebugFile; ADataOffset: QWord; ALength: QWord; AVersion: Word; AAbbrevOffset: QWord; AAddressSize: Byte; AIsDwarf64: Boolean);
   procedure FillLineInfo(AData: Pointer);
+  var
+    Version: Word;
+
     function CheckOldFpc322: boolean;
     var
       s: String;
@@ -5717,7 +5720,7 @@ constructor TDwarfCompilationUnit.Create(AOwner: TFpDwarfInfo; ADebugFile: PDwar
             debugln(FPDBG_DWARF_ERRORS or FPDBG_DWARF_VERBOSE, ['ReadDirectoryList unknown content type: ', FileDirFormatEncoding[i].ContentType]);
             exit;
           end;
-          if not SkipEntryDataForForm(AData, FileDirFormatEncoding[i].Form, FLineInfo.AddrSize, FIsDwarf64, FVersion) then begin
+          if not SkipEntryDataForForm(AData, FileDirFormatEncoding[i].Form, FLineInfo.AddrSize, FIsDwarf64, Version) then begin
             debugln(FPDBG_DWARF_ERRORS or FPDBG_DWARF_VERBOSE, ['ReadDirectoryList failed skip: ', FileDirFormatEncoding[i].Form]);
             exit;
           end;
@@ -5781,7 +5784,7 @@ constructor TDwarfCompilationUnit.Create(AOwner: TFpDwarfInfo; ADebugFile: PDwar
             debugln(FPDBG_DWARF_ERRORS or FPDBG_DWARF_VERBOSE, ['ReadFileList unknown content type: ', FileDirFormatEncoding[i].ContentType]);
             exit;
           end;
-          if not SkipEntryDataForForm(AData, FileDirFormatEncoding[i].Form, FLineInfo.AddrSize, FIsDwarf64, FVersion) then begin
+          if not SkipEntryDataForForm(AData, FileDirFormatEncoding[i].Form, FLineInfo.AddrSize, FIsDwarf64, Version) then begin
             debugln(FPDBG_DWARF_ERRORS or FPDBG_DWARF_VERBOSE, ['ReadFileList failed skip: ', FileDirFormatEncoding[i].Form]);
             exit;
           end;
@@ -5815,7 +5818,6 @@ constructor TDwarfCompilationUnit.Create(AOwner: TFpDwarfInfo; ADebugFile: PDwar
     Info: PDwarfLNPInfoHeader;
 
     UnitLength: QWord;
-    Version: Word;
     HeaderLength: QWord;
     Name: PChar;
     diridx: Cardinal;
@@ -5829,12 +5831,12 @@ constructor TDwarfCompilationUnit.Create(AOwner: TFpDwarfInfo; ADebugFile: PDwar
 
     if LNP64^.Signature = DWARF_HEADER64_SIGNATURE
     then begin
-      if FVersion < 3 then
-        DebugLn(FPDBG_DWARF_WARNINGS, ['Unexpected 64 bit signature found for DWARF version 2']); // or version 1...
       UnitLength := LNP64^.UnitLength;
       FLineInfo.DataEnd := Pointer(@LNP64^.Version) + UnitLength;
       Version := LNP64^.Version;
-      if FVersion < 5 then begin
+      if Version < 3 then
+        DebugLn(FPDBG_DWARF_WARNINGS, ['Unexpected 64 bit signature found for DWARF version 2']); // or version 1...
+      if Version < 5 then begin
         HeaderLength := LNP64^.HeaderLength;
         Info := @LNP64^.Info;
       end
@@ -5848,7 +5850,7 @@ constructor TDwarfCompilationUnit.Create(AOwner: TFpDwarfInfo; ADebugFile: PDwar
       UnitLength := LNP32^.UnitLength;
       FLineInfo.DataEnd := Pointer(@LNP32^.Version) + UnitLength;
       Version := LNP32^.Version;
-      if FVersion < 5 then begin
+      if Version < 5 then begin
         HeaderLength := LNP32^.HeaderLength;
         Info := @LNP32^.Info;
       end
@@ -5871,7 +5873,7 @@ constructor TDwarfCompilationUnit.Create(AOwner: TFpDwarfInfo; ADebugFile: PDwar
       (* Version 4 up, has an extra field => adjust the pointer
          Except for older FPC, which erroneously did not add the field
       *)
-      if (FVersion <> 4) or not CheckOldFpc322 then begin
+      if (Version <> 4) or not CheckOldFpc322 then begin
         inc(PByte(Info)); // All fields move by 1 byte // Dwarf-4 has a new field
         FLineInfo.MaximumInstructionLength := Info^.MinimumInstructionLength;
       end;
@@ -5890,7 +5892,7 @@ constructor TDwarfCompilationUnit.Create(AOwner: TFpDwarfInfo; ADebugFile: PDwar
 
     Name := PChar(@Info^.StandardOpcodeLengths);
     Inc(Name, Info^.OpcodeBase-1);
-    if FVersion < 5 then begin
+    if Version < 5 then begin
       FLineInfo.Directories.Add(''); // current dir
 
       // directories
