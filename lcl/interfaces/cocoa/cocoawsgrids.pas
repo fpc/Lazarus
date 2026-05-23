b1fd76ab0057aca7ab1e790225a3b65dcde9d201
rich2014 <rich2014.git@outlook.com>
2026-05-23 17:48:12 +0800

    Cocoa: support tlTop/tlCenter/tlBottom Editor Bounds in Grids

--- lcl/interfaces/cocoa/cocoawsgrids.pas
+++ lcl/interfaces/cocoa/cocoawsgrids.pas
@@ -14,6 +14,9 @@ type
   { TCocoaWSCustomGrid }
 
   TCocoaWSCustomGrid = class( TWSCustomGrid )
+    class function GetEditorBoundsFromCellRect(ACanvas: TCanvas;
+      const ACellRect: TRect; const AColumnLayout: TTextLayout): TRect;
+      override;
     class function GetPickListEditorBoundsFromCellRect(ACanvas: TCanvas;
       var ACellRect: TRect; const AColumnLayout: TTextLayout): Boolean;
       override;
@@ -23,15 +26,33 @@ implementation
 
 { TCocoaWSCustomGrid }
 
+class function TCocoaWSCustomGrid.GetEditorBoundsFromCellRect(ACanvas: TCanvas;
+  const ACellRect: TRect; const AColumnLayout: TTextLayout): TRect;
+begin
+  Result:= ACellRect;
+  Dec( Result.Left );
+  case AColumnLayout of
+    tlCenter: Dec( Result.Top );
+    tlBottom: Dec( Result.Bottom );
+  end;
+end;
+
 class function TCocoaWSCustomGrid.GetPickListEditorBoundsFromCellRect(
   ACanvas: TCanvas; var ACellRect: TRect; const AColumnLayout: TTextLayout
   ): Boolean;
+const
+  EDITOR_HEIGHT = 20;
 var
   offset: Integer;
 begin
-  offset:= (ACellRect.Height - 20) div 2;
-  Inc( ACellRect.Top, offset );
+  Dec( ACellRect.Left );
   Dec( ACellRect.Right );
+  case AColumnLayout of
+    tlTop: Inc( ACellRect.Top );
+    tlCenter: Inc( ACellRect.Top, (ACellRect.Height-EDITOR_HEIGHT) div 2 );
+    tlBottom: ACellRect.Top:= ACellRect.Bottom - EDITOR_HEIGHT - 1;
+  end;
+  ACellRect.Bottom:= ACellRect.Top + EDITOR_HEIGHT;
   Result:= True;
 end;
 
