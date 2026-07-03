c0fa85e426732c14f785c3e34170e3ea6cceac8f
zeljan1 <zeljko@holobit.hr>
2026-07-03 11:09:41 +0200

    IDE: gtk3 no longer needs CalcNonClientHeight menu workaround, related to issue #42396

--- ide/mainbar.pas
+++ ide/mainbar.pas
@@ -41,7 +41,7 @@ uses
   Classes, SysUtils, Math, System.UITypes,
   // LCL
   Forms, Controls, Menus, ComCtrls, ExtCtrls, LMessages,
-{$IF DEFINED(LCLGtk) OR DEFINED(LCLQt) OR DEFINED(LCLGtk3)}
+{$IF DEFINED(LCLGtk) OR DEFINED(LCLQt)}
   LCLIntf,
 {$ENDIF}
   // LazUtils
@@ -521,7 +521,7 @@ begin
 end;
 
 function TMainIDEBar.CalcNonClientHeight: Integer;
-{$IF DEFINED(LCLGtk) OR DEFINED(LCLQt) OR DEFINED(LCLGtk3)}
+{$IF DEFINED(LCLGtk) OR DEFINED(LCLQt)}
 var
   WindowRect, WindowClientRect: TRect;
 {$ENDIF}
@@ -542,7 +542,7 @@ begin
   if not Showing then
     Exit(0);
 
-  {$IF DEFINED(LCLGtk) OR DEFINED(LCLQt) OR DEFINED(LCLGtk3)}
+  {$IF DEFINED(LCLGtk) OR DEFINED(LCLQt)}
   //Gtk + Qt + Gtk3
   //retrieve real main menu height because
   // - Gtk, Qt:  SM_CYMENU does not work
