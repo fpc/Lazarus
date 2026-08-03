4a6f97f4beea71acde9ecb918ddc653952455538
n7800 <14154601-n7800@users.noreply.gitlab.com>
2026-08-03 20:12:48 +0000

    IDE/Options: Do not close the dialog by pressing [Esc] if some combobox is dropped down

--- ide/ideoptionsdlg.pas
+++ ide/ideoptionsdlg.pas
@@ -180,6 +180,7 @@ begin
   ButtonPanel.OKButton.OnClick := @OKButtonClick;
   ButtonPanel.OKButton.ModalResult := mrNone;
   ButtonPanel.CancelButton.OnClick := @CancelButtonClick;
+  ButtonPanel.CancelButton.Cancel := false; // FormKeyDown with KeyPreview will handle this
   ButtonPanel.HelpButton.OnClick := @HelpButtonClick;
 
   // caption
@@ -371,6 +372,8 @@ begin
   // dialog
   else if (Key = VK_ESCAPE) and (Shift = []) then
   begin
+    if assigned(ActiveControl) and (ActiveControl is TCustomComboBox) and TCustomComboBox(ActiveControl).DroppedDown then
+      exit;
     CancelButtonClick(Sender);
     Key := 0;
   end
