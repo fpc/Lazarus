bb32278d841c3dc67d238b60b2e5e63bbf1f69f9
Maxim Ganetsky <maxim@lazarus-ide.org>
2026-09-01 02:19:36 +0300

    IDE: Moved used unit from implementation to interface Uses clause, part of issue #42557.

--- ide/main.pp
+++ ide/main.pp
@@ -69,7 +69,7 @@ uses
   StdCodeTools, EventCodeTool, CodeCreationDlg, IdentCompletionTool,
   // LazUtils
   // use lazutf8, lazfileutils and lazfilecache after FileProcs and FileUtil
-  FileUtil, LazFileUtils, LazUtilities, LazUTF8, UTF8Process, ProjResConvert,
+  FileUtil, LazFileUtils, LazUtilities, LazUTF8, UTF8Process, ProjResConvert, ProjResProc,
   LConvEncoding, Laz2_XMLCfg, LazLoggerBase, LazLogger, LazFileCache, AvgLvlTree,
   GraphType, LazStringUtils, LazVersion, LazTracer,
   LCLExceptionStacktrace,
@@ -1027,9 +1027,6 @@ const
   CodeToolsIncludeLinkFile = 'includelinks.xml';
 
 implementation
-
-uses ProjResProc;
-
 
 var
   ParamBaseDirectory: string = '';
