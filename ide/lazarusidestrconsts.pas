7134324a0ce84f17b746f1bb42b4f4edef4d8f61
n7800 <14154601-n7800@users.noreply.gitlab.com>
2026-09-13 23:19:06 +0000

    IDE: Clarify the action of the temporary trust button for a command. Issue #42005

--- ide/lazarusidestrconsts.pas
+++ ide/lazarusidestrconsts.pas
@@ -2475,7 +2475,7 @@ resourcestring
   lisThePackageWantsToUseTheCompiler = 'The package "%s" wants to use the compiler:%s%s%s'
     +'This is not the default compiler configured in the IDE. Opening a package with a custom '
     +'compiler can run arbitrary programs.%sDo you want to trust and use this compiler?';
-  lisTrustCompilerThisTime = 'Trust this time';
+  lisTrustCompilerThisTime = 'Trust until project closes';
   lisTrustCompilerAlways = 'Trust always';
   lisDoNotTrustCompiler = 'Do not trust';
   lisUseDefaultCompiler = 'Use default compiler';
