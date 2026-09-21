e40ad63a53020a1985e8c29b9994f30cd5efd8cf
Maxim Ganetsky <maxim@lazarus-ide.org>
2026-09-22 01:54:01 +0300

    Gir2Pascal: added unique identifier to an interface declaration, issue #42597

--- tools/gir2pascal/girparser.pas
+++ tools/gir2pascal/girparser.pas
@@ -8,7 +8,7 @@ uses
   Classes, SysUtils, Dom;
 
 type
-  IgirParser = interface
+  IgirParser = interface ['{88D9E0C3-7170-47FC-87A5-8A2CCDC6CB6C}']
     procedure ParseNode(ANode: TDomNode);
   end;
 
