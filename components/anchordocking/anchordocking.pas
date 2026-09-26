73e2bd7c98946eb38628de1e0131d5a0793f0f8b
Martin <laz.git@mfriebe.de>
2026-09-26 11:22:09 +0200

    AnchorDocking: Undo part of 989070fa9144d8b353c413b7ff8409d80b7aaef7 issue #39923 (forms overlap). The commit did not solve the issue, but caused other regressions.

--- components/anchordocking/anchordocking.pas
+++ components/anchordocking/anchordocking.pas
@@ -488,7 +488,6 @@ type
     procedure StoreConstraints;
     function GetSitePreferredClientSize: TPoint;
     function IsEnabledControl(Control: TControl):Boolean; override;
-    function CanBeDoubleDocked:Boolean; override;
 
     property Site: TWinControl read FSite; // the associated TControl (a TAnchorDockHostSite or a custom dock site)
     property DockSite: TAnchorDockHostSite read FDockSite; // if Site is a TAnchorDockHostSite, this is it
@@ -8058,11 +8057,6 @@ end;
 function TAnchorDockManager.IsEnabledControl(Control: TControl):Boolean;
 begin
   Result := (DockMaster <> nil) and DockMaster.IsSite(Control);
-end;
-
-function TAnchorDockManager.CanBeDoubleDocked:Boolean;
-begin
-  Result := False;
 end;
 
 { TAnchorDockSplitter }
